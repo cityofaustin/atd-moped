65318cfa12898241ae21d243b402c05f4cb75155
Sergio Garcia <sergiogcx@gmail.com>
2020-09-23 19:36:44 -0500

    Incorporates AWS Cognito

--- moped-editor/src/app/services/jwtAuthService.js
+++ moped-editor/src/app/services/jwtAuthService.js
@@ -1,17 +1,22 @@
+import { Auth } from 'aws-amplify';
 import axios from "axios";
 import localStorageService from "./localStorageService";
 
 class JwtAuthService {
+  user = {}
 
-  // Dummy user object just for the demo
-  user = {
-    userId: "1",
-    role: 'ADMIN',
-    displayName: "Jason Alexander",
-    email: "jasonalexander@gmail.com",
-    photoURL: "./assets/images/face-6.jpg",
-    age: 25,
-    token: "faslkhfh423oiu4h4kj432rkj23h432u49ufjaklj423h4jkhkjh"
+  buildUserData = (awsCognitoData) => {
+    return {
+      role: "ADMIN",
+      userId: awsCognitoData.username,
+      username: awsCognitoData.username,
+      email: awsCognitoData.username,
+      token: awsCognitoData.signInUserSession.idToken.jwtToken,
+      tokenPayload: JSON.stringify(awsCognitoData.signInUserSession.payload),
+      displayName: "Authenticated User",
+      photoURL: "/assets/images/face-6.jpg",
+      age: 0,
+    }
   }
 
   // You need to send http request with email and passsword to your server in this method
@@ -19,17 +24,16 @@ class JwtAuthService {
   // User should have role property
   // You can define roles in app/auth/authRoles.js
   loginWithEmailAndPassword = (email, password) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(this.user);
-      }, 1000);
-    }).then(data => {
-      // Login successful
-      // Save token
-      this.setSession(data.token);
-      // Set user
-      this.setUser(data);
-      return data;
+    return Auth.signIn(email, password).then(data => {
+      localStorage.setItem("atd_aws_cognito_session", JSON.stringify(data));
+      localStorage.setItem("atd_aws_cognito_session_payload", JSON.stringify(data.signInUserSession.idToken.payload));
+      this.user = this.buildUserData(data);
+      this.setSession(this.user.token);
+      this.setUser(this.user);
+      return this.user;
+    }).catch((error) => {
+      console.log(error);
+      alert(error.message);
     });
   };
 
@@ -49,8 +53,10 @@ class JwtAuthService {
   };
 
   logout = () => {
-    this.setSession(null);
-    this.removeUser();
+    Auth.signOut().then(() => {
+      this.setSession(null);
+      this.removeUser();
+    });
   }
 
   // Set token to all http request header, so you don't need to attach everytime
@@ -71,6 +77,9 @@ class JwtAuthService {
   // Remove user from localstorage
   removeUser = () => {
     localStorage.removeItem("auth_user");
+    localStorage.removeItem("jwt_token");
+    localStorage.removeItem("atd_aws_cognito_session");
+    localStorage.removeItem("atd_aws_cognito_session_payload");
   }
 }
 
