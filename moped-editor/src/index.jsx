3ea3167da2474728013f113dcb9d1b230656d272
Sergio Garcia <sergiogcx@gmail.com>
2020-09-23 19:30:30 -0500

    Incorporates AWS Amplify

--- moped-editor/src/index.jsx
+++ moped-editor/src/index.jsx
@@ -1,14 +1,42 @@
 import "babel-polyfill";
 // import cssVars from "css-vars-ponyfill";
 
+import Amplify from "aws-amplify";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./_index.scss";
-
+import config from "./config";
 import * as serviceWorker from "./serviceWorker";
 import App from "./app/App";
 
 // cssVars();
+
+/**
+ * AWS Amplify
+ * @see https://github.com/aws-amplify/amplify-js
+ */
+
+Amplify.Logger.LOG_LEVEL = 'DEBUG';
+
+Amplify.configure({
+    Auth: {
+        mandatorySignIn: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    },
+    API: {
+        endpoints: [
+            {
+                name: "testApi",
+                endpoint: config.apiGateway.URL,
+                region: config.apiGateway.REGION
+            }
+        ]
+    }
+});
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
