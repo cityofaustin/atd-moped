5a9628151b7b587505e0eb102f1f3d47ecc29f02
Mike Dilley <mike.dilley@gmail.com>
2020-10-06 15:30:11 -0500

    Add is_valid_user helper and add to all routes

--- moped-api/user/user.py
+++ moped-api/user/user.py
@@ -2,6 +2,7 @@ import os, boto3
 from flask import Blueprint, jsonify
 from flask_cognito import cognito_auth_required, current_cognito_jwt
 from config import api_config
+from user.helpers import load_claims, is_valid_user
 
 # Import our custom code
 from claims import normalize_claims, get_claims
@@ -10,8 +11,6 @@ user_blueprint = Blueprint("user_blueprint", __name__)
 
 MOPED_API_CURRENT_ENVIRONMENT = os.getenv("MOPED_API_CURRENT_ENVIRONMENT", "STAGING")
 USER_POOL = api_config[MOPED_API_CURRENT_ENVIRONMENT]["COGNITO_USERPOOL_ID"]
-
-is_valid_user = True
 
 cognito_client = boto3.client("cognito-idp")
 
@@ -28,7 +27,7 @@ def user_index() -> str:
 @user_blueprint.route("/list_users")
 @cognito_auth_required
 def user_list_users():
-    if is_valid_user:
+    if is_valid_user(current_cognito_jwt):
         # Check if requester is admin and is a valid user
         response = cognito_client.list_users(UserPoolId=USER_POOL)
         return jsonify(response["Users"])
@@ -40,9 +39,16 @@ def user_list_users():
 @cognito_auth_required
 def user_get_user(id):
     # Check if requester is admin and is a valid user
-    if is_valid_user:
-        response = cognito_client.admin_get_user(UserPoolId=USER_POOL, Username=id)
-        return jsonify(response)
+    if is_valid_user(current_cognito_jwt):
+        user_dict = {}
+
+        user_info = cognito_client.admin_get_user(UserPoolId=USER_POOL, Username=id)
+
+        user_roles = load_claims(id)
+        user_dict.update(user_info)
+        user_dict.update(user_roles)
+
+        return user_dict
     else:
         abort(403)
 
@@ -50,7 +56,7 @@ def user_get_user(id):
 @user_blueprint.route("/create_user", methods=["POST"])
 @cognito_auth_required
 def user_create_user():
-    if is_valid_user:
+    if is_valid_user(current_cognito_jwt):
         return jsonify({"message": "Hello from create users! :)"})
     # Check if requester is admin and is a valid user
     # Create user in AWS
@@ -62,7 +68,7 @@ def user_create_user():
 @user_blueprint.route("/update_user/<id>", methods=["PUT"])
 @cognito_auth_required
 def user_update_user(id):
-    if is_valid_user:
+    if is_valid_user(current_cognito_jwt):
         return jsonify({"message": "Hello from update user! :)"})
     # Check if requester is admin and is a valid user
     # Update user in AWS
@@ -74,7 +80,7 @@ def user_update_user(id):
 @user_blueprint.route("/delete_user/<id>", methods=["DELETE"])
 @cognito_auth_required
 def user_delete_user(id):
-    if is_valid_user:
+    if is_valid_user(current_cognito_jwt):
         return jsonify({"message": "Hello from delete user! :)"})
     # Check if requester is admin and is a valid user
     # Delete user in AWS
