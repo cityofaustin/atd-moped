6f5cfb561bb33b2276ba3fc89984192f020df004
Mike Dilley <mike.dilley@gmail.com>
2020-10-15 15:05:46 -0500

    Test is_valid_user() with mock identity

--- moped-api/tests/test_users.py
+++ moped-api/tests/test_users.py
@@ -1,10 +1,27 @@
 from moto import mock_cognitoidp
 from tests.test_app import TestApp
-from unittest.mock import patch
+from unittest.mock import patch, Mock, MagicMock
+from users.helpers import is_valid_user
+
+
+test_user_identity = {
+    "email": "@austintexas.gov",
+    "cognito:username": "test_user",
+    "https://hasura.io/jwt/claims": ["user"],
+    "email_verified": True,
+    "aud": "test_aud",
+}
 
 
 class TestUsers(TestApp):
-    headers = {"Authorization": "Bearer TEST123"}
+    def test_is_valid_user(self):
+        """Validate user attributes in JWT."""
+        mock_jwt = Mock()
+        mock_jwt._get_current_object = Mock(return_value=test_user_identity)
+
+        result = is_valid_user(mock_jwt)
+
+        assert result is True
 
     @mock_cognitoidp
     def test_gets_users_no_auth(self):
@@ -21,21 +38,22 @@ class TestUsers(TestApp):
         )
         assert "Authorization Required" in response_dict.get("error", "")
 
-    @mock_cognitoidp
-    @patch("flask_cognito._cognito_auth_required")
-    def test_gets_users(self, mock_cognito_auth_required):
-        """Get users."""
-        response = self.client.get("/users/")
-        response_dict = self.parse_response(response.data)
+    # @mock_cognitoidp
+    # @patch("flask_cognito._cognito_auth_required")
+    # def test_gets_users(self, mock_cognito_auth_required):
+    #     """Get users."""
 
-        assert isinstance(response_dict, str)
-        assert "description" in response_dict
-        assert "error" in response_dict
-        assert (
-            "Request does not contain a well-formed access token"
-            in response_dict.get("description", "")
-        )
-        assert "Authorization Required" in response_dict.get("error", "")
+    #     response = self.client.get("/users/")
+    #     response_dict = self.parse_response(response.data)
+
+    #     assert isinstance(response_dict, str)
+    #     assert "description" in response_dict
+    #     assert "error" in response_dict
+    #     assert (
+    #         "Request does not contain a well-formed access token"
+    #         in response_dict.get("description", "")
+    #     )
+    #     assert "Authorization Required" in response_dict.get("error", "")
 
     @mock_cognitoidp
     def test_gets_user_no_auth(self):
