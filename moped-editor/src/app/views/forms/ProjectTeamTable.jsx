e7785938aa6d107de59f238a1e56dd19403fdbc7
lazavazav <lazav2021@gmail.com>
2020-10-23 11:18:16 -0400

    deleted  dependency, started  workgroup input

--- moped-editor/src/app/views/forms/ProjectTeamTable.jsx
+++ moped-editor/src/app/views/forms/ProjectTeamTable.jsx
@@ -51,6 +51,7 @@ const ProjectTeamTable = ({ formContent }) => {
         full_name
         first_name
         last_name
+        workgroup
       }
     }
   `;
@@ -63,16 +64,6 @@ const ProjectTeamTable = ({ formContent }) => {
   }
 `;
 
-const GET_GROUP_QUERY = gql`
-query GetGroup($last_name: name) {
-  moped_coa_staff(where: {last_name: {_eq: $last_name}}) {
-    workgroup
-  }
-}
-`;
-
-const { loading: groupLoading, error: groupError, data: group }  = useQuery(GET_GROUP_QUERY, {variables: {last_name}});
-
 const { loading: membersLoading, error: membersError, data: members }  = useQuery(MEMBERS_QUERY);
 
 const { loading: roleLoading, error: roleError, data: roles } = useQuery(ROLES_QUERY);
@@ -83,29 +74,17 @@ if (membersError) return `Error! ${membersError.message}`;
 if (roleLoading) return 'Loading...';
 if (roleError) return `Error! ${roleError.message}`;
 
-if (groupLoading) return 'Loading...';
-if (groupError) return `Error! ${groupError.message}`;
-
 let nameOption = [];
 members.moped_coa_staff.forEach((name) => nameOption.push(name.last_name));
 
-let roleOption = [];
-roles.moped_project_roles.forEach((role) => roleOption.push(role.project_role_name));
-
-let last_name = getValues("Last");
-let role_name = getValues("Role");
-let group_name = getValues("Group");
-
-const handleSetValue = () => {
-setValue("Last", last_name);
-setValue("Role", role_name);
-setValue("Group", group_name);
-console.log(last_name, role_name, group_name);
+const handleSetGroup = (name) => {
+  let input = document.getElementById("selectedGroup");
+  input.value=name.workgroup;
 }
 
-let groupOption = [];
-group.moped_coa_staff.map((group) => groupOption.push(group.workgroup));
-          
+let roleOption = [];
+roles.moped_project_roles.forEach((role) => roleOption.push(role.project_role_name));
+       
 return (
   <div>
     <form style={{padding: 10}}>    
@@ -128,11 +107,10 @@ return (
               name="Last"
               ref={register}
               options={nameOption}
-              onChange={handleSetValue}
               style={{ width: 150 }}
               renderInput={(params) => <TextField {...params} label="Select Staff" inputRef={register} name="Last" 
-              margin="normal" />}  
-            />
+              margin="normal" />}
+              onChange={handleSetGroup}/>      
          </TableCell>
          <TableCell>
             <Autocomplete
@@ -140,21 +118,17 @@ return (
               name="Role"
               ref={register}
               options={roleOption}
-              onChange={handleSetValue}
               style={{ width: 150 }}
               renderInput={(params) => <TextField {...params} label="Select a Role"  inputRef={register} name="Role" margin="normal" />} 
             />
          </TableCell> 
          <TableCell> 
-            <Autocomplete
-              id="selectedGroup"
-              ref={register}
-              name="Group"
-              options={groupOption}
-              onChange={handleSetValue}
-              style={{ width: 150 }}
-              renderInput={(params) => <TextField {...params} label="Select a WorkGroup" inputRef={register} name="Group" margin="normal" />} 
-            />
+                <TextField
+                id="selectedGroup"
+                inputRef={register}
+                name="Group"     
+                style={{ width: 150 }} 
+              />
          </TableCell>  
          <TableCell>
             <Button
