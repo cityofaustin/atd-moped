7b5175765ba2191c3b1d2dd007a5863028573903
lazavazav <lazav2021@gmail.com>
2020-11-07 10:54:27 -0500

    fixed teamtable:name is persisting through steps

--- moped-editor/src/app/views/forms/ProjectTeamTable.jsx
+++ moped-editor/src/app/views/forms/ProjectTeamTable.jsx
@@ -5,7 +5,6 @@ import { gql, useQuery } from "@apollo/client";
 import { makeStyles } from '@material-ui/core/styles';
 import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
  
-
 const ProjectTeamTable = ( props ) => {
   const useStyles = makeStyles({
     table: {
@@ -14,34 +13,35 @@ const ProjectTeamTable = ( props ) => {
     },
   });
   const classes = useStyles();
-  
+ 
   const handleAddRow = () => {
     let item = {
-      id: props.rows.length + 1,
+      id: props.StaffRows.length + 1,
       name: '',
       workgroup: '',
       role: ''
     };
-    props.setRows(rows => [...rows, item]);
+    props.setStaffRows(StaffRows => [...StaffRows, item]);
   };
 
   const handleRemoveRow = (index) => {
-    if(props.rows.length > 1) props.setRows(props.rows.slice(0, index));
+    if(props.StaffRows.length > 1) props.setStaffRows(props.StaffRows.slice(0, index));
   };
 
   const handleNameChange = (value, item, index) => {
-    console.log(item);
-    item.name = value.name; 
-    item.workgroup = value.workgroup; 
-    props.setRows(props.rows);
-    console.log(props.rows);
+    item.name = value;
+    props.setStaffRows(props.StaffRows);
   };
 
   const handleRoleChange = (value, item, index) => {
     item.role = value;
-    props.setRows(props.rows);
+    props.setStaffRows(props.StaffRows);
   };
 
+  const handleGroupChange = (value, item, index) => {
+    item.workgroup = value.workgroup;
+    props.setStaffRows(props.StaffRows);
+  }
 
   const MEMBERS_QUERY = gql`
     query Members {
@@ -92,7 +92,7 @@ const ProjectTeamTable = ( props ) => {
          </TableRow>
        </TableHead>
        <TableBody>
-         {props.rows.map((item, index) => (
+         {props.StaffRows.map((item, index) => (
           <TableRow key={index}>
            <TableCell>{item.id}</TableCell>
            <TableCell>
@@ -102,8 +102,9 @@ const ProjectTeamTable = ( props ) => {
                 options={nameOption}
                 getOptionLabel={(option) => option.name ? option.name : ""}
                 onChange = {(event, value) => {
-                handleNameChange(value, item, index);}}
-                defaultValue={item.name.name} 
+                handleNameChange(value, item, index);
+                handleGroupChange(value,item, index);}}
+                defaultValue={item.name} 
                 style={{ width: 150 }}
                 renderInput={(params) => <TextField {...params} label="Select Staff"
                 margin="normal" />}
@@ -116,7 +117,7 @@ const ProjectTeamTable = ( props ) => {
                 options={roleOption}
                 defaultValue={item.role}
                 onChange={(event, value) => {
-                  handleRoleChange(value, item, index);
+                handleRoleChange(value, item, index);
                 }}
                 style={{ width: 150 }}
                 renderInput={(params) => <TextField {...params} label="Select a Role" margin="normal" />}
@@ -154,175 +155,3 @@ const ProjectTeamTable = ( props ) => {
 
 export default ProjectTeamTable; 
 
-// import React, { useEffect, useState } from "react";
-// import { useFormContext, Controller } from "react-hook-form";
-// import { TextField, Button, Icon } from "@material-ui/core";
-// import Autocomplete from "@material-ui/lab/Autocomplete";
-// import { gql, useQuery } from "@apollo/client";
-// import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
-// import { DevTool } from "@hookform/devtools";
-
-// const ProjectTeamTable = ({ formContent }) => {
-//   const methods = useFormContext();
-//   const { reset, register, control, getValues } = methods;
-//   useEffect(() => {
-//     reset({ ...formContent.two }, { errors: true });
-//   }, []);
-
-//   const defaultRow = [{
-//     id: 1,
-//     Name: '',
-//     Group: '',
-//     Role: ''
-//   }];
-
-//   // easy way to get values of inputs if needed
-//   let nameValue = getValues("Last");
-//   let roleValue = getValues("Role");
-//   let groupValue = getValues("Group");
-
-//   const [userInput, setUserInput] = useState(nameValue);
-
-//   const saveRow = (input) => {
-//   setUserInput(input);
-//   }
-
-
-//   const [rows, setRows] = useState(defaultRow);
-    
-//   const handleAddRow = () => {
-//     let item = [{
-//       id: rows.length + 1,
-//       Name: '',
-//       Group: '',
-//       Role: ''
-//     }];   
-//     setRows([...rows, item]);
-//   };
-
-//   const handleRemoveRow = () => {
-//     if(rows.length > 1)
-//     setRows(rows.slice(0, -1));
-//   } 
-
-//   const MEMBERS_QUERY = gql`
-//     query Members {
-//       moped_coa_staff(order_by: {last_name: asc}) {
-//         full_name
-//         first_name
-//         last_name
-//         workgroup
-//       }
-//     }
-//   `;
-
-//   const ROLES_QUERY = gql`
-//   query Roles {
-//     moped_project_roles(order_by: {project_role_name: asc}) {
-//       project_role_name
-//     }
-//   }
-// `;
-
-// const { loading: membersLoading, error: membersError, data: members }  = useQuery(MEMBERS_QUERY);
-
-// const { loading: roleLoading, error: roleError, data: roles } = useQuery(ROLES_QUERY);
-
-// if (membersLoading) return 'Loading...';
-// if (membersError) return `Error! ${membersError.message}`;
- 
-// if (roleLoading) return 'Loading...';
-// if (roleError) return `Error! ${roleError.message}`;
-
-// let nameOption = [];
-// members.moped_coa_staff.forEach((name) => 
-// nameOption.push(name.last_name));
-
-// const handleSetGroup = () => {
-//   let input = document.getElementById("selectedGroup");
-//   input.value=members.moped_coa_staff.workgroup;
-// }
-
-// let roleOption = [];
-// roles.moped_project_roles.forEach((role) => roleOption.push(role.project_role_name));
-        
-// return (
-//   <div>
-//     <form style={{padding: 10}}>   
-//     <DevTool control={control} /> 
-//      <Table>
-//      <TableHead>
-//        <TableRow>
-//          <TableCell>Row</TableCell>
-//          <TableCell>Name</TableCell>
-//          <TableCell>Role</TableCell>
-//          <TableCell>WorkGroup</TableCell> 
-//        </TableRow>
-//      </TableHead>
-//      <TableBody>
-//        {rows.map((item, index) => (
-//         <TableRow id="add" key={index}>
-//          <TableCell>{item.id}</TableCell>
-//          <TableCell> 
-//            <Autocomplete
-//               id="selectedName"
-//               value={userInput}
-//               ref={register}
-//               name="Last"
-//               onChange={handleSetGroup}
-//               options={nameOption}
-//               style={{ width: 150 }}
-//               renderInput={(params) => <TextField {...params} label="Select Staff" inputRef={register} name="Last"  
-//               margin="normal" />}
-//              />      
-//          </TableCell>
-//          <TableCell>
-//             <Autocomplete
-//               id="selectedRole"
-//               name="Role"
-              
-//               ref={register}
-//               options={roleOption}
-//               autoComplete="off"
-//               style={{ width: 150 }}
-//               renderInput={(params) => <TextField {...params} label="Select a Role"  inputRef={register} name="Role" margin="normal" />} 
-//             />
-//          </TableCell>  
-//          <TableCell> 
-//                 <TextField
-//                 id="selectedGroup"
-                
-//                 inputRef={register}
-//                 name="Group"     
-//                 style={{ width: 150, paddingLeft: 10 }} 
-//               />
-//          </TableCell> 
-//          <TableCell>
-//             <Button
-//               color="secondary"
-//               onClick={saveRow}>
-//               <Icon>save</Icon>
-//             </Button>  
-//           </TableCell> 
-//          <TableCell>
-//             <Button
-//               color="secondary"
-//               onClick={handleRemoveRow}>
-//               <Icon>delete</Icon>
-//             </Button>  
-//           </TableCell>
-//       </TableRow> 
-//       ))}   
-//       </TableBody>   
-//     </Table> 
-//     <Button
-//       color="secondary"
-//       onClick={handleAddRow}>
-//     <Icon>person_add</Icon> 
-//     </Button>  
-//   </form>
-// </div>
-// );
-// }
-  
-// export default ProjectTeamTable;
