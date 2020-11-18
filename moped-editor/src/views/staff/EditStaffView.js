06eb4d7c503ce9821babde05da5219083a3600b1
Mike <mike.dilley@gmail.com>
2020-11-17 18:53:57 -0600

    Add EditStaffView and write user gql query

---
+++ moped-editor/src/views/staff/EditStaffView.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import StaffForm from "./StaffForm";
+
+import {
+  Box,
+  Container,
+  Card,
+  CardHeader,
+  CardContent,
+  Divider,
+  makeStyles,
+} from "@material-ui/core";
+import Page from "src/components/Page";
+
+const useStyles = makeStyles(() => ({
+  root: {},
+}));
+
+const USER_QUERY = id => gql`
+  query GetUser {
+    moped_users(
+      where: { staff_uuid: { _eq: ${id} } }
+    ) {
+      date_added
+      first_name
+      is_coa_staff
+      last_name
+      staff_uuid
+      status_id
+      title
+      user_id
+      workgroup
+      workgroup_id
+    }
+  }
+`;
+
+const EditStaffView = () => {
+  const classes = useStyles();
+  const { id } = useParams();
+
+  return (
+    <Page className={classes.root} title="Staff">
+      <Container maxWidth={false}>
+        <Box mt={3}>
+          <Card className={classes.root}>
+            <CardHeader title="Edit User" />
+            <Divider />
+            <CardContent>
+              <StaffForm />
+            </CardContent>
+          </Card>
+        </Box>
+      </Container>
+    </Page>
+  );
+};
+
+export default EditStaffView;
