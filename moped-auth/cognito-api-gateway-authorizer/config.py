ba6f3b7e112160532ea9e53b619853d79b04a2e7
Sergio Garcia <sergiogcx@gmail.com>
2020-11-30 11:28:29 -0600

    Refactor environment variables

--- moped-auth/cognito-api-gateway-authorizer/config.py
+++ moped-auth/cognito-api-gateway-authorizer/config.py
@@ -7,9 +7,9 @@ import urllib.request
 # A few constants
 #
 AWS_ATD_REGION = os.getenv("AWS_ATD_REGION")  # i.e., us-east-1
-AWS_ATD_USER_POOL = os.getenv("AWS_ATD_USER_POOL")  # i.e., ap-southeast-2_xxxxxxxxx
-AWS_ATD_APP_CLIENT_ID = os.getenv("AWS_ATD_APP_CLIENT_ID")
-AWS_ATD_API_GATEWAY_ARN = os.getenv("AWS_ATD_API_GATEWAY_ARN")  #i.e., arn:aws:execute-api:us-east-1:123456789123:abc321def654
+AWS_ATD_USER_POOL = os.getenv("AWS_ATD_MOPED_USER_POOL")  # i.e., ap-southeast-2_xxxxxxxxx
+AWS_ATD_APP_CLIENT_ID = os.getenv("AWS_ATD_MOPED_APP_CLIENT_ID")
+AWS_ATD_API_GATEWAY_ARN = os.getenv("AWS_ATD_MOPED_API_GATEWAY_ARN")  #i.e., arn:aws:execute-api:us-east-1:123456789123:abc321def654
 #
 # Generates a JWKs endpoint
 #
@@ -23,7 +23,3 @@ ATD_JWKS_ENDPOINT = "https://cognito-idp.{}.amazonaws.com/{}/.well-known/jwks.js
 with urllib.request.urlopen(ATD_JWKS_ENDPOINT) as f:
     response = f.read()
 ATD_JWK_KEYS = json.loads(response.decode("utf-8"))["keys"]
-
-
-def get_keys_url() -> str:
-    return ""
