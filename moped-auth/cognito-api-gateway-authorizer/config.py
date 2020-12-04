7afa8169cef317291624b8c1c79b3e56c205e98d
Sergio Garcia <sergiogcx@gmail.com>
2020-12-04 12:07:17 -0600

    Renames config variables

--- moped-auth/cognito-api-gateway-authorizer/config.py
+++ moped-auth/cognito-api-gateway-authorizer/config.py
@@ -1,25 +1,36 @@
 import os
 import json
 import urllib.request
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 #
 # A few constants
 #
-AWS_ATD_REGION = os.getenv("AWS_ATD_REGION")  # i.e., us-east-1
-AWS_ATD_USER_POOL = os.getenv("AWS_ATD_MOPED_USER_POOL")  # i.e., ap-southeast-2_xxxxxxxxx
-AWS_ATD_APP_CLIENT_ID = os.getenv("AWS_ATD_MOPED_APP_CLIENT_ID")
-AWS_ATD_API_GATEWAY_ARN = os.getenv("AWS_ATD_MOPED_API_GATEWAY_ARN")  #i.e., arn:aws:execute-api:us-east-1:123456789123:abc321def654
+AWS_ATD_MOPED_REGION = os.getenv("AWS_ATD_MOPED_REGION")  # i.e., us-east-1
+AWS_ATD_MOPED_USER_POOL = os.getenv("AWS_ATD_MOPED_USER_POOL")  # i.e., ap-southeast-2_xxxxxxxxx
+AWS_ATD_MOPED_APP_CLIENT_ID = os.getenv("AWS_ATD_MOPED_APP_CLIENT_ID")
+AWS_ATD_MOPED_API_GATEWAY_ARN = os.getenv("AWS_ATD_MOPED_API_GATEWAY_ARN")  #i.e., arn:aws:execute-api:us-east-1:123456789123:abc321def654
 
+# JWK Keys Management
+AWS_ATD_MOPED_JWK_KEYS_LOAD = os.getenv("AWS_ATD_MOPED_JWK_KEYS_LOAD", "FALSE")
+AWS_ATD_MOPED_JWK_KEYS = None
 #
 # Generates a JWKs endpoint
 #
-ATD_JWKS_ENDPOINT = "https://cognito-idp.{}.amazonaws.com/{}/.well-known/jwks.json".format(
-    AWS_ATD_REGION, AWS_ATD_USER_POOL
+AWS_ATD_MOPED_JWKS_ENDPOINT = "https://cognito-idp.{}.amazonaws.com/{}/.well-known/jwks.json".format(
+    AWS_ATD_MOPED_REGION, AWS_ATD_MOPED_USER_POOL
 )
 
 #
 # Keys, download JWK on cold start
 # https://aws.amazon.com/blogs/compute/container-reuse-in-lambda/
-with urllib.request.urlopen(ATD_JWKS_ENDPOINT) as f:
-    response = f.read()
-ATD_JWK_KEYS = json.loads(response.decode("utf-8"))["keys"]
+if AWS_ATD_MOPED_JWK_KEYS_LOAD == "TRUE":
+    with urllib.request.urlopen(AWS_ATD_MOPED_JWKS_ENDPOINT) as f:
+        response = f.read()
+    AWS_ATD_MOPED_JWK_KEYS = json.loads(response.decode("utf-8"))["keys"]
+else:
+    AWS_ATD_MOPED_JWK_KEYS = None
+    logger.info(f"Skipping loading JWK keys.")
