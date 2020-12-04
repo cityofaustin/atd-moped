8bbd542b1c999542fe033b3c29df0ecceb1b57cb
Sergio Garcia <sergiogcx@gmail.com>
2020-12-04 10:59:30 -0600

    Refactors cognito helper

--- .github/workflows/aws-cognito-helper.sh
+++ .github/workflows/aws-cognito-helper.sh
@@ -2,35 +2,35 @@
 
 case "${BRANCH_NAME}" in
 "production")
-  export WORKING_STAGE="production"
+  export WORKING_STAGE="production";
   ;;
 *)
-  export WORKING_STAGE="staging"
+  export WORKING_STAGE="staging";
   ;;
 esac
 
-echo "SOURCE -> BRANCH_NAME: ${BRANCH_NAME}"
-echo "SOURCE -> WORKING_STAGE: ${WORKING_STAGE}"
+echo "SOURCE -> BRANCH_NAME: ${BRANCH_NAME}";
+echo "SOURCE -> WORKING_STAGE: ${WORKING_STAGE}";
 
 #
 # First, we need to create the python package by installing requirements
 #
 function install_requirements() {
-  echo "Installing AWS's CLI"
-  pip install awscli
-  echo "Installing requirements..."
-  pip install -r "./requirements/${WORKING_STAGE}.txt" --target ./package
+  echo "Installing AWS's CLI";
+  pip install awscli;
+  echo "Installing requirements...";
+  pip install -r "./requirements/${WORKING_STAGE}.txt" --target ./package;
 }
 
 #
 # Secondly, we must bundle the package and python script into a single zip file bundle
 #
 function bundle_function() {
-  echo "Bundling function..."
-  cd package
-  zip -r9 ../function.zip .
-  cd ${OLDPWD}
-  zip -g function.zip handler.py
+  echo "Bundling function...";
+  cd package;
+  zip -r9 ../function.zip .;
+  cd ${OLDPWD};
+  zip -g function.zip *.py;
 }
 
 #
@@ -39,14 +39,15 @@ function bundle_function() {
 function generate_environment() {
   # If the config settings is not provided, then assume cognito pattern...
   if [[ "${1}" = "" ]]; then
-    CONFIGURATION_NAME="ATD_MOPED_COGNITO_HOOK_ENV";
+    echo "No configuration name detected, aborting.";
+    exit 1;
   else
-    CONFIGURATION_NAME=$1
+    CONFIGURATION_NAME=$1;
   fi;
 
   aws secretsmanager get-secret-value \
   --secret-id "${CONFIGURATION_NAME}_${WORKING_STAGE^^}" | \
-  jq -rc ".SecretString" > handler_config.json
+  jq -rc ".SecretString" > handler_config.json;
 }
 
 #
@@ -73,6 +74,7 @@ function deploy_cognito_function() {
   }
   echo "Resetting environment variables: ${FUNCTION_NAME} @ ${PWD}";
   aws lambda update-function-configuration \
+        --role "${ATD_MOPED_COGNITO_ROLE}" \
         --function-name "${FUNCTION_NAME}" \
         --cli-input-json file://$PWD/handler_config.json | jq -r ".LastModified";
   echo "Finished Lambda Update/Deployment"
@@ -82,19 +84,20 @@ function deploy_cognito_function() {
 # Deploys all functions in the events directory
 #
 function deploy_cognito_functions() {
-  MAIN_DIR=$PWD
-  for FUNCTION in $(find moped-auth -type d -mindepth 1 -maxdepth 1 ! -iname "tests"); do
-    FUNCTION_DIR=$(echo "${FUNCTION}" | cut -d "/" -f 2)
-    FUNCTION_NAME="atd-moped-${FUNCTION_DIR}-${WORKING_STAGE}"
-    echo "Current directory: ${PWD}"
-    echo "Building function '${FUNCTION_NAME}' @ path: '${FUNCTION}'"
-    cd $FUNCTION
-    echo "Entered directory: ${PWD}"
-    install_requirements
-    bundle_function
-    generate_environment "$CONFIGURATION_SETTINGS"
-    deploy_cognito_function "$FUNCTION_NAME"
-    cd $MAIN_DIR
-    echo "Exit, current path: ${PWD}"
-  done
+  FUNCTION=$1;
+
+  if [[ "${FUNCTION}" == "" ]]; then
+    echo "No function name specified, aborting.";
+    exit 1;
+  fi;
+
+  FUNCTION_NAME="atd-moped-${FUNCTION}-${WORKING_STAGE}";
+  echo "Current directory: ${PWD}";
+  echo "Building function '${FUNCTION_NAME}' @ path: '${FUNCTION}'";
+  cd $FUNCTION;
+  echo "Entered directory: ${PWD}";
+  install_requirements;
+  bundle_function;
+  generate_environment "$ATD_CONFIGURATION_SETTINGS";
+  deploy_cognito_function "$FUNCTION_NAME";
 }
