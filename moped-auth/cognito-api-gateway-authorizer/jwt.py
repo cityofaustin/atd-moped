a5ced683e1092009753cec0200a78bf70c449835
Sergio Garcia <sergiogcx@gmail.com>
2020-12-07 13:47:44 -0600

    Improves error handling for verify_jwt_token

--- moped-auth/cognito-api-gateway-authorizer/jwt.py
+++ moped-auth/cognito-api-gateway-authorizer/jwt.py
@@ -3,16 +3,13 @@ import time
 import logging
 import urllib.request
 
-from jose import jwk, jwt
+from jose import jwk, jwt, exceptions
 from jose.utils import base64url_decode
 
-from config import (
-    ATD_JWK_KEYS,
-    AWS_ATD_APP_CLIENT_ID
-)
+from config import AWS_ATD_MOPED_JWK_KEYS, AWS_ATD_MOPED_APP_CLIENT_ID
 
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.ERROR)
 
 
 def verify_jwt_token(token: str) -> (bool, dict):
@@ -21,25 +18,38 @@ def verify_jwt_token(token: str) -> (bool, dict):
     :param str token: The token being evaluated
     :return tuple:
     """
+    headers = None
     # Straight away, if the token is not a string return false.
     if not isinstance(token, str):
         return False, {}
-    
-    # get the kid from the headers prior to verification
-    headers = jwt.get_unverified_headers(token)
+
+    # Get the kid from the headers prior to verification
+    try:
+        headers = jwt.get_unverified_headers(token)
+        if not isinstance(headers, dict):
+            logger.error("Unable to parse headers")
+            return False, {}
+        if "kid" not in headers:
+            logger.error("KID not found in jwks.json")
+            return False, {}
+    except exceptions.JWTError as e:
+        logger.error(f"Exception: {str(e)}")
+        return False, {}
+
+    # We can assume kid is readable
     kid = headers["kid"]
     # search for the kid in the downloaded public keys
     key_index = -1
-    for i in range(len(ATD_JWK_KEYS)):
-        if kid == ATD_JWK_KEYS[i]["kid"]:
+    for i in range(len(AWS_ATD_MOPED_JWK_KEYS)):
+        if kid == AWS_ATD_MOPED_JWK_KEYS[i]["kid"]:
             key_index = i
             break
     if key_index == -1:
         logger.error("Public key not found in jwks.json")
-        return False
+        return False, {}
 
     # construct the public key
-    public_key = jwk.construct(ATD_JWK_KEYS[key_index])
+    public_key = jwk.construct(AWS_ATD_MOPED_JWK_KEYS[key_index])
     # get the last two sections of the token,
     # message and signature (encoded in base64)
     message, encoded_signature = str(token).rsplit(".", 1)
@@ -48,7 +58,7 @@ def verify_jwt_token(token: str) -> (bool, dict):
     # verify the signature
     if not public_key.verify(message.encode("utf8"), decoded_signature):
         logger.error("Signature verification failed")
-        return False
+        return False, {}
 
     logger.info("Signature successfully verified")
     # since we passed the verification, we can now safely
@@ -57,11 +67,11 @@ def verify_jwt_token(token: str) -> (bool, dict):
     # additionally we can verify the token expiration
     if time.time() > claims["exp"]:
         logger.error("Token is expired")
-        return False
+        return False, {}
     # and the Audience  (use claims['client_id'] if verifying an access token)
-    if claims["aud"] != AWS_ATD_APP_CLIENT_ID:
+    if claims["aud"] != AWS_ATD_MOPED_APP_CLIENT_ID:
         logger.error("Token was not issued for this audience")
-        return False
+        return False, {}
 
     # It appears all checks have passed
     return True, claims
