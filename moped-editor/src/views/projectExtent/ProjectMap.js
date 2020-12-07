e16ac921820c06abb2213cf8b1745bfa90d51f08
Mike <mike.dilley@gmail.com>
2020-12-07 14:47:05 -0600

    Render location polygons vector tile layer

--- moped-editor/src/views/projectExtent/ProjectMap.js
+++ moped-editor/src/views/projectExtent/ProjectMap.js
@@ -1,7 +1,34 @@
 import React, { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Layer } from "react-map-gl";
 
 const MAPBOX_TOKEN = `pk.eyJ1Ijoiam9obmNsYXJ5IiwiYSI6ImNraWV4dHR0ZjAwNnYyd3FwYjFoNHduaDcifQ.--3vRm2KHq1gh5K_L0pqtA`;
+
+const projectLayerConfig = {
+  id: "location-polygons",
+  // beforeId: "base-layer",
+  type: "line",
+  source: {
+    type: "vector",
+    tiles: [
+      "https://tiles.arcgis.com/tiles/0L95CJ0VTaxqcmED/arcgis/rest/services/location_polygons_vector_tiles_w_IDs/VectorTileServer/tile/{z}/{y}/{x}.pbf",
+    ],
+  },
+  "source-layer": "asmp_polygons",
+  // filter: ["==", "_symbol", parameters.filter],
+  // layout: {
+  //   "line-cap": "round",
+  //   "line-join": "round",
+  //   visibility: `${
+  //     overlay.options && overlay.options.includes(asmpLevel)
+  //       ? "visible"
+  //       : "none"
+  //   }`,
+  // },
+  // paint: {
+  //   "line-color": parameters.color,
+  //   "line-width": 2,
+  // },
+};
 
 const ProjectMap = () => {
   const [viewport, setViewport] = useState({
@@ -17,7 +44,9 @@ const ProjectMap = () => {
       height={1000}
       mapboxApiAccessToken={MAPBOX_TOKEN}
       onViewportChange={viewport => setViewport(viewport)}
-    />
+    >
+      <Layer key={"location-polygon"} {...projectLayerConfig} />
+    </ReactMapGL>
   );
 };
 
