761557af1923c471bb207e3aa2bd7dc98b2cb7db
Sergio Garcia <sergiogcx@gmail.com>
2020-12-11 12:53:08 -0600

    Adds Project Save Button

---
+++ moped-editor/src/views/projects/ProjectSaveButton.js
@@ -0,0 +1,102 @@
+import React from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { green } from "@material-ui/core/colors";
+import Button from "@material-ui/core/Button";
+import Fab from "@material-ui/core/Fab";
+import CheckIcon from "@material-ui/icons/Check";
+import SaveIcon from "@material-ui/icons/Save";
+
+/**
+ * Styles
+ */
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: "relative",
+  },
+  buttonSuccess: {
+    backgroundColor: green[500],
+    "&:hover": {
+      backgroundColor: green[500],
+    },
+    disabled: true,
+    cursor: "default",
+  },
+  fabProgress: {
+    color: green[500],
+    position: "absolute",
+    top: -6,
+    left: -6,
+    zIndex: 1,
+  },
+  buttonProgress: {
+    color: green[500],
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginTop: -12,
+    marginLeft: -12,
+  },
+}));
+
+
+/**
+ * ProjectSaveButton - A button that shows an animated status when saving.
+ * https://material-ui.com/components/progress/#interactive-integration
+ * @param {str} label - The string to show within the button
+ * @param {bool} loading - When true, it forces the button show loading spinner status.
+ * @param {bool} success - Then true, it forces the button to show green status with checkmark.
+ * @param {function} handleButtonClick - The onClick handler
+ * @return {JSX.Element}
+ * @constructor
+ */
+export default function ProjectSaveButton({
+  label,
+  loading,
+  success,
+  handleButtonClick,
+}) {
+  const classes = useStyles();
+
+  const buttonClassname = clsx({
+    [classes.buttonSuccess]: success,
+  });
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.wrapper}>
+        <Fab
+          aria-label="save"
+          color="primary"
+          className={buttonClassname}
+          onClick={success ? null : handleButtonClick}
+        >
+          {success ? <CheckIcon /> : <SaveIcon />}
+        </Fab>
+        {loading && (
+          <CircularProgress size={68} className={classes.fabProgress} />
+        )}
+      </div>
+      <div className={classes.wrapper}>
+        <Button
+          variant="contained"
+          color="primary"
+          className={buttonClassname}
+          disabled={loading}
+          onClick={success ? null : handleButtonClick}
+        >
+          {label}
+        </Button>
+        {loading && (
+          <CircularProgress size={24} className={classes.buttonProgress} />
+        )}
+      </div>
+    </div>
+  );
+}
