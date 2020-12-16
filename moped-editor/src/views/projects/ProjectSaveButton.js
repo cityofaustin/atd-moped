890d1a375a7375e2c74652c2149044dd13575c50
Sergio Garcia <sergiogcx@gmail.com>
2020-12-16 14:40:42 -0600

    Shows green checkmark animation on success

--- moped-editor/src/views/projects/ProjectSaveButton.js
+++ moped-editor/src/views/projects/ProjectSaveButton.js
@@ -4,6 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { green } from "@material-ui/core/colors";
 import Button from "@material-ui/core/Button";
+import {Icon} from "@material-ui/core";
 
 /**
  * Styles
@@ -71,10 +72,10 @@ export default function ProjectSaveButton({
           variant="contained"
           color="primary"
           className={buttonClassname}
-          disabled={loading}
+          disabled={loading && !success}
           onClick={success ? null : handleButtonClick}
         >
-          {label}
+          {success ? <Icon>check</Icon> : label}
         </Button>
         {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
       </div>
