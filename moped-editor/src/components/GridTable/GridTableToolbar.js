640a12bf71e27d904cfb813d4b22b1c6d30bfbd2
Sergio Garcia <sergiogcx@gmail.com>
2020-12-24 17:22:19 -0600

    Refactors GridTableToolbar

--- moped-editor/src/components/GridTable/GridTableToolbar.js
+++ moped-editor/src/components/GridTable/GridTableToolbar.js
@@ -1,19 +1,10 @@
 import React from "react";
-import PropTypes from "prop-types";
 import clsx from "clsx";
+
 import {
-    Box,
-    Button,
-    Card,
-    CardContent,
-    TextField,
-    InputAdornment,
-    SvgIcon,
     makeStyles,
-    Icon,
 } from "@material-ui/core";
-import { Search as SearchIcon } from "react-feather";
-import { Link as RouterLink } from "react-router-dom";
+
 
 const useStyles = makeStyles(theme => ({
     root: {},
@@ -25,53 +16,14 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-const GridTableToolbar = ({ change, className, ...rest }) => {
+const GridTableToolbar = ({ children, change, className, ...rest }) => {
     const classes = useStyles();
 
     return (
         <div className={clsx(classes.root, className)} {...rest}>
-            <Box display="flex" justifyContent="flex-end">
-                <Button className={classes.importButton}>Import</Button>
-                <Button className={classes.exportButton}>Export</Button>
-                <Button
-                    color="primary"
-                    variant="contained"
-                    component={RouterLink}
-                    to={"/moped/projects/new"}
-                    startIcon={<Icon>add_circle</Icon>}
-                >
-                    New Project
-                </Button>
-            </Box>
-            <Box mt={3}>
-                <Card>
-                    <CardContent>
-                        <Box maxWidth={500}>
-                            <TextField
-                                onChange={change}
-                                fullWidth
-                                InputProps={{
-                                    startAdornment: (
-                                        <InputAdornment position="start">
-                                            <SvgIcon fontSize="small" color="action">
-                                                <SearchIcon />
-                                            </SvgIcon>
-                                        </InputAdornment>
-                                    ),
-                                }}
-                                placeholder="Search project"
-                                variant="outlined"
-                            />
-                        </Box>
-                    </CardContent>
-                </Card>
-            </Box>
+            {children}
         </div>
     );
-};
-
-GridTableToolbar.propTypes = {
-    className: PropTypes.string,
 };
 
 export default GridTableToolbar;
