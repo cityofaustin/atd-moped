4e6cc4cb8f9c03e6c490ef03cc57d58f0ee7e887
Sergio Garcia <sergiogcx@gmail.com>
2020-12-29 18:14:29 -0600

    Implement search state changes

--- moped-editor/src/components/GridTable/GridTableSearchBar.js
+++ moped-editor/src/components/GridTable/GridTableSearchBar.js
@@ -1,11 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import {
-  Box,
   Button,
-  Card,
-  CardContent,
   TextField,
   InputAdornment,
   SvgIcon,
@@ -43,14 +40,87 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const GridTableSearch = ({ query, showFilters, setShowFilters }) => {
+const GridTableSearch = ({ query, searchState, filtersState }) => {
+  /**
+   * If we are not provided a search or field state, then render a message
+   */
   const classes = useStyles();
+
+  /**
+   * The contents of the search box
+   *  {string} searchFieldValue - Default: searchState.searchParameters.value
+   */
+  const [searchFieldValue, setSearchFieldValue] = useState(
+    (searchState.searchParameters && searchState.searchParameters.value) || ""
+  );
+
+  /**
+   * Stores the name of the column to search
+   * {string} fieldToSearch - Default: searchState.searchParameters.column or None
+   */
+  const [fieldToSearch, setFieldToSearch] = useState(
+    (searchState.searchParameters && searchState.searchParameters.column) || ""
+  );
+  /**
+   *  {boolean} isFieldSelected - True if a field is selected.
+   *          Default: False
+   */
+  const [isFieldSelected, setIsFieldSelected] = useState(
+    !!searchState.searchParameters || false
+  );
+
+  if (!searchState || !filtersState)
+    return <span>No search or filter state provided</span>;
+
+  /**
+   * Handles the submission of our search form
+   * @param {object} e - the event object
+   */
+  const handleSearchSubmission = e => {
+    e.preventDefault();
+
+    searchState.setSearchParameters({
+      column: fieldToSearch,
+      value: searchFieldValue,
+    });
+
+    // resetPage();
+  };
+
+  /**
+   * Clears the search results
+   */
+  const handleClearSearchResults = () => {
+    // clearFilters();
+    setSearchFieldValue("");
+    setFieldToSearch("");
+    setIsFieldSelected(false);
+    searchState.setSearchParameters({});
+  };
+
+  /**
+   * Handles the selection of our search mode in the dropdown
+   * @param {object} e - the event object
+   */
+  const handleFieldSelect = e => {
+    setIsFieldSelected(true);
+    setFieldToSearch(e.target.value);
+  };
+
+  /**
+   * Returns a human-readable label for a specific column
+   * @param {string} fieldKey - the raw column name from the database
+   * @returns {string}
+   */
+  const getFieldName = fieldKey => {
+    return query.getLabel(fieldKey, "search");
+  };
 
   /**
    * Toggles Showing filters
    */
   const handleFiltersClick = () => {
-    setShowFilters(!showFilters);
+    filtersState.setShowFilters(!filtersState.showFilters);
   };
 
   return (
@@ -62,7 +132,7 @@ const GridTableSearch = ({ query, showFilters, setShowFilters }) => {
           className={classes.formControl}
         >
           <TextField
-            onChange={null}
+            onChange={e => setSearchFieldValue(e.target.value)}
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
@@ -74,44 +144,59 @@ const GridTableSearch = ({ query, showFilters, setShowFilters }) => {
             }}
             placeholder="Search project name, description"
             variant="outlined"
+            value={searchFieldValue}
           />
         </FormControl>
       </Grid>
-      <Grid item xs={2}>
+      <Grid item xs={3}>
         <FormControl
           fullWidth
           variant="outlined"
           className={classes.formControl}
+          id="fieldToSearch"
         >
           <InputLabel id="demo-simple-select-outlined-label">Field</InputLabel>
           <Select
             fullWidth
             labelId="demo-simple-select-outlined-label"
             id="demo-simple-select-outlined"
-            value={"project_name"}
-            onChange={null}
+            value={fieldToSearch}
+            onChange={handleFieldSelect}
             label="field"
           >
-            <MenuItem value="">
-              <em>None</em>
+            <MenuItem value="" disabled>
+              <em>Select a field</em>
             </MenuItem>
             <MenuItem value={"project_name"}>Project Name</MenuItem>
             <MenuItem value={"project_description"}>Description</MenuItem>
           </Select>
         </FormControl>
       </Grid>
-      <Grid item xs={2}>
+      <Grid item xs={1}>
         <Button
           className={classes.filterButton}
           fullWidth
           variant="contained"
           color="primary"
           startIcon={<Icon>search</Icon>}
+          onClick={handleSearchSubmission}
         >
           Search
         </Button>
       </Grid>
-      <Grid item xs={2}>
+      <Grid item xs={1}>
+        <Button
+          className={classes.filterButton}
+          fullWidth
+          variant="outlined"
+          color="secondary"
+          startIcon={<Icon>backspace</Icon>}
+          onClick={handleClearSearchResults}
+        >
+          Clear
+        </Button>
+      </Grid>
+      <Grid item xs={1}>
         <Button
           className={classes.filterButtonFilters}
           fullWidth
