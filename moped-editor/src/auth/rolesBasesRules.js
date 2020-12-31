f15708b1103ec000dc82b7528e65d54b8116616c
Mike <mike.dilley@gmail.com>
2020-12-31 17:28:03 -0600

    Add role-based rules

---
+++ moped-editor/src/auth/rolesBasesRules.js
@@ -0,0 +1,34 @@
+// Read-only permissions not enforced by Can component
+// since all authorized users should have access
+const readOnlyStaticRules = [
+  "dashboard:visit",
+  "projects:visit",
+  "users:visit",
+  "user:get",
+];
+
+const editorStaticRules = [];
+
+const adminStaticRules = [
+  "user:create",
+  "user:edit",
+  "user:editRole",
+  "user:delete",
+];
+
+export const rules = {
+  "moped-viewer": {
+    label: "Read-only",
+    static: readOnlyStaticRules,
+  },
+  "moped-editor": {
+    label: "Editor",
+    static: [...readOnlyStaticRules, ...editorStaticRules],
+  },
+  "moped-admin": {
+    label: "Admin",
+    static: [...readOnlyStaticRules, ...editorStaticRules, ...adminStaticRules],
+  },
+};
+
+export default rules;
