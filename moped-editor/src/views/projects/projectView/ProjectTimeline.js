b67b22afed25c7ac4b864004e3c8f05e5c190f71
Mateo Clarke <code@mateoclarke.com>
2021-01-13 03:16:07 -0600

    adds basic project phases table

--- moped-editor/src/views/projects/projectView/ProjectTimeline.js
+++ moped-editor/src/views/projects/projectView/ProjectTimeline.js
@@ -1,30 +1,26 @@
 import React from "react";
+
+// Material
+import { makeStyles } from "@material-ui/core";
+import {
+  Box,
+  Button,
+  CardContent,
+  CircularProgress,
+  Grid,
+  Icon,
+  Table,
+  TableHead,
+  TableCell,
+  TableBody,
+  TableRow,
+  Typography,
+} from "@material-ui/core";
+
+// Query
+import { TIMELINE_QUERY } from "../../../queries/project";
 import { useQuery } from "@apollo/react-hooks";
 import { useParams } from "react-router-dom";
-import { Box, Grid, CardContent, CircularProgress } from "@material-ui/core";
-import { TIMELINE_QUERY } from "../../../queries/project";
-
-const formatValue = value => {
-  let formattedValue = value.field;
-  switch (value.type) {
-    case "boolean":
-      formattedValue = value.field === true ? "Yes" : "No";
-      break;
-    case "string":
-      formattedValue =
-        value.field === "" ? (
-          <Box color="text.disabled">
-            <p>No data</p>
-          </Box>
-        ) : (
-          value.field
-        );
-      break;
-    default:
-      break;
-  }
-  return formattedValue;
-};
 
 const ProjectTimeline = () => {
   const { projectId } = useParams();
@@ -38,12 +34,70 @@ const ProjectTimeline = () => {
 
   console.log(data);
 
+  const tableConfig = {
+    columns: {
+      project_id: {
+        label_table: "",
+      },
+      phase_name: {
+        label_table: "Phase Name",
+        format: value => value.charAt(0).toUpperCase() + value.slice(1),
+      },
+      is_current_phase: {
+        label_table: "Active?",
+      },
+      phase_start: {
+        label_table: "Start Date",
+      },
+      phase_end: {
+        label_table: "End Date",
+      },
+    },
+  };
+
+  const formatValue = (value, column) => {
+    console.log(value, column);
+    console.log(tableConfig.columns[column]);
+    if (!tableConfig.columns[column].format) return value;
+
+    const formattedValue = tableConfig.columns[column].format(value);
+    console.log(formattedValue);
+    return formattedValue;
+  };
+
   return (
     <CardContent>
       <Grid container spacing={2}>
         <Grid item xs={12} md={6}>
           <Grid container>
-            <code>{JSON.stringify(data)}</code>
+            <Typography>Project Phases</Typography>
+            <Table size="small">
+              <TableHead>
+                {Object.keys(tableConfig.columns).map(column => (
+                  <TableCell>
+                    {tableConfig.columns[column].label_table}
+                  </TableCell>
+                ))}
+              </TableHead>
+              <TableBody>
+                {data.moped_proj_phases.map(row => (
+                  <TableRow key={row.project_phase_id}>
+                    {Object.keys(tableConfig.columns).map(column => (
+                      <TableCell>{formatValue(row[column], column)}</TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+            <Button
+              color="primary"
+              variant="contained"
+              // component={RouterLink}
+              // to={"/moped/projects/new"}
+              startIcon={<Icon>add_circle</Icon>}
+            >
+              New Phase
+            </Button>
           </Grid>
         </Grid>
       </Grid>
