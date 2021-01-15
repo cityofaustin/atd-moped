5b52da8353403a66c1e34807d767a8aaf1712240
Sergio Garcia <sergiogcx@gmail.com>
2021-01-15 14:03:46 -0600

    Adds index removal

--- moped-database/migrations/1610398986915_moped_activity_log/down.sql
+++ moped-database/migrations/1610398986915_moped_activity_log/down.sql
@@ -5,3 +5,5 @@ drop index if exists moped_activity_log_record_id_index;
 drop index if exists moped_activity_log_record_type_id_index;
 
 drop index if exists moped_activity_log_updated_by_id_index;
+
+drop index if exists moped_activity_log_updated_by_index;
