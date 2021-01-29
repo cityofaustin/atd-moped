4fd6aad4f4a5ac143f7acc5672f4721ab411f1ac
Sergio Garcia <sergiogcx@gmail.com>
2021-01-29 11:53:12 -0600

    Adds user_id and workgroup_id to queries

--- moped-api/users/queries.py
+++ moped-api/users/queries.py
@@ -33,6 +33,10 @@ GRAPHQL_UPDATE_USER = """
     mutation update_moped_user($userBoolExp: moped_users_bool_exp!, $user: moped_users_set_input) {
       update_moped_users(where: $userBoolExp, _set: $user) {
         affected_rows
+        returning {
+          user_id
+          workgroup_id
+        }
       }
     }
 """
@@ -42,6 +46,10 @@ GRAPHQL_DEACTIVATE_USER = """
     mutation update_moped_user($userBoolExp: moped_users_bool_exp!) {
       update_moped_users(where: $userBoolExp, _set: { status_id: 0, cognito_user_id: null }) {
         affected_rows
+        returning {
+          user_id
+          workgroup_id
+        }
       }
     }
 """
