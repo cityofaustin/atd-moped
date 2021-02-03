213ffc404c763344eca6b4d1d9f9a5d72816ed35
Sergio Garcia <sergiogcx@gmail.com>
2021-02-03 14:49:14 -0600

    Adds avatar, comments format date

--- moped-editor/src/views/projects/projectView/ProjectActivityLog.js
+++ moped-editor/src/views/projects/projectView/ProjectActivityLog.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
+import { ProjectActivityLogTableMaps } from "./ProjectActivityLogTableMaps";
 
 import {
+  Avatar,
+  Box,
   CardContent,
   CircularProgress,
   Grid,
@@ -19,17 +22,28 @@ import {
 import { PROJECT_ACTIVITY_LOG } from "../../../queries/project";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   table: {
     minWidth: 650,
   },
-});
+  tableCell: {
+    verticalAlign: "top",
+  },
+  tableChangeItem: {
+    padding: "0 .5rem",
+  },
+  avatarSmall: {
+    width: theme.spacing(4),
+    height: theme.spacing(4),
+  },
+  avatarName: {
+    margin: "0.3rem 0 0 .5rem",
+  },
+}));
 
 const ProjectActivityLog = () => {
   const { projectId } = useParams();
   const classes = useStyles();
-
-  console.log("ProjectId: ", projectId);
 
   const { loading, error, data } = useQuery(PROJECT_ACTIVITY_LOG, {
     variables: { projectId },
@@ -38,7 +52,12 @@ const ProjectActivityLog = () => {
   if (loading) return <CircularProgress />;
   if (error) return `Error! ${error.message}`;
 
-  const formatDate = value => new Date(value).toLocaleDateString();
+  /**
+   * Formats the iso date into human-readable locale date.
+   * @param {string} date - The ISO date as a string
+   * @return {string}
+   */
+  const formatDate = date => new Date(date).toLocaleDateString();
 
   return (
     <CardContent>
@@ -48,9 +67,15 @@ const ProjectActivityLog = () => {
           <Table className={classes.table} aria-label="simple table">
             <TableHead>
               <TableRow>
-                <TableCell align="left"><b>Date</b></TableCell>
-                <TableCell align="left"><b>User</b></TableCell>
-                <TableCell align="left"><b>Change</b></TableCell>
+                <TableCell align="left">
+                  <b>Date</b>
+                </TableCell>
+                <TableCell align="left">
+                  <b>User</b>
+                </TableCell>
+                <TableCell align="left">
+                  <b>Change</b>
+                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -61,26 +86,62 @@ const ProjectActivityLog = () => {
                     component="th"
                     scope="row"
                     width="15%"
+                    className={classes.tableCell}
                   >
                     {formatDate(change.created_at)}
                   </TableCell>
-                  <TableCell align="left" width="25%">
-                    {change.moped_user.first_name} {change.moped_user.last_name}
+                  <TableCell
+                    align="left"
+                    width="25%"
+                    className={classes.tableCell}
+                  >
+                    <Box display="flex" p={0}>
+                      <Box p={0}>
+                        <Avatar
+                          alt={`${change.moped_user.first_name} ${change.moped_user.last_name}`}
+                          src="/moped/static/images/avatars/userAvatar.jpg"
+                          className={classes.avatarSmall}
+                        >
+                          {getInitials(
+                            `${change.moped_user.first_name} ${change.moped_user.last_name}`
+                          )}
+                        </Avatar>
+                      </Box>
+                      <Box p={0} flexGrow={1} className={classes.avatarName}>
+                        {change.moped_user.first_name}{" "}
+                        {change.moped_user.last_name}
+                      </Box>
+                    </Box>
                   </TableCell>
-                  <TableCell align="left" width="*">
-                    <Grid container>
-                      <Grid item style={{"padding": ".3rem"}}>
-                        <Icon>create</Icon>
-                      </Grid>
-                      {change.description.map(changeItem => {
-                        return (
-                          <Grid item>
-                            <b>{changeItem.field}</b> from{" "}
-                            <b>{changeItem.old}</b> to <b>{changeItem.new}</b>
-                          </Grid>
-                        );
-                      })}
-                    </Grid>
+                  <TableCell
+                    align="left"
+                    width="*"
+                    className={classes.tableCell}
+                  >
+                    <Box display="flex" p={0}>
+                      <Box p={0}>
+                        <Icon>{getChangeIcon(change.record_type)}</Icon>
+                      </Box>
+                      <Box p={0} flexGrow={1}>
+                        <Grid continer>
+                          {change.description.map(changeItem => {
+                            return (
+                              <Grid item className={classes.tableChangeItem}>
+                                <b>
+                                  {getRecordTypeLabel(change.record_type)}{" "}
+                                  {getHumanReadableField(
+                                    change.record_type,
+                                    changeItem.field
+                                  )}
+                                </b>{" "}
+                                from <b>&quot;{changeItem.old}&quot;</b> to{" "}
+                                <b>&quot;{changeItem.new}&quot;</b>
+                              </Grid>
+                            );
+                          })}
+                        </Grid>
+                      </Box>
+                    </Box>
                   </TableCell>
                 </TableRow>
               ))}
