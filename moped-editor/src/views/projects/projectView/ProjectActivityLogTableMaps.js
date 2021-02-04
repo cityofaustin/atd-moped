e82b52b8e60d1df21f8b9c315a206ef0caf471e7
Sergio Garcia <sergiogcx@gmail.com>
2021-02-04 15:25:20 -0600

    Adds labels for moped_proj_groups

--- moped-editor/src/views/projects/projectView/ProjectActivityLogTableMaps.js
+++ moped-editor/src/views/projects/projectView/ProjectActivityLogTableMaps.js
@@ -305,36 +305,36 @@ export const ProjectActivityLogTableMaps = {
     },
   },
   moped_proj_groups: {
-    label: "",
+    label: "Project Group",
     fields: {
       project_id: {
         icon: "",
-        label: "",
+        label: "Project ID",
         type: "int4",
       },
       date_added: {
         icon: "",
-        label: "",
+        label: "Date Added",
         type: "timestamptz",
       },
       added_by: {
         icon: "",
-        label: "",
+        label: "Added by",
         type: "int4",
       },
       group_id: {
         icon: "",
-        label: "",
+        label: "Group ID",
         type: "int4",
       },
       group_name: {
         icon: "",
-        label: "",
+        label: "Name",
         type: "text",
       },
       proj_group_id: {
         icon: "",
-        label: "",
+        label: "ID",
         type: "int4",
       },
     },
