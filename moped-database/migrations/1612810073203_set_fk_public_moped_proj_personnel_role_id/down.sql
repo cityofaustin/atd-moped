1e9eda4204872b58cb188287019110a25d1f89e7
Mike <mike.dilley@gmail.com>
2021-02-08 13:31:31 -0600

    Remove duplicate migrations

--- moped-database/migrations/1612810073203_set_fk_public_moped_proj_personnel_role_id/down.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."moped_proj_personnel" drop constraint "moped_proj_personnel_role_id_fkey2";
--- moped-database/migrations/1612812122643_delete_fk_public_moped_proj_personnel_moped_proj_personnel_role_id_fkey2/down.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."moped_proj_personnel" add foreign key ("role_id") references "public"."moped_project_roles"("project_role_id") on update restrict on delete restrict;
