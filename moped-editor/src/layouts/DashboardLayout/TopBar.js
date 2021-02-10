27129b0a323a8a58b77d023b7ef321621a66b9b1
Sergio Garcia <sergiogcx@gmail.com>
2021-02-10 10:41:51 -0600

    Uppercase initials

--- moped-editor/src/layouts/DashboardLayout/TopBar.js
+++ moped-editor/src/layouts/DashboardLayout/TopBar.js
@@ -35,7 +35,7 @@ const TopBar = ({ className, onMobileNavOpen, ...rest }) => {
         .replace("azure_ad", "")
         .replace(subdomain, "")
         .split(".");
-      return first.charAt(0) + last.charAt(0);
+      return String(first.charAt(0) + last.charAt(0)).toUpperCase();
     } catch {
       return null;
     }
