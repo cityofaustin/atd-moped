a4cee42a8c6ef06906e9292ac9b3046aa53366cc
Sergio Garcia <sergiogcx@gmail.com>
2021-02-19 13:51:04 -0600

    Clean up

--- moped-editor/src/components/ApolloErrorHandler.js
+++ moped-editor/src/components/ApolloErrorHandler.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Backdrop from "@material-ui/core/Backdrop";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { makeStyles } from "@material-ui/core/styles";
