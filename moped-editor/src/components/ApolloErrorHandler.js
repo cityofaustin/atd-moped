06addf6a930e76dee3beb28ce4c60bba1880c988
Sergio Garcia <sergiogcx@gmail.com>
2021-02-19 13:45:42 -0600

    Create error handler component

---
+++ moped-editor/src/components/ApolloErrorHandler.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
+  },
+  backdropMessage: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
+    marginLeft: "1rem",
+  },
+}));
+
+const ApolloErrorHandler = props => {
+  const classes = useStyles();
+
+  // Error Variables
+  const error = props?.error ?? null;
+  const errorString = JSON.stringify(error);
+  const jwtError = errorString.includes("JWT") || errorString.includes("token");
+
+  if (jwtError) {
+    setTimeout(() => {
+      window.location.reload();
+    }, 1500);
+  }
+
+  return (
+    <>
+      {jwtError ? (
+        <Backdrop className={classes.backdrop} open={true} onClick={null}>
+          <CircularProgress color="inherit" />
+          <p className={classes.backdropMessage}>Please Wait</p>
+        </Backdrop>
+      ) : error ? (
+        <p>Error: {error.message}</p>
+      ) : (
+        props.children
+      )}
+    </>
+  );
+};
+
+export default ApolloErrorHandler;
