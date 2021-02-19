3e72efb8e144ecf08cf37cf9fa98aed887156194
Sergio Garcia <sergiogcx@gmail.com>
2021-02-18 18:34:37 -0600

    Trace execution order (debug)

--- moped-editor/src/auth/useAuthentication.js
+++ moped-editor/src/auth/useAuthentication.js
@@ -29,22 +29,27 @@ import { signInButton, signInButtonContent } from "@aws-amplify/ui";
  * @see https://github.com/aws-amplify/amplify-js/blob/master/packages/amazon-cognito-identity-js/src/CognitoUser.js
  */
 const useAuthentication = () => {
+  console.log("[AUTH] useAuthentication() start");
   const [user, setUser] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
   const refreshState = useCallback(() => {
+
     setIsLoading(true);
+    console.log("[AUTH] useAuthentication() refresh state");
 
     Auth.currentAuthenticatedUser()
       .then(user => {
+        console.log("[AUTH] useAuthentication().currentAuthenticatedUser().then() setting user state");
         setUser(user);
         setIsAuthenticated(_isAuthenticated(user));
         setError(null);
         setIsLoading(false);
       })
       .catch(err => {
+        console.log("[AUTH] useAuthentication().currentAuthenticatedUser().catch() error");
         setUser(null);
         setIsAuthenticated(false);
         if (err === "not authenticated") {
@@ -58,11 +63,13 @@ const useAuthentication = () => {
 
   // Make sure our state is loaded before first render
   useLayoutEffect(() => {
+    console.log("[AUTH] useLayoutEffect() refreshing state");
     refreshState();
   }, [refreshState]);
 
   // Subscribe to auth events
   useEffect(() => {
+    console.log("[AUTH] useEffect() Subscribe to auth events");
     const handler = ({ payload }) => {
       switch (payload.event) {
         case "configured":
@@ -85,16 +92,25 @@ const useAuthentication = () => {
   }, [refreshState]);
 
   const signIn = useCallback(() => {
+    console.log("[AUTH] signIn() signing in");
     setIsLoading(true);
-    Auth.federatedSignIn({ provider: "AzureAD" }).catch(err => {
+    Auth.federatedSignIn({ provider: "AzureAD" }).then(_ => {
+      console.log("[AUTH] Auth.federatedSignIn().signIn() signing in");
+    }).catch(err => {
+      console.log("[AUTH] Auth.federatedSignIn().signIn() error");
       setError(err);
     });
   }, []);
 
   const signOut = useCallback(() => {
+    console.log("[AUTH] signIn() signing out");
     Auth.signOut()
-      .then(_ => refreshState())
+      .then(_ => {
+        console.log("[AUTH] signOut().Auth.signOut() success...");
+        refreshState();
+      })
       .catch(err => {
+        console.log("[AUTH] signOut().Auth.signOut() error...");
         setError(err);
       });
   }, [refreshState]);
@@ -120,6 +136,10 @@ const useAuthentication = () => {
 };
 
 const _isAuthenticated = user => {
+
+  console.log("_isAuthenticated: debugging..."); debugger;
+
+
   if (
     !user ||
     !user.signInUserSession ||
@@ -127,16 +147,20 @@ const _isAuthenticated = user => {
     !user.signInUserSession.accessToken ||
     !user.signInUserSession.accessToken.getExpiration
   ) {
+    console.log("[AUTH] _isAuthenticated() not signed in!");
     return false;
   }
 
+  console.log("[AUTH] _isAuthenticated() checking things...");
   const session = user.signInUserSession;
   const isValid = session.isValid() || false;
 
   const sessionExpiry = new Date(session.accessToken.getExpiration() * 1000);
   const isExpired = new Date() > sessionExpiry;
-
-  return isValid && !isExpired;
+  const output = isValid && !isExpired;
+  console.log("[AUTH] _isAuthenticated() checking things: " + output);
+  return output;
 };
+
 
 export default useAuthentication;
