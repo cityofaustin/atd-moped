1f7cdd6b7dac87331590fcc3deed4e6912861dd2
tillyw <tilly.whitson@austintexas.gov>
2021-03-02 10:40:22 -0600

    only render eCapris ID for projects that have them

--- moped-editor/src/components/ExternalLink.js
+++ moped-editor/src/components/ExternalLink.js
@@ -3,14 +3,17 @@ import Link from "@material-ui/core/Link";
 import OpenInNewIcon from "@material-ui/icons/OpenInNew";
 
 const ExternalLink = ({ url, text }) => {
-  console.log(text);
   return (
-    <div>
-      <Link href={url} target="_blank" rel="noopener noreferrer">
-        {text}
+    <span>
+      {!!text ? (
+        <Link href={url} target="_blank" rel="noopener noreferrer">
+          {text}
           <OpenInNewIcon style={{ fontSize: 16 }} />
-      </Link>
-    </div>
+        </Link>
+      ) : (
+        <span>-</span>
+      )}
+    </span>
   );
 };
 
