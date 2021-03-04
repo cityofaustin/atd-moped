b6c9a06335261827b782df62c3961252d7e7873e
Sergio Garcia <sergiogcx@gmail.com>
2021-03-03 18:27:22 -0600

    Refactors ProjectFiles tab

--- moped-editor/src/views/projects/projectView/ProjectFiles.js
+++ moped-editor/src/views/projects/projectView/ProjectFiles.js
@@ -1,84 +1,205 @@
 import React from "react";
 import {
-  AccordionDetails,
+  Button,
   CardContent,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
   Grid,
-  Typography,
+  Icon,
+  InputAdornment,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
 } from "@material-ui/core";
 
 import FileUpload from "../../../components/FileUpload/FileUpload";
 
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import clsx from "clsx";
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    width: "100%",
-  },
   title: {
     padding: "0rem 0 2rem 0",
+  },
+  uploadFileButton: {
+    float: "right",
   },
   secondaryHeading: {
     fontSize: theme.typography.pxToRem(15),
     color: theme.palette.text.secondary,
   },
-  icon: {
-    verticalAlign: "bottom",
-    height: 20,
-    width: 20,
+  textField: {
+    marginTop: "1rem",
   },
-  details: {
-    padding: 0,
+  textFieldAdornment: {
+    position: "relative",
+    top: "-1.6rem",
   },
-  list: {
-    flexBasis: "66.66%",
-    padding: 0,
-  },
-  uploads: {
-    flexBasis: "33.33%",
-  },
-  helper: {
-    borderLeft: `2px solid ${theme.palette.divider}`,
-    padding: theme.spacing(1, 2),
-  },
-  link: {
-    color: theme.palette.primary.main,
-    textDecoration: "none",
-    "&:hover": {
-      textDecoration: "underline",
-    },
+  textFieldAdornmentColor: {
+    color: "grey",
   },
 }));
 
 const ProjectFiles = props => {
   const classes = useStyles();
 
+  const [dialogOpen, setDialogOpen] = React.useState(false);
+
+  const handleClickUploadFile = () => {
+    setDialogOpen(true);
+  };
+
+  const handleClickCloseUploadFile = () => {
+    setDialogOpen(false);
+  };
+
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+
+  const rows = [
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+  ];
+
   return (
     <CardContent>
       <Grid container spacing={2}>
         <Grid item xs={12} md={12}>
-          <div className={classes.root}>
-            <h2 className={classes.title}>File Attachments</h2>
-
-            <AccordionDetails className={classes.details}>
-              <div className={classes.list}>This is a list!</div>
-              <div className={clsx(classes.uploads, classes.helper)}>
-                <FileUpload />
-                <Typography variant="caption">
-                  To upload, drag and drop your files here or click browse.<br />
-                  Compatible with Chrome, Safari, Edge, FireFox and Brave.<br />
-                  <a
-                      href="#secondary-heading-and-columns"
-                      className={classes.link}
-                  >
-                    Learn more
-                  </a>
-                </Typography>
-              </div>
-            </AccordionDetails>
-          </div>
+          <Grid container>
+            <Grid sm={12} md={6}>
+              <h2 className={classes.title}>File Attachments</h2>
+            </Grid>
+            <Grid sm={12} md={6}>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.uploadFileButton}
+                startIcon={<Icon>backup</Icon>}
+                onClick={handleClickUploadFile}
+              >
+                Upload File
+              </Button>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>File Name</TableCell>
+              <TableCell align="right">Description</TableCell>
+              <TableCell align="right">Uploaded by</TableCell>
+              <TableCell align="right">Upload Date</TableCell>
+              <TableCell align="right">File Size (mb)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Dialog
+        open={dialogOpen}
+        onClose={handleClickCloseUploadFile}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Upload Media"}
+        </DialogTitle>
+        <DialogContent>
+          <Grid container>
+            <Grid md={12}>
+              <TextField
+                className={classes.textField}
+                id="standard-multiline-flexible"
+                placeholder={"File Title"}
+                multiline
+                rowsMax={4}
+                value={null}
+                onChange={null}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment
+                      position="start"
+                      className={classes.textFieldAdornmentColor}
+                    >
+                      <Icon>info</Icon>
+                    </InputAdornment>
+                  ),
+                }}
+                fullWidth
+              />
+              <TextField
+                className={classes.textField}
+                id="standard-multiline-static"
+                placeholder={"File Description"}
+                multiline
+                rows={4}
+                defaultValue={null}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment
+                      position="start"
+                      className={`${classes.textFieldAdornment} ${classes.textFieldAdornmentColor}`}
+                    >
+                      <Icon>textsms</Icon>
+                    </InputAdornment>
+                  ),
+                }}
+                fullWidth
+              />
+            </Grid>
+            <Grid md={12} className={classes.fileUpload}>
+              <FileUpload
+                limit={1}
+                sizeLimit={"1024MB"}
+                principal={"project"}
+                projectId={"123"}
+              />
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleClickCloseUploadFile}
+            color="primary"
+            autoFocus
+          >
+            Cancel
+          </Button>
+          <Button
+            onClick={handleClickCloseUploadFile}
+            color="primary"
+            variant="contained"
+            startIcon={<Icon>save</Icon>}
+            disabled={true}
+          >
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
     </CardContent>
   );
 };
