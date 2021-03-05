c1bae37de36e33a87d78597302bcca2ce0210681
Sergio Garcia <sergiogcx@gmail.com>
2021-03-05 14:43:06 -0600

    Refactors migration

--- moped-database/migrations/1614966915065_set_fk_public_moped_project_files_created_by/down.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."moped_project_files" drop constraint "moped_project_files_created_by_fkey";
--- moped-database/migrations/1614966915065_set_fk_public_moped_project_files_created_by/up.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."moped_project_files"
-           add constraint "moped_project_files_created_by_fkey"
-           foreign key ("created_by")
-           references "public"."moped_users"
-           ("user_id") on update no action on delete no action;
