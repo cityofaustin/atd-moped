72e2ab28a301011d4c5ea6c18c9b5378ee90b2ab
Sergio Garcia <sergiogcx@gmail.com>
2021-03-10 12:32:01 -0600

    Account file upload manipulation

--- moped-editor/src/views/account/AccountView/Profile.js
+++ moped-editor/src/views/account/AccountView/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import clsx from "clsx";
 import {
@@ -11,9 +11,28 @@ import {
   Divider,
   Typography,
   makeStyles,
+  CircularProgress,
 } from "@material-ui/core";
-import { useUser } from "../../../auth/user";
+
+import {
+  getSessionDatabaseData,
+  setSessionDatabaseData,
+  getDatabaseId,
+  useUser,
+} from "../../../auth/user";
 import emailToInitials from "../../../utils/emailToInitials";
+import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
+import FileUploadDialogSimple from "../../../components/FileUpload/FileUploadDialogSimple";
+
+import {
+  ACCOUNT_USER_PROFILE_GET,
+  ACCOUNT_USER_PICTURE_UPDATE,
+  ACCOUNT_USER_PICTURE_DELETE,
+} from "../../../queries/account";
+
+import { useMutation, useQuery } from "@apollo/client";
+import CDNAvatar from "../../../components/CDN/Avatar";
+import { DeleteForever } from "@material-ui/icons";
 
 const useStyles = makeStyles(() => ({
   root: {},
@@ -31,41 +50,200 @@ const Profile = ({ className, ...rest }) => {
   const classes = useStyles();
   const { user } = useUser();
 
+  /**
+   * @constant {boolean} dialogOpen - True to make the save dialog visible
+   * @function setDialogOpen - Changes the state of the dialogOpen constant
+   */
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const { loading, error, data, refetch } = useQuery(ACCOUNT_USER_PROFILE_GET, {
+    variables: {
+      userId: getDatabaseId(user),
+    },
+  });
+
+  const [updateAccountPicture] = useMutation(ACCOUNT_USER_PICTURE_UPDATE);
+
+  const [deleteAccountPicture] = useMutation(ACCOUNT_USER_PICTURE_DELETE);
+
+  const resetUserPicture = fileKey => {
+    const userData = getSessionDatabaseData();
+    userData["picture"] = fileKey;
+    setSessionDatabaseData(userData);
+  };
+
+  /**
+   * Persists the file data into the database
+   * @param {Object} fileDataBundle - The file bundle as provided by the FileUpload component
+   */
+  const handleClickSaveFile = fileDataBundle => {
+    updateAccountPicture({
+      variables: {
+        userId: parseInt(getDatabaseId(user)),
+        picture: fileDataBundle.key,
+      },
+    })
+      .then(() => {
+        resetUserPicture(fileDataBundle.key);
+        setDialogOpen(false);
+        refetch();
+      })
+      .finally(() => {
+        window.location.reload();
+      });
+  };
+
+  /**
+   * Handles the upload file button onClick behavior
+   */
+  const handleClickUploadFile = () => {
+    setDialogOpen(true);
+  };
+
+  /**
+   * Handles the cancel button onClick behavior
+   */
+  const handleClickCloseUploadFile = () => {
+    setDialogOpen(false);
+  };
+
+  const handleClickDeleteFile = () => {
+    deleteAccountPicture({
+      variables: {
+        userId: parseInt(getDatabaseId(user)),
+      },
+    })
+      .then(() => {
+        resetUserPicture(null);
+        refetch();
+      })
+      .finally(() => {
+        window.location.reload();
+      });
+  };
+
+  const userProfile = data
+    ? "moped_users" in data && data["moped_users"].length > 0
+      ? data["moped_users"][0]
+      : null
+    : null;
+
   return (
-    <Card className={clsx(classes.root, className)} {...rest}>
-      <CardContent>
-        <Box alignItems="center" display="flex" flexDirection="column">
-          <Box>
-            <Avatar
-              className={classes.avatar}
-              src={user?.userAvatar}
-              style={{ backgroundColor: user?.userColor }}
-            >
-              <Typography className={classes.userInitials}>
-                {emailToInitials(user?.idToken?.payload?.email)}
+    <>
+      {loading && <CircularProgress />}
+      {error && (
+        <Card className={clsx(classes.root, className)} {...rest}>
+          <CardContent>
+            <Box alignItems="center" display="flex" flexDirection="column">
+              <Box>
+                <Avatar
+                  className={classes.avatar}
+                  src={user?.userAvatar}
+                  style={{ backgroundColor: user?.userColor }}
+                >
+                  <Typography className={classes.userInitials}>
+                    {emailToInitials(user?.idToken?.payload?.email)}
+                  </Typography>
+                </Avatar>
+              </Box>
+              <Typography color="textPrimary" gutterBottom variant="h3">
+                {String(
+                  user?.userName ?? user?.idToken?.payload?.email
+                ).toLowerCase()}
               </Typography>
-            </Avatar>
-          </Box>
-          <Typography color="textPrimary" gutterBottom variant="h3">
-            {String(
-              user?.userName ?? user?.idToken?.payload?.email
-            ).toLowerCase()}
-          </Typography>
-          <Typography color="textSecondary" variant="body1">
-            {user?.userJobTitle ?? "Austin Transportation"}
-          </Typography>
-          <Typography color="textSecondary" variant="body1">
-            {user?.userCity ?? "Austin, TX"}
-          </Typography>
-        </Box>
-      </CardContent>
-      <Divider />
-      <CardActions>
-        <Button color="primary" fullWidth variant="text">
-          Upload picture
-        </Button>
-      </CardActions>
-    </Card>
+              <Typography color="textSecondary" variant="body1">
+                {user?.userJobTitle ?? "Austin Transportation"}
+              </Typography>
+              <Typography color="textSecondary" variant="body1">
+                {user?.userCity ?? "Austin, TX"}
+              </Typography>
+            </Box>
+          </CardContent>
+          <Divider />
+          <CardActions>
+            <Button
+              fullWidth
+              color="primary"
+              variant="text"
+              startIcon={<AddAPhotoIcon />}
+              onClick={handleClickUploadFile}
+            >
+              Upload Picture
+            </Button>
+          </CardActions>
+          <FileUploadDialogSimple
+            title={"Upload Picture"}
+            dialogOpen={dialogOpen}
+            handleClickCloseUploadFile={handleClickCloseUploadFile}
+            handleClickSaveFile={handleClickSaveFile}
+            principal={"user"}
+          />
+        </Card>
+      )}
+
+      {userProfile && (
+        <Card
+          xs={12}
+          sm={6}
+          md={6}
+          className={clsx(classes.root, className)}
+          {...rest}
+        >
+          <CardContent>
+            <Box alignItems="center" display="flex" flexDirection="column">
+              <Box>
+                <CDNAvatar
+                  src={userProfile?.picture ?? null}
+                  initials={
+                    userProfile["first_name"][0] + userProfile["last_name"][0]
+                  }
+                />
+              </Box>
+              <Typography color="textPrimary" gutterBottom variant="h3">
+                {userProfile["first_name"]} {userProfile["last_name"]}
+              </Typography>
+              <Typography color="textPrimary" gutterBottom variant="h3">
+                {String(userProfile["email"]).toLowerCase()}
+              </Typography>
+              <Typography color="textSecondary" variant="body1">
+                {userProfile["title"] ?? "Austin Transportation"}
+              </Typography>
+              <Typography color="textSecondary" variant="body1">
+                {userProfile["workgroup"] ?? "Austin, TX"}
+              </Typography>
+            </Box>
+          </CardContent>
+          <Divider />
+          <CardActions>
+            <Button
+              fullWidth
+              color="primary"
+              variant="text"
+              startIcon={<AddAPhotoIcon />}
+              onClick={handleClickUploadFile}
+            >
+              Upload Picture
+            </Button>
+            <Button
+              fullWidth
+              color="primary"
+              variant="text"
+              startIcon={<DeleteForever />}
+              onClick={handleClickDeleteFile}
+            >
+              Delete Picture
+            </Button>
+          </CardActions>
+          <FileUploadDialogSimple
+            title={"Upload Picture"}
+            dialogOpen={dialogOpen}
+            handleClickCloseUploadFile={handleClickCloseUploadFile}
+            handleClickSaveFile={handleClickSaveFile}
+            principal={"user"}
+          />
+        </Card>
+      )}
+    </>
   );
 };
 
