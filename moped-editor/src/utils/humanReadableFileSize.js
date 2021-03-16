2a355858079dc8679c082ee13c4c2babf2e02857
Sergio Garcia <sergiogcx@gmail.com>
2021-03-16 17:31:34 -0500

    Clean up code a bit more

--- moped-editor/src/utils/humanReadableFileSize.js
+++ moped-editor/src/utils/humanReadableFileSize.js
@@ -14,11 +14,10 @@ export default (bytes, si=false, dp=1) => {
         return bytes + ' B';
     }
 
-    const units = si
-        ? ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-        : ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const units = ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
     let u = -1;
-    const r = 10**dp;
+    const r = 10 ** dp;
 
     do {
         bytes /= thresh;
