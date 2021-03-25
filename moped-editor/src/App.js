c97eaf0b4ede2fc2def4064b84e3570fecfeb128
Sergio Garcia <sergio.garcia@dataconfluent.com>
2021-03-24 23:40:42 -0500

    Add LInkError handler

--- moped-editor/src/App.js
+++ moped-editor/src/App.js
@@ -1,5 +1,5 @@
 import "react-perfect-scrollbar/dist/css/styles.css";
-import React from "react";
+import React, { useState } from "react";
 import { useRoutes } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core";
 import GlobalStyles from "src/components/GlobalStyles";
@@ -9,13 +9,17 @@ import { routes, restrictRoutes } from "src/routes";
 import { useUser, getJwt, getHighestRole } from "./auth/user";
 import { setContext } from "@apollo/client/link/context";
 
+import ApolloLinkErrorHandler from "./components/ApolloLinkErrorHandler";
+
 // Apollo GraphQL Client
 import {
   ApolloProvider,
   ApolloClient,
   InMemoryCache,
   createHttpLink,
+  ApolloLink,
 } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 
 const HASURA_ENDPOINT = process.env.REACT_APP_HASURA_ENDPOINT;
 
@@ -23,6 +27,8 @@ const App = () => {
   const restrictedRoutes = restrictRoutes(routes);
   const routing = useRoutes(restrictedRoutes);
   const { user } = useUser();
+
+  const [errors, setErrors] = useState(null);
 
   // https://www.apollographql.com/docs/react/networking/authentication/#header
   const httpLink = createHttpLink({ uri: HASURA_ENDPOINT });
@@ -42,9 +48,36 @@ const App = () => {
     };
   });
 
+  const loggerLink = new ApolloLink((operation, forward) => {
+    console.debug(`GraphQL Request: ${operation.operationName}`);
+    operation.setContext({ start: new Date() });
+    return forward(operation).map(response => {
+      const responseTime = new Date() - operation.getContext().start;
+      console.debug(`GraphQL Response took: ${responseTime}`);
+      return response;
+    });
+  });
+
+  const errorLink = onError(
+    ({ graphQLErrors, networkError, operation, forward }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, extensions }) => {
+          setErrors({ code: extensions.code, message: message });
+          return null;
+        });
+
+      if (networkError) {
+        setErrors({ code: "network-error", message: networkError });
+      }
+    }
+  );
+
   const client = new ApolloClient({
     // Join authLink and httpLink to handle auth in each request
-    link: authLink.concat(httpLink),
+    link: ApolloLink.from([
+      loggerLink,
+      errorLink.concat(authLink.concat(httpLink)),
+    ]),
     cache: new InMemoryCache(),
   });
 
@@ -52,7 +85,12 @@ const App = () => {
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
-        {routing}
+        <ApolloLinkErrorHandler
+          errors={errors}
+          clearErrors={() => setErrors(null)}
+        >
+          {routing}
+        </ApolloLinkErrorHandler>
       </ThemeProvider>
     </ApolloProvider>
   );
