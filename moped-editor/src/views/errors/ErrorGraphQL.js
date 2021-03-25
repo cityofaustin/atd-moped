99787b517bec7ce1fd860068dd4dcc98f7f23ec2
Sergio Garcia <sergio.garcia@dataconfluent.com>
2021-03-24 23:43:52 -0500

    Already had redirect implemented

--- moped-editor/src/views/errors/ErrorGraphQL.js
+++ moped-editor/src/views/errors/ErrorGraphQL.js
@@ -6,20 +6,9 @@ import {
   DialogTitle,
   DialogContent,
   DialogActions,
-  Backdrop,
-  CircularProgress,
 } from "@material-ui/core";
-import makeStyles from "@material-ui/core/styles/makeStyles";
-
-const useStyles = makeStyles(theme => ({
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-    color: "#fff",
-  },
-}));
 
 const ErrorGraphQL = props => {
-  const classes = useStyles();
   const { errors, clearErrors, children } = props;
 
   const [open] = useState(!!errors);
@@ -32,17 +21,6 @@ const ErrorGraphQL = props => {
     handleClose();
     window.history.back();
   };
-
-  if (open) {
-    if (errors.code === "invalid-jwt") {
-      window.location = "/moped";
-      return (
-        <Backdrop className={classes.backdrop} open={true} onClick={null}>
-          <CircularProgress color="inherit" />
-        </Backdrop>
-      );
-    }
-  }
 
   return (
     <>
