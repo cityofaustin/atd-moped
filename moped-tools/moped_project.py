11db6c96a185b36f3376d77df112a92042aca921
Sergio Garcia <sergio.garcia@dataconfluent.com>
2021-03-31 09:32:27 -0500

    Moped Project gets its own file

--- moped-tools/moped_project.py
+++ moped-tools/moped_project.py
@@ -9,3 +9,54 @@ def moped_project_cleanup(record):
 
     # Return record
     return record
+
+
+#
+# Main Configuration
+#
+moped_project_process = {
+    # Lave it here for now...
+    "table": "projects",
+
+    # SQL Query (the order of the columns affects the lambda function below)
+    "sql": "SELECT ProjectID,ProjectName,Description,PhaseSimple,ProjectInitiationDate,ECapris_ProjectID FROM PROJECTS",
+
+    # Basically, this lambda function will rename the keys
+    # so that it's compatible with Hasura by creating/replacing
+    # the current object with a new one.
+    "transform": lambda row: {
+        "project_id": row[0],
+        "project_name": row[1],
+        "project_description": row[2],
+        "current_status": row[3],
+        "start_date": row[4],
+        "eCapris_id": row[5],
+        # We need it to be false if ecapris is empty
+        "capitally_funded": False if row[5] == "" else True
+    },
+
+    # Special rules that cannot be put here
+    "cleanup": moped_project_cleanup,
+
+    # Mutation Template
+    "graphql": """
+        mutation MigrateMopedProjects($object: moped_project_insert_input!) {
+            insert_moped_project(
+                objects: [$object],
+                on_conflict: {
+                    constraint: moped_project_pkey,
+                    update_columns: [
+                        project_name,
+                        project_description,
+                        current_status,
+                        start_date,
+                        eCapris_id,
+                        capitally_funded,
+                    ]
+                }
+            ) {
+                affected_rows
+            }
+        }
+    """
+}
