176050f4d0ee163479c028683beeb6e6e547b084
tillyw <tilly.whitson@austintexas.gov>
2021-04-01 14:33:55 -0500

    updates stringEdit id

--- moped-editor/src/components/DataTable/DataTable.js
+++ moped-editor/src/components/DataTable/DataTable.js
@@ -321,7 +321,7 @@ const DataTable = ({ fieldConfiguration, data, loading, error, refetch }) => {
     return (
       <TextField
         fullWidth
-        id="date"
+        id="text"
         label={fieldConfig.label}
         type="text"
         defaultValue={editValue ?? initialValue}
