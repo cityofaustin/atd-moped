61c4cf101ad6ab5f77728cff8f5889a395cece42
Mike <mike.dilley@gmail.com>
2021-04-08 17:58:03 -0500

    Add layer order to config

--- moped-editor/src/utils/mapHelpers.js
+++ moped-editor/src/utils/mapHelpers.js
@@ -72,6 +72,7 @@ export const mapConfig = {
       layerIdName: "ctn-lines",
       layerIdField: "PROJECT_EXTENT_ID",
       layerIdGetPath: "properties.PROJECT_EXTENT_ID",
+      layerOrder: 1,
       layerColor: theme.palette.primary.main,
       layerUrl:
         "https://tiles.arcgis.com/tiles/0L95CJ0VTaxqcmED/arcgis/rest/services/CTN_Project_Extent_Vector_Tiles_with_Street_Name/VectorTileServer/tile/{z}/{y}/{x}.pbf",
@@ -112,6 +113,7 @@ export const mapConfig = {
       layerIdName: "project-component-points",
       layerIdField: "PROJECT_EXTENT_ID",
       layerIdGetPath: "properties.PROJECT_EXTENT_ID",
+      layerOrder: 2,
       layerColor: theme.palette.secondary.main,
       layerUrl:
         "https://tiles.arcgis.com/tiles/0L95CJ0VTaxqcmED/arcgis/rest/services/MOPED_intersection_points/VectorTileServer/tile/{z}/{y}/{x}.pbf",
