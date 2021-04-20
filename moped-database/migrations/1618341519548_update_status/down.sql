620fb4a07d8748f95aeaba97e60f7b5fae8dff7c
Sergio Garcia <sergiogcx@gmail.com>
2021-04-20 10:39:45 -0500

    Revert "Data Model | Update values in `moped_phases` #5381"

--- moped-database/migrations/1618341519548_update_status/down.sql
+++
@@ -1,3 +0,0 @@
--- Revert the changes made to the database
-UPDATE moped_status SET status_name = 'hold' WHERE status_id = 5;
-DELETE FROM moped_status WHERE status_id = 6;
--- moped-database/migrations/1618344795697_create_table_public_moped_subphases/down.sql
+++
@@ -1,5 +0,0 @@
--- Roll back changes
-ALTER TABLE moped_project DROP COLUMN subphase_id;
-DROP INDEX moped_project_subphase_id_index;
-DROP TABLE moped_subphases;
-DROP TABLE moped_proj_subphases;
