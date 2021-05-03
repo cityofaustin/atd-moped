7c37f2c29da4a603b6f42df82264e4b9dcaed903
Sergio Garcia <sergiogcx@gmail.com>
2021-05-03 15:58:35 -0500

    Updates deployment script

--- .github/workflows/aws-heroku-helper.sh
+++ .github/workflows/aws-heroku-helper.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-set -o errexit;
 
 #
 # Heroku Deployment Helper Script
@@ -41,13 +40,21 @@ function heroku_commit_and_push() {
 }
 
 function build_database() {
-  APPLICATION_NAME="atd-moped-editor-development"
+  APPLICATION_NAME="atd-moped-editor-development";
+  HASURA_REPO_NAME="graphql-engine-heroku";
+
+  print_header "Cleaning up"
+
+  rm -rf "${HASURA_REPO_NAME}" || echo "Nothing to clean";
+
   print_header "Cloning Hasura Engine";
   clone_hasura_repo;
 
+  cd "${HASURA_REPO_NAME}" || exit 1;
+
   print_header "Destroying Current Application";
   {
-    heroku apps:destroy --app "${APPLICATION_NAME}" --confirm "${APPLICATION_NAME}"
+    heroku apps:destroy --app "${APPLICATION_NAME}" --confirm "${APPLICATION_NAME}";
   } || {
     echo "Nothing to destroy."
   }
@@ -56,7 +63,15 @@ function build_database() {
   heroku apps:create "${APPLICATION_NAME}" --team=austin-dts --stack=container;
 
   print_header "Change application configuration settings";
-  heroku config:set --app="${APPLICATION_NAME}" ADMIN_SECRET=8N029N81 DISABLE_SUCH_AND_SUCH=9s83109d3+583493190
+  heroku config:set --app="${APPLICATION_NAME}"  \
+    MOPED_API_ACTIONS_URL="http://localhost/actions" \
+    MOPED_API_EVENTS_URL="http://localhost/events" \
+    MOPED_API_APIKEY="" \
+    HASURA_GRAPHQL_DEV_MODE=false \
+    HASURA_GRAPHQL_ENABLE_CONSOLE=true \
+    HASURA_GRAPHQL_ADMIN_SECRET="${ATD_MOPED_DEVSTAGE_HASURA_GRAPHQL_ADMIN_SECRET}" \
+    HASURA_GRAPHQL_JWT_SECRET="${ATD_MOPED_DEVSTAGE_HASURA_GRAPHQL_JWT_SECRET}" &> /dev/null;
+  echo "Done (muted result)";
 
   print_header "Create add-ons";
   heroku addons:create heroku-postgresql:hobby-dev -a "${APPLICATION_NAME}";
@@ -68,8 +83,25 @@ function build_database() {
 }
 
 function run_database_migration() {
+  cd "moped-database";
+
   print_header "Applying All Migrations";
+  echo "From directory: $(pwd)";
+
   print_header "Apply Migrations";
+  hasura migrate apply \
+    --admin-secret "${ATD_MOPED_DEVSTAGE_HASURA_GRAPHQL_ADMIN_SECRET}" \
+    --endpoint "https://atd-moped-editor-development.herokuapp.com";
+
   print_header "Apply Metadata";
+  hasura metadata apply \
+    --admin-secret "${ATD_MOPED_DEVSTAGE_HASURA_GRAPHQL_ADMIN_SECRET}" \
+    --endpoint "https://atd-moped-editor-development.herokuapp.com";
+
   print_header "Apply Seed data";
+  hasura seeds apply \
+    --admin-secret "${ATD_MOPED_DEVSTAGE_HASURA_GRAPHQL_ADMIN_SECRET}" \
+    --endpoint "https://atd-moped-editor-development.herokuapp.com";
+
+  cd ..;
 }
