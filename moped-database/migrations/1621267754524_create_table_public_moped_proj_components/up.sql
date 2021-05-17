cc3cb2b24a61986fc6fc2fd60b7c0f20b223d140
Sergio Garcia <sergiogcx@gmail.com>
2021-05-17 14:24:11 -0500

    Add two columns to moped_proj_components

--- moped-database/migrations/1621267754524_create_table_public_moped_proj_components/up.sql
+++ moped-database/migrations/1621267754524_create_table_public_moped_proj_components/up.sql
@@ -2,6 +2,8 @@ CREATE TABLE "public"."moped_proj_components"(
     "project_component_id" serial  NOT NULL,
     "project_id"           integer NOT NULL,
     "component_id"         integer NOT NULL,
+    "name"                 text NOT NULL,
+    "description"          text NULL,
     PRIMARY KEY ("project_component_id"),
     FOREIGN KEY ("project_id")
         REFERENCES "public"."moped_project" ("project_id")
