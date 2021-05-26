921b53a99a89eb54e927d86bf47e79051c684c1d
John Clary <john.clary@austintexas.gov>
2021-05-26 17:23:44 -0500

    remove desktopDrawer styles

--- moped-editor/src/layouts/DashboardLayout/NavBar/NavBar.js
+++ moped-editor/src/layouts/DashboardLayout/NavBar/NavBar.js
@@ -43,11 +43,6 @@ const useStyles = makeStyles(() => ({
   mobileDrawer: {
     width: 256,
   },
-  desktopDrawer: {
-    width: 256,
-    top: 64,
-    height: "calc(100% - 64px)",
-  },
   avatar: {
     cursor: "pointer",
     width: 64,
