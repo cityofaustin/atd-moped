0cf804caac30065b5994627f4d3e5b1378c9a115
Sergio Garcia <sergiogcx@gmail.com>
2021-06-03 11:15:35 -0500

    Creates new react component ProjectComponents

---
+++ moped-editor/src/views/projects/projectView/ProjectComponents.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+
+const ProjectComponents = () => {
+
+    const [componentsList, setComponentsList] = useState({});
+
+    return <div>
+        Project Components
+    </div>;
+};
+
+export default ProjectComponents;
