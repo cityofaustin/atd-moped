302c1bffd23af400dda6253f6dc9adfe2a7e2498
tillyw <tilly.whitson@austintexas.gov>
2021-06-03 17:06:56 -0500

    formatted code

--- moped-editor/src/views/projects/projectView/ProjectView.js
+++ moped-editor/src/views/projects/projectView/ProjectView.js
@@ -272,7 +272,11 @@ const ProjectView = () => {
 
   return (
     <ApolloErrorHandler error={error}>
-      <Page title={loading ? "Project Summary Page" : data.moped_project[0].project_name}>
+      <Page
+        title={
+          loading ? "Project Summary Page" : data.moped_project[0].project_name
+        }
+      >
         <Container maxWidth="xl">
           <Card className={classes.cardWrapper}>
             {loading ? (
