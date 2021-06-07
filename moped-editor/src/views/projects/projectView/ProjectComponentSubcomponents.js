9d3ce14e75d60bac94ae0c48c85ae4e9389c679a
Sergio Garcia <sergiogcx@gmail.com>
2021-06-06 22:28:59 -0500

    Create ProjectComponentSubcomponents component

---
+++ moped-editor/src/views/projects/projectView/ProjectComponentSubcomponents.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { Chip, FormControl, Grid, TextField } from "@material-ui/core";
+import { Alert, Autocomplete } from "@material-ui/lab";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles(theme => ({
+  alert: {
+    margin: theme.spacing(2),
+  },
+}));
+
+/**
+ * Subcomponent List Tag editor
+ * @param {Number} componentId - An integer that contains the current compoent id selected
+ * @param {Object[]} subcomponentList - A list of objects containing all the available subcomponents
+ * @param {Number[]} selectedSubcomponents - The current state containing a list of selected components
+ * @param {function} setSelectedSubcomponents - The state setter for the current list state
+ * @return {JSX.Element}
+ * @constructor
+ */
+const ProjectComponentSubcomponents = ({
+  componentId,
+  subcomponentList,
+  selectedSubcomponents,
+  setSelectedSubcomponents,
+}) => {
+  const classes = useStyles();
+
+  // There is nothing to render if the component id is not there
+  if (componentId === null)
+    return (
+      <Alert className={classes.alert} severity="warning">
+        You must select a subtype.
+      </Alert>
+    );
+
+  return (
+    <Grid item xs={12}>
+      <FormControl variant="filled" fullWidth>
+        <Autocomplete
+          multiple
+          fullWidth
+          id="moped-subcomponents"
+          value={selectedSubcomponents}
+          onChange={(event, newValue) => {
+            setSelectedSubcomponents([...newValue]);
+          }}
+          options={subcomponentList.filter(
+            subComponent => subComponent.component_id === componentId
+          )}
+          // The list changes depending on available subcomponents for that specific type
+          getOptionLabel={option => option.subcomponent_name}
+          renderTags={(tagValue, getTagProps) =>
+            tagValue.map((option, index) => (
+              <Chip
+                label={option.subcomponent_name}
+                {...getTagProps({ index })}
+              />
+            ))
+          }
+          renderInput={params => (
+            <TextField
+              {...params}
+              label="Subcomponents"
+              variant="outlined"
+              placeholder={null}
+            />
+          )}
+        />
+      </FormControl>
+    </Grid>
+  );
+};
+
+export default ProjectComponentSubcomponents;
