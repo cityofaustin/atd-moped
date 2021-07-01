09cdc0fac0a0974039c06eee4e01b653d43123b9
Chia Berry <chia.berry@austintexas.gov>
2021-07-01 11:51:53 -0500

    add a footer and color external link

--- moped-editor/src/layouts/DashboardLayout/DashboardLayout.js
+++ moped-editor/src/layouts/DashboardLayout/DashboardLayout.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { Outlet, Navigate } from "react-router-dom";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Typography } from "@material-ui/core";
 import NavBar from "./NavBar/NavBar";
 import TopBar from "./TopBar";
 import { useUser } from "../../auth/user";
+import ExternalLink from "../../components/ExternalLink";
+var pckg = require("../../../package.json");
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -29,7 +31,29 @@ const useStyles = makeStyles(theme => ({
     height: "100%",
     overflow: "auto",
   },
+  footer: {
+    paddingLeft: theme.spacing(4),
+    paddingBottom: theme.spacing(2),
+    color: "#1492ff",
+  },
 }));
+
+const Footer = () => {
+  const classes = useStyles();
+  console.log(pckg);
+  return (
+    <div className={classes.footer}>
+      <Typography variant="caption" color="textSecondary">
+        Moped{" "}
+        <ExternalLink
+          text={`v${pckg.version}`}
+          url={`https://github.com/cityofaustin/atd-moped/releases/tag/v${pckg.version}`}
+          linkColor="inherit"
+        />
+      </Typography>
+    </div>
+  );
+};
 
 const DashboardLayout = () => {
   const classes = useStyles();
@@ -41,14 +65,12 @@ const DashboardLayout = () => {
   return user ? (
     <div className={classes.root}>
       <TopBar onOpen={() => setOpen(true)} />
-      <NavBar
-        onClose={() => setOpen(false)}
-        isOpen={isOpen}
-      />
+      <NavBar onClose={() => setOpen(false)} isOpen={isOpen} />
       <div className={classes.wrapper}>
         <div className={classes.contentContainer}>
           <div className={classes.content}>
             <Outlet />
+            <Footer />
           </div>
         </div>
       </div>
