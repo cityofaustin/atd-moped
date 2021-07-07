e2771b6e9d6c161040d9beceb70a21c40f3f4e8e
Sergio Garcia <sergiogcx@gmail.com>
2021-07-07 11:08:51 -0400

    Fix WSOD bug

--- moped-editor/src/utils/mapHelpers.js
+++ moped-editor/src/utils/mapHelpers.js
@@ -686,7 +686,8 @@ export function useFeatureCollectionToFitBounds(
      * @return {Object} Viewport object with updated attributes based on project's features
      */
     const fitViewportToBounds = viewport => {
-      if (featureCollection.features.length === 0) return viewport;
+      // Let's check if we have any features are all, otherwise return the state of viewport...
+      if ((featureCollection?.features?.length ?? 0) === 0) return viewport;
       const featureViewport = new WebMercatorViewport({
         viewport,
         width: currentMap?._width ?? window.innerWidth * 0.45,
