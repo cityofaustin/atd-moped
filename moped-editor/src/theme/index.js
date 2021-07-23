804727804bd91df7b2cc0c222b7c0e8a8762e2c0
jorge <jgabitto1792@gmail.com>
2021-07-23 14:16:27 -0500

    Revert "Update them"
    This reverts commit e783f51155117b10d024bf444232d58cc6f06e6f.

--- moped-editor/src/theme/index.js
+++ moped-editor/src/theme/index.js
@@ -1,27 +1,25 @@
 import { createMuiTheme, colors } from "@material-ui/core";
 import shadows from "./shadows";
-// import typography from "./typography";
+import typography from "./typography";
 
 const theme = createMuiTheme({
   palette: {
     background: {
       dark: "#E1E2E1",
-      default: "#edeeed",
-      paper: "#ffffff",
+      default: colors.common.white,
+      paper: colors.common.white,
       mapControls: "#FFFFFF",
       mapControlsHover: "#e6e6e6",
     },
     primary: {
       main: "#1e88e5",
-      dark: "#005cb2",
-      light: "#61b5ff",
     },
     secondary: {
-      main: "#ff6549",
+      main: "#ff3d00",
     },
     text: {
-      primary: "#212121",
-      secondary: "#848484",
+      primary: "#000000",
+      secondary: "#000000",
     },
     map: {
       transparent: "rgba(0,0,0,0)",
@@ -29,7 +27,7 @@ const theme = createMuiTheme({
     },
   },
   shadows,
-  // typography,
+  typography,
 });
 
 export default theme;
