f84a8e75c0298bad2a3b85aee6b69d0260165a92
jorge <jgabitto1792@gmail.com>
2021-07-26 09:12:40 -0500

    Remove colors import

--- moped-editor/src/theme/index.js
+++ moped-editor/src/theme/index.js
@@ -1,4 +1,4 @@
-import { createMuiTheme, colors } from "@material-ui/core";
+import { createMuiTheme } from "@material-ui/core";
 import shadows from "./shadows";
 import typography from "./typography";
 
