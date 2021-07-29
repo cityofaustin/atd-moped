b2577fa85fffa521d72764b4d9f25a397995fbb9
Sergio Garcia <sergiogcx@gmail.com>
2021-07-29 14:30:34 -0400

    Restore onDelete

--- moped-editor/src/views/projects/newProjectView/MapDrawToolbar.js
+++ moped-editor/src/views/projects/newProjectView/MapDrawToolbar.js
@@ -9,23 +9,32 @@ import { ToggleButtonGroup } from "@material-ui/lab";
  * Generates a draw toolbar for the map
  * @param {Object} selectedModeId - The current object representation of the selected tool
  * @param {function} onSwitchMode - Handler that is run whenever we switch tools
+ * @param {function} onDelete - Handler that is run whenever we delete an item
  * @return {JSX.Element}
  * @constructor
  */
-const DrawToolbar = ({ selectedModeId, onSwitchMode, containerRef = null }) => {
+const DrawToolbar = ({
+  selectedModeId,
+  onSwitchMode,
+  onDelete,
+  containerRef = null,
+}) => {
+  /**
+   * Calls onDelete function to delete a point feature from the draw UI
+   * @param {Object} e - Event object for click
+   */
+  const onDeleteClick = e => {
+    onDelete(e);
+  };
+
   return containerRef && containerRef.current
     ? ReactDOM.createPortal(
-        <ToggleButtonGroup
-          value={selectedModeId}
-          exclusive
-          onChange={null}
-          aria-label=""
-        >
+        <ToggleButtonGroup value={selectedModeId} exclusive>
           {MODES.map(mode => {
             return (
               <MapDrawToolbarButton
                 selectedModeId={selectedModeId}
-                onClick={onSwitchMode}
+                onClick={mode.id === "delete" ? onDeleteClick : onSwitchMode}
                 key={mode.id}
                 mode={mode}
               />
