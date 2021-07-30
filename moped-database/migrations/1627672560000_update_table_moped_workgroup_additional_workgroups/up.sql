5d364c96f9c5803ddc62441d717278e3ef168ee2
Sergio Garcia <sergiogcx@gmail.com>
2021-07-30 16:04:52 -0400

    Add new workgroups migration

---
+++ moped-database/migrations/1627672560000_update_table_moped_workgroup_additional_workgroups/up.sql
@@ -0,0 +1,8 @@
+/*
+ INSERTS NEW VALUES FOR THE WORKGROUP TABLE
+*/
+INSERT INTO public.moped_workgroup (workgroup_name, workgroup_id, department_id) VALUES ('Urban Trails', 19, 3);
+INSERT INTO public.moped_workgroup (workgroup_name, workgroup_id, department_id) VALUES ('Safe Routes to School', 20, 3);
+INSERT INTO public.moped_workgroup (workgroup_name, workgroup_id, department_id) VALUES ('Corridor Program Office', 21, 3);
+INSERT INTO public.moped_workgroup (workgroup_name, workgroup_id, department_id) VALUES ('Sidewalks', 22, 3);
+INSERT INTO public.moped_workgroup (workgroup_name, workgroup_id, department_id) VALUES ('Engineering Services Division', 23, 3);
