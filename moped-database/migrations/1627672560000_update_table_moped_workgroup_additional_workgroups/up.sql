7bfd721a6e3e5131ea5f763186b187e5b2921efe
Sergio Garcia <sergiogcx@gmail.com>
2021-08-03 10:56:42 -0400

    Update other workgroup

--- moped-database/migrations/1627672560000_update_table_moped_workgroup_additional_workgroups/up.sql
+++ moped-database/migrations/1627672560000_update_table_moped_workgroup_additional_workgroups/up.sql
@@ -6,4 +6,9 @@ INSERT INTO public.moped_workgroup (workgroup_name, workgroup_id, department_id)
 INSERT INTO public.moped_workgroup (workgroup_name, workgroup_id, department_id) VALUES ('Corridor Program Office', 21, 2);
 INSERT INTO public.moped_workgroup (workgroup_name, workgroup_id, department_id) VALUES ('Sidewalks', 22, 3);
 INSERT INTO public.moped_workgroup (workgroup_name, workgroup_id, department_id) VALUES ('Engineering Services Division', 23, 3);
-INSERT INTO public.moped_workgroup (workgroup_name, workgroup_id, department_id) VALUES ('Other', 24, 4);
+
+-- Other already exists, we are going to update its department
+UPDATE public.moped_workgroup
+    SET department_id = 4
+        WHERE workgroup_id = 18   -- Other
+;
