eaf63f6aa1571d6b1841fc66115a05fc45068a01
Sergio Garcia <sergiogcx@gmail.com>
2021-08-03 10:47:05 -0400

    Add other and update corridor program office

--- moped-database/migrations/1627672560000_update_table_moped_workgroup_additional_workgroups/up.sql
+++ moped-database/migrations/1627672560000_update_table_moped_workgroup_additional_workgroups/up.sql
@@ -3,6 +3,7 @@
 */
 INSERT INTO public.moped_workgroup (workgroup_name, workgroup_id, department_id) VALUES ('Urban Trails', 19, 3);
 INSERT INTO public.moped_workgroup (workgroup_name, workgroup_id, department_id) VALUES ('Safe Routes to School', 20, 3);
-INSERT INTO public.moped_workgroup (workgroup_name, workgroup_id, department_id) VALUES ('Corridor Program Office', 21, 3);
+INSERT INTO public.moped_workgroup (workgroup_name, workgroup_id, department_id) VALUES ('Corridor Program Office', 21, 2);
 INSERT INTO public.moped_workgroup (workgroup_name, workgroup_id, department_id) VALUES ('Sidewalks', 22, 3);
 INSERT INTO public.moped_workgroup (workgroup_name, workgroup_id, department_id) VALUES ('Engineering Services Division', 23, 3);
+INSERT INTO public.moped_workgroup (workgroup_name, workgroup_id, department_id) VALUES ('Other', 24, 4);
