db47158ae38a5b74668aad03a5984582b337bff1
Sergio Garcia <sergiogcx@gmail.com>
2021-08-04 11:36:11 -0400

    Initial CDNAvatar refactor

--- moped-editor/src/components/CDN/Avatar.js
+++ moped-editor/src/components/CDN/Avatar.js
@@ -1,35 +1,63 @@
 import React from "react";
 import { Avatar, Icon, makeStyles, Typography } from "@material-ui/core";
-import { useUser } from "../../auth/user";
 
 import config from "../../config";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(theme => ({
   root: {},
   avatar: {
     height: 100,
     width: 100,
     marginBottom: 8,
+    backgroundColor: theme.palette.grey["300"],
   },
   userInitials: {
     fontSize: "2rem",
   },
 }));
 
-const CDNAvatar = props => {
-  const classes = useStyles();
-  const { user } = useUser();
 
-  const imageSrc = props?.src ? `${config.env.APP_CLOUDFRONT}/${props.src}` : null
+/**
+ * A wrapper for Material's Avatar component.
+ * @param {Object} className - The class name override
+ * @param {String} src - The image in the CDN
+ * @param {String} initials - The initials to render within the avatar (when no image is present).
+ * @param {boolean} largeInitials - If true, it makes the initials a little larger.
+ * @param {String} userColor - The background color to render under the initials.
+ * @return {JSX.Element}
+ * @constructor
+ */
+const CDNAvatar = ({className, src, initials, largeInitials, userColor}) => {
+  const classes = useStyles();
+
+  /**
+   * The image source from CloudFront CDN
+   * @type {string|null}
+   */
+  const imageSource = src
+    ? `${config.env.APP_CLOUDFRONT}/${src}`
+    : null; // If src is null, the avatar will default to initials
+
+  /**
+   * Overrides the default background color if provided.
+   * @type {{backgroundColor: (string)}|null}
+   */
+  const imageStyleOverride = userColor
+    ? {
+        backgroundColor: userColor,
+      }
+    : null;
 
   return (
     <Avatar
-      className={props?.className ? props.className : classes.avatar}
-      src={imageSrc}
-      style={{ backgroundColor: imageSrc ? "white" : user?.userColor }}
+      className={className ? className : classes.avatar}
+      src={imageSource}
+      style={imageStyleOverride}
     >
-      <Typography className={props?.largeInitials ? classes.userInitials : null}>
-        {props?.initials ? props.initials : <Icon>user</Icon>}
+      <Typography
+        className={largeInitials ? classes.userInitials : null}
+      >
+        {initials ? initials : <Icon>user</Icon>}
       </Typography>
     </Avatar>
   );
