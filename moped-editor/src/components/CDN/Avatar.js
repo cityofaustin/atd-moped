7448a0484cf003582434ca0c3cd6fb9b5ae84d8f
Sergio Garcia <sergiogcx@gmail.com>
2021-08-04 15:25:31 -0400

    Refactor CDN Avatar

--- moped-editor/src/components/CDN/Avatar.js
+++ moped-editor/src/components/CDN/Avatar.js
@@ -16,7 +16,6 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-
 /**
  * A wrapper for Material's Avatar component.
  * @param {Object} className - The class name override
@@ -24,25 +23,31 @@ const useStyles = makeStyles(theme => ({
  * @param {String} initials - The initials to render within the avatar (when no image is present).
  * @param {boolean} largeInitials - If true, it makes the initials a little larger.
  * @param {String} userColor - The background color to render under the initials.
+ * @param {boolean} useGenericAvatar - If true, whenever src is not provided it uses a generic avatar as opposed to initials.
  * @return {JSX.Element}
  * @constructor
  */
-const CDNAvatar = ({className, src, initials, largeInitials, userColor}) => {
+const CDNAvatar = ({
+  className,
+  src,
+  initials,
+  largeInitials,
+  userColor,
+  useGenericAvatar,
+}) => {
   const classes = useStyles();
 
   /**
    * The image source from CloudFront CDN
    * @type {string|null}
    */
-  const imageSource = src
-    ? `${config.env.APP_CLOUDFRONT}/${src}`
-    : null; // If src is null, the avatar will default to initials
+  const imageSource = !!src ? `${config.env.APP_CLOUDFRONT}/${src}` : null;
 
   /**
    * Overrides the default background color if provided.
    * @type {{backgroundColor: (string)}|null}
    */
-  const imageStyleOverride = userColor
+  const imageStyleOverride = !!userColor
     ? {
         backgroundColor: userColor,
       }
@@ -50,15 +55,18 @@ const CDNAvatar = ({className, src, initials, largeInitials, userColor}) => {
 
   return (
     <Avatar
+      alt={initials}
       className={className ? className : classes.avatar}
       src={imageSource}
       style={imageStyleOverride}
     >
-      <Typography
-        className={largeInitials ? classes.userInitials : null}
-      >
-        {initials ? initials : <Icon>user</Icon>}
-      </Typography>
+      {!!!useGenericAvatar && initials && initials.length > 0 ? (
+        <Typography className={largeInitials ? classes.userInitials : null}>
+          {initials}
+        </Typography>
+      ) : (
+        <Icon>person</Icon>
+      )}
     </Avatar>
   );
 };
