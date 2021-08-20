a47d3fc0abe6c521a8ecd2c9f668111b560ff1ab
John Clary <john.clary@austintexas.gov>
2021-08-19 20:08:43 -0500

    remove unused VisuallyHidden style

--- moped-editor/src/components/GridTable/GridTable.js
+++ moped-editor/src/components/GridTable/GridTable.js
@@ -51,17 +51,6 @@ const useStyles = makeStyles(theme => ({
   table: {
     minWidth: 750,
   },
-  visuallyHidden: {
-    border: 0,
-    clip: "rect(0 0 0 0)",
-    height: 1,
-    margin: -1,
-    overflow: "hidden",
-    padding: 0,
-    position: "absolute",
-    top: 20,
-    width: 1,
-  },
   tableCell: {
     "text-transform": "capitalize",
     "white-space": "pre-wrap",
