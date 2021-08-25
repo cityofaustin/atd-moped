c50474994ec1c837e8a1a7c0d36d8063dc87eac4
Chia Berry <chia.berry@austintexas.gov>
2021-08-25 13:28:06 -0500

    change heading sizes in theme

--- moped-editor/src/theme/typography.js
+++ moped-editor/src/theme/typography.js
@@ -1,32 +1,33 @@
 export default {
   h1: {
     fontWeight: 500,
-    fontSize: 35,
+    fontSize: 29,
     letterSpacing: "-0.24px",
   },
   h2: {
     fontWeight: 500,
-    fontSize: 29,
+    fontSize: 25,
     letterSpacing: "-0.24px",
+    color: "#1276D1",
   },
   h3: {
-    fontWeight: 500,
-    fontSize: 24,
-    letterSpacing: "-0.06px",
-  },
-  h4: {
     fontWeight: 500,
     fontSize: 20,
     letterSpacing: "-0.06px",
   },
-  h5: {
+  h4: {
     fontWeight: 500,
     fontSize: 16,
+    letterSpacing: "-0.06px",
+  },
+  h5: {
+    fontWeight: 500,
+    fontSize: 14,
     letterSpacing: "-0.05px",
   },
   h6: {
     fontWeight: 500,
-    fontSize: 14,
+    fontSize: 12,
     letterSpacing: "-0.05px",
   },
   overline: {
