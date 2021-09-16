dfba1ede1ccf8f31bda7cfaffbefd651f6ca8ff3
Sergio Garcia <sergiogcx@gmail.com>
2021-09-16 15:43:21 -0400

    Update up stage

--- moped-database/migrations/1631716080737_update_phases_milestones/up.sql
+++ moped-database/migrations/1631716080737_update_phases_milestones/up.sql
@@ -1,18 +1,22 @@
 /**
   Phases
  */
+UPDATE public.moped_phases SET phase_name = 'Planned', phase_description = 'Project is planned but has yet to move into additional phases', phase_order = 2, phase_average_length = null, required_phase = null, subphases = '{4,9,15}' WHERE phase_id = 2;
+UPDATE public.moped_phases SET phase_name = 'Design', phase_description = 'Project is undergoing a traditional 30%, 60%, 90%, 100% design phase', phase_order = 6, phase_average_length = null, required_phase = null, subphases = '{16,17,18,19}' WHERE phase_id = 6;
 UPDATE public.moped_phases SET phase_name = 'Construction-ready', phase_description = 'Project is ready for construction pending schedules, work orders, etc', phase_order = 9, phase_average_length = null, required_phase = null, subphases = null WHERE phase_id = 8;
 UPDATE public.moped_phases SET phase_name = 'Construction', phase_description = 'Project is in construction', phase_order = 10, phase_average_length = null, required_phase = null, subphases = '{6,7}' WHERE phase_id = 9;
-UPDATE public.moped_phases SET phase_name = 'Post-construction', phase_description = 'Project is fully complete but pending actions or items to close it out', phase_order = 11, phase_average_length = null, required_phase = null, subphases = '{1}' WHERE phase_id = 10;
+UPDATE public.moped_phases SET phase_name = 'Post-construction', phase_description = 'Project is fully complete but pending actions or items to close it out', phase_order = 11, phase_average_length = null, required_phase = null, subphases = '{1,21}' WHERE phase_id = 10;
 UPDATE public.moped_phases SET phase_name = 'Complete', phase_description = 'Project is fully complete and fully closed out', phase_order = 12, phase_average_length = null, required_phase = null, subphases = '{10}' WHERE phase_id = 11;
 -- Now insert the new phase
 INSERT INTO public.moped_phases (phase_name, phase_description, phase_order, phase_average_length, required_phase, phase_id, subphases) VALUES ('Bid/Award/Execution', 'Project is under bidding, awarding, or execution state. ', 8, null, null, 12, '{20}');
+
 
 /**
   Subphases
  */
 -- We need to alter this as to allow subphases sharing the same name
-alter table moped_subphases drop constraint moped_subphases_subphase_name_key;
+alter table moped_subphases drop constraint if exists moped_subphases_subphase_name_key;
+
 -- Potential's Sub-phase Changes
 UPDATE public.moped_subphases SET subphase_name = 'Study recommended', subphase_description = null, subphase_order = 11, subphase_average_length = null, required_subphase = null, related_phase_id = 1 WHERE subphase_id = 11;
 INSERT INTO public.moped_subphases (subphase_id, subphase_name, subphase_description, subphase_order, subphase_average_length, required_subphase, related_phase_id) VALUES (12, 'Study in progress', null, 12, null, null, 1);
@@ -22,6 +26,10 @@ INSERT INTO public.moped_subphases (subphase_id, subphase_name, subphase_descrip
 UPDATE public.moped_subphases SET subphase_name = 'Environmental study', subphase_description = null, subphase_order = 9, subphase_average_length = null, required_subphase = null, related_phase_id = 2 WHERE subphase_id = 9;
 INSERT INTO public.moped_subphases (subphase_id, subphase_name, subphase_description, subphase_order, subphase_average_length, required_subphase, related_phase_id) VALUES (15, 'Ready for design', null, 15, null, null, 2);
 -- Design Sub-phase Changes
+DELETE FROM moped_subphases WHERE subphase_id = 8; -- Deletes design by others
+DELETE FROM moped_subphases WHERE subphase_id = 10; -- Deletes minor modification in progress
+
+--
 INSERT INTO public.moped_subphases (subphase_id, subphase_name, subphase_description, subphase_order, subphase_average_length, required_subphase, related_phase_id) VALUES (16, '30% Design', null, 16, null, null, 6);
 INSERT INTO public.moped_subphases (subphase_id, subphase_name, subphase_description, subphase_order, subphase_average_length, required_subphase, related_phase_id) VALUES (17, '60% Design', null, 17, null, null, 6);
 INSERT INTO public.moped_subphases (subphase_id, subphase_name, subphase_description, subphase_order, subphase_average_length, required_subphase, related_phase_id) VALUES (18, '90% Design', null, 18, null, null, 6);
@@ -50,10 +58,11 @@ UPDATE public.moped_milestones SET milestone_name = 'Construction contract execu
 UPDATE public.moped_milestones SET milestone_name = 'Design Notice-to-Proceed', milestone_description = null, milestone_order = 9, milestone_average_length = null, required_milestone = null, related_phase_id = 2 WHERE milestone_id = 9;
 UPDATE public.moped_milestones SET milestone_name = 'Environmental field work complete', milestone_description = null, milestone_order = 10, milestone_average_length = null, required_milestone = null, related_phase_id = 11 WHERE milestone_id = 10;
 UPDATE public.moped_milestones SET milestone_name = 'Environmentally cleared', milestone_description = null, milestone_order = 11, milestone_average_length = null, required_milestone = null, related_phase_id = 1 WHERE milestone_id = 11;
-UPDATE public.moped_milestones SET milestone_name = 'Feasibilty study', milestone_description = null, milestone_order = 12, milestone_average_length = null, required_milestone = null, related_phase_id = 1 WHERE milestone_id = 12;
+UPDATE public.moped_milestones SET milestone_name = 'Feasibility study', milestone_description = null, milestone_order = 12, milestone_average_length = null, required_milestone = null, related_phase_id = 1 WHERE milestone_id = 12;
 UPDATE public.moped_milestones SET milestone_name = 'First advertisement', milestone_description = null, milestone_order = 13, milestone_average_length = null, required_milestone = null, related_phase_id = 1 WHERE milestone_id = 13;
 UPDATE public.moped_milestones SET milestone_name = 'Initial field visit complete', milestone_description = null, milestone_order = 14, milestone_average_length = null, required_milestone = null, related_phase_id = 1 WHERE milestone_id = 14;
 UPDATE public.moped_milestones SET milestone_name = 'Issue COA striping work order for striping', milestone_description = null, milestone_order = 15, milestone_average_length = null, required_milestone = null, related_phase_id = 1 WHERE milestone_id = 15;
+
 -- Insertions
 INSERT INTO public.moped_milestones (milestone_id, milestone_name, milestone_description, milestone_order, milestone_average_length, required_milestone, related_phase_id) VALUES (16, 'Issue DO for construction', null, 16, null, null, null);
 INSERT INTO public.moped_milestones (milestone_id, milestone_name, milestone_description, milestone_order, milestone_average_length, required_milestone, related_phase_id) VALUES (17, 'Markings construction end', null, 17, null, null, null);
