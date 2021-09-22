0f9c6e5a67092c531594076333561ae37e079950
Sergio Garcia <sergiogcx@gmail.com>
2021-09-22 15:55:43 -0500

    Update migration defaults

--- moped-database/migrations/1632338636658_alter_table_public_moped_proj_notes_add_column_project_note_type/up.sql
+++ moped-database/migrations/1632338636658_alter_table_public_moped_proj_notes_add_column_project_note_type/up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "public"."moped_proj_notes" ADD COLUMN "project_note_type" integer NULL DEFAULT 0;
-UPDATE "public"."moped_proj_notes" SET project_note_type = 0 WHERE project_note_id > 0;
+ALTER TABLE "public"."moped_proj_notes" ADD COLUMN "project_note_type" integer NULL DEFAULT 1;
+UPDATE "public"."moped_proj_notes" SET project_note_type = 1 WHERE project_note_id > 0;
