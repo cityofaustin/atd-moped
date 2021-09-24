9dc5cd4546c2dce75a4851a43ce22ec69cdb30cf
Sergio Garcia <sergiogcx@gmail.com>
2021-09-24 09:13:58 -0500

    Updates project summary styles

--- moped-editor/src/views/projects/projectView/ProjectSummary.js
+++ moped-editor/src/views/projects/projectView/ProjectSummary.js
@@ -5,7 +5,12 @@ import ProjectSummaryTable from "./ProjectSummaryTable";
 import ProjectSummaryMap from "./ProjectSummaryMap";
 import { createFeatureCollectionFromProjectFeatures } from "../../../utils/mapHelpers";
 
-import { Grid, CardContent, CircularProgress } from "@material-ui/core";
+import {
+  Grid,
+  CardContent,
+  CircularProgress,
+  TextField,
+} from "@material-ui/core";
 import ApolloErrorHandler from "../../../components/ApolloErrorHandler";
 
 /*
@@ -13,12 +18,23 @@ import ApolloErrorHandler from "../../../components/ApolloErrorHandler";
 */
 import ProjectSummaryMapFallback from "./ProjectSummaryMapFallback";
 import { ErrorBoundary } from "react-error-boundary";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles(theme => ({
+  fieldGridItem: {
+    margin: theme.spacing(2),
+  },
+}));
 
 const ProjectSummary = ({ loading, error, data, refetch }) => {
   const { projectId } = useParams();
+  const classes = useStyles();
 
   const [makeSureRefresh, setMakeSureRefresh] = useState(false);
   const [mapError, setMapError] = useState(false);
+
+  const [statusUpdate, setStatusUpdate] = useState(null);
+  const [statusUpdateEditable, setStatusUpdateEditable] = useState(false);
 
   if (loading) return <CircularProgress />;
   if (error) return `Error! ${error.message}`;
@@ -28,7 +44,26 @@ const ProjectSummary = ({ loading, error, data, refetch }) => {
     projectFeatureRecords
   );
 
-  refetch();
+  const statusUpdateChange = e => {
+    setStatusUpdate(e.target.value);
+  };
+
+  /**
+   * Retrieve the last note made as a status update (by date)
+   */
+  const getStatusUpdate = (fieldName = "project_note") => {
+    const lastItem =
+      (data?.moped_project[0]?.moped_proj_notes?.length ?? 0) - 1;
+    if (lastItem >= 0) {
+      // Get the data from the note
+      const note =
+        data.moped_project[0].moped_proj_notes[lastItem][fieldName] ?? "";
+      // Remove any HTML tags
+      return note ? String(note).replace(/(<([^>]+)>)/gi, "") : null;
+    }
+    return null;
+  };
+
   if (projectFeatureRecords.length === 0 && !makeSureRefresh)
     setMakeSureRefresh(true);
 
@@ -37,6 +72,19 @@ const ProjectSummary = ({ loading, error, data, refetch }) => {
       <CardContent>
         <Grid container spacing={2}>
           <Grid item xs={12} md={6}>
+            {/*Status Update*/}
+            <Grid item xs={12} className={classes.fieldGridItem}>
+              <TextField
+                id="project_note_id"
+                label="Status Update"
+                data-id={getStatusUpdate("project_note_id")}
+                defaultValue={getStatusUpdate()}
+                onChange={statusUpdateChange}
+                InputProps={{
+                  readOnly: !statusUpdateEditable,
+                }}
+              />
+            </Grid>
             <ProjectSummaryTable
               loading={loading}
               data={data}
