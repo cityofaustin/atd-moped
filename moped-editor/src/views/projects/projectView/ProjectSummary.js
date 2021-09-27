f0dfbed57fad6e6e7f7013023affaa4f2db44005
Sergio Garcia <sergiogcx@gmail.com>
2021-09-27 13:46:17 -0500

    Add controls and initial behavior

--- moped-editor/src/views/projects/projectView/ProjectSummary.js
+++ moped-editor/src/views/projects/projectView/ProjectSummary.js
@@ -10,8 +10,12 @@ import {
   CardContent,
   CircularProgress,
   TextField,
+  Icon,
+  Box,
 } from "@material-ui/core";
 import ApolloErrorHandler from "../../../components/ApolloErrorHandler";
+
+import ControlPointIcon from "@material-ui/icons/ControlPoint";
 
 /*
   Error Handler and Fallback Component
@@ -24,6 +28,16 @@ const useStyles = makeStyles(theme => ({
   fieldGridItem: {
     margin: theme.spacing(2),
   },
+  editIcon: {
+    cursor: "pointer",
+    margin: "28px 0 0 8px",
+    fontSize: "20px",
+  },
+  editIconConfirm: {
+    cursor: "pointer",
+    margin: "28px 0 0 8px",
+    fontSize: "24px",
+  },
 }));
 
 const ProjectSummary = ({ loading, error, data, refetch }) => {
@@ -32,21 +46,6 @@ const ProjectSummary = ({ loading, error, data, refetch }) => {
 
   const [makeSureRefresh, setMakeSureRefresh] = useState(false);
   const [mapError, setMapError] = useState(false);
-
-  const [statusUpdate, setStatusUpdate] = useState(null);
-  const [statusUpdateEditable, setStatusUpdateEditable] = useState(false);
-
-  if (loading) return <CircularProgress />;
-  if (error) return `Error! ${error.message}`;
-
-  const projectFeatureRecords = data?.moped_project[0]?.moped_proj_features;
-  const projectFeatureCollection = createFeatureCollectionFromProjectFeatures(
-    projectFeatureRecords
-  );
-
-  const statusUpdateChange = e => {
-    setStatusUpdate(e.target.value);
-  };
 
   /**
    * Retrieve the last note made as a status update (by date)
@@ -64,6 +63,63 @@ const ProjectSummary = ({ loading, error, data, refetch }) => {
     return null;
   };
 
+  /**
+   * Status Update State Variables
+   */
+  const [statusUpdate, setStatusUpdate] = useState(getStatusUpdate());
+  const [statusUpdateAddNew, setStatusUpdateAddNew] = useState(false);
+  const [statusUpdateEditable, setStatusUpdateEditable] = useState(false);
+
+  if (loading) return <CircularProgress />;
+  if (error) return `Error! ${error.message}`;
+
+  const projectFeatureRecords = data?.moped_project[0]?.moped_proj_features;
+  const projectFeatureCollection = createFeatureCollectionFromProjectFeatures(
+    projectFeatureRecords
+  );
+
+  const handleStatusUpdateChange = e => {
+    setStatusUpdate(e.target.value);
+  };
+
+  /**
+   * Handles the edit click for status update
+   */
+  const handleStatusUpdateEdit = () => {
+    setStatusUpdateEditable(true);
+  };
+
+  /**
+   * Handles adding a new status update
+   */
+  const handleStatusUpdateAddNew = () => {
+    setStatusUpdateAddNew(true);
+    setStatusUpdateEditable(true);
+    setStatusUpdate("");
+  };
+
+  /**
+   * Handles saving the status update
+   */
+  const handleStatusUpdateSave = () => {
+    if (statusUpdateAddNew) {
+      // Create a new status update
+      console.log("Create a new status update");
+    } else {
+      // Update the existing status update
+      console.log("Update the existing status update");
+    }
+    setStatusUpdateEditable(false);
+  };
+
+  /**
+   * Handle canceling the status update
+   */
+  const handleStatusUpdateCancel = () => {
+    setStatusUpdate(getStatusUpdate());
+    setStatusUpdateEditable(false);
+  };
+
   if (projectFeatureRecords.length === 0 && !makeSureRefresh)
     setMakeSureRefresh(true);
 
@@ -74,16 +130,56 @@ const ProjectSummary = ({ loading, error, data, refetch }) => {
           <Grid item xs={12} md={6}>
             {/*Status Update*/}
             <Grid item xs={12} className={classes.fieldGridItem}>
-              <TextField
-                id="project_note_id"
-                label="Status Update"
-                data-id={getStatusUpdate("project_note_id")}
-                defaultValue={getStatusUpdate()}
-                onChange={statusUpdateChange}
-                InputProps={{
-                  readOnly: !statusUpdateEditable,
-                }}
-              />
+              <Box display="flex" justifyContent="flex-end">
+                {/*The text field is only an input/output component only,
+                it does not track or change the state.*/}
+                <TextField
+                  fullWidth
+                  {...(statusUpdateEditable ? { variant: "filled" } : {})}
+                  id="project_note_id"
+                  label="Status Update"
+                  data-id={getStatusUpdate("project_note_id")}
+                  value={statusUpdate}
+                  onChange={handleStatusUpdateChange}
+                  InputProps={{
+                    readOnly: !statusUpdateEditable,
+                  }}
+                />
+                {/*Edit Button*/}
+                {!statusUpdateEditable && (
+                  <Icon
+                    className={classes.editIcon}
+                    onClick={handleStatusUpdateEdit}
+                  >
+                    edit
+                  </Icon>
+                )}
+                {/*Add New Button*/}
+                {!statusUpdateEditable && (
+                  <ControlPointIcon
+                    className={classes.editIcon}
+                    onClick={handleStatusUpdateAddNew}
+                  />
+                )}
+                {/*Save Button*/}
+                {statusUpdateEditable && (
+                  <Icon
+                    className={classes.editIconConfirm}
+                    onClick={handleStatusUpdateSave}
+                  >
+                    check
+                  </Icon>
+                )}
+                {/*Cancel Button*/}
+                {statusUpdateEditable && (
+                  <Icon
+                    className={classes.editIconConfirm}
+                    onClick={handleStatusUpdateCancel}
+                  >
+                    close
+                  </Icon>
+                )}
+              </Box>
             </Grid>
             <ProjectSummaryTable
               loading={loading}
