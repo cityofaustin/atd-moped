80e1158ff8ed6bebc4f61883917392caa350004b
Frank Hereford <frank@frankhereford.com>
2021-10-08 08:56:25 -0500

    New Map Placeholder Content

--- moped-editor/src/views/projects/projectView/ProjectSummaryMapFallback.js
+++ moped-editor/src/views/projects/projectView/ProjectSummaryMapFallback.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Box, Button, Card, Icon, makeStyles } from "@material-ui/core";
+import { Box, Button, Card, Icon, makeStyles, Typography} from "@material-ui/core";
 
 import { Link } from "react-router-dom";
 
@@ -20,6 +20,12 @@ const useStyles = makeStyles(theme => ({
   },
   paragraphGroup: {
     margin: "1rem 0",
+  },
+  mapPlaceholder: {
+    color: "#848484",
+    fontFamily: "Roboto",
+    fontWeight: 500,
+    fontSize: "18px",
   },
   button: {
     margin: "1rem",
@@ -48,34 +54,20 @@ const ProjectSummaryMapFallback = ({ error, mapData }) => {
   return (
     <Box>
       <Card className={classes.card} color="secondary">
-        <img
-          className={classes.mapIcon}
-          alt="Logo"
-          src={`${process.env.PUBLIC_URL}/static/map-error.svg`}
+        <img 
+          alt="Map Unavailable"
+          src={`${process.env.PUBLIC_URL}/static/images/map_unavailable.png`}
         />
-        <h3 className={classes.mapErrorTitle}>No map available</h3>
-        <div className={classes.paragraphGroup}>
-          <p>
-            This project does not have components or is outdated. Create new
-            components or{" "}
-            <a
-              href={
-                "https://atd.knack.com/dts#new-service-request/?view_249_vars=%7B%22field_398%22%3A%22Bug%20Report%20%E2%80%94%20Something%20is%20not%20working%22%2C%22field_399%22%3A%22Moped%22%7D"
-              }
-            >
-              submit a support request
-            </a>{" "}
-            for more options.
-          </p>
+        <div className={classes.mapPlaceholder}>
+          Define and map the assets and features included in this project.
         </div>
         <Link to="?tab=map">
           <Button
             className={classes.button}
             variant="contained"
             color="primary"
-            startIcon={<Icon>edit</Icon>}
           >
-            MAP PROJECT
+            Add Components
           </Button>
         </Link>
       </Card>
