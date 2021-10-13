abab1abc90451dae1eca985bb551cdd3e0eb5771
Frank Hereford <frank@frankhereford.com>
2021-10-13 14:34:48 -0500

    Reverse order of comments via mod to query

--- moped-editor/src/queries/comments.js
+++ moped-editor/src/queries/comments.js
@@ -4,7 +4,7 @@ export const COMMENTS_QUERY = gql`
   query GetProjectComments($projectNoteConditions: moped_proj_notes_bool_exp!) {
     moped_proj_notes(
       where: $projectNoteConditions
-      order_by: { date_created: asc }
+      order_by: { date_created: desc }
     ) {
       added_by
       added_by_user_id
