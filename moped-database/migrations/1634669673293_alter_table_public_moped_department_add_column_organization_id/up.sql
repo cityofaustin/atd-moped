4b5a5b811810c705768dd40ee1a1d42acf6519c9
Chia Berry <analuciaby@gmail.com>
2021-10-19 14:36:54 -0500

    update moped_department table

---
+++ moped-database/migrations/1634669673293_alter_table_public_moped_department_add_column_organization_id/up.sql
@@ -0,0 +1,10 @@
+ALTER TABLE "public"."moped_department" ADD COLUMN "organization_id" integer NULL;
+
+INSERT INTO moped_department (department_id, department_name, organization_id, department_abbreviation)
+  VALUES 
+  (5, 'Housing & Planning', 1, NULL), 
+  (6, 'Aviation', 1, NULL),
+  (7, 'Economic Development', 1, NULL),
+  (8, 'Parks & Recreation', 1, NULL),
+  (9, 'Watershed', 1, NULL),
+  (10, 'Development Services', 1, 'DSD')
