0ba6b9a4f89c19d040cccc9cc88216561a07fb81
Chia Berry <analuciaby@gmail.com>
2021-10-20 10:54:27 -0500

    fix sql migration syntax

--- moped-database/migrations/1634669673293_alter_table_public_moped_department_add_column_organization_id/down.sql
+++ moped-database/migrations/1634669673293_alter_table_public_moped_department_add_column_organization_id/down.sql
@@ -1,3 +1,3 @@
 ALTER TABLE "public"."moped_department" DROP COLUMN "organization_id";
 
-DELETE FROM public.moped_department WHERE department_id > 4
+DELETE FROM public.moped_department WHERE department_id > 4;
--- moped-database/migrations/1634672655232_modify_primarykey_public_moped_workgroup/down.sql
+++ moped-database/migrations/1634672655232_modify_primarykey_public_moped_workgroup/down.sql
@@ -1,23 +1,23 @@
 alter table "public"."moped_workgroup" drop constraint "moped_workgroup_pkey";
 
 -- Restore workgroup abbreviations
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 1
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 2
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 3
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 5
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 6
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 7
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 8
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 10
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 11
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 12
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 13
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 14
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 15
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 16
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 17
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 20
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 21
-UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 22
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 1;
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 2;
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 3;
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 5;
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 6;
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 7;
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 8;
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 10;
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 11;
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 12;
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 13;
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 14;
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 15;
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 16;
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 17;
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 20;
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 21;
+UPDATE "public"."moped_workgroup" SET workgroup_abbreviation = NULL where workgroup_id = 22;
 
-DELETE FROM public.moped_workgroup WHERE workgroup_id > 23
+DELETE FROM public.moped_workgroup WHERE workgroup_id > 23;
