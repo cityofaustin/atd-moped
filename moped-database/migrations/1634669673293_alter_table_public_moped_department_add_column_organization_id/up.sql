efeba11faf836cb67bd9d60887b962e315183434
Chia Berry <analuciaby@gmail.com>
2021-10-25 08:44:23 -0500

    add abbreviation and id for departments

--- moped-database/migrations/1634669673293_alter_table_public_moped_department_add_column_organization_id/up.sql
+++ moped-database/migrations/1634669673293_alter_table_public_moped_department_add_column_organization_id/up.sql
@@ -7,4 +7,8 @@ INSERT INTO public.moped_department (department_id, department_name, organizatio
   (7, 'Economic Development', 1, NULL),
   (8, 'Parks & Recreation', 1, NULL),
   (9, 'Watershed', 1, NULL),
-  (10, 'Development Services', 1, 'DSD')
+  (10, 'Development Services', 1, 'DSD');
+
+UPDATE public.moped_department SET department_abbreviation = 'ATD', organization_id=1  where department_id = 1;
+UPDATE public.moped_department SET department_abbreviation = 'CPO', organization_id=1  where department_id = 2;
+UPDATE public.moped_department SET department_abbreviation = 'PWD', organization_id=1  where department_id = 3;
