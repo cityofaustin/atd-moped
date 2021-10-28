bd6e57d549322afeaa2eaacbdb99dee701e26d0e
Sergio Garcia <sergiogcx@gmail.com>
2021-10-28 14:42:41 -0500

    Separate project status update component

---
+++ moped-editor/src/views/projects/projectView/ProjectSummary/ProjectSummaryStatusUpdate.js
@@ -0,0 +1,222 @@
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+
+import { Box, Grid, Icon, TextField, Typography } from "@material-ui/core";
+import ControlPointIcon from "@material-ui/icons/ControlPoint";
+
+import {
+  PROJECT_SUMMARY_STATUS_UPDATE_INSERT,
+  PROJECT_SUMMARY_STATUS_UPDATE_UPDATE,
+} from "../../../../queries/project";
+
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import { getSessionDatabaseData } from "../../../../auth/user";
+
+const useStyles = makeStyles(theme => ({
+  fieldGridItem: {
+    margin: theme.spacing(2),
+  },
+  editIcon: {
+    cursor: "pointer",
+    margin: "28px 0 0 8px",
+    fontSize: "20px",
+  },
+  editIconConfirm: {
+    cursor: "pointer",
+    margin: "28px 0 0 8px",
+    fontSize: "24px",
+  },
+  fieldBox: {
+    width: "100%",
+  },
+  fieldBoxTypography: {
+    width: "100%",
+  },
+}));
+
+/**
+ * ProjectSummaryStatusUpdate Component
+ * @param {Number} projectId - The id of the current project being viewed
+ * @param {Object} data - The data object from the GraphQL query
+ * @param {function} refetch - The refetch function from apollo
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const ProjectSummaryStatusUpdate = ({ projectId, data, refetch }) => {
+  const classes = useStyles();
+  const userSessionData = getSessionDatabaseData();
+
+  const [updateProjectStatusUpdateInsert] = useMutation(
+    PROJECT_SUMMARY_STATUS_UPDATE_INSERT
+  );
+
+  const [updateProjectStatusUpdateUpdate] = useMutation(
+    PROJECT_SUMMARY_STATUS_UPDATE_UPDATE
+  );
+
+  /**
+   * Retrieve the last note made as a status update (which are ordered by date)
+   * @param {String} fieldName - The name of the field to retrieve in the moped_proj_note object
+   * @return {string|null}
+   */
+  const getStatusUpdate = (fieldName = "project_note") => {
+    const lastItem =
+        (data?.moped_project[0]?.moped_proj_notes?.length ?? 0) - 1;
+    if (lastItem >= 0) {
+      // Get the data from the note
+      const note = data
+          ? data?.moped_project[0].moped_proj_notes[lastItem][fieldName] ?? ""
+          : null;
+      // Remove any HTML tags
+      return note ? String(note).replace(/(<([^>]+)>)/gi, "") : null;
+    }
+    return null;
+  };
+
+  /**
+   * Status Update State Variables
+   */
+  const [statusUpdate, setStatusUpdate] = useState(getStatusUpdate());
+  const [statusUpdateAddNew, setStatusUpdateAddNew] = useState(false);
+  const [statusUpdateEditable, setStatusUpdateEditable] = useState(false);
+
+  /**
+   * Handles updating the state for "status update"
+   * @param {Object} event
+   */
+  const handleStatusUpdateChange = event => setStatusUpdate(event.target.value);
+
+  /**
+   * Handles the edit click for status update
+   */
+  const handleStatusUpdateEdit = () => {
+    setStatusUpdateEditable(true);
+  };
+
+  /**
+   * Handles adding a new status update
+   */
+  const handleStatusUpdateAddNew = () => {
+    setStatusUpdateAddNew(true);
+    setStatusUpdateEditable(true);
+    setStatusUpdate("");
+  };
+
+  /**
+   * Handles saving the status update
+   */
+  const handleStatusUpdateSave = () => {
+    // Retrieve a commentId or get a null
+    const commentId = getStatusUpdate("project_note_id");
+    const addedBy = `${userSessionData.first_name} ${userSessionData.last_name}`;
+    const isStatusUpdateInsert = statusUpdateAddNew || !commentId;
+
+    (isStatusUpdateInsert
+      ? updateProjectStatusUpdateInsert
+      : updateProjectStatusUpdateUpdate)({
+      variables: {
+        ...(isStatusUpdateInsert
+          ? {
+              statusUpdate: {
+                project_id: Number(projectId),
+                added_by: addedBy,
+                project_note: statusUpdate,
+                status_id: 1,
+                project_note_type: 2,
+              },
+            }
+          : {
+              project_note_id: { _eq: Number(commentId) },
+              added_by: addedBy,
+              project_note: statusUpdate,
+            }),
+      },
+    })
+      .then(() => {
+        console.log((commentId ? "Updated" : "Created") + " project note");
+        refetch().then(() => {
+          setStatusUpdateEditable(false);
+          setStatusUpdateAddNew(false);
+        });
+      })
+      .catch(err => {
+        console.log("Error: " + err);
+      });
+  };
+
+  /**
+   * Handle canceling the status update
+   */
+  const handleStatusUpdateCancel = () => {
+    // Retrieve original value
+    setStatusUpdate(getStatusUpdate());
+    // Reset edit-mode state
+    setStatusUpdateEditable(false);
+    setStatusUpdateAddNew(false);
+  };
+
+  return (
+    <Grid item xs={12} className={classes.fieldGridItem}>
+      <Box display="flex" justifyContent="flex-start">
+        {/*The text field is only an input/output component only,
+                it does not track or change the state. If nothing shows,
+                make sure your comments are type id = 2*/}
+        {statusUpdateEditable && (
+          <TextField
+            fullWidth
+            id="project_note_id"
+            label="Status update"
+            value={statusUpdate}
+            onChange={handleStatusUpdateChange}
+            multiline={true}
+            rows={3}
+            InputProps={{
+              readOnly: !statusUpdateEditable,
+            }}
+          />
+        )}
+        {!statusUpdateEditable && (
+          <Box className={classes.fieldBox}>
+            <Typography variant="h4">Status update</Typography>
+            <Typography className={classes.fieldBoxTypography}>
+              {statusUpdate}
+            </Typography>
+          </Box>
+        )}
+        {/*Edit Button*/}
+        {!statusUpdateEditable && statusUpdate && (
+          <Icon className={classes.editIcon} onClick={handleStatusUpdateEdit}>
+            edit
+          </Icon>
+        )}
+        {/*Add New Button*/}
+        {!statusUpdateEditable && (
+          <ControlPointIcon
+            className={classes.editIcon}
+            onClick={handleStatusUpdateAddNew}
+          />
+        )}
+        {/*Save Button*/}
+        {statusUpdateEditable && (
+          <Icon
+            className={classes.editIconConfirm}
+            onClick={handleStatusUpdateSave}
+          >
+            check
+          </Icon>
+        )}
+        {/*Cancel Button*/}
+        {statusUpdateEditable && (
+          <Icon
+            className={classes.editIconConfirm}
+            onClick={handleStatusUpdateCancel}
+          >
+            close
+          </Icon>
+        )}
+      </Box>
+    </Grid>
+  );
+};
+
+export default ProjectSummaryStatusUpdate;
