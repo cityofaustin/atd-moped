882e1661b9bb4544737c5e5a5a40cac22334e064
Sergio Garcia <sergiogcx@gmail.com>
2021-10-29 17:40:08 -0500

    Add project sponsor to graphql query

--- moped-editor/src/queries/project.js
+++ moped-editor/src/queries/project.js
@@ -37,6 +37,7 @@ export const SUMMARY_QUERY = gql`
       ecapris_subproject_id
       fiscal_year
       project_priority
+      project_sponsor
       moped_proj_features(where: { status_id: { _eq: 1 } }) {
         feature_id
         project_id
