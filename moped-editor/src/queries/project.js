a017edf25442e6c49086b7217082067f91b0616d
Sergio Garcia <sergiogcx@gmail.com>
2021-10-29 17:40:40 -0500

    Linter

--- moped-editor/src/queries/project.js
+++ moped-editor/src/queries/project.js
@@ -448,7 +448,11 @@ export const PROJECT_FILE_ATTACHMENTS_UPDATE = gql`
   ) {
     update_moped_project_files(
       where: { project_file_id: { _eq: $fileId } }
-      _set: { file_name: $fileName, file_type: $fileType, file_description: $fileDescription }
+      _set: {
+        file_name: $fileName
+        file_type: $fileType
+        file_description: $fileDescription
+      }
     ) {
       affected_rows
     }
