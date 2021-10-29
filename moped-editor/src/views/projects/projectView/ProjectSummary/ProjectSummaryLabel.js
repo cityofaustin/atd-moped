40d91c7a37037a24688f08e34434a6e72001738e
Sergio Garcia <sergiogcx@gmail.com>
2021-10-29 17:38:54 -0500

    Create summary label

--- moped-editor/src/views/projects/projectView/ProjectSummary/ProjectSummaryLabel.js
+++ moped-editor/src/views/projects/projectView/ProjectSummary/ProjectSummaryLabel.js
@@ -1,23 +1,23 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
-import makeStyles from "@material-ui/core/styles/makeStyles";
-
-const useStyles = makeStyles(theme => ({
-  label: {
-    fontSize: "1rem",
-    color: theme.palette.grey["600"],
-  },
-}));
+import { Icon, Typography } from "@material-ui/core";
 
 /**
  *
- * @param {String} label - Whatever the label should say
+ * @param {String} text - Label text
+ * @param {Object} classes - The style classes from parent
+ * @param {function} onClickEdit - The function to call on edit click
  * @returns {JSX.Element}
  * @constructor
  */
-const ProjectSummaryLabel = ({ label }) => {
-  const classes = useStyles();
-  return <Typography className={classes.label}>{label}</Typography>;
+const ProjectSummaryLabel = ({ text, classes, onClickEdit }) => {
+  return (
+    <>
+      <Typography className={classes.fieldLabelText}>{text}</Typography>
+      <Icon className={classes.editIcon} onClick={onClickEdit}>
+        edit
+      </Icon>
+    </>
+  );
 };
 
 export default ProjectSummaryLabel;
