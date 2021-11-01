7fe5e023197b2c47d20e37e1da8f73b863da5f0b
Sergio Garcia <sergiogcx@gmail.com>
2021-11-01 18:49:04 -0500

    Create new partners component

---
+++ moped-editor/src/views/projects/projectView/ProjectSummary/ProjectSummaryProjectPartners.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+/**
+ * ProjectSummaryProjectPartners Component
+ * @param {Number} projectId - The id of the current project being viewed
+ * @param {Object} data - The data object from the GraphQL query
+ * @param {function} refetch - The refetch function from apollo
+ * @param {Object} classes - The shared style settings
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const ProjectSummaryProjectPartners = ({
+  projectId,
+  data,
+  refetch,
+  classes,
+}) => {
+  return null;
+};
+
+export default ProjectSummaryProjectPartners;
