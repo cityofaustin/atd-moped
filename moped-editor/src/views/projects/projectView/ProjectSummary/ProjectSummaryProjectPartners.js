3a29597ded436eb1723e1ea6edd52ddc41c5adc2
Sergio Garcia <sergiogcx@gmail.com>
2021-11-01 18:53:01 -0500

    Implement the component's dom

--- moped-editor/src/views/projects/projectView/ProjectSummary/ProjectSummaryProjectPartners.js
+++ moped-editor/src/views/projects/projectView/ProjectSummary/ProjectSummaryProjectPartners.js
@@ -30,7 +30,82 @@ const ProjectSummaryProjectPartners = ({
   refetch,
   classes,
 }) => {
-  return null;
+  const entityList = data?.moped_entity ?? [];
+
+  const nonePartner = entityList.find(e => e.entity_id === 0);
+  const originalPartner = entityList.find(
+      e => e.entity_id === data?.moped_project?.[0]?.project_sponsor
+  );
+  const [editMode, setEditMode] = useState(false);
+
+  const [partner, setPartner] = useState(originalPartner ?? nonePartner);
+
+  
+
+  return (
+      <Grid item xs={12} className={classes.fieldGridItem}>
+        <Typography className={classes.fieldLabel}>Project Partners</Typography>
+        <Box
+            display="flex"
+            justifyContent="flex-start"
+            className={classes.fieldBox}
+        >
+          {editMode && (
+              <>
+                <Select
+                    id={`moped-project-summary-partner-select-${projectId}`}
+                    multiple
+                    value={personName}
+                    onChange={handleChange}
+                    input={<Input />}
+                    renderValue={selected => selected.join(", ")}
+                    /*
+                      There appears to be a problem with MenuProps in version 4.x (which is fixed in 5.0),
+                      this is fixed by overriding the function "getContentAnchorEl".
+                          Source: https://github.com/mui-org/material-ui/issues/19245#issuecomment-620488016
+                    */
+                    MenuProps={{
+                      getContentAnchorEl: () => null,
+                      style: {
+                        maxHeight: 500,
+                        width: 450
+                      }
+                    }}
+                    className={classes.fieldSelectItem}
+                >
+                  {entityList.map(entity => (
+                      <MenuItem key={entity.entity_name} value={entity.entity_name}>
+                        <Checkbox
+                            checked={personName.indexOf(entity.entity_name) > -1}
+                        />
+                        <ListItemText primary={entity.entity_name} />
+                      </MenuItem>
+                  ))}
+                </Select>
+                <Icon
+                    className={classes.editIconConfirm}
+                    onClick={handleProjectSponsorSave}
+                >
+                  check
+                </Icon>
+                <Icon
+                    className={classes.editIconConfirm}
+                    onClick={handleProjectSponsorClose}
+                >
+                  close
+                </Icon>
+              </>
+          )}
+          {!editMode && (
+              <ProjectSummaryLabel
+                  text={partner?.entity_name || "n/a"}
+                  classes={classes}
+                  onClickEdit={() => setEditMode(true)}
+              />
+          )}
+        </Box>
+      </Grid>
+  );
 };
 
 export default ProjectSummaryProjectPartners;
