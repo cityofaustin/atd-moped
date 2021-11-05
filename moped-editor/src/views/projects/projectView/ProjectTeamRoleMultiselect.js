097e56495cbcd0107ec1253f9988497f9791f8ee
Sergio Garcia <sergiogcx@gmail.com>
2021-11-05 00:48:48 -0500

    Remove old menu props

--- moped-editor/src/views/projects/projectView/ProjectTeamRoleMultiselect.js
+++ moped-editor/src/views/projects/projectView/ProjectTeamRoleMultiselect.js
@@ -26,14 +26,6 @@ const useStyles = makeStyles(theme => ({
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
 
 const ProjectTeamRoleMultiselect = ({
   roles,
