3cb3d13f7dbc9c8b1c26975d12cbb485f5d7ea57
Sergio Garcia <sergiogcx@gmail.com>
2021-11-08 16:48:31 -0600

    Allow style override

--- moped-editor/src/views/projects/projectView/ProjectSummary/ProjectSummaryLabel.js
+++ moped-editor/src/views/projects/projectView/ProjectSummary/ProjectSummaryLabel.js
@@ -7,13 +7,21 @@ import CreateOutlinedIcon from "@material-ui/icons/CreateOutlined";
  * @param {String} text - Label text
  * @param {Object} classes - The style classes from parent
  * @param {function} onClickEdit - The function to call on edit click
+ * @param {Object} className - Class name override if present
  * @returns {JSX.Element}
  * @constructor
  */
-const ProjectSummaryLabel = ({ text, classes, onClickEdit }) => {
+const ProjectSummaryLabel = ({
+  text,
+  classes,
+  onClickEdit,
+  className = null,
+}) => {
   return (
     <>
-      <Typography className={classes.fieldLabelText}>{text}</Typography>
+      <Typography className={className ?? classes.fieldLabelText}>
+        {text}
+      </Typography>
       <CreateOutlinedIcon className={classes.editIcon} onClick={onClickEdit} />
     </>
   );
