85802307c670af752723a4843b9be2da6fbfdcf7
Sergio Garcia <sergiogcx@gmail.com>
2021-11-09 14:51:14 -0600

    Tiny adjustment

--- moped-editor/src/views/projects/projectView/ProjectSummary/ProjectSummary.js
+++ moped-editor/src/views/projects/projectView/ProjectSummary/ProjectSummary.js
@@ -50,7 +50,7 @@ const useStyles = makeStyles(theme => ({
     width: "calc(100% - 2rem)",
   },
   fieldLabelLink: {
-    width: "calc(100% - 4rem)",
+    width: "calc(100% - 4.2rem)",
     overflow: "hidden",
     whiteSpace: "nowrap",
   },
