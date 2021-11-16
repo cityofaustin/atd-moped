06b7e8c9b4239775e167f2368b8f47cee1693cd1
Chia Berry <analuciaby@gmail.com>
2021-11-16 14:15:37 -0600

    query only active features in project

--- moped-editor/src/views/projects/signalProjectTable/RenderSignalLink.js
+++ moped-editor/src/views/projects/signalProjectTable/RenderSignalLink.js
@@ -2,18 +2,23 @@ import React from "react";
 import Link from "@material-ui/core/Link";
 
 const RenderSignalLink = ({ signals }) => {
-  const cleanSignals = signals.filter(s => s.knack_id);
   return (
     <span>
-      {cleanSignals.map((signal, index) => (
-        <Link
-          href={`https://atd.knack.com/amd#projects/signal-details/${signal.knack_id}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {signal.signal_id}
-          {index < cleanSignals.length - 1 && ", "}
-        </Link>
+      {signals.map((signal, index) => (
+        <>
+          {signal.knack_id ? (
+            <Link
+              href={`https://atd.knack.com/amd#projects/signal-details/${signal.knack_id}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {signal.signal_id}
+            </Link>
+          ) : (
+            signal.signal_id
+          )}
+          {index < signals.length - 1 && ", "}
+        </>
       ))}
     </span>
   );
