496e311d5ce48882be30487b5d57da4e0e2af1fe
Chia Berry <analuciaby@gmail.com>
2021-11-16 11:28:35 -0600

    drop undefined knack_ids from signals

--- moped-editor/src/views/projects/signalProjectTable/RenderSignalLink.js
+++ moped-editor/src/views/projects/signalProjectTable/RenderSignalLink.js
@@ -1,19 +1,22 @@
 import React from "react";
 import Link from "@material-ui/core/Link";
 
-const RenderSignalLink = ({ signals }) => (
-  <span>
-    {signals.map((signal, index) => (
-      <Link
-        href={`https://atd.knack.com/amd#projects/signal-details/${signal.knack_id}`}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {signal.signal_id}
-        {index < signals.length - 1 && ", "}
-      </Link>
-    ))}
-  </span>
-);
+const RenderSignalLink = ({ signals }) => {
+  const cleanSignals = signals.filter(s => s.knack_id);
+  return (
+    <span>
+      {cleanSignals.map((signal, index) => (
+        <Link
+          href={`https://atd.knack.com/amd#projects/signal-details/${signal.knack_id}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {signal.signal_id}
+          {index < cleanSignals.length - 1 && ", "}
+        </Link>
+      ))}
+    </span>
+  );
+};
 
 export default RenderSignalLink;
