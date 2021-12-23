18a3a3fcc4bdd919f3f79017e7217c7d35fbd4c4
Sergio Garcia <sergiogcx@gmail.com>
2021-12-23 15:22:31 -0600

    Implement IGNORE_BRANCH and modify default working stage

--- .github/workflows/aws-moped-cognito-edge-helper.sh
+++ .github/workflows/aws-moped-cognito-edge-helper.sh
@@ -10,16 +10,40 @@ case "${BRANCH_NAME}" in
     export WORKING_STAGE="staging";
   ;;
   *)
-    export WORKING_STAGE="other";
+    export WORKING_STAGE="test";
   ;;
 esac;
 
-echo "Preparing Build & Deployment";
-if [[ "${WORKING_STAGE}" == "other" ]]; then
-  echo "Builds are only supported for production or staging at this moment";
-  exit 0;
+#
+# If IGNORE_BRANCH is set to TRUE, skip branch check...
+#
+if [[ "${IGNORE_BRANCH}" != "TRUE" ]]; then
+  if [[ "${WORKING_STAGE}" == "test" ]]; then
+    echo "Builds are only supported for production or staging at this moment";
+    exit 0;
+  else
+      echo "Preparing Build & Deployment for environment: ${WORKING_STAGE}";
+  fi;
+else
+  echo "Ignoring branch build, assuming ${WORKING_STAGE} environment";
 fi;
 
+#
+# Builds the bundle into a zip file.
+#
+function build() {
+  echo "Building Node Module...";
+  cp "${WORKING_STAGE}.js" index.js;
+
+  # At the time of this writing, Lambda@Edge only supports node v14.x.x
+  docker run -it --rm --workdir="/app" \
+     --volume "$(pwd):/app" node:14-alpine \
+     sh -c "apk update && apk add zip && npm install && zip -r9 -r function.zip node_modules index.js package.json package-lock.json";
+}
+
+#
+# Updates lambda@edge function
+#
 function deploy() {
   FUNCTION_NAME="atd-moped-cloudfront-auth-${WORKING_STAGE}";
   echo "Deploying function: ${FUNCTION_NAME}";
