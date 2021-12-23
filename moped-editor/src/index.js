de406f8eca293c3a6d6db6b9368dcdf49dd19b75
Sergio Garcia <sergiogcx@gmail.com>
2021-12-23 15:00:30 -0600

    Remove identity pool id

--- moped-editor/src/index.js
+++ moped-editor/src/index.js
@@ -20,7 +20,6 @@ Amplify.configure({
     mandatorySignIn: true,
     region: config.cognito.REGION,
     userPoolId: config.cognito.USER_POOL_ID,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID,
     userPoolWebClientId: config.cognito.APP_CLIENT_ID,
     oauth: {
       domain: config.cognito.DOMAIN,
