cb516852c9bf8b8a13cafb488826aa329bfbab0f
Frank Hereford <frankhereford@users.noreply.github.com>
2021-12-27 10:01:03 -0600

    define query to pull projects from moped

--- moped-etl/app/moped_data-tracker_sync_projects.py
+++ moped-etl/app/moped_data-tracker_sync_projects.py
@@ -2,4 +2,18 @@
 
 import json
 import requests
+from string import Template
 
+
+
+# Query to gather a subset of list of specific Locations
+get_all_projects = Template("""
+query get_all_projects {
+  moped_project {
+    project_id
+    project_name
+    current_status
+    knack_project_id
+  }
+}
+""")
