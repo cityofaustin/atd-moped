9d9cdae6833cc0c714367b05f3c01f603ac60a40
Frank Hereford <frankhereford@users.noreply.github.com>
2021-12-27 15:57:07 -0600

    capture moped data

--- moped-etl/app/moped_data-tracker_sync_projects.py
+++ moped-etl/app/moped_data-tracker_sync_projects.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import json
+import pprint
 import requests
 from process.request import run_query
 
+pp = pprint.PrettyPrinter(width=120, indent=2)
 
 get_all_projects = """
 query get_all_projects {
@@ -16,6 +18,7 @@ query get_all_projects {
 }
 """
 
-result = run_query(get_all_projects)
+moped_data = run_query(get_all_projects)
+pp.pprint(moped_data)
 
-print(result)
+
