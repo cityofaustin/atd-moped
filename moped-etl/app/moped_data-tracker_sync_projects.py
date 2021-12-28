e5cc81b7de4046dfde447a37029a257a418a8666
Frank Hereford <frankhereford@users.noreply.github.com>
2021-12-28 14:00:10 -0600

    Compare datasets and produce changeset dictionary

--- moped-etl/app/moped_data-tracker_sync_projects.py
+++ moped-etl/app/moped_data-tracker_sync_projects.py
@@ -29,12 +29,10 @@ for variable in list(os.environ):
     if match:
         key = match.group('object_key')
         knack_object_keys[key.lower()] = os.getenv(variable)
-
+#print(knack_object_keys)
 
 moped_data = run_query(get_all_projects)
 #pp.pprint(moped_data)
-
-#knack_data = run_knack_project_query(knack_object_keys)
 
 app = knackpy.App(app_id=KNACK_DATA_TRACKER_APP_ID)
 records = app.get('view_' + KNACK_DATA_TRACKER_VIEW, generate=1)
@@ -42,8 +40,17 @@ knack_records = {}
 for record in records:
     if (record[knack_object_keys['project_id']] == None):
         continue
-    #print(record[knack_object_keys['PROJECT_ID']]) # accessing a particular field value defined by env variables
     knack_records[record[knack_object_keys['project_id']]] = record
-
 #print(knack_records)
 
+for moped_project in moped_data['data']['moped_project']:
+    #print(moped_project)
+    #print(knack_records[moped_project['project_id']].format())
+    required_knack_updates = {}
+    for key in knack_object_keys:
+        print(key)
+        if not moped_project[key] == knack_records[moped_project['project_id']][knack_object_keys[key]]:
+            print('mismatch!')
+            required_knack_updates[knack_object_keys[key]] = moped_project[key]
+        print()
+    print(required_knack_updates)
