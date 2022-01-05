69307f20f7d775ce06df2ecc8c7b52deced10b27
Sergio Garcia <sergiogcx@gmail.com>
2022-01-05 07:33:55 -0600

    Add useEffect

--- moped-editor/src/views/projects/projectView/ProjectSummary/ProjectSummary.js
+++ moped-editor/src/views/projects/projectView/ProjectSummary/ProjectSummary.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import ProjectSummaryMap from "./ProjectSummaryMap";
