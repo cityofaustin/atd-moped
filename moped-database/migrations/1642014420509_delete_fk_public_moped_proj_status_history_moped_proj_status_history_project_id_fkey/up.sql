45ff45b8a803e7c5891ce81f20a2c5eaa90c517f
zappfyllc <info@zappfy.com>
2022-01-12 14:08:17 -0500

    remove moped_proj_status_history and moped_proj_status_notes
    made obsolete by activity log and project comments features

---
+++ moped-database/migrations/1642014420509_delete_fk_public_moped_proj_status_history_moped_proj_status_history_project_id_fkey/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."moped_proj_status_history" drop constraint "moped_proj_status_history_project_id_fkey";
---
+++ moped-database/migrations/1642014449257_delete_fk_public_moped_proj_status_notes_moped_proj_status_notes_project_id_fkey/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."moped_proj_status_notes" drop constraint "moped_proj_status_notes_project_id_fkey";
