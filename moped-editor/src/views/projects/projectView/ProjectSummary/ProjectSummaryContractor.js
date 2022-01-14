c3085e4618b7c41a07f52d539f6d0694b1de082b
Sergio Garcia <sergiogcx@gmail.com>
2022-01-14 04:20:30 -0600

    Add return statement

--- moped-editor/src/views/projects/projectView/ProjectSummary/ProjectSummaryContractor.js
+++ moped-editor/src/views/projects/projectView/ProjectSummary/ProjectSummaryContractor.js
@@ -43,6 +43,49 @@ const ProjectSummaryContractor = ({
   const [updateProjectContractor] = useMutation(PROJECT_UPDATE_CONTACTOR);
   const [clearProjectContractor] = useMutation(PROJECT_CLEAR_CONTACTOR);
 
+  return (
+    <Grid item xs={12} className={classes.fieldGridItem}>
+      <Typography className={classes.fieldLabel}>
+        Contractor/contract
+      </Typography>
+      <Box
+        display="flex"
+        justifyContent="flex-start"
+        className={classes.fieldBox}
+      >
+        {editMode && (
+          <>
+            <TextField
+              fullWidth
+              id="moped-project-website"
+              label={null}
+              onChange={handleProjectContractorChange}
+              value={contractor}
+            />
+            <Icon
+              className={classes.editIconConfirm}
+              onClick={handleProjectContractorSave}
+            >
+              check
+            </Icon>
+            <Icon
+              className={classes.editIconConfirm}
+              onClick={handleProjectContractorClose}
+            >
+              close
+            </Icon>
+          </>
+        )}
+        {!editMode && (
+          <ProjectSummaryLabel
+            text={(contractor ?? "").length > 0 ? contractor : "None"}
+            classes={classes}
+            onClickEdit={() => setEditMode(true)}
+          />
+        )}
+      </Box>
+    </Grid>
+  );
 };
 
 export default ProjectSummaryContractor;
