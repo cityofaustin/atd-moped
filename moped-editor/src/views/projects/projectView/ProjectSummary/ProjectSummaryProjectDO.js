9d1653dc41fec5309a85b8c7756bbe3b02bde08b
Sergio Garcia <sergiogcx@gmail.com>
2022-01-14 04:22:41 -0600

    Add return statement

--- moped-editor/src/views/projects/projectView/ProjectSummary/ProjectSummaryProjectDO.js
+++ moped-editor/src/views/projects/projectView/ProjectSummary/ProjectSummaryProjectDO.js
@@ -51,6 +51,52 @@ const ProjectSummaryProjectDO = ({
     PROJECT_CLEAR_PURCHASE_ORDER_NUMBER
   );
 
+
+  return (
+    <Grid item xs={12} className={classes.fieldGridItem}>
+      <Typography className={classes.fieldLabel}>DO #</Typography>
+      <Box
+        display="flex"
+        justifyContent="flex-start"
+        className={classes.fieldBox}
+      >
+        {editMode && (
+          <>
+            <TextField
+              fullWidth
+              id="moped-project-website"
+              label={null}
+              onChange={handleProjectPurchaseOrderNumberChange}
+              value={projectPurchaseOrderNumber}
+            />
+            <Icon
+              className={classes.editIconConfirm}
+              onClick={handleProjectOrderNumberSave}
+            >
+              check
+            </Icon>
+            <Icon
+              className={classes.editIconConfirm}
+              onClick={handleProjectOrderNumberClose}
+            >
+              close
+            </Icon>
+          </>
+        )}
+        {!editMode && (
+          <ProjectSummaryLabel
+            text={
+              (projectPurchaseOrderNumber ?? "").length > 0
+                ? projectPurchaseOrderNumber
+                : "None"
+            }
+            classes={classes}
+            onClickEdit={() => setEditMode(true)}
+          />
+        )}
+      </Box>
+    </Grid>
+  );
 };
 
 export default ProjectSummaryProjectDO;
