9e8c2dffe53507218b7345c13dba27e049e5a9b3
Frank Hereford <frankhereford@users.noreply.github.com>
2022-01-25 12:55:26 -0600

    rest of the tables in jd's comment

--- moped-database/cleanup.sql
+++ moped-database/cleanup.sql
@@ -1,10 +1,21 @@
--- 1
+-- handle tables in https://github.com/cityofaustin/atd-data-tech/issues/6233#issuecomment-964247418
+-- grouped by interconnected FKs
+--
 drop table moped_proj_fund_opp;
 drop table moped_fund_opp;
--- 2
+--
 drop table moped_proj_groups;
 drop table moped_group;
--- 3
+--
 alter table moped_proj_notes drop column comm_id;
 drop table moped_proj_communication;
+--
+drop table moped_proj_dates;
+--
+drop table moped_proj_location;
+--
+drop table moped_proj_status_history;
+--
+drop table moped_proj_status_notes;
 -- 
+drop table moped_proj_timeline;
