74d380d097571d4201011ba0726cd56c1f9516ee
Frank Hereford <frankhereford@users.noreply.github.com>
2022-01-25 12:47:16 -0600

    drop tables

---
+++ moped-database/cleanup.sql
@@ -0,0 +1,10 @@
+-- 1
+drop table moped_proj_fund_opp;
+drop table moped_fund_opp;
+-- 2
+drop table moped_proj_groups;
+drop table moped_group;
+-- 3
+alter table moped_proj_notes drop column comm_id;
+drop table moped_proj_communication;
+-- 
