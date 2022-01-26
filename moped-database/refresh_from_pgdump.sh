6ea2fec6d4d0d06dcf0c9aa0e10c8052a2b2e23a
Frank Hereford <frankhereford@users.noreply.github.com>
2022-01-26 13:44:12 -0600

    dont let this just get run anymore

--- moped-database/refresh_from_pgdump.sh
+++ moped-database/refresh_from_pgdump.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
 
+# restore to pre-work state
 docker-compose -f ./docker-compose.yml -f docker-compose.arm64.yml stop;
 docker container prune -f;
 docker volume rm moped-database_moped-db-vol -f;
@@ -9,3 +9,18 @@ docker exec -i moped-database_moped-pgsql_1 psql -U moped < ./moped_dump.sql;
 docker-compose -f ./docker-compose.yml -f docker-compose.arm64.yml up -d hasura;
 sleep 15;
 hasura console;
+
+
+
+
+# find problems in seed file technique
+docker-compose -f ./docker-compose.yml -f docker-compose.arm64.yml stop;
+docker container prune -f;
+docker volume rm moped-database_moped-db-vol -f;
+docker-compose -f ./docker-compose.yml -f docker-compose.arm64.yml up -d moped-pgsql;
+sleep 5;
+docker-compose -f ./docker-compose.yml -f docker-compose.arm64.yml up -d hasura;
+sleep 15;
+hasura migrate apply;
+hasura metadata apply;
+docker exec -i moped-database_moped-pgsql_1 psql -v ON_ERROR_STOP=1 -U moped < ./seeds/1602292389297_initial_seed_staging.sql
