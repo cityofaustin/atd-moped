8f5fbaa50c5d300e656e55d913b2600aea620edd
Frank Hereford <frankhereford@users.noreply.github.com>
2022-01-27 10:35:37 -0600

    Add beep and disable graphql-engine for speed

--- moped-database/refresh_from_pgdump.sh
+++ moped-database/refresh_from_pgdump.sh
@@ -7,8 +7,8 @@ docker volume rm moped-database_moped-db-vol -f;
 docker-compose -f ./docker-compose.yml -f docker-compose.arm64.yml up -d moped-pgsql;
 sleep 5;
 docker exec -i moped-database_moped-pgsql_1 psql -U moped < ./staging_moped_dump.sql;
-docker-compose -f ./docker-compose.yml -f docker-compose.arm64.yml up -d hasura;
-sleep 15;
+#docker-compose -f ./docker-compose.yml -f docker-compose.arm64.yml up -d hasura;
+#sleep 15;
 echo -ne '\007'; # beep
 #hasura console;
 
