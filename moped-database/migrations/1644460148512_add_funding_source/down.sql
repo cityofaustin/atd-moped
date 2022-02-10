341c2bd54a91f1571d9e024ca21656bd5bb4f622
Frank Hereford <frankhereford@users.noreply.github.com>
2022-02-10 00:20:55 -0600

    remove unused migration

--- moped-database/migrations/1644460148512_add_funding_source/down.sql
+++
@@ -1,1 +0,0 @@
-DELETE FROM moped_fund_sources WHERE funding_source_category = 0 AND funding_source_name = 'None identified';
--- moped-database/migrations/1644460148512_add_funding_source/up.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO moped_fund_sources
-            (funding_source_name, funding_source_category)
-VALUES      ('None identified',   0);
