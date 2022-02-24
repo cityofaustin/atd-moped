e89f97dbe8a2068053502624422c03c8c6df3009
Frank Hereford <frankhereford@users.noreply.github.com>
2022-02-24 15:51:59 -0600

    add missing commas

--- moped-editor/src/views/projects/projectView/ProjectActivityLogTableMaps.js
+++ moped-editor/src/views/projects/projectView/ProjectActivityLogTableMaps.js
@@ -535,12 +535,12 @@ export const ProjectActivityLogTableMaps = {
         icon: "",
         label: "Status ID",
         type: "integer",
-      }
+      },
       subphase_name: {
         icon: "",
         label: "Subphase Name",
         type: "text",
-      }
+      },
       subphase_id: {
         icon: "",
         label: "Subphase ID",
