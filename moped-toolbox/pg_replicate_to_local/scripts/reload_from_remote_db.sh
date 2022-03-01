dcafdc2e403d7511f1a9710b37880ac188eccccb
Frank Hereford <frankhereford@users.noreply.github.com>
2022-03-01 09:43:45 -0600

    Primary script automating replication

---
+++ moped-toolbox/pg_replicate_to_local/scripts/reload_from_remote_db.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+PLATFORM=$(uname -a | rev | cut -d " " -f1 | rev);
+
+# bring down local graphql-engine service
+docker-compose -f ../../moped-database/docker-compose.yml -f ../../moped-database/docker-compose.${PLATFORM}.yml stop hasura;
+
+# use docker utility image to download remote DB
+docker-compose up
+
+echo "DROP DATABASE moped" | docker exec -i moped-database_moped-pgsql_1 psql -U moped postgres;
+echo "CREATE DATABASE moped" | docker exec -i moped-database_moped-pgsql_1 psql -U moped postgres;
+echo "CREATE EXTENSION postgis" | docker exec -i moped-database_moped-pgsql_1 psql -U moped moped;
+
+# upgrading to pg v.13 provides the gen_random_uuid() function,
+# otherwise, you need the following line and to cast the UUIDs into ::text in the exported SQL
+#echo "CREATE EXTENSION pgcrypto" | docker exec -i moped-database_moped-pgsql_1 psql -U moped moped;
+
+cat workspace/heroku_db.sql | docker exec -i moped-database_moped-pgsql_1 psql -U moped -v ON_ERROR_STOP=ON moped;
+docker-compose -f ../../moped-database/docker-compose.yml -f ../../moped-database/docker-compose.${PLATFORM}.yml up -d hasura;
