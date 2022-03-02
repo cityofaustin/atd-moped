b1ae64c9f4a03de36635e1d5b77810066af3a7bb
Frank Hereford <frankhereford@users.noreply.github.com>
2022-03-02 08:53:49 -0600

    Let multiple edits stack up instead of remaining inline

--- moped-editor/src/views/projects/projectView/ProjectActivityLog.js
+++ moped-editor/src/views/projects/projectView/ProjectActivityLog.js
@@ -45,8 +45,12 @@ const useStyles = makeStyles(theme => ({
   tableCell: {
     verticalAlign: "top",
   },
+  updateGridEntries: {
+    display: "block",
+    padding: "0 0 0 .5rem",
+  },
   tableChangeItem: {
-    padding: "0 .5rem",
+    padding: "0 .5rem 0 0",
   },
   avatarSmall: {
     width: theme.spacing(4),
