de882f40007dc2d819742e99ce378e8986eb9c65
Tilly Whitson <35410637+tillyw@users.noreply.github.com>
2022-03-10 16:52:50 -0600

    Fixed migration

--- moped-database/migrations/1646942455000/down.sql
+++
@@ -1,2 +0,0 @@
--- Handle inserting row in local database instances
-DELETE FROM "public"."moped_project_roles" WHERE project_role_id = 17;
--- moped-database/migrations/1646942455000/up.sql
+++
@@ -1,4 +0,0 @@
--- Handle inserting row in local database instances
-INSERT INTO "public"."moped_project_roles"
-            (project_role_id, project_role_name, active_role, role_order, project_role_description)
-VALUES      (17, 'Signal engineer', true, 17, 'Develops signal timing plans and monitors signal operations');
