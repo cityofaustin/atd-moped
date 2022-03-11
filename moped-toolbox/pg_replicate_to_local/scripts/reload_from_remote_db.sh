5697e8ebdfb2f52f8194d055c1393bf8c474ec58
Frank Hereford <frankhereford@users.noreply.github.com>
2022-03-11 16:03:40 -0600

    docker-compose V2 naming scheme

--- moped-toolbox/pg_replicate_to_local/scripts/reload_from_remote_db.sh
+++ moped-toolbox/pg_replicate_to_local/scripts/reload_from_remote_db.sh
@@ -8,13 +8,13 @@ docker-compose -f ../../moped-database/docker-compose.yml -f ../../moped-databas
 # use docker utility image to download remote DB
 docker-compose up
 
-echo "DROP DATABASE moped" | docker exec -i moped-database_moped-pgsql_1 psql -U moped postgres;
-echo "CREATE DATABASE moped" | docker exec -i moped-database_moped-pgsql_1 psql -U moped postgres;
-echo "CREATE EXTENSION postgis" | docker exec -i moped-database_moped-pgsql_1 psql -U moped moped;
+echo "DROP DATABASE moped" | docker exec -i moped-database-moped-pgsql-1 psql -U moped postgres;
+echo "CREATE DATABASE moped" | docker exec -i moped-database-moped-pgsql-1 psql -U moped postgres;
+echo "CREATE EXTENSION postgis" | docker exec -i moped-database-moped-pgsql-1 psql -U moped moped;
 
 # upgrading to pg v.13 provides the gen_random_uuid() function,
 # otherwise, you need the following line and to cast the UUIDs into ::text in the exported SQL
-#echo "CREATE EXTENSION pgcrypto" | docker exec -i moped-database_moped-pgsql_1 psql -U moped moped;
+#echo "CREATE EXTENSION pgcrypto" | docker exec -i moped-database-moped-pgsql-1 psql -U moped moped;
 
-cat workspace/db.sql | docker exec -i moped-database_moped-pgsql_1 psql -U moped -v ON_ERROR_STOP=ON moped;
+cat workspace/db.sql | docker exec -i moped-database-moped-pgsql-1 psql -U moped -v ON_ERROR_STOP=ON moped;
 docker-compose -f ../../moped-database/docker-compose.yml -f ../../moped-database/docker-compose.${PLATFORM}.yml up -d hasura;
