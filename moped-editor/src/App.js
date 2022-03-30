189956f9e83e4058d9a53a6e1770ee06a9c72975
Chia Berry <analuciaby@gmail.com>
2022-03-30 13:13:19 -0500

    remove dashboardview and chartjs packg

--- moped-editor/src/App.js
+++ moped-editor/src/App.js
@@ -1,9 +1,7 @@
-import "react-perfect-scrollbar/dist/css/styles.css";
 import React from "react";
 import { useRoutes } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core";
 import GlobalStyles from "src/components/GlobalStyles";
-import "src/mixins/chartjs";
 import theme from "src/theme";
 import { routes, restrictRoutes } from "src/routes";
 import { useUser, getJwt, getHighestRole } from "./auth/user";
