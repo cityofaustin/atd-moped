bdb3b2338c8d4bd4dfef1af2d5bd105c096ed314
Frank Hereford <frankhereford@users.noreply.github.com>
2022-04-13 13:54:08 -0500

    Iterate over the knack projects and pull up data from moped rr + linting

--- moped-toolbox/backfill_data_tracker_url_field/app/backfill_url_and_label.py
+++ moped-toolbox/backfill_data_tracker_url_field/app/backfill_url_and_label.py
@@ -5,6 +5,32 @@ import os
 import knackpy
 import psycopg2
 
-pg = psycopg2.connect(host=os.getenv('MOPED_RR_HOSTNAME'), database=os.getenv('MOPED_RR_DATABASE'), user=os.getenv('MOPED_RR_USERNAME'), password=os.getenv('MOPED_RR_PASSWORD'))
+from psycopg2 import (
+    extras,
+)
 
-knack = knackpy.App(app_id=os.getenv('KNACK_APP_ID'),  api_key=os.getenv('KNACK_API_KEY'))
+pg = psycopg2.connect(
+    host=os.getenv("MOPED_RR_HOSTNAME"),
+    database=os.getenv("MOPED_RR_DATABASE"),
+    user=os.getenv("MOPED_RR_USERNAME"),
+    password=os.getenv("MOPED_RR_PASSWORD"),
+)
+
+knack = knackpy.App(
+    app_id=os.getenv("KNACK_APP_ID"), api_key=os.getenv("KNACK_API_KEY")
+)
+
+records = knack.get(os.getenv("KNACK_PROJECT_VIEW"))
+
+for record in records:
+    if record[os.getenv("KNACK_MOPED_ID_FIELD")]:
+        project_id = record[os.getenv("KNACK_MOPED_ID_FIELD")]
+        sql = """
+          select * 
+          from moped_project
+          where project_id = %s
+          """
+        cursor = pg.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cursor.execute(sql, (project_id,))
+        moped_record = cursor.fetchall()
+        cursor.close()
