14f756bc392057aef75c4736669674767f433115
Frank Hereford <frankhereford@users.noreply.github.com>
2022-04-13 13:12:20 -0500

    connect to knack

--- moped-toolbox/backfill_data_tracker_url_field/app/backfill_url_and_label.py
+++ moped-toolbox/backfill_data_tracker_url_field/app/backfill_url_and_label.py
@@ -5,5 +5,6 @@ import os
 import knackpy
 import psycopg2
 
-moped = psycopg2.connect(host=os.getenv('MOPED_RR_HOSTNAME'), database=os.getenv('MOPED_RR_DATABASE'), user=os.getenv('MOPED_RR_USERNAME'), password=os.getenv('MOPED_RR_PASSWORD'))
+pg = psycopg2.connect(host=os.getenv('MOPED_RR_HOSTNAME'), database=os.getenv('MOPED_RR_DATABASE'), user=os.getenv('MOPED_RR_USERNAME'), password=os.getenv('MOPED_RR_PASSWORD'))
 
+knack = knackpy.App(app_id=os.getenv('KNACK_APP_ID'),  api_key=os.getenv('KNACK_API_KEY'))
