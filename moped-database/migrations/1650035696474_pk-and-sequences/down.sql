4f16d2e6f8bc6f7134365a7ac877487c683a5869
Frank Hereford <frankhereford@users.noreply.github.com>
2022-04-15 11:06:06 -0500

    Set the IDs

--- moped-database/migrations/1650035696474_pk-and-sequences/down.sql
+++ moped-database/migrations/1650035696474_pk-and-sequences/down.sql
@@ -1,3 +1,4 @@
+UPDATE moped_city_fiscal_years set id is null;
 DROP SEQUENCE city_fiscal_years_id_seq;
 ALTER TABLE moped_city_fiscal_years ADD CONSTRAINT moped_city_fiscal_years_pkey PRIMARY KEY (fiscal_year_value);
 ALTER TABLE moped_city_fiscal_years DROP COLUMN id;
--- moped-database/migrations/1650035696474_pk-and-sequences/up.sql
+++ moped-database/migrations/1650035696474_pk-and-sequences/up.sql
@@ -1,3 +1,4 @@
 ALTER TABLE moped_city_fiscal_years ADD COLUMN id integer;
 ALTER TABLE moped_city_fiscal_years DROP CONSTRAINT moped_city_fiscal_years_pkey;
 CREATE SEQUENCE city_fiscal_years_id_seq AS INT INCREMENT BY 1 START WITH 7 OWNED BY moped_city_fiscal_years.id;
+UPDATE moped_city_fiscal_years set id = fiscal_year_value::integer - 2015;
