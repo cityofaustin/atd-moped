cc86db5f188f3546f729175c1eb796df99f3f74b
Frank Hereford <frankhereford@users.noreply.github.com>
2022-04-15 14:48:07 -0500

    Make these updates "safe" to run without modification in my SQL client for dev

--- moped-database/migrations/1650035696474_pk-and-sequences/up.sql
+++ moped-database/migrations/1650035696474_pk-and-sequences/up.sql
@@ -1,6 +1,6 @@
 ALTER TABLE moped_city_fiscal_years ADD COLUMN id integer;
 ALTER TABLE moped_city_fiscal_years DROP CONSTRAINT moped_city_fiscal_years_pkey;
-UPDATE moped_city_fiscal_years set id = fiscal_year_value::integer - 2015;
+UPDATE moped_city_fiscal_years set id = fiscal_year_value::integer - 2015 where 1 = 1;
 ALTER TABLE moped_city_fiscal_years ALTER COLUMN id SET NOT NULL;
 CREATE SEQUENCE city_fiscal_years_id_seq AS INT INCREMENT BY 1 START WITH 7 OWNED BY moped_city_fiscal_years.id;
 ALTER TABLE moped_city_fiscal_years ALTER COLUMN id SET default nextval('city_fiscal_years_id_seq');
