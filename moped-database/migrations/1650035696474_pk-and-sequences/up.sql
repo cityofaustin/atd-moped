d9f87dc0df5364fb53bf96d8c10c79b4f1af48ae
Frank Hereford <frankhereford@users.noreply.github.com>
2022-04-15 15:17:04 -0500

    make a note of testing syntax

--- moped-database/migrations/1650035696474_pk-and-sequences/up.sql
+++ moped-database/migrations/1650035696474_pk-and-sequences/up.sql
@@ -5,3 +5,6 @@ ALTER TABLE moped_city_fiscal_years ALTER COLUMN id SET NOT NULL;
 CREATE SEQUENCE city_fiscal_years_id_seq AS INT INCREMENT BY 1 START WITH 7 OWNED BY moped_city_fiscal_years.id;
 ALTER TABLE moped_city_fiscal_years ALTER COLUMN id SET default nextval('city_fiscal_years_id_seq');
 ALTER TABLE moped_city_fiscal_years ADD CONSTRAINT moped_city_fiscal_years_pkey PRIMARY KEY (id);
+
+-- ✅ This is the form of a working, tested INSERT
+-- insert into moped_city_fiscal_years (fiscal_year_value, fiscal_year_start_date, fiscal_year_end_date, active_fy) values ('2038', null, null, true);
