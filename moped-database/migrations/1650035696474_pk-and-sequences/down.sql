de067354339cbb31fe13f307dc81338fb1c51cb8
Frank Hereford <frankhereford@users.noreply.github.com>
2022-04-15 10:58:39 -0500

    take up linefeeds

--- moped-database/migrations/1650035696474_pk-and-sequences/down.sql
+++ moped-database/migrations/1650035696474_pk-and-sequences/down.sql
@@ -1,4 +1,2 @@
 ALTER TABLE moped_city_fiscal_years DROP COLUMN id;
-ALTER TABLE moped_city_fiscal_years
-  ADD CONSTRAINT moped_city_fiscal_years_pkey 
-    PRIMARY KEY (fiscal_year_value);
+ALTER TABLE moped_city_fiscal_years ADD CONSTRAINT moped_city_fiscal_years_pkey PRIMARY KEY (fiscal_year_value);
