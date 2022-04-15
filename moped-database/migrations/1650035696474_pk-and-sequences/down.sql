f4aa4da53b415924e89d7382013516d5f22ce175
Frank Hereford <frankhereford@users.noreply.github.com>
2022-04-15 11:07:12 -0500

    old habit from using mysql

--- moped-database/migrations/1650035696474_pk-and-sequences/down.sql
+++ moped-database/migrations/1650035696474_pk-and-sequences/down.sql
@@ -1,4 +1,4 @@
-UPDATE moped_city_fiscal_years set id is null;
+UPDATE moped_city_fiscal_years set id = null;
 DROP SEQUENCE city_fiscal_years_id_seq;
 ALTER TABLE moped_city_fiscal_years ADD CONSTRAINT moped_city_fiscal_years_pkey PRIMARY KEY (fiscal_year_value);
 ALTER TABLE moped_city_fiscal_years DROP COLUMN id;
