e6d6157d0aba29d775d8741b61375152dbb13707
Frank Hereford <frankhereford@users.noreply.github.com>
2022-04-15 11:02:52 -0500

    Create PK sequence

--- moped-database/migrations/1650035696474_pk-and-sequences/down.sql
+++ moped-database/migrations/1650035696474_pk-and-sequences/down.sql
@@ -1,2 +1,3 @@
+DROP SEQUENCE city_fiscal_years_id_seq;
 ALTER TABLE moped_city_fiscal_years ADD CONSTRAINT moped_city_fiscal_years_pkey PRIMARY KEY (fiscal_year_value);
 ALTER TABLE moped_city_fiscal_years DROP COLUMN id;
--- moped-database/migrations/1650035696474_pk-and-sequences/up.sql
+++ moped-database/migrations/1650035696474_pk-and-sequences/up.sql
@@ -1,2 +1,3 @@
 ALTER TABLE moped_city_fiscal_years ADD COLUMN id integer;
 ALTER TABLE moped_city_fiscal_years DROP CONSTRAINT moped_city_fiscal_years_pkey;
+CREATE SEQUENCE city_fiscal_years_id_seq AS INT INCREMENT BY 1 START WITH 7 OWNED BY moped_city_fiscal_years.id;
