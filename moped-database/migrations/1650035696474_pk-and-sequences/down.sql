7aff79d4073048d8ee804c642ee44abd89678320
Frank Hereford <frankhereford@users.noreply.github.com>
2022-04-15 14:33:39 -0500

    add primary key constraint

--- moped-database/migrations/1650035696474_pk-and-sequences/down.sql
+++ moped-database/migrations/1650035696474_pk-and-sequences/down.sql
@@ -1,9 +1,7 @@
---ALTER TABLE moped_city_fiscal_years DROP CONSTRAINT moped_city_fiscal_years_pkey;
-
-
-
+ALTER TABLE moped_city_fiscal_years DROP CONSTRAINT moped_city_fiscal_years_pkey;
+ALTER TABLE moped_city_fiscal_years ALTER COLUMN id SET default NULL; 
 DROP SEQUENCE city_fiscal_years_id_seq;
-ALTER TABLE moped_city_fiscal_years ALTER COLUMN id SET NULL;
+ALTER TABLE moped_city_fiscal_years ALTER COLUMN id SET default NULL; -- this is a non-op, but included to make a 1:1 alignment to the up.sql file
 UPDATE moped_city_fiscal_years set id = null;
 ALTER TABLE moped_city_fiscal_years ADD CONSTRAINT moped_city_fiscal_years_pkey PRIMARY KEY (fiscal_year_value);
 ALTER TABLE moped_city_fiscal_years DROP COLUMN id;
--- moped-database/migrations/1650035696474_pk-and-sequences/up.sql
+++ moped-database/migrations/1650035696474_pk-and-sequences/up.sql
@@ -4,6 +4,4 @@ UPDATE moped_city_fiscal_years set id = fiscal_year_value::integer - 2015;
 ALTER TABLE moped_city_fiscal_years ALTER COLUMN id SET NOT NULL;
 CREATE SEQUENCE city_fiscal_years_id_seq AS INT INCREMENT BY 1 START WITH 7 OWNED BY moped_city_fiscal_years.id;
 ALTER TABLE moped_city_fiscal_years ALTER COLUMN id SET default nextval('city_fiscal_years_id_seq');
-
-
---ALTER TABLE moped_city_fiscal_years ADD CONSTRAINT moped_city_fiscal_years_pkey PRIMARY KEY (id);
+ALTER TABLE moped_city_fiscal_years ADD CONSTRAINT moped_city_fiscal_years_pkey PRIMARY KEY (id);
