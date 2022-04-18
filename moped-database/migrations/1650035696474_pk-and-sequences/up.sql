b1cd0cb05bbd4adbb2afce0fe378de8dd657eff1
Frank Hereford <frankhereford@users.noreply.github.com>
2022-04-18 11:24:33 -0500

    add comment with working insert

--- moped-database/migrations/1650035696474_pk-and-sequences/up.sql
+++ moped-database/migrations/1650035696474_pk-and-sequences/up.sql
@@ -13,3 +13,6 @@ ALTER TABLE moped_phases DROP CONSTRAINT moped_phases_phase_id_key;
 DROP SEQUENCE IF EXISTS moped_phases_phase_id_seq; -- There is an existing, unused sequence. Just drop it and recreate.
 CREATE SEQUENCE moped_phases_phase_id_seq AS INT INCREMENT BY 1 START WITH 13 OWNED BY moped_phases.phase_id;
 ALTER TABLE moped_phases ALTER COLUMN phase_id SET default nextval('moped_phases_phase_id_seq');
+
+-- ✅ This is the form of a working, tested INSERT
+-- insert into moped_phases (phase_name, phase_description, phase_order, phase_average_length, required_phase)  values ('a name', 'a description', 0, 1, true);
