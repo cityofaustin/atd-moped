b9e7d43a73d5b8e54abd024c65eefc70e2a405d9
Chia Berry <analuciaby@gmail.com>
2022-04-26 16:50:22 -0500

    its one to many not one to one

--- moped-database/migrations/1651001224000_alter_project_list_view_funding/up.sql
+++ moped-database/migrations/1651001224000_alter_project_list_view_funding/up.sql
@@ -11,6 +11,15 @@ with moped_team_members as (
         LEFT JOIN public.moped_project_roles mpr ON ((mpp.role_id = mpr.project_role_id))
       where (mpp.status_id = 1)
       group by mpp.project_id
+),
+ moped_funding_sources as (
+  select
+    mpf.project_id,
+    string_agg(mfs.funding_source_name, ', ') as funding_source_name
+  from
+    moped_proj_funding mpf
+    left join moped_fund_sources mfs on mpf.funding_source_id = mfs.funding_source_id
+    group by mpf.project_id
 )
   SELECT 
     mp.project_uuid,
@@ -49,17 +58,16 @@ with moped_team_members as (
     mt.type_name,
     mfs.funding_source_name
    FROM public.moped_project mp
-   left join moped_team_members mtm on (mp.project_id = mtm.project_id)
-     LEFT JOIN public.moped_entity me ON ((me.entity_id = mp.project_sponsor))
-     LEFT JOIN public.moped_proj_partners mpp2 ON (((mp.project_id = mpp2.project_id) AND (mpp2.status_id = 1)))
-     LEFT JOIN public.moped_entity me2 ON ((mpp2.entity_id = me2.entity_id))
+   left join moped_team_members mtm on mp.project_id = mtm.project_id
+      left join moped_funding_sources mfs on mfs.project_id = mp.project_id
+     LEFT JOIN public.moped_entity me ON me.entity_id = mp.project_sponsor
+     LEFT JOIN public.moped_proj_partners mpp2 ON (mp.project_id = mpp2.project_id AND mpp2.status_id = 1)
+     LEFT JOIN public.moped_entity me2 ON mpp2.entity_id = me2.entity_id
      left join jsonb_array_elements(mp.task_order) as task_order_filter on true
      left join moped_proj_components mpc on mpc.project_id = mp.project_id and mpc.status_id = 1
      left join moped_proj_features mpf on mpc.project_component_id = mpf.project_component_id
      left join public.moped_project_types mpt on mpt.project_id = mp.project_id
      left join public.moped_types mt on mpt.project_type_id = mt.type_id
-     left join moped_proj_funding mpfd on mp.project_id = mpfd.project_id
-     left join moped_fund_sources mfs on mpfd.funding_source_id = mfs.funding_source_id
   GROUP BY mp.project_uuid,
     mp.project_id,
     mp.project_name,
