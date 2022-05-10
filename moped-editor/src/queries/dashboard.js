dc653bc11a0792fb58b74eb01165c11c70db0dec
Tilly Whitson <35410637+tillyw@users.noreply.github.com>
2022-05-10 17:04:16 -0500

    add query for project personnel table

--- moped-editor/src/queries/dashboard.js
+++ moped-editor/src/queries/dashboard.js
@@ -21,3 +21,24 @@ export const USER_FOLLOWED_PROJECTS_QUERY = gql`
     }
   }
 `;
+
+export const USER_PERSONNEL_PROJECTS_QUERY = gql`
+  query UserPersonnelProjectsQuery($userId: Int) {
+    moped_proj_personnel(where: {user_id: {_eq: 1}}) {
+        project_id
+        user_id
+        project {
+          project_id
+          project_name
+          current_phase
+          current_status
+          moped_proj_notes(where: {project_note_type: {_eq: 2}}, order_by: {date_created: desc}) {
+            added_by
+            project_note_type
+            status_id
+            project_note
+          }
+        }
+      }
+  }
+`;
