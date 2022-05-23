274747ddae605f44397915e7d3c1e1a2f60cbafa
Chia Berry <analuciaby@gmail.com>
2022-05-23 11:06:45 -0500

    expand on milestones migration

--- moped-database/migrations/1653067649000_alter_table_moped_proj_milestones/down.sql
+++ moped-database/migrations/1653067649000_alter_table_moped_proj_milestones/down.sql
@@ -1,2 +1,19 @@
+-- drop milestone_name and milestone_related_phase_id since those can now be accessed through fk relationship
+ALTER TABLE moped_proj_milestones ADD COLUMN milestone_name TEXT NOT NULL;
+ALTER TABLE moped_proj_milestones ADD COLUMN milestone_related_phase_id INT;
+
+
+-- get milestone_name based on milestone_id
+update moped_proj_milestones mpm
+set milestone_name = LOWER(mm.milestone_name)
+from moped_milestones mm
+where mpm.milestone_id = mm.milestone_id;
+
+-- get milestone_related_phase_id based on milestone_id
+update moped_proj_milestones mpm
+set milestone_related_phase_id = mm.related_phase_id
+from moped_milestones mm
+where mpm.milestone_id = mm.milestone_id;
+
 -- delete milestone_id column from moped_proj_milestones
-ALTER TABLE moped_proj_milestones DELETE COLUMN milestone_id integer;
+ALTER TABLE moped_proj_milestones DROP COLUMN milestone_id;
