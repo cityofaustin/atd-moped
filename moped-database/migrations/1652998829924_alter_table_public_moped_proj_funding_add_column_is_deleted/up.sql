0e46bd57ce373c1e86f94fc5e205926a7225f851
Mike Dilley <mike.dilley@gmail.com>
2022-05-23 17:39:41 -0500

    Add comments to new columns

--- moped-database/migrations/1652998829924_alter_table_public_moped_proj_funding_add_column_is_deleted/up.sql
+++ moped-database/migrations/1652998829924_alter_table_public_moped_proj_funding_add_column_is_deleted/up.sql
@@ -1,5 +1,7 @@
 ALTER TABLE "public"."moped_proj_funding" ADD COLUMN "is_deleted" boolean NOT NULL DEFAULT false;
 
+COMMENT ON COLUMN moped_proj_funding.is_deleted IS 'Indicates soft deletion';
+
 UPDATE moped_proj_funding
 SET is_deleted = TRUE
 WHERE funding_status_id = 0;
--- moped-database/migrations/1653078890676_alter_table_public_moped_proj_notes_add_column_is_deleted/up.sql
+++ moped-database/migrations/1653078890676_alter_table_public_moped_proj_notes_add_column_is_deleted/up.sql
@@ -1,5 +1,7 @@
 ALTER TABLE "public"."moped_proj_notes" ADD COLUMN "is_deleted" boolean NOT NULL DEFAULT false;
 
+COMMENT ON COLUMN moped_proj_notes.is_deleted IS 'Indicates soft deletion';
+
 UPDATE moped_proj_notes
 SET is_deleted = TRUE
 WHERE status_id = 0;
