c65bf987582d22294684c3ef86ac3a13e7a1d6fd
Chia Berry <analuciaby@gmail.com>
2022-05-24 10:01:48 -0500

    set not null constraint after running milestone_id update

--- moped-database/migrations/1653067649000_alter_table_moped_proj_milestones/up.sql
+++ moped-database/migrations/1653067649000_alter_table_moped_proj_milestones/up.sql
@@ -1,11 +1,13 @@
 -- add milestone_id column to moped_proj_milestones
-ALTER TABLE moped_proj_milestones ADD COLUMN milestone_id integer NOT null;
+ALTER TABLE moped_proj_milestones ADD COLUMN milestone_id integer;
 
 -- get milestone_id based on milestone_name
 update moped_proj_milestones mpm
 set milestone_id = mm.milestone_id
 from moped_milestones mm
 where mpm.milestone_name = LOWER(mm.milestone_name);
+
+ALTER TABLE moped_proj_milestones ALTER COLUMN milestone_id SET NOT NULL;
 
 -- set milestone_id as foreign key
 ALTER TABLE moped_proj_milestones
