1ff178ceb49338a7195dd6f03aab19403992e9b1
Mike Dilley <mike.dilley@gmail.com>
2022-05-25 12:15:23 -0500

    Add comments to up migrations

--- moped-database/migrations/1652998829924_alter_table_public_moped_proj_funding_add_column_is_deleted/up.sql
+++ moped-database/migrations/1652998829924_alter_table_public_moped_proj_funding_add_column_is_deleted/up.sql
@@ -2,6 +2,7 @@ ALTER TABLE "public"."moped_proj_funding" ADD COLUMN "is_deleted" boolean NOT NU
 
 COMMENT ON COLUMN moped_proj_funding.is_deleted IS 'Indicates soft deletion';
 
+-- Update new is_deleted columns to preserve existing soft deletes that used status_id = 0
 UPDATE moped_proj_funding
 SET is_deleted = TRUE
 WHERE funding_status_id = 0;
