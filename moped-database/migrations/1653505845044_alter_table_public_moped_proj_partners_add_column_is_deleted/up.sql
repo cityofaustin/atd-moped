70c63d332eff7e42eeb987d78da90f6c25bc48c6
Mike Dilley <mike.dilley@gmail.com>
2022-05-25 14:43:13 -0500

    Add is_deleted column to partners and types, update permissions

---
+++ moped-database/migrations/1653505845044_alter_table_public_moped_proj_partners_add_column_is_deleted/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."moped_proj_partners" ADD COLUMN "is_deleted" boolean NOT NULL DEFAULT false;
---
+++ moped-database/migrations/1653505865231_alter_table_public_moped_project_types_add_column_is_deleted/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."moped_project_types" ADD COLUMN "is_deleted" boolean NOT NULL DEFAULT false;
