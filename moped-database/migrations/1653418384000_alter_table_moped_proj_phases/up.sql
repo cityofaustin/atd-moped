823c7f6cee69dd4ad05111f1cacb5684d201598a
Chia Berry <analuciaby@gmail.com>
2022-05-25 12:46:25 -0500

    also set subphase_id based on subphase_name

--- moped-database/migrations/1653418384000_alter_table_moped_proj_phases/up.sql
+++ moped-database/migrations/1653418384000_alter_table_moped_proj_phases/up.sql
@@ -4,3 +4,8 @@ set phase_id = phases.phase_id
 from moped_phases phases
 where mpp.phase_name = LOWER(phases.phase_name);
 
+-- set subphase_id based on subphase_name
+update moped_proj_phases mpp
+set subphase_id = subphases.subphase_id
+from moped_subphases subphases
+where mpp.subphase_name = LOWER(subphases.subphase_name)
