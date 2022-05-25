03a212142aa933a98f2a2b3b6252d31a11173d2f
Mike Dilley <mike.dilley@gmail.com>
2022-05-25 14:58:41 -0500

    Update queries and mutations for project partners

--- moped-editor/src/queries/project.js
+++ moped-editor/src/queries/project.js
@@ -72,7 +72,7 @@ export const SUMMARY_QUERY = gql`
       }
     }
     moped_proj_partners(
-      where: { project_id: { _eq: $projectId }, status_id: { _eq: 1 } }
+      where: { project_id: { _eq: $projectId }, is_deleted: { _eq: false } }
     ) {
       proj_partner_id
       project_id
@@ -115,7 +115,7 @@ export const SUMMARY_QUERY = gql`
       status_name
     }
     moped_user_followed_projects(
-      where: { project_id: { _eq: $projectId }, user_id: { _eq: $userId} } 
+      where: { project_id: { _eq: $projectId }, user_id: { _eq: $userId } }
     ) {
       project_id
       user_id
@@ -420,7 +420,9 @@ export const PROJECT_FOLLOW = gql`
 
 export const PROJECT_UNFOLLOW = gql`
   mutation UnfollowProject($project_id: Int!, $user_id: Int!) {
-    delete_moped_user_followed_projects(where: { project_id: { _eq: $project_id }, user_id: { _eq: $user_id } }) {
+    delete_moped_user_followed_projects(
+      where: { project_id: { _eq: $project_id }, user_id: { _eq: $user_id } }
+    ) {
       affected_rows
     }
   }
@@ -715,7 +717,7 @@ export const PROJECT_UPDATE_PARTNERS = gql`
     }
     update_moped_proj_partners(
       where: { proj_partner_id: { _in: $deleteList } }
-      _set: { status_id: 0 }
+      _set: { is_deleted: true }
     ) {
       affected_rows
     }
