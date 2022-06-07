bd66add24ef32ba20800545e2ef7193c25d73172
Chia Berry <analuciaby@gmail.com>
2022-06-07 13:46:15 -0500

    import popper

--- moped-editor/src/views/dashboard/DashboardEditModal.js
+++ moped-editor/src/views/dashboard/DashboardEditModal.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   Typography,
   Popper,
+  Paper
 } from "@material-ui/core";
 
 
 const DashboardEditModal = ({displayText}) => {
+  const divRef = React.useRef();
+
+  // anchor element for dashboard edit modal to "attach" to
+  const [dashboardEditAnchor, setDashboardEditAnchor] = useState(null);
+
+  const handleDashboardEditClose = () => {
+    setDashboardEditAnchor(null);
+  };
 
   console.log(displayText)
   return (
-    <Typography>{displayText}</Typography>
+    <>
+    <Typography onClick={()=>setDashboardEditAnchor(divRef.current)}>
+      {displayText} HIHIHI
+    </Typography>
+    <Popper
+        id="DashboardEditModal"
+        open={Boolean(dashboardEditAnchor)}
+        anchorEl={dashboardEditAnchor}
+        onClose={handleDashboardEditClose}
+        placement={"bottom"}
+        // className={classes.advancedSearchRoot}
+      >
+        <Paper> {//className={classes.advancedSearchPaper}>
+      }
+          "EDIT"
+        </Paper>
+      </Popper>
+    </>
   );
 }
 
