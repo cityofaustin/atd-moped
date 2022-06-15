7da4a5e2456b6a9786e41a16f7a0e51dfe9c1469
John Clary <john.clary@austintexas.gov>
2022-06-14 22:20:21 -0400

    lower cases

--- moped-toolbox/amd_milestones_backfill/secrets.py
+++ moped-toolbox/amd_milestones_backfill/secrets.py
@@ -1,10 +1,10 @@
 HASURA = {
-    "HASURA_GRAPHQL_ENDPOINT": {
+    "hasura_graphql_endpoint": {
         "local": "http://localhost:8080/v1/graphql",
         "staging": "<redacted>",
         "prod": "<redacted>",
     },
-    "HASURA_GRAPHQL_ADMIN_SECRET": {
+    "hasura_graphql_admin_secret": {
         "local": "DuMmyApiKeyHFVOVto19otC1wX6sP2N0VSKrCD70L10B7Sm525ZR6L672i2F79M9!",
         "staging": "<redacted>",
         "prod": "<redacted>",
