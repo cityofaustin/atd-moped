aeabc5c4fbe122c91592b11c5c4f70a65e4d30b3
Chia Berry <analuciaby@gmail.com>
2022-06-17 14:32:32 -0500

    add down migration

--- moped-database/migrations/1655407355843_alter_table_public_moped_project_add_column_parent_project_id/down.sql
+++ moped-database/migrations/1655407355843_alter_table_public_moped_project_add_column_parent_project_id/down.sql
@@ -1,7 +1,1 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- alter table "public"."moped_project" add column "parent_project_id" integer
---  null;
-
-
-alter table "public"."moped_project" drop constraint "moped_project_parent_project_id_fkey";
+ALTER TABLE "public"."moped_project" DROP COLUMN "parent_project_id";
