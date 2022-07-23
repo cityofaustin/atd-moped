260986332c8c4ce5f1a7c4469316df307113224b
Mike <mike.dilley@gmail.com>
2022-07-23 12:41:02 -0500

    Add more notes

--- moped-etl/prefect/tasks/activity_log.py
+++ moped-etl/prefect/tasks/activity_log.py
@@ -1,4 +1,5 @@
 import os
+import boto3
 
 import prefect
 from prefect import task
@@ -7,12 +8,31 @@ from prefect import task
 logger = prefect.context.get("logger")
 
 # SQS deployment needs:
-sqs_config = {
-    "Description": "AWS Moped Data Event: %FUNCTION_NAME% - TEST",
+# Comes from deploy_event_function
+# FUNCTION_NAME = "atd-moped-events-"
+# Comes from
+# activity_log
+
+# ShellScript command
+# Use helper functions individually for tasks
+# source $(pwd)/.github/workflows/aws-moped-sqs-helper.sh
+
+# Call:
+# 1. install_requirements
+# 2. bundle_function
+# 3. generate_env_vars - rework this?
+# 4. deploy_lambda_function
+# 5. deploy_sqs which calls deploy_event_source_mapping
+
+# Return SQS endpoint
+
+# This is the config and env vars for the Lambda (atd-moped-events-activity_log_test)
+lambda_config = {
+    "Description": "AWS Moped Data Event: atd-moped-events-activity_log_moped-test - TEST",
     "Environment": {
         "Variables": {
-            "HASURA_ENDPOINT": "https://atd-moped-editor-development.herokuapp.com/v1/graphql",
-            "HASURA_ADMIN_SECRET": "n@kUHnB6OG6Nu1469uqob6v45jnVrWP4G9LCflf427y2w4HzLOwoOv7dVybUU6!d",
+            "HASURA_ENDPOINT": "",
+            "HASURA_ADMIN_SECRET": "",
             "API_ENVIRONMENT": "TEST",
             "COGNITO_DYNAMO_TABLE_NAME": "atd-moped-users-staging",
         }
@@ -32,6 +52,9 @@ def create_activity_log_lambda():
     # Use boto3 to create activity log event lambda
     logger.info("creating activity log Lambda")
     return True
+
+
+# Remove SQS and Lambda with boto3
 
 
 @task
