8e37563864911eb5eaa0d79d3a4b0a2fa8b72f6c
Frank Hereford <frankhereford@users.noreply.github.com>
2022-07-24 08:31:29 -0500

    Add netlify build hook to env variables

--- moped-etl/prefect/moped_test_create_flow.py
+++ moped-etl/prefect/moped_test_create_flow.py
@@ -238,16 +238,16 @@ if __name__ == "__main__":
 
     # flow execution is serialized!
 
-    print("\n🍄 Decomissioning Database\n")
-    database_decommission.run(basename=database)
-    print("\n🍄 Comissioning Database\n")
-    database_commission.run(basename=database)
+    #print("\n🍄 Decomissioning Database\n")
+    #database_decommission.run(basename=database)
+    #print("\n🍄 Comissioning Database\n")
+    #database_commission.run(basename=database)
 
-    print("\n🤖 Decomissioning ECS\n")
-    ecs_decommission.run(parameters=dict(basename=basename))
-    time.sleep(5)
-    print("\n🤖 Comissioning ECS\n")
-    ecs_commission.run(parameters=dict(basename=basename, database=database))
+    #print("\n🤖 Decomissioning ECS\n")
+    #ecs_decommission.run(parameters=dict(basename=basename))
+    #time.sleep(5) # this isn't needed, just for debugging
+    #print("\n🤖 Comissioning ECS\n")
+    #ecs_commission.run(parameters=dict(basename=basename, database=database))
 
     # ecs_decommission.register(project_name="Moped")
     # ecs_commission.register(project_name="Moped")
