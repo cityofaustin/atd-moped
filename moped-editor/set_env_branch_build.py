a0e0a3522fc3b9635766e60856f2df7f595c1ab6
Frank Hereford <frankhereford@users.noreply.github.com>
2022-07-24 14:23:40 -0500

    Add back old default behavior

--- moped-editor/set_env_branch_build.py
+++ moped-editor/set_env_branch_build.py
@@ -3,11 +3,16 @@
 import json
 import os
 
-payload = os.environ.get('INCOMING_HOOK_BODY')
-decoded_vars = json.loads(payload)
+cmd = ""
+if os.environ.has_key("INCOMING_HOOK_BODY"):
+    payload = os.environ.get("INCOMING_HOOK_BODY")
+    decoded_vars = json.loads(payload)
+    environment = " ".join(
+        ["{}={}".format(key, value) for key, value in decoded_vars.items()]
+    )
+    cmd = environment + " npm run build:local"
+else:
+    cmd = "env-cmd -e netlifypr npm run build:local"
 
-environment = " ".join(['{}={}'.format(key, value) for key, value in decoded_vars.items()])
-
-cmd = environment + " npm run build:local"
 result = os.system(cmd)
 print(result)
