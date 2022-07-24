1529f4e4b9e6122d519263544a8a9e7d4b98ea7c
Frank Hereford <frankhereford@users.noreply.github.com>
2022-07-24 16:14:51 -0500

    simply variable overload program

--- moped-editor/set_env_branch_build.py
+++ moped-editor/set_env_branch_build.py
@@ -5,18 +5,14 @@ import os
 
 cmd = ""
 
-if os.environ.get("INCOMING_HOOK_BODY"):
-    print("Using payload defined environment variables")
-    payload = os.environ.get("INCOMING_HOOK_BODY")
-    decoded_vars = json.loads(payload)
-    environment = " ".join(
-        ["{}={}".format(key, value) for key, value in decoded_vars.items()]
-    )
-    cmd = environment + " env-cmd --no-override -e netlifypr npm run build:local"
-else:
-    print("Using env-cmd defined environment variables")
-    cmd = "env-cmd -e netlifypr npm run build:local"
+print("Using payload defined environment variables to overload 'netlifypr' from .env_cmdrc")
+payload = os.environ.get("INCOMING_HOOK_BODY")
+decoded_vars = json.loads(payload)
+environment = " ".join(
+    ["{}={}".format(key, value) for key, value in decoded_vars.items()]
+)
+cmd = environment + " env-cmd --no-override -e netlifypr npm run build:local"
 
-result = os.system(cmd)
-# TODO, this is returning the return code, not STDOUT -- hiding build logs in netlify
-print(result)
+result = os.popen(cmd)
+
+print(result.read())
