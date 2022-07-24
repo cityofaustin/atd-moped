c52b10e0e94c38da17e63021d4663d64fb089e3b
Frank Hereford <frankhereford@users.noreply.github.com>
2022-07-24 14:03:42 -0500

    Set env vars from branch build hook payload

--- moped-editor/set_env_branch_build.py
+++ moped-editor/set_env_branch_build.py
@@ -3,8 +3,17 @@
 import json
 import os
 
+os.environ["INCOMING_HOOK_BODY"] = json.dumps({
+        "FOO": "bar",
+        "FIZZ": "buzz",
+})
+
 payload = os.environ.get('INCOMING_HOOK_BODY')
 decoded_vars = json.loads(payload)
 
-for var in decoded_vars:
-        print(var + ': ' + decoded_vars[var])
+environment = " ".join(['{}={}'.format(key, value) for key, value in decoded_vars.items()])
+print(environment)
+
+cmd = environment + " npm run build:local"
+os.system(cmd)
+
