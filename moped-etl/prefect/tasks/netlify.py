470f39c80d96bcda9c904cc6aeb90e1d24f3f8aa
Frank Hereford <frankhereford@users.noreply.github.com>
2022-07-24 08:48:31 -0500

    pretty-print helper

--- moped-etl/prefect/tasks/netlify.py
+++ moped-etl/prefect/tasks/netlify.py
@@ -1,7 +1,15 @@
 import os
+import prefect
+import requests
 
 # set up the prefect logging system
 logger = prefect.context.get("logger")
 
-# AWS ARN-like identifiers
 NETLIFY_BUILD_HOOK = os.environ["NETLIFY_BUILD_HOOK"]
+
+
+def pprint(string):
+    print("")
+    pp = pretty_printer.PrettyPrinter(indent=2)
+    pp.pprint(string)
+    print("")
