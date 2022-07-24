0a64e10fb7eb7f5686f00b9c9f4b78a557764540
Frank Hereford <frankhereford@users.noreply.github.com>
2022-07-24 09:06:48 -0500

    Add task to trigger netlify deployment

--- moped-etl/prefect/tasks/netlify.py
+++ moped-etl/prefect/tasks/netlify.py
@@ -1,5 +1,6 @@
 import os
 import prefect
+from prefect import Flow, task
 import requests
 
 # set up the prefect logging system
@@ -13,3 +14,21 @@ def pprint(string):
     pp = pretty_printer.PrettyPrinter(indent=2)
     pp.pprint(string)
     print("")
+
+
+@task
+def trigger_netlify_build():
+    logger.info("Triggering netlify build")
+
+    HTTP_parameters = {
+        "trigger_branch": "netlify-test-deployment",
+        "trigger_title": "Dev Test Build",
+    }
+
+    request = requests.request("POST", NETLIFY_BUILD_HOOK, params=HTTP_parameters)
+
+    pprint(request)
+
+    # r = requests.post(NETLIFY_BUILD_HOOK)
+    # logger.info("netlify build triggered")
+    # return r.status_code
