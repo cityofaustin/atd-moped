6cb42c270826c8ef9abcd1f6a6dae85f78a613b1
Frank Hereford <frankhereford@users.noreply.github.com>
2022-07-24 14:12:21 -0500

    Remove debugging print to protect secrets

--- moped-editor/set_env_branch_build.py
+++ moped-editor/set_env_branch_build.py
@@ -3,17 +3,11 @@
 import json
 import os
 
-#os.environ["INCOMING_HOOK_BODY"] = json.dumps({
-        #"FOO": "bar",
-        #"FIZZ": "buzz",
-#})
-
 payload = os.environ.get('INCOMING_HOOK_BODY')
 decoded_vars = json.loads(payload)
 
 environment = " ".join(['{}={}'.format(key, value) for key, value in decoded_vars.items()])
-print(environment)
 
 cmd = environment + " npm run build:local"
-os.system(cmd)
-
+result = os.system(cmd)
+print(result)
