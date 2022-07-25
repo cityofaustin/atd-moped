c6c88d15465a0cefbb5ed3f86ac09692f281c1bc
Mike <mike.dilley@gmail.com>
2022-07-25 13:30:34 -0500

    Fix comments

--- moped-etl/prefect/moped_test_create_flow.py
+++ moped-etl/prefect/moped_test_create_flow.py
@@ -240,7 +240,7 @@ with Flow(
 if __name__ == "__main__":
     print("main()")
 
-    basename = "md-test-again"
+    basename = "flh-parameter-test"
     database = basename.replace("-", "_")
     database_data_stage = "staging"
 
@@ -248,14 +248,14 @@ if __name__ == "__main__":
 
     print("\n🍄 Decomissioning Database\n")
     database_decommission.run(basename=database)
-    # print("\n🍄 Comissioning Database\n")
-    # database_commission.run(basename=database, stage=database_data_stage)
+    print("\n🍄 Comissioning Database\n")
+    database_commission.run(basename=database, stage=database_data_stage)
 
-    # print("\n🤖 Decomissioning ECS\n")
-    # ecs_decommission.run(parameters=dict(basename=basename))
-    # time.sleep(5)
-    # print("\n🤖 Comissioning ECS\n")
-    # ecs_commission.run(parameters=dict(basename=basename, database=database))
+    print("\n🤖 Decomissioning ECS\n")
+    ecs_decommission.run(parameters=dict(basename=basename))
+    time.sleep(5)
+    print("\n🤖 Comissioning ECS\n")
+    ecs_commission.run(parameters=dict(basename=basename, database=database))
 
     # ecs_decommission.register(project_name="Moped")
     # ecs_commission.register(project_name="Moped")
