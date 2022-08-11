eabd977f497e3bc0d5b175beeb06b05b4d144d81
Tilly Whitson <35410637+tillyw@users.noreply.github.com>
2022-08-11 17:49:50 -0500

    adds new milestones

---
+++ moped-database/migrations/1660258008607_insert_moped_milestones/down.sql
@@ -0,0 +1,4 @@
+-- Delete added moped_milestone records
+DELETE FROM public.moped_milestones WHERE milestone_name = 'Draft Study Submitted';
+DELETE FROM public.moped_milestones WHERE milestone_name = 'Draft Preliminary Schematic Submitted';
+DELETE FROM public.moped_milestones WHERE milestone_name = 'Need to Assign Study';
