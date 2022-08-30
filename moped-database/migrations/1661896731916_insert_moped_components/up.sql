822beb97e447e322933b0d46dbd0a6763a79cfee
Tilly Whitson <35410637+tillyw@users.noreply.github.com>
2022-08-30 17:24:36 -0500

    Inserts new moped components

---
+++ moped-database/migrations/1661896731916_insert_moped_components/up.sql
@@ -0,0 +1,9 @@
+-- Insert new moped_components
+INSERT INTO public.moped_components (
+        component_name,
+        status_id,
+        component_subtype,
+        line_representation
+    ) VALUES
+        ('Signage', 1, 'RRFB', false),
+        ('Intersection', 1, 'Roundabout', false);
