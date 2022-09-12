d9cb4a0a5a3231b6ce24f0e7c02c5335ebe85231
John Clary <john.clary@austintexas.gov>
2022-09-12 16:56:49 -0400

    update and add new milestones

---
+++ moped-database/migrations/1663016156653_pdd_new_milestones/down.sql
@@ -0,0 +1,19 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- -- Add "Construction Bid Published" with phase "Bid/Award/Execution"
+-- INSERT INTO moped_milestones (milestone_name, related_phase_id) VALUES ('Construction Bid Published', 12);
+--
+-- -- Add "Construction Bid Due" with phase "Bid/Award/Execution"
+-- INSERT INTO moped_milestones (milestone_name, related_phase_id) VALUES ('Construction Bid Due', 12);
+--
+-- -- Add "Design RFQ Published" with no phase
+-- INSERT INTO moped_milestones (milestone_name) VALUES ('Design RFQ Published');
+--
+-- -- Add "Design RFQ Due" with no phase
+-- INSERT INTO moped_milestones (milestone_name) VALUES ('Design RFQ Due');
+--
+-- -- Add "Design Contract Execution" with no phase
+-- INSERT INTO moped_milestones (milestone_name) VALUES ('Design Contract Execution');
+--
+-- -- Add "Inter-agency Agreement Council Award" with phase "Design"
+-- INSERT INTO moped_milestones (milestone_name, related_phase_id) VALUES ('Inter-agency Agreement', 6);
