87d0b05850bd70a0b535c84b199d8af679f675e7
Frank Hereford <frankhereford@users.noreply.github.com>
2022-09-14 09:06:17 -0500

    shell args

--- moped-toolbox/populate_geometry_transform/reload_db.sh
+++ moped-toolbox/populate_geometry_transform/reload_db.sh
@@ -5,5 +5,5 @@ echo "drop database moped;" | docker exec -i moped-database_moped-pgsql_1 psql -
 echo "create database moped;" | docker exec -i moped-database_moped-pgsql_1 psql -U moped postgres
 echo "create extension postgis;" | docker exec -i moped-database_moped-pgsql_1 psql -U moped moped
 
-cat 2022_09_14_08_26.moped.sql | docker exec -i moped-database_moped-pgsql_1 psql -U moped moped
+cat $1 | docker exec -i moped-database_moped-pgsql_1 psql -U moped moped
 docker-compose -f ../../moped-database/docker-compose.yml -f ../../moped-database/docker-compose.arm64.yml stop hasura
