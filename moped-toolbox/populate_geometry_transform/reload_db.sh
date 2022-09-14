f368d7a498ccf948f2308f831cfc79e043b7d6d5
Frank Hereford <frankhereford@users.noreply.github.com>
2022-09-14 09:04:43 -0500

    correctly locate docker-compose configuration

--- moped-toolbox/populate_geometry_transform/reload_db.sh
+++ moped-toolbox/populate_geometry_transform/reload_db.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-docker-compose -f ./docker-compose.yml -f docker-compose.arm64.yml stop hasura
+docker-compose -f ../../moped-database/docker-compose.yml -f ../../moped-database/docker-compose.arm64.yml stop hasura
 echo "drop database moped;" | docker exec -i moped-database_moped-pgsql_1 psql -U moped postgres
 echo "create database moped;" | docker exec -i moped-database_moped-pgsql_1 psql -U moped postgres
 echo "create extension postgis;" | docker exec -i moped-database_moped-pgsql_1 psql -U moped moped
 
 cat 2022_09_14_08_26.moped.sql | docker exec -i moped-database_moped-pgsql_1 psql -U moped moped
-docker-compose -f ./docker-compose.yml -f docker-compose.arm64.yml stop hasura
+docker-compose -f ../../moped-database/docker-compose.yml -f ../../moped-database/docker-compose.arm64.yml stop hasura
