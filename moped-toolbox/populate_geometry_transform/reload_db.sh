195a50229fbbbb82e9d64564f25b8cdbeaedaaaf
Frank Hereford <frankhereford@users.noreply.github.com>
2022-09-19 09:08:36 -0500

    make sure to restart graphql-engine service

--- moped-toolbox/populate_geometry_transform/reload_db.sh
+++ moped-toolbox/populate_geometry_transform/reload_db.sh
@@ -6,4 +6,4 @@ echo "create database moped;" | docker exec -i moped-database_moped-pgsql_1 psql
 echo "create extension postgis;" | docker exec -i moped-database_moped-pgsql_1 psql -U moped moped
 
 cat $1 | docker exec -i moped-database_moped-pgsql_1 psql -U moped moped
-docker-compose -f ../../moped-database/docker-compose.yml -f ../../moped-database/docker-compose.arm64.yml stop hasura
+docker-compose -f ../../moped-database/docker-compose.yml -f ../../moped-database/docker-compose.arm64.yml up -d hasura
