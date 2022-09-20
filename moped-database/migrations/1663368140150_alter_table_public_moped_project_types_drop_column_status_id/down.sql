ed2689e8a15c8c691fcc16a87be621d67e4f48c4
Mike <mike.dilley@gmail.com>
2022-09-20 12:26:37 -0500

    Fix down migrations

--- moped-database/migrations/1663368140150_alter_table_public_moped_project_types_drop_column_status_id/down.sql
+++ moped-database/migrations/1663368140150_alter_table_public_moped_project_types_drop_column_status_id/down.sql
@@ -1,4 +1,4 @@
-comment on column "public"."moped_project_types"."status_id" is E'List of related project types';
-alter table "public"."moped_project_types" alter column "status_id" set default 0;
-alter table "public"."moped_project_types" alter column "status_id" drop not null;
 alter table "public"."moped_project_types" add column "status_id" int4;
+alter table "public"."moped_project_types" alter column "status_id" drop not null;
+alter table "public"."moped_project_types" alter column "status_id" set default 0;
+comment on column "public"."moped_project_types"."status_id" is E'List of related project types';
