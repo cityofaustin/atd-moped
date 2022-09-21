9583f94f16d4c091df030f03493e83c6078b8738
Frank Hereford <frankhereford@users.noreply.github.com>
2022-09-20 22:16:29 -0500

    component archtypes

--- moped-toolbox/geometry_prototype/create_prototype.sql
+++ moped-toolbox/geometry_prototype/create_prototype.sql
@@ -7,3 +7,12 @@ create table projects (
 insert into projects (id, name) values (1, 'project_a');
 insert into projects (id, name) values (2, 'project_b');
 insert into projects (id, name) values (3, 'project_c');
+
+create type representation as enum ('point', 'line');
+
+create table component_types (
+    id serial primary key,
+    component_type character varying,
+    component_subtype character varying,
+    representation_type representation
+    );
