8af920abb5865784ebf959ca8e06b3e9cd422332
Mike <mike.dilley@gmail.com>
2022-09-22 12:13:37 -0500

    Add some more tests for staff form helpers

--- moped-editor/src/views/staff/test/helpers.test.js
+++ moped-editor/src/views/staff/test/helpers.test.js
@@ -23,3 +23,59 @@ describe("passwordLooksGood()", () => {
     expect(passwordLooksGood(validPassword)).toBe(true);
   });
 });
+
+describe("roleLooksGood()", () => {
+  it("returns true for no role or invalid role", () => {
+    const noRoleSelected = [];
+    const invalidRole = ["not-a-real-role"];
+
+    expect(roleLooksGood(noRoleSelected)).toBe(false);
+    expect(roleLooksGood(invalidRole)).toBe(false);
+  });
+
+  it("returns true for an valid role", () => {
+    const validRole = "moped-editor";
+
+    expect(roleLooksGood(validRole)).toBe(true);
+  });
+});
+
+describe("transformFormDataIntoDatabaseTypes()", () => {
+  const formData = {
+    workgroup_id: "1",
+    roles: "moped-editor",
+  };
+
+  it("transforms a workgroup ID string from the form to an integer the DB expects", () => {
+    const transformed = transformFormDataIntoDatabaseTypes(formData);
+    const { workgroup_id } = transformed;
+
+    expect(workgroup_id).toBe(1);
+    expect(workgroup_id).toEqual(expect.any(Number));
+  });
+
+  it("transforms a role string from the form to an array the DB expects", () => {
+    const transformed = transformFormDataIntoDatabaseTypes(formData);
+    const { roles } = transformed;
+
+    expect(roles.includes("moped-editor")).toBe(true);
+    expect(roles).toEqual(expect.any(Array));
+  });
+});
+
+describe("isUserNonLoginUser()", () => {
+  const mopedUserRolesArray = ["moped-editor"];
+  const nonLoginUserRolesArray = [nonLoginUserRole];
+
+  it("returns false if user has a role only assigned to Moped users that can login", () => {
+    const result = isUserNonLoginUser(mopedUserRolesArray);
+
+    expect(result).toBe(false);
+  });
+
+  it("returns true if user has non-login user role", () => {
+    const result = isUserNonLoginUser(nonLoginUserRolesArray);
+
+    expect(result).toBe(true);
+  });
+});
