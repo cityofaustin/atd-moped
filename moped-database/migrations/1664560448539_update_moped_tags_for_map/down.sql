17cb0bbdea39583484976ecfca83a34e396f3390
John Clary <john.clary@austintexas.gov>
2022-09-30 14:16:51 -0400

    fixup down migration

--- moped-database/migrations/1664560448539_update_moped_tags_for_map/down.sql
+++ moped-database/migrations/1664560448539_update_moped_tags_for_map/down.sql
@@ -1,19 +1,17 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- update moped_tags set is_deleted = true where slug = 'map_signals2023';
--- update moped_tags set is_deleted = true where slug = 'map_transitenhancement2023';
--- update moped_tags set is_deleted = true where slug = 'map_vzero_systemicsafety_curves2023';
--- update moped_tags set is_deleted = true where slug = 'map_vzero_intersectionspotential2023';
--- update moped_tags set is_deleted = true where slug = 'map_vzero_intersections2023';
--- update moped_tags set is_deleted = true where slug = 'map_vzero_speedmanagement2023';
--- insert into moped_tags (type, name, slug) values ('Work Plan','MAP Transit Speed and Reliability (CapMetro ILA)','map-transit-speed-and-reliability-capmetro-ila');
--- insert into moped_tags (type, name, slug) values ('Work Plan','MAP Transit Enhancement','map-transit-enhancement');
--- insert into moped_tags (type, name, slug) values ('Work Plan','MAP Vision Zero - Major Safety','map-vision-zero-major-safety');
--- insert into moped_tags (type, name, slug) values ('Work Plan','MAP Vision Zero - Ped Safety','map-vision-zero-ped-safety');
--- insert into moped_tags (type, name, slug) values ('Work Plan','MAP Vision Zero - Rapid Response/Small Scale','map-vision-zero-rapid-response-small-scale');
--- insert into moped_tags (type, name, slug) values ('Work Plan','MAP Vision Zero - Signal Safety','map-vision-zero-signal-safety');
--- insert into moped_tags (type, name, slug) values ('Work Plan','MAP Vision Zero - Speed Management','map-vision-zero-speed-management');
--- insert into moped_tags (type, name, slug) values ('Work Plan','MAP Vision Zero - Street Lighting','map-vision-zero-street-lighting');
--- insert into moped_tags (type, name, slug) values ('Work Plan','MAP Vision Zero - Systemic Safety','map-vision-zero-systemic-safety');
--- insert into moped_tags (type, name, slug) values ('Work Plan','MAP Vision Zero - Systemic Safety Access Management','map-vision-zero-systemic-safety-access-management');
--- insert into moped_tags (type, name, slug) values ('Work Plan','MAP Vision Zero - Systemic Safety Curve Warnings','map-vision-zero-systemic-safety-curve-warnings');
+update moped_tags set is_deleted = false where slug = 'map_signals2023';
+update moped_tags set is_deleted = false where slug = 'map_transitenhancement2023';
+update moped_tags set is_deleted = false where slug = 'map_vzero_systemicsafety_curves2023';
+update moped_tags set is_deleted = false where slug = 'map_vzero_intersectionspotential2023';
+update moped_tags set is_deleted = false where slug = 'map_vzero_intersections2023';
+update moped_tags set is_deleted = false where slug = 'map_vzero_speedmanagement2023';
+delete from moped_tags where slug = 'map-transit-speed-and-reliability-capmetro-ila';
+delete from moped_tags where slug = 'map-transit-enhancement';
+delete from moped_tags where slug = 'map-vision-zero-major-safety';
+delete from moped_tags where slug = 'map-vision-zero-ped-safety';
+delete from moped_tags where slug = 'map-vision-zero-rapid-response-small-scale';
+delete from moped_tags where slug = 'map-vision-zero-signal-safety';
+delete from moped_tags where slug = 'map-vision-zero-speed-management';
+delete from moped_tags where slug = 'map-vision-zero-street-lighting';
+delete from moped_tags where slug = 'map-vision-zero-systemic-safety';
+delete from moped_tags where slug = 'map-vision-zero-systemic-safety-access-management';
+delete from moped_tags where slug = 'map-vision-zero-systemic-safety-curve-warnings';
