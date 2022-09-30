e76b4e0f9a249db24c900fc7327f1f25a0c72471
Mike <mike.dilley@gmail.com>
2022-09-30 15:34:04 -0500

    Find a config with a lookup and find field and operator dropdowns

--- moped-editor/src/views/projects/projectsListView/test/projectListView.test.js
+++ moped-editor/src/views/projects/projectsListView/test/projectListView.test.js
@@ -1,16 +1,22 @@
-import { render, screen } from "src/utils/testUtils";
+import { render, screen, fireEvent } from "src/utils/testUtils";
 import "@testing-library/jest-dom";
 import GQLAbstract from "src/libs/GQLAbstract";
 import GridTableFilters from "src/components/GridTable/GridTableFilters";
 import { ProjectsListViewQueryConf } from "../ProjectsListViewQueryConf";
+import { ProjectsListViewFiltersConf } from "../ProjectsListViewFiltersConf";
 import { createBrowserHistory } from "history";
 
-// Show DOM in test runner -
-screen.debug();
+// Show DOM in test runner
+// screen.debug();
 
 let projectsQuery = new GQLAbstract(ProjectsListViewQueryConf);
 const filterQuery = new URLSearchParams("");
 const history = createBrowserHistory();
+
+// Find a config with a lookup key
+const filterConfigsWithLookups = ProjectsListViewFiltersConf.fields.filter(
+  (config) => config.hasOwnProperty("lookup")
+);
 
 describe("ProjectListView", () => {
   it("renders advanced filters with dropdown options & autocomplete when a lookup config is set", () => {
@@ -26,6 +32,19 @@ describe("ProjectListView", () => {
 
     const addFilterButton = screen.getByText("Add Filter");
 
+    fireEvent.click(addFilterButton);
+
+    const fieldDropdownInput = screen.getByText("Field");
+    const operatorDropdownInput = screen.getByText("Operator");
+
+    const lookupConfig = filterConfigsWithLookups[0];
+    const fieldChoiceThatWillUseALookup = lookupConfig.label;
+    fireEvent.click(fieldDropdownInput);
+
+    screen.debug();
+
     expect(addFilterButton).toBeInTheDocument();
+    // expect(fieldDropdownInput).toBeInTheDocument();
+    // expect(operatorDropdownInput).toBeInTheDocument();
   });
 });
