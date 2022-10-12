ae376657fd2b04aff5d8abd61f85ccdab75f528b
Frank Hereford <frankhereford@users.noreply.github.com>
2022-10-12 11:37:23 -0500

    spelling

--- moped-toolbox/geometry_prototype/create_prototype.sql
+++ moped-toolbox/geometry_prototype/create_prototype.sql
@@ -3,7 +3,7 @@ create table projects (
   name character varying
   );
   
--- laying down IDs so we can just insert carte blance on this DB
+-- laying down IDs so we can just insert carte blanche on this DB
 insert into projects (id, name) values (1, 'project_a');
 insert into projects (id, name) values (2, 'project_b');
 insert into projects (id, name) values (3, 'project_c');
