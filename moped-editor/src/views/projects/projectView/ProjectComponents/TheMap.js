05606e67eb5bbf4b6dbbb3487da298517cac84e1
Mike <mike.dilley@gmail.com>
2022-10-13 14:06:08 -0500

    Remove commented out import

--- moped-editor/src/views/projects/projectView/ProjectComponents/TheMap.js
+++ moped-editor/src/views/projects/projectView/ProjectComponents/TheMap.js
@@ -15,7 +15,6 @@ import { MAP_STYLES } from "./mapStyleSettings";
 import { getIntersectionLabel, useFeatureTypes } from "./utils";
 import { useFeatureService } from "./agolUtils";
 import "mapbox-gl/dist/mapbox-gl.css";
-// import "./MapboxStyleOverrides.css";
 
 // See https://github.com/visgl/react-map-gl/issues/1266#issuecomment-753686953
 import mapboxgl from "mapbox-gl";
