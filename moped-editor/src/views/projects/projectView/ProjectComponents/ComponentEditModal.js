02c34dbb8eef0ccce327cfe93a6900f573ffab52
Mike <mike.dilley@gmail.com>
2022-10-14 00:25:47 -0500

    Set component type options in autocomplete with fetched data

--- moped-editor/src/views/projects/projectView/ProjectComponents/ComponentEditModal.js
+++ moped-editor/src/views/projects/projectView/ProjectComponents/ComponentEditModal.js
@@ -13,7 +13,6 @@ import { CheckCircle } from "@material-ui/icons";
 import { Autocomplete } from "@material-ui/lab";
 import { makeStyles } from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
-import { COMPONENTS } from "./data/components";
 import { COMPONENT_FORM_FIELDS } from "./utils";
 import { GET_COMPONENTS_FORM_OPTIONS } from "src/queries/components";
 
@@ -33,57 +32,57 @@ const componentLabel = ({ component_name, component_subtype }) => {
     : `${component_name}`;
 };
 
-const useComponentOptions = () =>
+const useComponentOptions = (data) =>
   useMemo(() => {
-    const options = COMPONENTS.moped_components.map((comp) => ({
+    if (data === undefined) return [];
+
+    const options = data.moped_components.map((comp) => ({
       value: comp.component_id,
       label: componentLabel(comp),
       data: comp,
     }));
     // add empty option for default state
     return [...options, { value: "", label: "" }];
-  }, []);
+  }, [data]);
 
 // TODO: Use uuid here
 const randomComponentId = () => Math.floor(Math.random() * 10000000);
 
-const CustomAutocomplete = ({
-  fieldKey,
-  autoFocus,
-  dispatchComponentFormState,
-  value,
-  fieldLabel,
-}) => {
-  const options = useComponentOptions();
-
-  return (
-    <Autocomplete
-      id="combo-box-demo"
-      options={options}
-      getOptionLabel={(option) => {
-        return option.label || "";
-      }}
-      value={value}
-      onChange={(e, option) => {
-        dispatchComponentFormState({
-          key: fieldKey,
-          value: option,
-          action: "update",
-        });
-      }}
-      getOptionSelected={(option, value) => option.value === value.value}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          size="small"
-          label={fieldLabel}
-          variant="outlined"
-          autoFocus={autoFocus}
-        />
-      )}
-    />
-  );
-};
+// const CustomAutocomplete = ({
+//   fieldKey,
+//   autoFocus,
+//   dispatchComponentFormState,
+//   value,
+//   fieldLabel,
+// }) => {
+//   return (
+//     <Autocomplete
+//       id="combo-box-demo"
+//       options={options}
+//       getOptionLabel={(option) => {
+//         return option.label || "";
+//       }}
+//       value={value}
+//       onChange={(e, option) => {
+//         dispatchComponentFormState({
+//           key: fieldKey,
+//           value: option,
+//           action: "update",
+//         });
+//       }}
+//       getOptionSelected={(option, value) => option.value === value.value}
+//       renderInput={(params) => (
+//         <TextField
+//           {...params}
+//           size="small"
+//           label={fieldLabel}
+//           variant="outlined"
+//           autoFocus={autoFocus}
+//         />
+//       )}
+//     />
+//   );
+// };
 
 const ComponentEditModal = ({
   showDialog,
@@ -103,6 +102,8 @@ const ComponentEditModal = ({
     loading: areOptionsLoading,
     error: optionsError,
   } = useQuery(GET_COMPONENTS_FORM_OPTIONS);
+
+  const componentOptions = useComponentOptions(optionsData);
 
   const onSave = (e) => {
     e.preventDefault();
@@ -142,15 +143,48 @@ const ComponentEditModal = ({
         <form onSubmit={(e) => e.preventDefault()}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
-              <CustomAutocomplete
+              {/* Component Type
+              This shows "component - subtype" (if there is a subtype) */}
+              {/* <CustomAutocomplete
                 fieldKey={COMPONENT_FORM_FIELDS[0].key}
                 fieldLabel={COMPONENT_FORM_FIELDS[0].label}
                 autoFocus
                 dispatchComponentFormState={dispatchComponentFormState}
                 value={componentFormState[COMPONENT_FORM_FIELDS[0].key] || ""}
+              /> */}
+              <Autocomplete
+                id="components-type"
+                options={areOptionsLoading ? [] : componentOptions}
+                getOptionLabel={(option) => {
+                  return option.label || "";
+                }}
+                value={""}
+                onChange={(e, option) => {
+                  // dispatchComponentFormState({
+                  //   key: fieldKey,
+                  //   value: option,
+                  //   action: "update",
+                  // });
+                  console.log(option);
+                }}
+                getOptionSelected={(option, value) =>
+                  option.value === value.value
+                }
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    size="small"
+                    label={"Component Type"}
+                    variant="outlined"
+                    autoFocus
+                  />
+                )}
               />
             </Grid>
             <Grid item xs={12}>
+              {/* Subcomponents
+              This shows available subcomponents
+              grey out if none available for selected component type */}
               <TextField
                 fullWidth
                 size="small"
@@ -186,7 +220,7 @@ const ComponentEditModal = ({
                 }}
               />
             </Grid>
-            <Grid item xs={12}>
+            {/* <Grid item xs={12}>
               <TextField
                 fullWidth
                 size="small"
@@ -211,7 +245,7 @@ const ComponentEditModal = ({
                 }}
                 variant="outlined"
               />
-            </Grid>
+            </Grid> */}
           </Grid>
           <Grid container spacing={4} display="flex" justifyContent="flex-end">
             <Grid item style={{ margin: 5 }}>
