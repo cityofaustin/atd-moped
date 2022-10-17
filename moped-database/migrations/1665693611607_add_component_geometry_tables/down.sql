bcac3c9570e7602e04e85bbb4b0c296c31d1fdb2
Frank Hereford <frankhereford@users.noreply.github.com>
2022-10-17 09:21:37 -0500

    and the down changes

--- moped-database/migrations/1665693611607_add_component_geometry_tables/down.sql
+++ moped-database/migrations/1665693611607_add_component_geometry_tables/down.sql
@@ -1,4 +1,8 @@
-alter table moped_components
-  drop column feature_layer_id;
+drop table feature_street_segments;
+drop table feature_intersections;
+drop table feature_signals;
+drop table features;
+
+alter table moped_components drop column feature_layer_id;
 
 drop table feature_layers;
