0bab84a43fd57616d53f13aeef69e66cfffc6543
Frank Hereford <frankhereford@users.noreply.github.com>
2022-10-17 09:20:58 -0500

    refine feature_layers table

--- moped-database/migrations/1665693611607_add_component_geometry_tables/up.sql
+++ moped-database/migrations/1665693611607_add_component_geometry_tables/up.sql
@@ -1,14 +1,15 @@
 create table feature_layers (   
     id serial primary key,
-    internal_table character varying not null, -- I want to call this "table" but that's a very reserved word. Any other ideas? 
+    internal_table character varying not null,
     reference_layer_primary_key_column character varying
 );
 
-insert into feature_layers (internal_table, reference_layer_primary_key_column) values ('drawn_points', null);
-insert into feature_layers (internal_table, reference_layer_primary_key_column) values ('drawn_lines', null);
+insert into feature_layers (internal_table, reference_layer_primary_key_column) values ('feature_drawn_points', null);
+insert into feature_layers (internal_table, reference_layer_primary_key_column) values ('feature_drawn_lines', null);
 -- the following entries in this table are records of outside data sources
-insert into feature_layers (internal_table, reference_layer_primary_key_column) values ('signals', 'signal_id');
-insert into feature_layers (internal_table, reference_layer_primary_key_column) values ('segments', 'segment_id');
-insert into feature_layers (internal_table, reference_layer_primary_key_column) values ('intersections', 'intersection_id');
+insert into feature_layers (internal_table, reference_layer_primary_key_column) values ('feature_signals', 'signal_id');
+insert into feature_layers (internal_table, reference_layer_primary_key_column) values ('feature_street_segments', 'segment_id');
+insert into feature_layers (internal_table, reference_layer_primary_key_column) values ('feature_intersections', 'intersection_id');
+
 
 -- todo: update rows to point to right layer in above table
