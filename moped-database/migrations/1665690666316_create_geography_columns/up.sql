0c78eb97177fc9f575644b0b3c20396bdc436ff8
Frank Hereford <frankhereford@users.noreply.github.com>
2022-10-17 13:27:21 -0500

    went another direction

--- moped-database/migrations/1665690666316_create_geography_columns/up.sql
+++
@@ -1,5 +0,0 @@
-alter table moped_proj_features 
-    add column geography_collection geography(GEOMETRYCOLLECTION, 4326),
-    add column geography_point geography(POINT, 4326),
-    add column geography_linestring geography(LINESTRING, 4326),
-    add column attributes jsonb;
