eb7f859a5a1065fb284472ad406a6b431707d5a4
Frank Hereford <frankhereford@users.noreply.github.com>
2022-10-17 17:17:10 -0500

    improve observing incoming features

--- moped-toolbox/populate_geometry_transform/transpose_geometry.py
+++ moped-toolbox/populate_geometry_transform/transpose_geometry.py
@@ -8,6 +8,9 @@ import psycopg2
 import psycopg2.extras
 import geojson
 
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
 
 DB_HOST = os.environ.get("DB_HOST", "host.docker.internal")
 DB_USER = os.environ.get("DB_USER", "moped")
@@ -19,8 +22,10 @@ pg = psycopg2.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, dbname=D
 
 def moped_proj_features(args):
     sql = """
-    select feature_id, feature::character varying as feature
+    select moped_proj_components.project_id, moped_components.component_name, moped_components.component_subtype, feature_id, feature::character varying as feature
     from moped_proj_features
+    join moped_proj_components on (moped_proj_components.project_component_id = moped_proj_features.project_component_id)
+    join moped_components on (moped_proj_components.component_id = moped_components.component_id)
     order by random();
     """
 
@@ -34,7 +39,17 @@ def moped_proj_features(args):
         if not feature.is_valid:
             raise Exception("Invalid feature")
 
+        if str(feature["geometry"]["type"]) == 'Point':
+            continue
+
+        print("Project ID: ", record["project_id"])
+        print("Component Name: ", record["component_name"])
+        print("Component Subtype: ", record["component_subtype"])
         print(str(record["feature_id"]) + ": " + str(feature["geometry"]["type"]))
+        pp.pprint(feature["properties"])
+        print("\n")
+
+        continue;
 
         if feature["geometry"]["type"] == "Point":
             sql = """
