05200a6a4ca444a3f785e3d360ccb0de0fe2f6f3
Frank Hereford <frankhereford@users.noreply.github.com>
2022-10-17 21:50:57 -0500

    this didn't need to be in the loop

--- moped-toolbox/populate_geometry_transform/transpose_geometry.py
+++ moped-toolbox/populate_geometry_transform/transpose_geometry.py
@@ -95,6 +95,16 @@ truncate features;
         #if str(feature["geometry"]["type"]) == 'Point':
             #continue
 
+        if True:
+            print("Project ID: ", record["project_id"])
+            print("Component Name: ", record["component_name"])
+            print("Component Subtype: ", record["component_subtype"])
+            print("Feature ID: ", record["feature_id"])
+            print("Default internal table: ", record["internal_table"])
+            print(str(record["feature_id"]) + ": " + str(feature["geometry"]["type"]))
+            pp.pprint(feature["properties"])
+            pp.pprint(feature["geometry"])
+
         feature_id = None
         # These need special handling, I think they will be half intersections, half segments.
         # It is a genuinely un-geographically typed feature. 🤔
