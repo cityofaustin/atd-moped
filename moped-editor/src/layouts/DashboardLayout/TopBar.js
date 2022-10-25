da574ca63f15fe29b8dbe517b554aa625017f974
Chia Berry <analuciaby@gmail.com>
2022-10-25 11:54:19 -0500

    move help items into avatar

--- moped-editor/src/layouts/DashboardLayout/TopBar.js
+++ moped-editor/src/layouts/DashboardLayout/TopBar.js
@@ -24,7 +24,27 @@ import { getSessionDatabaseData, useUser } from "../../auth/user";
 import emailToInitials from "../../utils/emailToInitials";
 import CDNAvatar from "../../components/CDN/Avatar";
 import NavLink from "src/components/NavLink";
+import ExternalLink from "../../components/ExternalLink";
 import { getInitials } from "src/utils/userNames";
+
+const helpItems = [
+  {
+    href: "https://atd.knack.com/dts#new-service-request/?view_249_vars=%7B%22field_398%22%3A%22Bug%20Report%20%E2%80%94%20Something%20is%20not%20working%22%2C%22field_399%22%3A%22Moped%22%7D",
+    title: "Report a bug ",
+  },
+  {
+    href: "https://atd.knack.com/dts#new-service-request/?view_249_vars=%7B%22field_398%22%3A%22Feature%20or%20Enhancement%20%E2%80%94%20An%20application%20I%20use%20could%20be%20improved%22%2C%22field_399%22%3A%22Moped%22%7D",
+    title: "Request an enhancement ",
+  },
+  {
+    href: "https://teams.microsoft.com/l/channel/19%3ab1179ddfc92d44ea9abb23db713eb60c%40thread.tacv2/General?groupId=54a90854-d3fa-4053-9173-5352715bab37&tenantId=5c5e19f6-a6ab-4b45-b1d0-be4608a9a67f",
+    title: "Ask a question ",
+  },
+  {
+    href: "https://atd-dts.gitbook.io/moped/",
+    title: "Moped user guide ",
+  },
+];
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -165,6 +185,22 @@ const TopBar = ({ className, ...rest }) => {
             >
               Account
             </MenuItem>
+                    <MenuItem onClick={() => {
+                handleAvatarClose();
+                navigate("/moped/dev/lookups");
+              }}>
+          Lookups
+        </MenuItem>
+        {helpItems.map((item) => (
+          <MenuItem key={item.href} onClick={handleAvatarClose}>
+            <ExternalLink
+              url={item.href}
+              text={item.title}
+              linkColor="inherit"
+              underline="none"
+            />
+          </MenuItem>
+        ))}
             <MenuItem onClick={() => navigate("/moped/logout")}>
               Logout
             </MenuItem>
