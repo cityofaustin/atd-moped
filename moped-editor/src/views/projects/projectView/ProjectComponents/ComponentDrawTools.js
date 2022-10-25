54dc0d95a3c2dc8ad7a8d838bf41079ea3d3fc28
Mike <mike.dilley@gmail.com>
2022-10-25 15:23:13 -0500

    Add created draw features to draft component and add uuids

--- moped-editor/src/views/projects/projectView/ProjectComponents/ComponentDrawTools.js
+++ moped-editor/src/views/projects/projectView/ProjectComponents/ComponentDrawTools.js
@@ -1,5 +1,7 @@
 import React, { useRef } from "react";
 import ComponentsDrawControl from "src/components/Maps/ComponentsDrawControl";
+import { cloneDeep } from "lodash";
+import { v4 as uuidv4 } from "uuid";
 
 const ComponentDrawTools = ({
   draftComponent,
@@ -14,10 +16,17 @@ const ComponentDrawTools = ({
   // TODO: Add/update/remove draftComponent.features
 
   const onCreate = ({ features: createdFeaturesArray }) => {
-    console.log({ createdFeaturesArray });
+    const drawnFeatures = cloneDeep(createdFeaturesArray);
 
-    // TODO: iterate createdFeaturesArray, update IDs with uuids and add to draftComponent.features
-    // probably need to update the ids of the features in the draw tools tracking too
+    // Add a unique id to each drawn feature's properties
+    drawnFeatures.forEach(
+      (feature) => (feature.properties["DRAW_ID"] = uuidv4())
+    );
+
+    setDraftComponent((prevDraftComponent) => ({
+      ...prevDraftComponent,
+      features: [...draftComponent.features, ...drawnFeatures],
+    }));
   };
 
   const onUpdate = ({ features: updatedFeaturesArray, action }) => {
@@ -37,6 +46,8 @@ const ComponentDrawTools = ({
     // If we are not drawing, set isDrawing to false so we can select layer features as components
     const { mode } = e;
 
+    // TODO: Move this back to isDrawing state toggle and control cursor in the map component
+    // Handle isHovering the same way so that one isn't overriding the other
     if (mode === "draw_point" || mode === "draw_line_string") {
       setCursor("crosshair");
     } else {
