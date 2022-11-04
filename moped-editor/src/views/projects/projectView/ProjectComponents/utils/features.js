fcff288a8cfb5a2d4e69b05e1378d43e8f70342b
Mike <mike.dilley@gmail.com>
2022-11-04 17:37:34 -0500

    Start building map to translate CTN feature properties names to DB field names

--- moped-editor/src/views/projects/projectView/ProjectComponents/utils/features.js
+++ moped-editor/src/views/projects/projectView/ProjectComponents/utils/features.js
@@ -1,1 +1,18 @@
-export const featureTableFieldMap = {};
+// Columns to add to object
+// component_id
+// source_layer (from _layerId in map) Ex. "ctn-lines"
+// geography from geometry
+
+export const featureTableFieldMap = {
+  feature_street_segments: {
+    full_street_name: "FULL_STREET_NAME",
+    ctn_segment_id: "CTN_SEGMENT_ID",
+    from_address_min: "FROM_ADDRESS_MIN",
+    to_address_max: "TO_ADDRESS_MAX",
+    line_type: "LINE_TYPE",
+    // project_extent_id: undefined,
+    // knack_id: undefined,
+    // symbol: undefined,
+    // render_type: undefined,
+  },
+};
