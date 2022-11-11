87726c598a099a42e5b4eb74dc5e676987b3196a
John Clary <john.clary@austintexas.gov>
2022-11-11 13:50:41 -0500

    create current_phase_view

---
+++ moped-database/migrations/1668192315933_create_current_phase_view/up.sql
@@ -0,0 +1,16 @@
+CREATE VIEW public.current_phase_view AS (
+    SELECT DISTINCT ON (project_id, is_current_phase)
+        project_id,
+        project_phase_id,
+        phase_id
+    FROM
+        moped_proj_phases
+    WHERE
+        is_deleted = FALSE
+        AND is_current_phase = TRUE
+        -- order by ensures consistent DISTINCT ON results in edge case that a project has multiple curr phases
+    ORDER BY
+        project_id,
+        is_current_phase,
+        project_phase_id
+);
