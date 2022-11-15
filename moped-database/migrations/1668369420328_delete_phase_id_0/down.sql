69e05866fbb8fe4a91969d9597b9c92c466eda1b
John Clary <john.clary@austintexas.gov>
2022-11-15 12:35:35 -0500

    fix up impossible down migration

--- moped-database/migrations/1668369420328_delete_phase_id_0/down.sql
+++ moped-database/migrations/1668369420328_delete_phase_id_0/down.sql
@@ -1,4 +1,2 @@
 -- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- DELETE FROM public.moped_proj_phases where phase_id = 0;
--- DELETE FROM public.moped_phases where phase_id = 0;
+SELECT 0;
