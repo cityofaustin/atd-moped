abc32a1caa46f995f61e10b967ebceab23e035c3
Chia Berry <analuciaby@gmail.com>
2022-11-15 15:42:55 -0600

    add userid to activity log table

---
+++ moped-database/migrations/1668538438134_activity_log_add_updated_user_id/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."moped_activity_log" add column "updated_by_user_id" integer null;
---
+++ moped-database/migrations/1668538527315_set_fk_activity_log_moped_user/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."moped_activity_log" drop constraint "moped_activity_log_updated_by_user_id_fkey";
