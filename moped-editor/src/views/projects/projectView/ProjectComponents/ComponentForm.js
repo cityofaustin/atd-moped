36e5363dee10ea00476ff7bc3c34d6b319ccc8bb
Mike <mike.dilley@gmail.com>
2022-11-17 13:10:36 -0600

    Pull form out of ComponentEditModal, create ComponentForm component

--- moped-editor/src/views/projects/projectView/ProjectComponents/ComponentForm.js
+++ moped-editor/src/views/projects/projectView/ProjectComponents/ComponentForm.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ComponentForm from "./ComponentForm";
 import { useQuery } from "@apollo/client";
 import { Controller, useForm } from "react-hook-form";
 import {
@@ -76,18 +75,15 @@ const ControlledAutocomplete = ({
   />
 );
 
-const ComponentEditModal = ({
-  showDialog,
-  setShowDialog,
-  setDraftComponent,
-  setLinkMode,
-  setIsCreatingComponent,
-}) => {
-  const classes = useStyles();
-
+const ComponentForm = ({ formButtonText, onSave }) => {
   const { register, handleSubmit, control, reset, watch } = useForm({
     defaultValues: initialFormValues,
   });
+
+  const handleFormSubmit = (formData) => {
+    onSave(formData);
+    reset(initialFormValues);
+  };
 
   // Get and format component and subcomponent options
   const { data: optionsData, loading: areOptionsLoading } = useQuery(
@@ -97,62 +93,66 @@ const ComponentEditModal = ({
   const { component } = watch();
   const subcomponentOptions = useSubcomponentOptions(component);
 
-  const onSave = (formData) => {
-    const {
-      component: {
-        data: {
-          component_id,
-          component_name,
-          component_subtype,
-          line_representation,
-          feature_layer: { internal_table },
-        },
-      },
-      subcomponents,
-      description,
-    } = formData;
-
-    const newComponent = {
-      component_id,
-      component_name,
-      component_subtype,
-      line_representation,
-      internal_table,
-      moped_subcomponents: subcomponents,
-      description,
-      label: component_name,
-      features: [],
-    };
-
-    const linkMode = newComponent.line_representation ? "lines" : "points";
-
-    setDraftComponent(newComponent);
-    setLinkMode(linkMode);
-    // switch to components tab
-    setShowDialog(false);
-  };
-
-  const onClose = () => {
-    setLinkMode(null);
-    setDraftComponent(null);
-    setIsCreatingComponent(false);
-    setShowDialog(false);
-    reset(initialFormValues);
-  };
-
   return (
-    <Dialog open={showDialog} onClose={onClose} fullWidth scroll="body">
-      <DialogTitle disableTypography className={classes.dialogTitle}>
-        <h3>New component</h3>
-        <IconButton onClick={onClose}>
-          <CloseIcon />
-        </IconButton>
-      </DialogTitle>
-      <DialogContent dividers={true}>
-        <ComponentForm onSave={onSave} formButtonText="Continue" />
-      </DialogContent>
-    </Dialog>
+    <form onSubmit={handleSubmit(handleFormSubmit)}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <ControlledAutocomplete
+            id="component"
+            label="Component Type"
+            options={areOptionsLoading ? [] : componentOptions}
+            renderOption={(option) => (
+              <ComponentOptionWithIcon option={option} />
+            )}
+            name="component"
+            control={control}
+            autoFocus
+          />
+        </Grid>
+        {/* Hide unless there are subcomponents for the chosen component */}
+        {subcomponentOptions.length !== 0 && (
+          <Grid item xs={12}>
+            <ControlledAutocomplete
+              id="subcomponents"
+              label="Subcomponents"
+              multiple
+              options={subcomponentOptions}
+              name="subcomponents"
+              control={control}
+            />
+          </Grid>
+        )}
+        <Grid item xs={12}>
+          <TextField
+            inputRef={register}
+            fullWidth
+            size="small"
+            name="description"
+            id="description"
+            label={"Description"}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="outlined"
+            multiline
+            minRows={4}
+          />
+        </Grid>
+      </Grid>
+      <Grid container spacing={4} display="flex" justifyContent="flex-end">
+        <Grid item style={{ margin: 5 }}>
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<CheckCircle />}
+            type="submit"
+          >
+            {formButtonText}
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
   );
 };
 
-export default ComponentEditModal;
+export default ComponentForm;
