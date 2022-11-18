b20d3919f6760e6151b3589eb2a7be0358b83e6d
Mike <mike.dilley@gmail.com>
2022-11-18 12:25:25 -0600

    Remove migration to set default vals for render_type

--- moped-database/migrations/1668201314245_alter_table_public_feature_drawn_lines_alter_column_render_type/down.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "public"."feature_drawn_lines" ALTER COLUMN "render_type" drop default;
-ALTER TABLE "public"."feature_drawn_points" ALTER COLUMN "render_type" drop default;
-ALTER TABLE "public"."feature_intersections" ALTER COLUMN "render_type" drop default;
-ALTER TABLE "public"."feature_signals" ALTER COLUMN "render_type" drop default;
-ALTER TABLE "public"."feature_street_segments" ALTER COLUMN "render_type" drop default;
