776cb8703a7bf9c4fceeda31de0896df90936066
John Clary <john.clary@austintexas.gov>
2022-12-05 13:43:24 -0500

    adds metadata api helpers

--- moped-toolbox/access_migration/migrate_data/utils/graphql.js
+++ moped-toolbox/access_migration/migrate_data/utils/graphql.js
@@ -1,27 +1,74 @@
 const { request } = require("graphql-request");
+const { HASURA_AUTH } = require("./secrets");
 
-const HASURA = {
-  endpoint: {
-    local: "http://localhost:8080/v1/graphql",
-  },
-  admin_secret: {
-    local: "DuMmyApiKeyHFVOVto19otC1wX6sP2N0VSKrCD70L10B7Sm525ZR6L672i2F79M9!",
-  },
+const getRequestHeaders = (env) => ({
+  "X-Hasura-Admin-Secret": HASURA_AUTH.admin_secret[env],
+  "content-type": "application/json",
+});
+
+const metadataRequest = async ({ env, query }) => {
+  const url = `${HASURA_AUTH.endpoint[env]}/v1/metadata`;
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(query),
+    headers: getRequestHeaders(env),
+  });
+  if (!response.ok) {
+    const text = await response.text();
+    console.error(text);
+    throw text;
+  }
+  data = await response.json();
+  return data;
 };
 
-function makeHasuraRequest({ query, variables, env = "local" }) {
-  const headers = {
-    "X-Hasura-Admin-Secret": HASURA.admin_secret[env],
-    "content-type": "application/json",
+const reloadMetadata = async ({ env }) => {
+  // https://hasura.io/docs/latest/api-reference/metadata-api/manage-metadata/#metadata-reload-metadata-syntax
+  const query = {
+    type: "reload_metadata",
+    args: {
+      // this resets hasura's tracking of DB triggers - https://github.com/hasura/graphql-engine/issues/8376
+      recreate_event_triggers: true,
+    },
   };
+  const data = await metadataRequest({ env, query });
+  return data;
+};
+
+const replaceMetadata = async ({ env, metadata }) => {
+  // https://hasura.io/docs/latest/api-reference/metadata-api/manage-metadata/#metadata-replace-metadata
+  const query = {
+    type: "replace_metadata",
+    version: 2,
+    args: { metadata, allow_inconsistent_metadata: false },
+  };
+  const data = await metadataRequest({ env, query });
+  return data;
+};
+
+const exportMetadata = async ({ env }) => {
+  const query = {
+    type: "export_metadata",
+    version: 2,
+    args: {},
+  };
+  const data = await metadataRequest({ env, query });
+  return data.metadata;
+};
+
+const makeHasuraRequest = ({ query, variables, env }) => {
+  const url = `${HASURA_AUTH.endpoint[env]}/v1/graphql`;
   return request({
-    url: HASURA.endpoint[env],
+    url,
     document: query,
     variables,
-    requestHeaders: headers,
+    requestHeaders: getRequestHeaders(env),
   }).then((data) => data);
-}
+};
 
 module.exports = {
+  exportMetadata,
   makeHasuraRequest,
+  replaceMetadata,
+  reloadMetadata,
 };
