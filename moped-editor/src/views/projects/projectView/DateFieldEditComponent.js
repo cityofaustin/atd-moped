af54b914fdd12f54be9efac913ed48eaf8c6a0b4
Rose <67130963+roseeichelmann@users.noreply.github.com>
2022-12-27 15:51:11 -0600

    add new MUI datepicker, still troubleshooting

--- moped-editor/src/views/projects/projectView/DateFieldEditComponent.js
+++ moped-editor/src/views/projects/projectView/DateFieldEditComponent.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { TextField } from "@material-ui/core";
 import { handleKeyEvent } from "../../../utils/materialTableHelpers";
+import DateFnsUtils from "@date-io/date-fns";
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import { useState } from "react";
 
 /**
  * DateFieldEditComponent - renders a Date type Calendar select
@@ -9,19 +12,34 @@ import { handleKeyEvent } from "../../../utils/materialTableHelpers";
  * @constructor
  */
 
-const DateFieldEditComponent = (props) => (
-  <TextField
-    name={props.name}
-    label={props.label}
-    type="date"
-    variant="standard"
-    value={props.value || ""}
-    onChange={(e) => props.onChange(e.target.value)}
-    onKeyDown={(e) => handleKeyEvent(e)}
-    InputLabelProps={{
-      shrink: true,
-    }}
-  />
-);
+// const DateFieldEditComponent = (props) => (
+//   <TextField
+//     name={props.name}
+//     label={props.label}
+//     type="date"
+//     variant="standard"
+//     value={props.value || ""}
+//     onChange={(e) => props.onChange(e.target.value)}
+//     onKeyDown={(e) => handleKeyEvent(e)}
+//     InputLabelProps={{
+//       shrink: true,
+//     }}
+//   />
+// );
+
+const DateFieldEditComponent = (props) => {
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <DatePicker
+        format="MM/dd/yyyy"
+        value={props.value || null}
+        onChange={(e) => props.onChange(e.target.value)}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+    </MuiPickersUtilsProvider>
+  );
+};
 
 export default DateFieldEditComponent;
