5fbd47e245ae57f21047f8c70620102bae5f92f5
John Clary <john.clary@austintexas.gov>
2022-12-30 12:20:38 -0500

    use moped_user instead of added_by

--- moped-editor/src/queries/comments.js
+++ moped-editor/src/queries/comments.js
@@ -6,8 +6,10 @@ export const COMMENTS_QUERY = gql`
       where: $projectNoteConditions
       order_by: { date_created: desc }
     ) {
-      added_by
-      added_by_user_id
+      moped_user {
+        first_name
+        last_name
+      }
       project_note
       project_id
       date_created
