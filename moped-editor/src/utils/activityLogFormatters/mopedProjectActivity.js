12cafee6527f710d3982a1cf4e7513b4896bb060
Chia Berry <analuciaby@gmail.com>
2023-01-03 20:08:07 -0600

    change the formatting from blank to none

--- moped-editor/src/utils/activityLogFormatters/mopedProjectActivity.js
+++ moped-editor/src/utils/activityLogFormatters/mopedProjectActivity.js
@@ -9,15 +9,15 @@ export const formatProjectActivity = (change, entityList) => {
 
   const changeData = change.record_data.event.data;
 
-  // project creation
+  // Project creation
   if (change.description.length === 0) {
-    // created project as (bold project name)
     changeDescription = "Created project as ";
     changeValue = changeData.new.project_name;
     changeIcon = <BeenhereOutlinedIcon />;
     return { changeIcon, changeDescription, changeValue };
   }
 
+  // the field that was changed in the activity
   const changedField = change.description[0].field;
 
   // need to use a lookup table
@@ -33,16 +33,25 @@ export const formatProjectActivity = (change, entityList) => {
         entityList[change.description[0].new]
       }" as `;
       changeValue = entryMap.fields[changedField].label;
+      return { changeIcon, changeDescription, changeValue };
     }
+
+    // if the new field is null or undefined, its because something was removed
+    if (!entityList[changeData.new[changedField]]) {
+      changeDescription = `Removed ${entryMap.fields[changedField].label} `;
+      changeValue = "";
+      return { changeIcon, changeDescription, changeValue };
+    }
+
     // Changing a field, but need to use lookup table to display
     changeDescription = `Changed ${entryMap.fields[changedField].label} to `;
     changeValue = entityList[changeData.new[changedField]];
   } else {
-
-    // If the update is an object, show just the field name that was updated. 
+    // If the update is an object, show just the field name that was updated.
     if (typeof changeData.new[changedField] === "object") {
       changeDescription = `Changed ${entryMap.fields[changedField].label}`;
       changeValue = "";
+      return { changeIcon, changeDescription, changeValue };
     }
 
     // the update can be rendered as a string
@@ -52,7 +61,7 @@ export const formatProjectActivity = (change, entityList) => {
     changeValue =
       changeData.new[changedField].length > 0
         ? changeData.new[changedField]
-        : "(blank)";
+        : "(none)";
   }
 
   return { changeIcon, changeDescription, changeValue };
