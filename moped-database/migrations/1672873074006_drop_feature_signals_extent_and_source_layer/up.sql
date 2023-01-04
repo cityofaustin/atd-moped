5190dd51fe3cf2bfcc40204740868a343700dffa
Mike <mike.dilley@gmail.com>
2023-01-04 17:08:54 -0600

    Drop extent id and source layer from feature_signals table, update query

---
+++ moped-database/migrations/1672873074006_drop_feature_signals_extent_and_source_layer/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE feature_signals DROP project_extent_id, DROP source_layer;
--- moped-editor/src/utils/signalComponentHelpers.js
+++ moped-editor/src/utils/signalComponentHelpers.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
 import { TextField } from "@material-ui/core";
 
 /*
