0ace7cee48518432f1fa8d9f8419488e831b64d3
Mike <mike.dilley@gmail.com>
2023-01-04 10:17:41 -0600

    Center alert vertically in top bar

--- moped-editor/src/views/projects/projectView/ProjectComponents/MapAlertSnackbar.js
+++ moped-editor/src/views/projects/projectView/ProjectComponents/MapAlertSnackbar.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { Snackbar } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
+import { makeStyles } from "@material-ui/core/styles";
 
-// 8 px from the top
+const useStyles = makeStyles(() => ({
+  root: {
+    // ComponentMapToolbar is 64 px tall and the alert is 48 px tall
+    // Set alert 8px from top to center vertically
+    top: "8px",
+  },
+}));
 
-const MapAlertSnackbar = ({ message, severity, isOpen }) => (
-  <Snackbar
-    anchorOrigin={{ vertical: "top", horizontal: "center" }}
-    open={isOpen}
-    key={"map-alert-snackbar"}
-  >
-    <Alert severity={severity}>{message}</Alert>
-  </Snackbar>
-);
+const MapAlertSnackbar = ({ message, severity, isOpen }) => {
+  const classes = useStyles();
+
+  return (
+    <Snackbar
+      classes={classes}
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      open={isOpen}
+      key={"map-alert-snackbar"}
+    >
+      <Alert severity={severity}>{message}</Alert>
+    </Snackbar>
+  );
+};
 
 export default MapAlertSnackbar;
