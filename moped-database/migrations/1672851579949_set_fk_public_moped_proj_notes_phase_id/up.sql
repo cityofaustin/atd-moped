4215946ab425159eb89277ebdc52ee3c32cb71c3
John Clary <john.clary@austintexas.gov>
2023-01-05 08:31:44 -0500

    on moped_phase delete set null

--- moped-database/migrations/1672851579949_set_fk_public_moped_proj_notes_phase_id/up.sql
+++ moped-database/migrations/1672851579949_set_fk_public_moped_proj_notes_phase_id/up.sql
@@ -3,4 +3,4 @@ alter table "public"."moped_proj_notes"
   add constraint "moped_proj_notes_phase_id_fkey"
   foreign key ("phase_id")
   references "public"."moped_phases"
-  ("phase_id") on update cascade;
+  ("phase_id") on update cascade on delete set null;
