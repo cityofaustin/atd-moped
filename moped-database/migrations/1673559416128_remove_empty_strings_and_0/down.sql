bfaa637ea8b426170b834b2bd5d05e0e3df64754
Chia Berry <analuciaby@gmail.com>
2023-01-17 12:41:40 -0600

    drop fund program with id 0

--- moped-database/migrations/1673559416128_remove_empty_strings_and_0/down.sql
+++ moped-database/migrations/1673559416128_remove_empty_strings_and_0/down.sql
@@ -1,3 +1,7 @@
 UPDATE moped_proj_funding SET fund_dept_unit = '' WHERE fund_dept_unit = null;
 UPDATE moped_proj_funding SET funding_description = '' WHERE funding_description = null;
 UPDATE moped_proj_funding SET funding_program_id = 0 WHERE funding_program_id = null;
+UPDATE moped_proj_funding SET funding_amount = 0 WHERE funding_amount = null;
+
+-- delete moped_fund_programs program with id of 0
+DELETE FROM moped_fund_programs WHERE funding_program_id = 0;
--- moped-database/migrations/1673559416128_remove_empty_strings_and_0/up.sql
+++ moped-database/migrations/1673559416128_remove_empty_strings_and_0/up.sql
@@ -2,3 +2,6 @@
 UPDATE moped_proj_funding SET fund_dept_unit = null WHERE fund_dept_unit = '';
 UPDATE moped_proj_funding SET funding_description = null WHERE funding_description = '';
 UPDATE moped_proj_funding SET funding_program_id = null WHERE funding_program_id = 0;
+UPDATE moped_proj_funding SET funding_amount = null WHERE funding_amount = 0;
+
+INSERT INTO moped_fund_programs (funding_program_id, funding_program_name) VALUES (0, '');
