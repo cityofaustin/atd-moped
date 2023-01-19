aa8b48565ac15aa56953bb44f7d5c268d3c0875c
Tilly Whitson <35410637+tillyw@users.noreply.github.com>
2023-01-19 14:12:02 -0600

    combine alter table migrations

--- moped-database/migrations/1672776840767_alter_table_public_moped_project_add_column_public_process_status_id/up.sql
+++ moped-database/migrations/1672776840767_alter_table_public_moped_project_add_column_public_process_status_id/up.sql
@@ -1,1 +1,6 @@
 alter table "public"."moped_project" add column "public_process_status_id" integer;
+alter table "public"."moped_project"
+  add constraint "moped_project_public_process_status_id_fkey"
+  foreign key ("public_process_status_id")
+  references "public"."moped_public_process_statuses"
+  ("id") on update set null on delete set null;
--- moped-database/migrations/1672776894714_set_fk_public_moped_project_public_process_status_id/down.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."moped_project" drop constraint "moped_project_public_process_status_id_fkey";
