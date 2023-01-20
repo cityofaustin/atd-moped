fc18ab7f7a0fd5d3f5312d897868d05a4258a8e3
Tilly Whitson <35410637+tillyw@users.noreply.github.com>
2023-01-20 14:37:01 -0600

    removed log

--- moped-editor/src/utils/activityLogFormatters/mopedPartnersActivity.js
+++ moped-editor/src/utils/activityLogFormatters/mopedPartnersActivity.js
@@ -5,8 +5,6 @@ export const formatPartnersActivity = (change, entityList) => {
   let changeDescription = "Project partners updated";
   let changeValue = ""
 
-  console.log(change.record_data.event.data.new);
-
   // Adding a new partner
   if (change.description.length === 0) {
     changeDescription = "Added project partner "
