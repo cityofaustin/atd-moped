09cc248f15fd3e86b02a3db4bfdf26e07bc00b01
Chia Berry <analuciaby@gmail.com>
2023-01-21 18:08:29 -0600

    update funding activity to use array of changes

--- moped-editor/src/utils/activityLogFormatters/mopedFundingActivity.js
+++ moped-editor/src/utils/activityLogFormatters/mopedFundingActivity.js
@@ -10,46 +10,66 @@ export const formatFundingActivity = (
   const entryMap = ProjectActivityLogTableMaps["moped_proj_funding"];
 
   const changeIcon = <MonetizationOnOutlinedIcon />;
-  let changeDescription = "Project funding updated";
-  let changeValue = "";
+  let changeText = [{ text: "Project funding updated", style: null }];
 
   // add a new funding source
   if (change.description.length === 0) {
-    changeDescription = "Added a new funding source";
+    changeText = [{ text: "Added a new funding source", style: null }];
     // if the added record has a funding source, use that as the change value
     if (change.record_data.event.data.new.funding_source_id) {
-      changeValue =
-        fundingSources[change.record_data.event.data.new.funding_source_id];
+      changeText = [
+        { text: "Added ", style: null },
+        {
+          text: fundingSources[
+            change.record_data.event.data.new.funding_source_id
+          ],
+          style: "boldText",
+        },
+        { text: " as a new funding source." },
+      ];
       // if not, then check if theres a funding program
     } else if (change.record_data.event.data.new.funding_program_id) {
-      changeValue =
-        fundingPrograms[change.record_data.event.data.new.funding_program_id];
+      changeText = [
+        { text: "Added ", style: null },
+        {
+          text: fundingPrograms[
+            change.record_data.event.data.new.funding_program_id
+          ],
+          style: "boldText",
+        },
+        { text: " as a new funding source." },
+      ];
     }
-    // if we have a change value, tack on a : at the end
-    if (changeValue) {
-      changeDescription = "Added a new funding source: ";
-    }
-    // if there isnt a funding source or program added, then default to ""
-    return { changeIcon, changeDescription, changeValue };
+    return { changeIcon, changeText };
   }
 
   // delete an existing record
   if (change.description[0].field === "is_deleted") {
-    changeDescription = "Deleted a funding source";
+    changeText = [{ text: "Deleted a funding source", style: null }];
     // if the added record has a funding source, use that as the change value
     if (change.record_data.event.data.new.funding_source_id) {
-      changeValue =
-        fundingSources[change.record_data.event.data.new.funding_source_id];
+      changeText = [
+        { text: "Deleted a funding source: ", style: null },
+        {
+          text: fundingSources[
+            change.record_data.event.data.new.funding_source_id
+          ],
+          style: "boldText",
+        },
+      ];
       // if not, then check if theres a funding program
     } else if (change.record_data.event.data.new.funding_program_id) {
-      changeValue =
-        fundingPrograms[change.record_data.event.data.new.funding_program_id];
+      changeText = [
+        { text: "Deleted a funding source: ", style: null },
+        {
+          text: fundingPrograms[
+            change.record_data.event.data.new.funding_program_id
+          ],
+          style: "boldText",
+        },
+      ];
     }
-    // if we have a change value, tack on a : at the end
-    if (changeValue) {
-      changeDescription = "Deleted a funding source: ";
-    }
-    return { changeIcon, changeDescription, changeValue };
+    return { changeIcon, changeText };
   }
 
   // Multiple fields in the moped_proj_funding table can be updated at once
@@ -71,8 +91,13 @@ export const formatFundingActivity = (
     }
   });
 
-  changeDescription = "Edited a funding source ";
-  changeValue = changes.join(", ");
+  changeText = [
+    { text: "Edited a funding source by updating the ", style: null },
+    {
+      text: changes.join(", "),
+      style: "boldText",
+    },
+  ];
 
-  return { changeIcon, changeDescription, changeValue };
+  return { changeIcon, changeText };
 };
