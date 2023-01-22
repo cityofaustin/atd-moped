eeba5e3c21c54698ea634fb3f9a31eb51e8dab12
Chia Berry <analuciaby@gmail.com>
2023-01-21 18:17:03 -0600

    refactor project partners to use array

--- moped-editor/src/utils/activityLogFormatters/mopedPartnersActivity.js
+++ moped-editor/src/utils/activityLogFormatters/mopedPartnersActivity.js
@@ -2,18 +2,27 @@ import GroupOutlined from "@material-ui/icons/GroupOutlined";
 
 export const formatPartnersActivity = (change, entityList) => {
   const changeIcon = <GroupOutlined />;
-  let changeDescription = "Project partners updated";
-  let changeValue = ""
+  let changeText = [{ text: "Project partners updated", style: null }];
 
   // Adding a new partner
   if (change.description.length === 0) {
-    changeDescription = "Added project partner "
-    changeValue = entityList[change.record_data.event.data.new.entity_id]
+    changeText = [
+      { text: "Added project partner ", style: null },
+      {
+        text: entityList[change.record_data.event.data.new.entity_id],
+        style: "boldText",
+      },
+    ];
   } else {
     // Soft deleting a partner is the only update a user can do (is_deleted is set to true)
-    changeDescription = "Removed project partner "
-    changeValue = entityList[change.record_data.event.data.new.entity_id]
+    changeText = [
+      { text: "Removed project partner ", style: null },
+      {
+        text: entityList[change.record_data.event.data.new.entity_id],
+        style: "boldText",
+      },
+    ];
   }
 
-  return { changeIcon, changeDescription, changeValue };
+  return { changeIcon, changeText };
 };
