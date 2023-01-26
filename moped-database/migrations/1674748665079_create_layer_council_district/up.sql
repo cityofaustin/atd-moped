473a36368f764ddf9bb2fae78b38c940b5be21e9
John Clary <john.clary@austintexas.gov>
2023-01-26 12:16:06 -0500

    add coordinate ref system

--- moped-database/migrations/1674748665079_create_layer_council_district/up.sql
+++ moped-database/migrations/1674748665079_create_layer_council_district/up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE layer_council_district (
     id serial PRIMARY KEY,
     council_district integer UNIQUE NOT NULL,
-    geography geography ('MULTIPOLYGON') NOT NULL
+    geography geography ('MULTIPOLYGON', 4326) NOT NULL
 );
