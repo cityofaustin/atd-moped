fc81d4e41f4ed5b9983c5c7454f4dd6ecdec169e
Chia Berry <analuciaby@gmail.com>
2023-02-07 17:01:31 -0600

    update with correct outlined icon

--- moped-editor/src/utils/activityLogFormatters/mopedPhasesActivity.js
+++ moped-editor/src/utils/activityLogFormatters/mopedPhasesActivity.js
@@ -1,10 +1,10 @@
-import EventNoteIcon from "@material-ui/icons/EventNote";
+import EventNoteOutlinedIcon from '@material-ui/icons/EventNoteOutlined';
 import { ProjectActivityLogTableMaps } from "../../views/projects/projectView/ProjectActivityLogTableMaps";
 
 export const formatPhasesActivity = (change, phaseList, subphaseList) => {
   const entryMap = ProjectActivityLogTableMaps["moped_proj_phases"];
 
-  const changeIcon = <EventNoteIcon />;
+  const changeIcon = <EventNoteOutlinedIcon />;
   const phase = phaseList[change.record_data.event.data.new.phase_id];
   const subphase = subphaseList[change.record_data.event.data.new.subphase_id];
   const phaseText = {
