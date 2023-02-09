2f20fc54af8b192529bafc0f4f3f20a141027ef8
Tilly Whitson <35410637+tillyw@users.noreply.github.com>
2023-02-08 18:31:47 -0600

    adds moped_proj_contract to activity log

--- moped-editor/src/utils/activityLogHelpers.js
+++ moped-editor/src/utils/activityLogHelpers.js
@@ -8,6 +8,7 @@ import { formatPersonnelActivity } from "./activityLogFormatters/mopedPersonnelA
 import { formatNotesActivity } from "./activityLogFormatters/mopedNotesActivity";
 import { formatComponentsActivity } from "./activityLogFormatters/mopedComponentsActivity";
 import { formatProjectTypesActivity } from "./activityLogFormatters/mopedProjectTypesActivity";
+import { formatContractsActivity } from "./activityLogFormatters/mopedContractsActivity";
 
 export const formatActivityLogEntry = (change, lookupData) => {
   const changeText = [{ text: "Project was updated", style: null }];
@@ -55,6 +56,8 @@ export const formatActivityLogEntry = (change, lookupData) => {
       return formatComponentsActivity(change, lookupData.componentList);
     case "moped_project_types":
       return formatProjectTypesActivity(change, lookupData.projectTypeList);
+    case "moped_proj_contract":
+      return formatContractsActivity(change);
     default:
       return { changeIcon, changeText };
   }
