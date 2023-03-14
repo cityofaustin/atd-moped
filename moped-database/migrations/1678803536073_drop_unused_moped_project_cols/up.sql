9e968eb2e8cc2b31bc9ad822d893fe8f552d0ec5
John Clary <john.clary@austintexas.gov>
2023-03-14 12:03:18 -0400

    copy old work_assignment_ids into new contract records

--- moped-database/migrations/1678803536073_drop_unused_moped_project_cols/up.sql
+++ moped-database/migrations/1678803536073_drop_unused_moped_project_cols/up.sql
@@ -1,3 +1,22 @@
+-- copy work_assignment_id from moped_project into new moped_proj_contract_records
+DO $$
+DECLARE
+    project record;
+BEGIN
+    FOR project IN
+    SELECT
+        project_id,
+        work_assignment_id
+    FROM
+        moped_project
+    WHERE
+        work_assignment_id IS NOT NULL LOOP
+            INSERT INTO moped_proj_contract (project_id, work_assignment_id)
+                values(project.project_id, project.work_assignment_id);
+        END LOOP;
+    END$$;
+
+-- drop unused columns
 ALTER TABLE moped_project
     DROP COLUMN project_uuid,
     DROP COLUMN project_description_public,
