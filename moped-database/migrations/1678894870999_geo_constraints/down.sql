028f185ae481227bcf6b78aae398b9bb8ea2baf2
John Clary <john.clary@austintexas.gov>
2023-03-15 13:26:19 -0400

    add more constraints 🔒

--- moped-database/migrations/1678894870999_geo_constraints/down.sql
+++ moped-database/migrations/1678894870999_geo_constraints/down.sql
@@ -1,25 +1,2 @@
-ALTER TABLE "public"."features" ALTER COLUMN "component_id" DROP NOT NULL;
-ALTER TABLE "public"."feature_signals" ALTER COLUMN "component_id" DROP NOT NULL;
-ALTER TABLE "public"."feature_signals" ALTER COLUMN "knack_id" DROP NOT NULL;
-ALTER TABLE "public"."feature_signals" ALTER COLUMN "geography" DROP NOT NULL;
-ALTER TABLE "public"."feature_signals" ALTER COLUMN "geography" SET DEFAULT NULL::geography;
-ALTER TABLE "public"."feature_signals" ALTER COLUMN "signal_type" DROP NOT NULL;
-ALTER TABLE "public"."feature_signals" ALTER COLUMN "location_name" DROP NOT NULL;
-ALTER TABLE "public"."feature_signals" ALTER COLUMN "signal_id" DROP NOT NULL;
-
-ALTER TABLE "public"."features"
-    ADD COLUMN name text;
-
-ALTER TABLE "public"."feature_street_segments"
-    ADD COLUMN knack_id text,
-        ADD COLUMN project_extent_id uuid,
-            ADD COLUMN render_type text;
-
-ALTER TABLE "public"."feature_intersections"
-    ADD COLUMN project_extent_id uuid, DRADDOP COLUMN render_type text;
-
-ALTER TABLE "public"."feature_drawn_lines"
-    ADD COLUMN render_type text;
-
-ALTER TABLE "public"."feature_drawn_points"
-    ADD COLUMN render_type text;
+-- see original table declarations in 1666978291816_add_component_geometry_tables/up.sql
+SELECT 0;
--- moped-database/migrations/1678894870999_geo_constraints/up.sql
+++ moped-database/migrations/1678894870999_geo_constraints/up.sql
@@ -1,13 +1,46 @@
-ALTER TABLE "public"."feature_signals" ALTER COLUMN "signal_id" SET NOT NULL;
-ALTER TABLE "public"."feature_signals" ALTER COLUMN "location_name" SET NOT NULL;
-ALTER TABLE "public"."feature_signals" ALTER COLUMN "signal_type" SET NOT NULL;
-ALTER TABLE "public"."feature_signals" ALTER COLUMN "geography" DROP DEFAULT;
-ALTER TABLE "public"."feature_signals" ALTER COLUMN "geography" SET NOT NULL;
-ALTER TABLE "public"."feature_signals" ALTER COLUMN "knack_id" SET NOT NULL;
-ALTER TABLE "public"."feature_signals" ALTER COLUMN "component_id" SET NOT NULL;
-ALTER TABLE "public"."features" ALTER COLUMN "component_id" SET NOT NULL;
-ALTER TABLE "public"."features" DROP COLUMN name;
-ALTER TABLE "public"."feature_street_segments" DROP COLUMN knack_id, DROP COLUMN project_extent_id, DROP COLUMN render_type;
-ALTER TABLE "public"."feature_intersections" DROP COLUMN project_extent_id, DROP COLUMN render_type;
-ALTER TABLE "public"."feature_drawn_lines" DROP COLUMN render_type;
-ALTER TABLE "public"."feature_drawn_points" DROP COLUMN render_type;
+ALTER TABLE "public"."features" DROP COLUMN name, ALTER COLUMN "component_id" SET NOT NULL;
+
+ALTER TABLE "public"."feature_signals"
+    DROP COLUMN render_type,
+    ALTER COLUMN "signal_id" SET NOT NULL,
+    ALTER COLUMN "location_name" SET NOT NULL,
+    ALTER COLUMN "signal_type" SET NOT NULL,
+    ALTER COLUMN "geography" DROP DEFAULT,
+    ALTER COLUMN "geography" SET NOT NULL,
+    ALTER COLUMN "is_deleted" SET NOT NULL,
+    ALTER COLUMN "knack_id" SET NOT NULL;
+
+ALTER TABLE "public"."feature_street_segments"
+    DROP COLUMN knack_id,
+    DROP COLUMN project_extent_id,
+    DROP COLUMN render_type,
+    ALTER COLUMN "is_deleted" SET NOT NULL,
+    ALTER COLUMN "ctn_segment_id" SET NOT NULL,
+    ALTER COLUMN "source_layer" SET NOT NULL,
+    ALTER COLUMN "geography" DROP DEFAULT,
+    ALTER COLUMN "geography" SET NOT NULL;
+
+ALTER TABLE "public"."feature_intersections"
+    DROP COLUMN project_extent_id,
+    DROP COLUMN render_type,
+    ALTER COLUMN "is_deleted" SET NOT NULL,
+    ALTER COLUMN "intersection_id" SET NOT NULL,
+    ALTER COLUMN "source_layer" SET NOT NULL,
+    ALTER COLUMN "geography" DROP DEFAULT,
+    ALTER COLUMN "geography" SET NOT NULL;
+
+ALTER TABLE "public"."feature_drawn_lines"
+    DROP COLUMN render_type,
+    ALTER COLUMN "is_deleted" SET NOT NULL,
+    ALTER COLUMN "project_extent_id" SET NOT NULL,
+    ALTER COLUMN "source_layer" SET NOT NULL,
+    ALTER COLUMN "geography" DROP DEFAULT,
+    ALTER COLUMN "geography" SET NOT NULL;
+
+ALTER TABLE "public"."feature_drawn_points"
+    DROP COLUMN render_type,
+    ALTER COLUMN "is_deleted" SET NOT NULL,
+    ALTER COLUMN "project_extent_id" SET NOT NULL,
+    ALTER COLUMN "source_layer" SET NOT NULL,
+    ALTER COLUMN "geography" DROP DEFAULT,
+    ALTER COLUMN "geography" SET NOT NULL;
