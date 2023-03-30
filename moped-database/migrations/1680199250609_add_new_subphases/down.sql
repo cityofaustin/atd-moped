6c58fe7ea1895c4add9c6bcdc7a3c8b54a72cc37
Chia Berry <analuciaby@gmail.com>
2023-03-30 15:03:45 -0500

    add down migration

---
+++ moped-database/migrations/1680199250609_add_new_subphases/down.sql
@@ -0,0 +1,3 @@
+DELETE FROM moped_subphases WHERE subphase_name = 'Requested by partner';
+DELETE FROM moped_subphases WHERE subphase_name = 'Fiber/Communications';
+DELETE FROM moped_subphases WHERE subphase_name = 'Detection/CCTV';
