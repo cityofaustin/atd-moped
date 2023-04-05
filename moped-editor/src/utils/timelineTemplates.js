531e640986b2a7d61cb5d110a0fbe041cd856b58
Tilly Whitson <35410637+tillyw@users.noreply.github.com>
2023-04-05 14:25:23 -0500

    creates new AMD milestone templates

--- moped-editor/src/utils/timelineTemplates.js
+++ moped-editor/src/utils/timelineTemplates.js
@@ -1,161 +1,161 @@
-export const returnSignalPHBMilestoneTemplate = projectId => {
+export const returnAMDSignalInfrastructureMilestoneTemplate = (projectId) => {
   return [
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
-      milestone_id: 34,
+      milestone_id: 36,
       milestone_order: 1,
     },
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
-      milestone_id: 35,
+      milestone_id: 38,
       milestone_order: 2,
     },
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
-      milestone_id: 36,
+      milestone_id: 39,
       milestone_order: 3,
     },
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
-      milestone_id: 37,
+      milestone_id: 40,
       milestone_order: 4,
     },
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
-      milestone_id: 31,
+      milestone_id: 41,
       milestone_order: 5,
     },
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
-      milestone_id: 38,
+      milestone_id: 63,
       milestone_order: 6,
     },
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
-      milestone_id: 39,
+      milestone_id: 43,
       milestone_order: 7,
     },
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
-      milestone_id: 40,
+      milestone_id: 64,
       milestone_order: 8,
     },
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
-      milestone_id: 41,
+      milestone_id: 44,
       milestone_order: 9,
     },
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
-      milestone_id: 42,
+      milestone_id: 47,
       milestone_order: 10,
     },
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
-      milestone_id: 43,
+      milestone_id: 50,
       milestone_order: 11,
     },
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
-      milestone_id: 44,
+      milestone_id: 51,
       milestone_order: 12,
     },
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
-      milestone_id: 45,
-      milestone_description: "traffic and pedestrian",
+      milestone_id: 53,
       milestone_order: 13,
+    },
+  ];
+};
+
+export const returnAMDInspectionOnlyMilestoneTemplate = (projectId) => {
+  return [
+    {
+      project_id: projectId,
+      is_deleted: false,
+      completed: false,
+      milestone_id: 63,
+      milestone_order: 1,
     },
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
-      milestone_id: 46,
-      milestone_order: 14,
+      milestone_id: 43,
+      milestone_order: 2,
+    },
+    {
+      project_id: projectId,
+      is_deleted: false,
+      completed: false,
+      milestone_id: 64,
+      milestone_order: 3,
+    },
+    {
+      project_id: projectId,
+      is_deleted: false,
+      completed: false,
+      milestone_id: 44,
+      milestone_order: 4,
     },
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
       milestone_id: 47,
-      milestone_order: 15,
-    },
-    {
-      project_id: projectId,
-      is_deleted: false,
-      completed: false,
-      milestone_id: 48,
-      milestone_order: 16,
-    },
-    {
-      project_id: projectId,
-      is_deleted: false,
-      completed: false,
-      milestone_id: 49,
-      milestone_order: 17,
+      milestone_order: 5,
     },
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
       milestone_id: 50,
-      milestone_order: 18,
+      milestone_order: 6,
     },
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
       milestone_id: 51,
-      milestone_order: 19,
-    },
-    {
-      project_id: projectId,
-      is_deleted: false,
-      completed: false,
-      milestone_id: 52,
-      milestone_description: "typically 30 days",
-      milestone_order: 20,
+      milestone_order: 7,
     },
     {
       project_id: projectId,
       is_deleted: false,
       completed: false,
       milestone_id: 53,
-      milestone_description:
-        "for signals constructed by others/documentation that burn in period is complete",
-      milestone_order: 21,
+      milestone_order: 8,
     },
   ];
 };
 
-
-export const returnPDDMilestoneTemplate = projectId => {
+export const returnPDDMilestoneTemplate = (projectId) => {
   return [
     {
       project_id: projectId,
