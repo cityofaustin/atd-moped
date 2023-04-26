88837e79c1657f1f5c4b57e459fd5d8cac0ae4e1
Chia Berry <analuciaby@gmail.com>
2023-04-26 14:21:12 -0500

    hamburger menu gray

--- moped-editor/src/layouts/DashboardLayout/NavBar/DropdownMenu.js
+++ moped-editor/src/layouts/DashboardLayout/NavBar/DropdownMenu.js
@@ -34,6 +34,7 @@ const useStyles = makeStyles((theme) => ({
   dropdownButton: {
     borderRadius: "50%",
     height: "64px",
+    color: theme.palette.text.primary,
   },
   helpHeader: {
     paddingLeft: "16px",
