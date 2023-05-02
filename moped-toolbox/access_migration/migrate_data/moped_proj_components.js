366dec8a11b12763ecbddb8953077cc183850c11
John Clary <john.clary@austintexas.gov>
2023-05-02 14:11:45 -0400

    begin subcomponent mappings

--- moped-toolbox/access_migration/migrate_data/moped_proj_components.js
+++ moped-toolbox/access_migration/migrate_data/moped_proj_components.js
@@ -1,16 +1,16 @@
 const { loadJsonFile } = require("./utils/loader");
-const FACILITIES_FNAME = "./data/raw/project_facilities.json";
 const { COMPONENTS_MAP } = require("./mappings/components");
 const { mapRow } = require("./utils/misc");
-
 /**
  * Facility_Attributes - actuals associated with project facility instances (many to one)
- * Facility_AttributesTypes - domain/lookup for facility attributes
+ * Facility_AttributesTypes - domain/lookup for facility attributes - moped subcomponents
  * Facility_AttributeTypeApplicability - defines which facility types apply to which facility attributes
  * Facility_PhysicalProtectionTypes - lookup for physical protection on project_facilities instances
  * Project_Facilities - instances of project facilities - effectively moped_proj_components
  * Project_FacilityTypes - facility domain - effectively moped_components
  */
+const FACILITIES_FNAME = "./data/raw/project_facilities.json";
+const FACILITY_ATTRS_FNAME = "./data/raw/facility_attributes.json";
 
 const fields = [
   {
@@ -48,7 +48,9 @@ const fields = [
 
 function getComponents() {
   const facilities = loadJsonFile(FACILITIES_FNAME);
+  const facilityAttrs = loadJsonFile(FACILITY_ATTRS_FNAME);
 
+  debugger;
   const components = facilities.map((row) => mapRow(row, fields));
 
   return components
@@ -63,6 +65,7 @@ function getComponents() {
       return index;
     }, {});
 }
-``;
+
+// getComponents();
 
 exports.getComponents = getComponents;
