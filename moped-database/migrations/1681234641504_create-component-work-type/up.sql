ae88b0206e8d64feda5d0d7e6eeab3f5a788f6cf
John Clary <john.clary@austintexas.gov>
2023-05-05 14:34:48 -0400

    populates component_work_types table

--- moped-database/migrations/1681234641504_create-component-work-type/up.sql
+++ moped-database/migrations/1681234641504_create-component-work-type/up.sql
@@ -1,3 +1,4 @@
+-- create and fill moped_work_types
 CREATE TABLE moped_work_types (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
@@ -6,25 +7,6 @@ CREATE TABLE moped_work_types (
 );
 
 COMMENT ON TABLE moped_work_types IS 'Lookup table for component work types';
-
-CREATE TABLE moped_proj_component_work_types (
-    id SERIAL PRIMARY KEY,
-    project_component_id INTEGER NOT NULL REFERENCES moped_proj_components(project_component_id) ON UPDATE CASCADE ON DELETE CASCADE,
-    work_type_id INTEGER NOT NULL REFERENCES moped_work_types(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    is_deleted BOOLEAN NOT NULL DEFAULT FALSE
-);
-
-COMMENT ON TABLE moped_proj_component_work_types IS 'Tracks many work types per each project component';
-
-CREATE TABLE moped_component_work_types (
-    id SERIAL PRIMARY KEY,
-    work_type_id INTEGER NOT NULL REFERENCES moped_work_types(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    component_id INTEGER NOT NULL REFERENCES moped_components(component_id) ON UPDATE CASCADE ON DELETE CASCADE
-);
-
-ALTER TABLE moped_component_work_types ADD CONSTRAINT unique_work_type_component UNIQUE (work_type_id, component_id);
-
-COMMENT ON TABLE moped_component_work_types IS 'Tracks allowed moped_work_types by component type';
 
 INSERT INTO moped_work_types (name, key) VALUES 
     ('Construction Inspection', 'construction_inspection'),
@@ -40,3 +22,44 @@ INSERT INTO moped_work_types (name, key) VALUES
     ('Remove Double Yellow', 'remove_double_yellow'),
     ('Replacement', 'replacement'),
     ('Signal Take Over', 'signal_take_over');
+
+
+-- create and fill moped_component_work_types
+CREATE TABLE moped_component_work_types (
+    id SERIAL PRIMARY KEY,
+    work_type_id INTEGER NOT NULL REFERENCES moped_work_types(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    component_id INTEGER NOT NULL REFERENCES moped_components(component_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+ALTER TABLE moped_component_work_types ADD CONSTRAINT unique_work_type_component UNIQUE (work_type_id, component_id);
+
+COMMENT ON TABLE moped_component_work_types IS 'Tracks allowed moped_work_types by component type';
+
+-- for now, set new, mod, and maintenance as the default allowed types for all components
+WITH inserts_todo AS (
+    SELECT
+        id AS work_type_id,
+        component_id
+    FROM (
+        values('new'),
+            ('modification'),
+            ('maintenance_repair')) AS data (work_type_key)
+        LEFT JOIN moped_components ON TRUE
+        LEFT JOIN moped_work_types mwt ON data.work_type_key = mwt.key
+    WHERE
+        moped_components.status_id = 1
+) INSERT INTO moped_component_work_types (work_type_id, component_id)
+SELECT
+    *
+FROM
+    inserts_todo;
+
+-- create moped_proj_component_work_types to hold project data
+CREATE TABLE moped_proj_component_work_types (
+    id SERIAL PRIMARY KEY,
+    project_component_id INTEGER NOT NULL REFERENCES moped_proj_components(project_component_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    work_type_id INTEGER NOT NULL REFERENCES moped_work_types(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+COMMENT ON TABLE moped_proj_component_work_types IS 'Tracks many work types per each project component';
