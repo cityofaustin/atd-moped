c01281f02877f226b0594885da9c53ee2faa23dc
Mike <mike.dilley@gmail.com>
2023-05-10 13:28:57 -0500

    Remove components status_id, add is_deleted, update permissions metadata

---
+++ moped-database/migrations/1683740879611_alter_table_public_moped_components_add_column_is_deleted/down.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "public"."moped_components" ADD column "status_id" int4 NOT NULL DEFAULT 0;
+
+-- Go back to status_id for soft deletes
+UPDATE moped_components
+SET status_id = 0
+WHERE is_deleted = TRUE;
+
+ALTER TABLE "public"."moped_components" DROP COLUMN "is_deleted";
