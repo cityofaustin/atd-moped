fb76b3232925d911a8eaec1dc20d88e1e0688266
Chia Berry <analuciaby@gmail.com>
2023-05-30 20:45:34 -0500

    remove yet another console log i left

--- moped-editor/src/auth/user.js
+++ moped-editor/src/auth/user.js
@@ -114,7 +114,6 @@ export const initializeUserDBObject = (userObject) => {
           console.error(resData.errors)
         }
         if (resData?.data?.moped_users) {
-          console.log(resData?.data)
           setSessionDatabaseData(resData.data.moped_users[0]);
           window.location.reload();
         }
