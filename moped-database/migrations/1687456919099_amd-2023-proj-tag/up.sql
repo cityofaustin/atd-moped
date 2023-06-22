434c4e4351bd6cf39b18b8d4ea5decdfda2c5927
John Clary <john.clary@austintexas.gov>
2023-06-22 14:08:42 -0400

    remove hyphen

--- moped-database/migrations/1687456919099_amd-2023-proj-tag/up.sql
+++ moped-database/migrations/1687456919099_amd-2023-proj-tag/up.sql
@@ -1,2 +1,2 @@
 INSERT INTO moped_tags (name, TYPE, slug)
-        values('Arterial Management - 2023', 'Other', 'arterial_management_2023');
+        values('Arterial Management 2023', 'Other', 'arterial_management_2023');
