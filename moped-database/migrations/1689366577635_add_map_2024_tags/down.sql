3a5eebd7f8148101a371ec91bc4e1a3507dbe6d0
Chia Berry <analuciaby@gmail.com>
2023-07-17 09:58:31 -0500

    add new project tags for map 2024

---
+++ moped-database/migrations/1689366577635_add_map_2024_tags/down.sql
@@ -0,0 +1,10 @@
+DELETE from moped_tags WHERE slug = 'map_signals2024';
+DELETE from moped_tags WHERE slug = 'map_transitenhancement2024';
+DELETE from moped_tags WHERE slug = 'map-transit-speed-and-reliability-capmetro-ila-2024';
+DELETE from moped_tags WHERE slug = 'map-vision-zero-major-safety-2024';
+DELETE from moped_tags WHERE slug = 'map-vision-zero-ped-safety-2024';
+DELETE from moped_tags WHERE slug = 'map-vision-zero-rapid-response-small-scale-2024';
+DELETE from moped_tags WHERE slug = 'map_vzero_speedmanagement2024';
+DELETE from moped_tags WHERE slug = 'map-vision-zero-street-lighting-2024';
+DELETE from moped_tags WHERE slug = 'map-vision-zero-systemic-safety-2024';
+DELETE from moped_tags WHERE slug = 'map_vzero_systemicsafety_curves2024';
---
+++ moped-database/migrations/1689366577635_add_map_2024_tags/up.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.moped_tags ("type", name, slug) values
+        ('Work Plan', 'MAP Signals 2024', 'map_signals2024'),
+        ('Work Plan', 'MAP Transit Enhancement 2024','map_transitenhancement2024'),
+        ('Work Plan', 'MAP Transit Speed and Reliability (CapMetro ILA) 2024', 'map-transit-speed-and-reliability-capmetro-ila-2024'),
+        ('Work Plan', 'MAP Vision Zero - Major Safety 2024', 'map-vision-zero-major-safety-2024'),
+        ('Work Plan', 'MAP Vision Zero - Ped Safety 2024', 'map-vision-zero-ped-safety-2024'),
+        ('Work Plan', 'MAP Vision Zero - Rapid Response/Small Scale 2024', 'map-vision-zero-rapid-response-small-scale-2024'),
+        ('Work Plan', 'MAP Vision Zero - Speed Management 2024', 'map_vzero_speedmanagement2024'),
+        ('Work Plan', 'MAP Vision Zero - Street Lighting 2024', 'map-vision-zero-street-lighting-2024'),
+        ('Work Plan', 'MAP Vision Zero - Systemic Safety 2024', 'map-vision-zero-systemic-safety-2024'),
+        ('Work Plan', 'MAP Vision Zero - Systemic Safety Curves 2024', 'map_vzero_systemicsafety_curves2024');
