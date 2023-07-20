380f25de085b256b45693dae98de00c6da66531a
John Clary <john.clary@austintexas.gov>
2023-07-20 15:41:59 -0400

    create db migrations for work activities

---
+++ moped-database/migrations/1686243614837_create_work_auth_table/down.sql
@@ -0,0 +1,20 @@
+UPDATE moped_activity_log SET record_type = 'moped_proj_contract'
+    WHERE record_type = 'moped_proj_work_activity';
+
+ALTER TABLE moped_proj_work_activity
+    DROP CONSTRAINT status_id_fkey,
+    DROP CONSTRAINT created_by_user_fkey,
+    DROP CONSTRAINT updated_by_user_fkey,
+    DROP COLUMN interim_work_order_id_old,
+    DROP COLUMN implementation_workgroup,
+    DROP COLUMN task_orders,
+    DROP COLUMN status_id,
+    DROP COLUMN status_note,
+    DROP COLUMN created_by_user_id,
+    DROP COLUMN created_at,
+    DROP COLUMN updated_by_user_id,
+    DROP COLUMN updated_at;
+
+DROP TABLE moped_proj_work_activity_status;
+
+ALTER TABLE moped_proj_work_activity RENAME TO moped_proj_contract;
--- moped-database/migrations/1686243614837_create_work_auth_table/up.sql
+++ moped-database/migrations/1686243614837_create_work_auth_table/up.sql
@@ -1,15 +1,75 @@
-CREATE TABLE moped_proj_work_auth (
+CREATE TABLE moped_proj_work_activity_status (
     id serial PRIMARY KEY,
-    project_id integer not null references moped_project.project_id,
-    interim_work_order_id_old text,
-    implementation_workgroup_id integer references some_table.id,
-    contract_number text,
-    contractor_name text,
-    task_orders jsonb,
-    description text,
-    updated_by_user_id integer references moped_users.user_id,
-    updated_at timestamp with time zone not null default now(),
-    is_deleted boolean not null default false,
-    created_by_user_id integer references moped_users.user_id,
-    created_at timestamp with time zone not null default now(),
+    KEY text UNIQUE,
+    name text,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE
 );
+
+ALTER TABLE moped_proj_contract RENAME TO moped_proj_work_activity;
+
+ALTER TABLE moped_proj_work_activity
+    ADD COLUMN interim_work_order_id_old text,
+    ADD COLUMN implementation_workgroup text,
+    ADD COLUMN task_orders jsonb,
+    ADD COLUMN status_id integer,
+    ADD COLUMN status_note text,
+    ADD COLUMN created_by_user_id integer,
+    ADD COLUMN created_at timestamp WITH time zone NOT NULL DEFAULT now(),
+    ADD COLUMN updated_by_user_id integer,
+    ADD COLUMN updated_at timestamp WITH time zone NOT NULL DEFAULT now(),
+    ADD CONSTRAINT status_id_fkey FOREIGN KEY (status_id)
+        REFERENCES moped_proj_work_activity_status (id)
+        ON UPDATE CASCADE ON DELETE SET NULL,
+    ADD CONSTRAINT created_by_user_fkey FOREIGN KEY (created_by_user_id)
+        REFERENCES moped_users (user_id)
+        ON UPDATE CASCADE ON DELETE SET NULL, 
+    ADD CONSTRAINT updated_by_user_fkey FOREIGN KEY (updated_by_user_id)
+        REFERENCES moped_users (user_id)
+        ON UPDATE CASCADE ON DELETE SET NULL;
+
+UPDATE moped_activity_log SET record_type = 'moped_proj_work_activity'
+    WHERE record_type = 'moped_proj_contract';
+
+
+-- backfill created_by, created_at, updated_at
+-- this will need to be done during a live session bc
+-- we should disable event triggers during these updates
+-- UPDATE
+--     moped_proj_work_activity
+-- SET
+--     created_at = work_activity_insert_event.created_at,
+--     created_by_user_id = work_activity_insert_event.updated_by_user_id,
+--     updated_by_user_id = work_activity_insert_event.updated_by_user_id
+-- FROM (
+--     SELECT
+--         record_id,
+--         created_at,
+--         updated_by_user_id
+--     FROM
+--         moped_activity_log
+--     WHERE
+--         record_type = 'moped_proj_work_activity'
+--         AND operation_type = 'INSERT') AS work_activity_insert_event
+-- WHERE
+--     moped_proj_work_activity.id = work_activity_insert_event.record_id;
+
+-- -- backfill updated_at, updated_by if update events exist
+-- UPDATE moped_proj_work_activity
+-- SET
+--     updated_at = work_activity_update_event.created_at,
+--     updated_by_user_id = work_activity_update_event.updated_by_user_id
+--     FROM (
+-- SELECT DISTINCT ON (record_id)
+--     record_id,
+--     created_at,
+--     updated_by_user_id
+-- FROM
+--     moped_activity_log
+-- WHERE
+--     operation_type = 'UPDATE' AND
+--     record_type = 'moped_proj_work_activity'
+-- ORDER BY
+--     record_id,
+--     created_at DESC)  AS work_activity_update_event
+-- WHERE
+--     moped_proj_work_activity.id = work_activity_update_event.record_id;
