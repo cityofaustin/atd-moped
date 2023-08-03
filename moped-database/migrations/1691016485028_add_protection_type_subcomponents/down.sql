76683548352def608e7b74874eaf6f95baadac56
John Clary <john.clary@austintexas.gov>
2023-08-03 14:05:29 -0400

    create migrations for protection type subcomponents

---
+++ moped-database/migrations/1691016485028_add_protection_type_subcomponents/down.sql
@@ -0,0 +1,12 @@
+delete from moped_subcomponents where subcomponent_name in (
+    'Protection Type - Car Free',
+    'Protection Type - Concrete',
+    'Protection Type - Concrete and On-Street Parking',
+    'Protection Type - Concrete Traffic Bump',
+    'Protection Type - Concrete with Landscape',
+    'Protection Type - Flex Posts',
+    'Protection Type - Flex Posts (City Post)',
+    'Protection Type - Flex Posts with Curb Stops',
+    'Protection Type - Flex Posts with On-Street Parking',
+    'Protection Type - Planters',
+    'Protection Type - Raised & Behind Curb');
