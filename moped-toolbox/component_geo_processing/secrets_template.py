9667796edfa6b69f4004c457bca12f9da95aeb79
John Clary <john.clary@austintexas.gov>
2023-08-18 11:36:36 -0400

    use the right local admin secret

--- moped-toolbox/component_geo_processing/secrets_template.py
+++ moped-toolbox/component_geo_processing/secrets_template.py
@@ -3,6 +3,6 @@ HASURA = {
         "local": "http://localhost:8080/v1/graphql",
     },
     "hasura_graphql_admin_secret": {
-        "local": "DuMmyApiKeyHFVOVto19otC1wX6sP2N0VSKrCD70L10B7Sm525ZR6L672i2F79M9!",
+        "local": "hasurapassword",
     },
 }
