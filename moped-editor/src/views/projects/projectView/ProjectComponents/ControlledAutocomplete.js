6f32a6c63addb61bddf32f793d75079efd34ca22
John Clary <john.clary@austintexas.gov>
2023-08-25 14:12:46 -0400

    further abstract the MUI autocomplete

--- moped-editor/src/views/projects/projectView/ProjectComponents/ControlledAutocomplete.js
+++ moped-editor/src/views/projects/projectView/ProjectComponents/ControlledAutocomplete.js
@@ -2,6 +2,11 @@ import { Autocomplete } from "@mui/material";
 import { Controller } from "react-hook-form";
 import { TextField } from "@mui/material";
 
+const defaultOnChangeHandler = (option, field) => field.onChange(option);
+
+const defaultIsOptionEqualToValue = (option, value) =>
+  option?.value === value?.value;
+
 /**
  * A react-hook-form wrapper of the MUI autocomplete component
  * @return {JSX.Element}
@@ -15,41 +20,50 @@ export default function ControlledAutocomplete({
   label,
   autoFocus = false,
   multiple = false,
+  size = "small",
   disabled,
   helperText,
   error,
+  onChangeHandler = defaultOnChangeHandler,
+  isOptionEqualToValue = defaultIsOptionEqualToValue,
+  valueHandler,
+  filterOptions,
 }) {
   return (
     <Controller
       id={id}
       name={name}
       control={control}
-      render={({ field }) => (
-        <Autocomplete
-          {...field}
-          options={options}
-          multiple={multiple}
-          getOptionLabel={(option) => option?.label || ""}
-          isOptionEqualToValue={(option, value) =>
-            option?.value === value?.value
-          }
-          renderOption={renderOption}
-          disabled={disabled}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              inputRef={field.ref}
-              size="small"
-              label={label}
-              variant="outlined"
-              autoFocus={autoFocus}
-              helperText={helperText}
-              error={error}
-            />
-          )}
-          onChange={(_event, option) => field.onChange(option)}
-        />
-      )}
+      render={({ field }) => {
+        name === "task_orders" &&
+          console.log("RFH task order value", field.value);
+        return (
+          <Autocomplete
+            {...field}
+            value={valueHandler ? valueHandler(field.value) : field.value}
+            options={options}
+            multiple={multiple}
+            getOptionLabel={(option) => option?.label || ""}
+            isOptionEqualToValue={isOptionEqualToValue}
+            filterOptions={filterOptions || undefined}
+            renderOption={renderOption}
+            disabled={disabled}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                inputRef={field.ref}
+                size={size}
+                label={label}
+                variant="outlined"
+                autoFocus={autoFocus}
+                helperText={helperText}
+                error={error}
+              />
+            )}
+            onChange={(_event, option) => onChangeHandler(option, field)}
+          />
+        );
+      }}
     />
   );
 }
