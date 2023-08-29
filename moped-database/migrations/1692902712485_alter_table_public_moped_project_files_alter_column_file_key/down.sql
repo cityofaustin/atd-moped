e53cc1d6562e966b8b222dd58b8828274b95c57e
chiaberry <analuciaby@gmail.com>
2023-08-29 16:12:40 -0500

    add check constraint

--- moped-database/migrations/1692902712485_alter_table_public_moped_project_files_alter_column_file_key/down.sql
+++ moped-database/migrations/1692902712485_alter_table_public_moped_project_files_alter_column_file_key/down.sql
@@ -1,1 +1,5 @@
 alter table "public"."moped_project_files" alter column "file_key" set not null;
+
+
+ALTER TABLE "public"."moped_project_files"
+DROP CONSTRAINT file_location_not_null;
--- moped-database/migrations/1692902712485_alter_table_public_moped_project_files_alter_column_file_key/up.sql
+++ moped-database/migrations/1692902712485_alter_table_public_moped_project_files_alter_column_file_key/up.sql
@@ -1,1 +1,8 @@
 alter table "public"."moped_project_files" alter column "file_key" drop not null;
+
+ALTER TABLE "public"."moped_project_files"
+ADD CONSTRAINT file_location_not_null
+CHECK (
+	file_key is not null
+	OR file_url is not null
+);
