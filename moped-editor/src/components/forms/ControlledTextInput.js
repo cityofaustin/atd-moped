3424780d0411741fc45113965771ad2c572a3a11
John Clary <john.clary@austintexas.gov>
2023-08-29 16:28:25 -0400

    add value and change overrides

--- moped-editor/src/components/forms/ControlledTextInput.js
+++ moped-editor/src/components/forms/ControlledTextInput.js
@@ -1,26 +1,32 @@
 import { Controller } from "react-hook-form";
 import { TextField } from "@mui/material";
 
-// const defaultIsOptionEqualToValue = (option, value) =>
-//   option?.value === value?.value;
+const defaultOnChangeHandler = (value, field) => field.onChange(value || null);
+
+const defaultValueHandler = (value) => value || "";
 
 /**
  * A react-hook-form wrapper of the MUI autocomplete component
  * @return {JSX.Element}
  */
-export default function ControlledTextInput({ name, control, ...inputProps }) {
+export default function ControlledTextInput({
+  name,
+  control,
+  onChangeHandler = defaultOnChangeHandler,
+  valueHandler = defaultValueHandler,
+  ...inputProps
+}) {
   return (
     <Controller
       name={name}
       control={control}
       render={({ field }) => {
-        console.log("FIELD", field.value);
         return (
           <TextField
             {...field}
             {...inputProps}
-            onChange={(e) => field.onChange(e.target.value || null)}
-            value={field.value || ""}
+            onChange={(e) => onChangeHandler(e.target.value, field)}
+            value={valueHandler(field.value)}
           />
         );
       }}
