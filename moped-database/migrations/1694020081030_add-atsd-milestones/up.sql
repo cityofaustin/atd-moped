d9be342995ca6ab7a3ea05a805b2657092b67a50
John Clary <john.clary@austintexas.gov>
2023-09-06 14:48:53 -0400

    cleans up milestone columns

---
+++ moped-database/migrations/1694020081030_add-atsd-milestones/up.sql
@@ -0,0 +1,15 @@
+ALTER TABLE moped_proj_milestones
+    DROP COLUMN completion_percentage,
+    DROP COLUMN milestone_status,
+    DROP COLUMN milestone_privacy,
+    DROP COLUMN milestone_priority,
+    DROP COLUMN milestone_date_type,
+    DROP COLUMN milestone_start,
+    DROP COLUMN milestone_related_phase_id,
+    DROP COLUMN started_by_user_id,
+    DROP COLUMN completed_by_user_id,
+    DROP COLUMN is_current_milestone;
+
+ALTER TABLE moped_proj_milestones RENAME COLUMN milestone_description TO description;
+ALTER TABLE moped_proj_milestones RENAME COLUMN milestone_end TO end_date;
+ALTER TABLE moped_proj_milestones RENAME COLUMN milestone_estimate TO end_date_estimate;
