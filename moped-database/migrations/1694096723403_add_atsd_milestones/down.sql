a3a56f1c6f6cf0dc88669bacb20ecbb33278eaae
John Clary <john.clary@austintexas.gov>
2023-09-07 12:31:41 -0400

    create migration to add new milestones

---
+++ moped-database/migrations/1694096723403_add_atsd_milestones/down.sql
@@ -0,0 +1,6 @@
+DELETE FROM moped_milestones WHERE milestone_name IN (
+    'Public meeting',
+    'Start project development',
+    'Evaluation - Post',
+    'Evaluation - Pre'
+);
---
+++ moped-database/migrations/1694096723403_add_atsd_milestones/up.sql
@@ -0,0 +1,5 @@
+INSERT INTO moped_milestones (milestone_name) VALUES 
+    ('Public meeting'),
+    ('Start project development'),
+    ('Evaluation - Post'),
+    ('Evaluation - Pre');
