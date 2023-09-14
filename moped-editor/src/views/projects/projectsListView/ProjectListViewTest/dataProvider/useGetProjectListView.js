e66502c0b3cbaf0a709ae40ab69d55c8b04da262
Mike <mike.dilley@gmail.com>
2023-09-14 17:57:12 -0500

    Paste in current query and make this project list view table specific

--- moped-editor/src/views/projects/projectsListView/ProjectListViewTest/dataProvider/useGetProjectListView.js
+++ moped-editor/src/views/projects/projectsListView/ProjectListViewTest/dataProvider/useGetProjectListView.js
@@ -1,7 +1,25 @@
-const useGetProjectListView = ({
-  tableName,
+import { gql } from "apollo-boost";
+
+// TODO: We could add a table parameter to this function to allow for different tables to be queried
+export const useGetProjectListView = ({
   pagination: { limit, offset, page },
+  columnsToReturn,
 }) => {
+  const query = gql`{
+        project_list_view (
+            gqlAbstractFilters
+        ) {
+            ${columnsToReturn.join("\n")}
+        },
+        project_list_view_aggregate (
+            gqlAbstractAggregateFilters
+        ) {
+          aggregate {
+            count
+          }
+        }
+      }`;
+
   // manage query
   const { data, loading, error } = useQuery(query.gql, {
     fetchPolicy: "cache-first",
@@ -9,20 +27,6 @@ const useGetProjectListView = ({
 
   // build pagination
   // limit, offset, page
-  const queryTemplate = `{
-    ${tableName} (
-        gqlAbstractFilters
-    ) {
-        gqlAbstractColumns
-    },
-    gqlAbstractTableAggregateName (
-        gqlAbstractAggregateFilters
-    ) {
-      aggregate {
-        count
-      }
-    }
-  }`;
 
   return {
     data,
@@ -30,3 +34,50 @@ const useGetProjectListView = ({
     error,
   };
 };
+
+// This is what comes out of the current query using gqlabstract
+// This is what we need to build the query
+// {
+//     project_list_view(
+//       limit: 250
+//       offset: 0
+//       where: {}
+//       order_by: {updated_at: desc}
+//     ) {
+//       project_id
+//       project_name
+//       project_description
+//       current_phase
+//       current_phase_key
+//       project_team_members
+//       project_lead
+//       project_sponsor
+//       project_partner
+//       ecapris_subproject_id
+//       updated_at
+//       public_process_status
+//       task_order
+//       task_order_name
+//       project_feature
+//       type_name
+//       funding_source_name
+//       project_note
+//       construction_start_date
+//       completion_end_date
+//       project_inspector
+//       project_designer
+//       contractors
+//       contract_numbers
+//       project_tags
+//       added_by
+//       interim_project_id
+//       __typename
+//     }
+//     project_list_view_aggregate(where: {}, order_by: {updated_at: desc}) {
+//       aggregate {
+//         count
+//         __typename
+//       }
+//       __typename
+//     }
+//   }
