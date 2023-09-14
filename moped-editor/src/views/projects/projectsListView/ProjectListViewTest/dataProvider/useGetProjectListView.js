e226df86997a7cecb8e1e00a749bae2aa96b6a69
Mike <mike.dilley@gmail.com>
2023-09-14 17:46:44 -0500

    Add query template and start pagination props

--- moped-editor/src/views/projects/projectsListView/ProjectListViewTest/dataProvider/useGetProjectListView.js
+++ moped-editor/src/views/projects/projectsListView/ProjectListViewTest/dataProvider/useGetProjectListView.js
@@ -1,10 +1,32 @@
-const useGetProjectListView = () => {
+const useGetProjectListView = ({
+  tableName,
+  pagination: { limit, offset, page },
+}) => {
   // manage query
-  const { data, loading, error } = useQuery(
-    query.gql,
-    query.config.options.useQuery
-  );
+  const { data, loading, error } = useQuery(query.gql, {
+    fetchPolicy: "cache-first",
+  });
 
   // build pagination
   // limit, offset, page
+  const queryTemplate = `{
+    ${tableName} (
+        gqlAbstractFilters
+    ) {
+        gqlAbstractColumns
+    },
+    gqlAbstractTableAggregateName (
+        gqlAbstractAggregateFilters
+    ) {
+      aggregate {
+        count
+      }
+    }
+  }`;
+
+  return {
+    data,
+    loading,
+    error,
+  };
 };
