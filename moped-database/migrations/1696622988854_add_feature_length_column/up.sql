7e8bff515cb70f23c43539251697213dc0419e99
John Clary <john.clary@austintexas.gov>
2023-10-09 09:18:49 -0400

    no rounding needed

--- moped-database/migrations/1696622988854_add_feature_length_column/up.sql
+++ moped-database/migrations/1696622988854_add_feature_length_column/up.sql
@@ -1,7 +1,7 @@
 ALTER TABLE feature_street_segments
     ADD COLUMN length_feet INTEGER GENERATED always AS (
-Round(ST_Length(geography) * 3.28084)) stored;
+ST_Length(geography) * 3.28084) stored;
 
 ALTER TABLE feature_drawn_lines
     ADD COLUMN length_feet INTEGER GENERATED always AS (
-Round(ST_Length(geography) * 3.28084)) stored;
+ST_Length(geography) * 3.28084) stored;
