ad65ca00e102675ef44886e7c6a5c78a2b4e9d1e
John Clary <john.clary@austintexas.gov>
2023-10-12 22:14:15 -0400

    these settings

--- moped-etl/arcgis/settings.py
+++ moped-etl/arcgis/settings.py
@@ -1,72 +1,58 @@
-""" Add column names here to include them in output feature properties. The column
-must be available in the `project_list_view` view. To be compatible with ArcGIS Online
-the column must return a scalar - no objects or arrays."""
-PROJECT_LIST_KEYS = [
-    "project_id",
-    "project_description",
-    "completion_end_date",
-    "construction_start_date",
-    "current_phase",
-    "date_added",
-    "ecapris_subproject_id",
-    "funding_source_name",
-    "is_deleted",
-    "project_designer",
-    "project_inspector",
-    "project_name",
-    "project_note",
-    "project_partner",
-    "project_sponsor",
-    "project_team_members",
-    "task_order_name",
-    "type_name",
-    "updated_at",
-]
+UPLOAD_CHUNK_SIZE = 200
 
-"""This query fetches projects with components, and separately all projects from the
-`project_list_view` view. Todo: once we transition to native PostGIS data types,
-we can save a lot of code by creating a view which returns geojson"""
-QUERY_TEMPLATE = """
-    query ProjectComponentsQuery {
-    moped_project(where: {is_deleted: {_eq: false}}) {
-            project_id
-            moped_proj_tags(where: {is_deleted: {_eq: false}}) {
-                moped_tag {
-                    name
-                }
-            }
-            moped_proj_components(where: {is_deleted: {_eq: false}}) {
-                project_component_id
-      					moped_proj_component_tags {
-                  moped_component_tag {
-                    name
-                  }
-                }
-      					moped_proj_component_work_types {
-                  moped_work_type {
-                    name
-                  }
-                }
-                moped_components {
-                    component_name
-                    component_subtype
-                    line_representation
-                }
-                moped_proj_components_subcomponents {
-                  moped_subcomponent {
-                    subcomponent_name
-                  }
-                }
-            }
-        }
-        project_geography(where: { is_deleted: { _eq: false}}) {
-          feature_id
+SERVICE_NAMES = {"points": "MOPED_Points", "lines": "MOPED_Lines"}
+
+COMPONENTS_QUERY = """
+  query {
+      component_arcgis_online_view {
+          added_by
+          completion_date
+          completion_end_date
+          component_description
           component_id
-          geometry: geography
+          component_name
+          component_name_full
+          component_phase_id
+          component_phase_name
+          component_subtype
+          component_tags
+          construction_start_date
+          contract_numbers
+          contractors
           council_districts
-        }
-        project_list_view {
-            $keys
-        }
-    }
+          current_phase_name
+          current_phase_name_simple
+          ecapris_subproject_id
+          feature_ids
+          funding_source_name
+          geometry
+          interim_project_component_id
+          interim_project_id
+          is_project_component_deleted
+          is_project_deleted
+          location_description
+          project_component_id
+          project_description
+          project_designer
+          project_id
+          project_inspector
+          project_lead
+          project_name
+          project_note
+          project_partner
+          project_phase_id
+          project_phase_name
+          project_sponsor
+          project_tags
+          project_team_members
+          public_process_status
+          signal_ids
+          srts_id
+          subcomponents
+          task_order_name
+          type_name
+          updated_at
+          work_types
+      }
+  }
 """
