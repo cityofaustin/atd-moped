194bd0be039c436e192eddb1bb80a0643a03a22c
chiaberry <analuciaby@gmail.com>
2023-10-23 13:56:39 -0500

    update down migration to cant undo

--- moped-database/migrations/1698070704301_backfill_confirmed_phase_dates/down.sql
+++ moped-database/migrations/1698070704301_backfill_confirmed_phase_dates/down.sql
@@ -1,2 +1,2 @@
-UPDATE moped_proj_phases SET is_phase_start_confirmed = false WHERE phase_start <= current_date;
-UPDATE moped_proj_phases SET is_phase_end_confirmed = false WHERE phase_end <= current_date;
+-- can't undo
+select 0;
