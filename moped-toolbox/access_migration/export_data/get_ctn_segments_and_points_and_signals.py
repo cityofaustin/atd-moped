65b157bd857e2b055a6735f77ba2f4f17baaef98
John Clary <john.clary@austintexas.gov>
2023-10-28 00:51:44 -0400

    hack around weird new curve geometry

--- moped-toolbox/access_migration/export_data/get_ctn_segments_and_points_and_signals.py
+++ moped-toolbox/access_migration/export_data/get_ctn_segments_and_points_and_signals.py
@@ -6,13 +6,16 @@ from secrets import AGOL_USER, AGOL_PW
 out_dir = "./migrate_data/data/agol"
 
 services = [
-    { "name": "ctn_segments", "id": "24455c781dbd48669c601ede4b365d1f"},
-    { "name": "ctn_intersections", "id": "d92be8916fd24966aa9c33c481cd1a2c"},
-    {"name": "signals", "id": "e6eb94d1e7cc45c2ac452af6ae6aa534"}
+    {"name": "ctn_segments", "id": "24455c781dbd48669c601ede4b365d1f"},
+    {"name": "ctn_intersections", "id": "d92be8916fd24966aa9c33c481cd1a2c"},
+    {"name": "signals", "id": "e6eb94d1e7cc45c2ac452af6ae6aa534"},
 ]
 
+
 def main():
-    gis = arcgis.GIS(url="https://austin.maps.arcgis.com", username=AGOL_USER, password=AGOL_PW)
+    gis = arcgis.GIS(
+        url="https://austin.maps.arcgis.com", username=AGOL_USER, password=AGOL_PW
+    )
 
     for service_def in services:
         print(f"Getting {service_def['name']}")
@@ -23,13 +26,27 @@ def main():
         featureSet = layer.query(out_sr=4326)
         # some features for whatever reason do not have a geometry - which will cause the `to_geojson` method to fail
         # so create a new featureSet with only features who have a valid geomtery
-        featureSetGeomOnly = arcgis.features.FeatureSet( [f for f in featureSet.features if f.geometry])
+        if service_def["name"] == "ctn_segments":
+            #  ingores "curvePaths" - which seriously wtf
+            # unclear wtf these are - they appeared on 27 oct 2023
+            featureSetGeomOnly = arcgis.features.FeatureSet(
+                [
+                    f
+                    for f in featureSet.features
+                    if f.geometry and f.geometry.get("paths")
+                ]
+            )
+        else:
+            featureSetGeomOnly = arcgis.features.FeatureSet(
+                [f for f in featureSet.features if f.geometry]
+            )
         # save this data as geojson
         geojson = featureSetGeomOnly.to_geojson
         fname = f"{out_dir}/{service_def['name']}.geojson"
         print(f"Saving to {fname}")
         with open(fname, "w") as fout:
             fout.write(geojson)
+
 
 if __name__ == "__main__":
     main()
