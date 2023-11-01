de095b173e52250fb059942a4da864f723508533
chiaberry <analuciaby@gmail.com>
2023-10-31 20:38:27 -0500

    generated column and update view

---
+++ moped-database/migrations/1698783764345_add_generated_column_moped_components/up.sql
@@ -0,0 +1,5 @@
+ALTER TABLE moped_components
+    ADD COLUMN component_name_with_subtype text 
+        GENERATED ALWAYS AS (component_name::text || ' - ' || component_subtype::text) STORED;
+
+COMMENT ON COLUMN moped_components.component_name_with_subtype IS 'component name concatenated with component subtype, separated by -'
