f850c15ad35e0150ffa48db7a671fdb1f76061d4
John Clary <john.clary@austintexas.gov>
2023-11-02 14:55:29 -0400

    remove dupe code

--- moped-database/migrations/1698950636188_create_shared_use_trail_dual_path_component/up.sql
+++ moped-database/migrations/1698950636188_create_shared_use_trail_dual_path_component/up.sql
@@ -22,25 +22,6 @@ FROM
     LEFT JOIN moped_components mc ON mc.component_name = inserts_todo.component_name
         AND mc.component_subtype = inserts_todo.component_subtype;
 
--- insert all "protection type" subcomponents into moped_components_subcomponents
-WITH inserts_todo AS (
-    SELECT
-        subcomponent_id,
-        'Trail' AS component_name,
-        'Shared Use Path (Paved Dual Trail)' AS component_subtype
-    FROM
-        moped_subcomponents
-    WHERE
-        subcomponent_name LIKE 'Protection Type%'
-) INSERT INTO moped_components_subcomponents (component_id, subcomponent_id)
-SELECT
-    mc.component_id, inserts_todo.subcomponent_id
-FROM
-    inserts_todo
-    -- gets the component id of the new component we created
-    LEFT JOIN moped_components mc ON mc.component_name = inserts_todo.component_name
-        AND mc.component_subtype = inserts_todo.component_subtype;
-
 -- insert the same moped_component_work_types work types as the Trail - Shared Use Path (Paved) component
 with inserts_todo as (SELECT
     work_type_id,
