d74328583c41cc00d62f647e2c17a3db1c261eb1
chiaberry <analuciaby@gmail.com>
2023-11-02 18:02:40 -0500

    how come the cache is borking it

--- moped-editor/src/views/projects/projectView/ProjectView.js
+++ moped-editor/src/views/projects/projectView/ProjectView.js
@@ -209,6 +209,7 @@ const ProjectView = () => {
    */
   const { loading, error, data, refetch } = useQuery(SUMMARY_QUERY, {
     variables: { projectId, userId },
+    fetchPolicy: "no-cache",
   });
 
   const isFollowing = data?.moped_user_followed_projects.length > 0;
