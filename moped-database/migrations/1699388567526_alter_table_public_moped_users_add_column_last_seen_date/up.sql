d0ab3216f5d6afb0ef8c458a3cdf5a03eac99a82
John Clary <john.clary@austintexas.gov>
2023-11-07 18:04:35 -0500

    fix the syntax

--- moped-database/migrations/1699388567526_alter_table_public_moped_users_add_column_last_seen_date/up.sql
+++ moped-database/migrations/1699388567526_alter_table_public_moped_users_add_column_last_seen_date/up.sql
@@ -1,7 +1,7 @@
 ALTER TABLE "public"."moped_users"
     ADD COLUMN "last_seen_date" timestamptz NULL;
 
-COMMENT on COLUMN moped_users.last_seen_date is 'Tracks the last time a user loaded the Moped app in their browser. This value is set by the set_last_seen_date function. This value is not 100% reliable because it is updated by an API call that can be blocked by the client.'
+COMMENT on COLUMN moped_users.last_seen_date is 'Tracks the last time a user loaded the Moped app in their browser. This value is set by the set_last_seen_date function. This value is not 100% reliable because it is updated by an API call that can be blocked by the client.';
 
 -- custom function which can be tracked as a hasur mutation
 -- https://hasura.io/docs/latest/schema/postgres/custom-functions/
@@ -30,6 +30,6 @@ BEGIN
         moped_users.user_id = user_db_id;
 END;
 $$
-LANGUAGE PLPGSQL
+LANGUAGE PLPGSQL;
 
-COMMENT on FUNCTION set_last_seen_date is 'Update the moped_users.last_seen_date based on the user ID extracted from the session variable that invokes this function. It is exposed as a mutation through the Hasura grapql schema.'
+COMMENT on FUNCTION set_last_seen_date is 'Update the moped_users.last_seen_date based on the user ID extracted from the session variable that invokes this function. It is exposed as a mutation through the Hasura grapql schema.';
