32c2261818f0f19ee183b23ddcd630fd470a504b
John Clary <john.clary@austintexas.gov>
2023-11-07 17:47:14 -0500

    docstrings

--- moped-editor/src/components/ActivityMetrics.js
+++ moped-editor/src/components/ActivityMetrics.js
@@ -2,13 +2,18 @@ import { Fragment, useEffect } from "react";
 import { useMutation } from "@apollo/client";
 import { LAST_SEEN_MUTATION } from "src/queries/users";
 
+/**
+ * This wrapper calls the set_last_seen endpoint, which updates the user's
+ * last_seen_date timestamp based on their session ID. this action happens
+ * once each time this component mounts
+ */
 export default function ActivityMetrics({ children }) {
   const [setLastSeen] = useMutation(LAST_SEEN_MUTATION);
   useEffect(() => {
     try {
       setLastSeen();
     } catch (error) {
-      console.error("Failed to set last seen date date");
+      console.error("Failed to set the last seen date for the current user.");
     }
   }, []);
   return <Fragment>{children}</Fragment>;
