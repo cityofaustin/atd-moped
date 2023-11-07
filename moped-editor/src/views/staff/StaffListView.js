6929e89afaf97b423873d08da64714b44f53eb84
John Clary <john.clary@austintexas.gov>
2023-11-07 17:59:37 -0500

    add last seen to staff list

--- moped-editor/src/views/staff/StaffListView.js
+++ moped-editor/src/views/staff/StaffListView.js
@@ -87,6 +87,14 @@ const staffColumns = [
     // if the user has been deleted (is_deleted === True), then they are not active
     valueGetter: (props) => (props.value ? "No" : "Yes"),
   },
+  {
+    headerName: "Last seen",
+    field: "last_seen_date",
+    // if the user has been deleted (is_deleted === True), then they are not active
+    valueGetter: (props) =>
+      props.value ? new Date(props.value).toLocaleString() : "",
+    width: 200,
+  },
 ];
 
 const StaffListView = () => {
