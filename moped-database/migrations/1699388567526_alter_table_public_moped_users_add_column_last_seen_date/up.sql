1e85c2cc6f1457a311b4990e1cc4b532b458f4c5
John Clary <john.clary@austintexas.gov>
2023-11-07 18:02:43 -0500

    comments

--- moped-database/migrations/1699388567526_alter_table_public_moped_users_add_column_last_seen_date/up.sql
+++ moped-database/migrations/1699388567526_alter_table_public_moped_users_add_column_last_seen_date/up.sql
@@ -3,6 +3,8 @@ ALTER TABLE "public"."moped_users"
 
 COMMENT on COLUMN moped_users.last_seen_date is 'Tracks the last time a user loaded the Moped app in their browser. This value is set by the set_last_seen_date function. This value is not 100% reliable because it is updated by an API call that can be blocked by the client.'
 
+-- custom function which can be tracked as a hasur mutation
+-- https://hasura.io/docs/latest/schema/postgres/custom-functions/
 CREATE FUNCTION set_last_seen_date (hasura_session json)
     RETURNS SETOF moped_users
     AS $$
@@ -29,3 +31,5 @@ BEGIN
 END;
 $$
 LANGUAGE PLPGSQL
+
+COMMENT on FUNCTION set_last_seen_date is 'Update the moped_users.last_seen_date based on the user ID extracted from the session variable that invokes this function. It is exposed as a mutation through the Hasura grapql schema.'
