114f74ea12910145ec6e11333164da80a3ff065b
John Clary <john.clary@austintexas.gov>
2023-11-07 17:54:28 -0500

    add a comment

--- moped-database/migrations/1699388567526_alter_table_public_moped_users_add_column_last_seen_date/up.sql
+++ moped-database/migrations/1699388567526_alter_table_public_moped_users_add_column_last_seen_date/up.sql
@@ -1,6 +1,8 @@
 ALTER TABLE "public"."moped_users"
     ADD COLUMN "last_seen_date" timestamptz NULL;
 
+COMMENT on COLUMN moped_users.last_seen_date is 'Tracks the last time a user loaded the Moped app in their browser. This value is set by the set_last_seen_date function. This value is not 100% reliable because it is updated by an API call that can be blocked by the client.'
+
 CREATE FUNCTION set_last_seen_date (hasura_session json)
     RETURNS SETOF moped_users
     AS $$
