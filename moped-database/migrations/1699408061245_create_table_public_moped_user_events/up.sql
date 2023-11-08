bc7308a7e68cd58280c644d9eaeb086772a01a01
John Clary <john.clary@austintexas.gov>
2023-11-07 22:35:28 -0500

    created_at is a better col name

--- moped-database/migrations/1699408061245_create_table_public_moped_user_events/up.sql
+++ moped-database/migrations/1699408061245_create_table_public_moped_user_events/up.sql
@@ -2,7 +2,7 @@ CREATE TABLE "public"."moped_user_events" (
     "id" serial NOT NULL,
     "user_id" integer NOT NULL,
     "description" text NOT NULL,
-    "event_date" timestamptz NOT NULL DEFAULT now(),
+    "created_at" timestamptz NOT NULL DEFAULT now(),
     PRIMARY KEY ("id"),
     FOREIGN KEY ("user_id") REFERENCES "public"."moped_users" ("user_id") ON UPDATE CASCADE ON DELETE CASCADE
 );
