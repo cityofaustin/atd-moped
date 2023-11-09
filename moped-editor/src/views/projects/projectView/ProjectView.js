bad988ffa61d14323fd14717b1419aceb864b3f7
John Clary <john.clary@austintexas.gov>
2023-11-09 15:09:26 -0500

    dont use cache at all on this page :/

--- moped-editor/src/views/projects/projectView/ProjectView.js
+++ moped-editor/src/views/projects/projectView/ProjectView.js
@@ -209,7 +209,7 @@ const ProjectView = () => {
    */
   const { loading, error, data, refetch } = useQuery(SUMMARY_QUERY, {
     variables: { projectId, userId },
-    fetchPolicy: "cache-and-network",
+    fetchPolicy: "network-only",
   });
 
   const isFollowing = data?.moped_user_followed_projects.length > 0;
