2884cf1fb56b1a6edf089ce1de762175d2a6433f
John Clary <john.clary@austintexas.gov>
2023-11-14 11:43:08 -0500

    fix name and slug

--- moped-database/migrations/1699899884388_atsd_component_tags/up.sql
+++ moped-database/migrations/1699899884388_atsd_component_tags/up.sql
@@ -5,4 +5,4 @@ INSERT INTO public.moped_component_tags (name, "type", slug)
         values
         ( '2024', 'MAP Ped Crossing', 'map_ped_crossing_2024'),
         ('All', 'Ped Crossing Priority', 'ped_crossing_priority_all'),
-        ('2024', 'Bikeways – Work Plan', 'bikeways_work_plan_2025');
+        ('2024', 'Bikeways - Work Plan', 'bikeways_work_plan_2024');
