d20c83cdf8758f78883f093e4f0585376477eac9
Frank Hereford <frankhereford@users.noreply.github.com>
2023-11-14 09:25:25 -0600

    use existing function

--- moped-database/migrations/1699568640283_squashed-migration-add-audit-field-to-proj-components/down.sql
+++ moped-database/migrations/1699568640283_squashed-migration-add-audit-field-to-proj-components/down.sql
@@ -1,7 +1,5 @@
 DROP TRIGGER IF EXISTS set_updated_at_before_insert_or_update ON public.moped_proj_components;
 
-DROP FUNCTION IF EXISTS update_updated_at_column();
-
 ALTER TABLE public.moped_proj_components DROP COLUMN IF EXISTS updated_at;
 
 ALTER TABLE public.moped_proj_components DROP COLUMN IF EXISTS updated_by_user_id;
--- moped-database/migrations/1699568640283_squashed-migration-add-audit-field-to-proj-components/up.sql
+++ moped-database/migrations/1699568640283_squashed-migration-add-audit-field-to-proj-components/up.sql
@@ -6,17 +6,7 @@ alter table "public"."moped_proj_components" add column "updated_by_user_id" int
 
 alter table "public"."moped_proj_components" add column "updated_at" timestamptz null;
 
-CREATE OR REPLACE FUNCTION update_updated_at_column()
-RETURNS trigger
-LANGUAGE plpgsql
-AS $$ 
-BEGIN
-  NEW.updated_at := now();
-  RETURN NEW;
-END;
-$$;
-
 CREATE TRIGGER set_updated_at_before_insert_or_update
 BEFORE INSERT OR UPDATE ON moped_proj_components
 FOR EACH ROW
-EXECUTE FUNCTION update_updated_at_column();
+EXECUTE FUNCTION public.set_updated_at();
