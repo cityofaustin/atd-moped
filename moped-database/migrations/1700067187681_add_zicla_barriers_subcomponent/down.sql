1ac19877707ca2e28581377f3b7a31664f4b4e38
chiaberry <analuciaby@gmail.com>
2023-11-15 17:10:41 -0600

    add zicla barriers

---
+++ moped-database/migrations/1700067187681_add_zicla_barriers_subcomponent/down.sql
@@ -0,0 +1,12 @@
+DELETE FROM moped_components_subcomponents WHERE id IN (
+    SELECT
+        mcs.id
+    FROM 
+        moped_components_subcomponents mcs 
+    JOIN 
+        moped_subcomponents ms ON ms.subcomponent_id = mcs.subcomponent_id
+    WHERE 
+        subcomponent_name = 'Protection Type - ZICLA Barriers')
+
+DELETE FROM moped_subcomponents WHERE subcomponent_name = 'Protection Type - ZICLA Barriers';
+
