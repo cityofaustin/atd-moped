3b71a9ea4a5076782c415760010da9fcd72521ed
Mike <mike.dilley@gmail.com>
2023-11-20 16:52:42 -0600

    Rename file to match name of hook inside it

--- moped-editor/src/views/projects/projectsListView/ProjectsListViewTable.js
+++ moped-editor/src/views/projects/projectsListView/ProjectsListViewTable.js
@@ -24,7 +24,7 @@ import {
   useCsvExport,
   CsvDownloadDialog,
 } from "./useProjectListViewQuery/useCsvExport";
-import { useCurrentData } from "./useProjectListViewQuery/usePreviousData";
+import { useCurrentData } from "./useProjectListViewQuery/useCurrentData";
 
 /**
  * GridTable Style
