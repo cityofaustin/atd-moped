1860a4a55ca414f7b6aa0892835bfa15b4b79c3a
Frank Hereford <frankhereford@users.noreply.github.com>
2023-11-29 14:23:10 -0600

    add comments

--- moped-database/migrations/1700149643857_add_audit_fields_to_feature_tables/up.sql
+++ moped-database/migrations/1700149643857_add_audit_fields_to_feature_tables/up.sql
@@ -27,3 +27,30 @@ ADD COLUMN created_at timestamptz NOT NULL DEFAULT now(),
 ADD COLUMN created_by_user_id int4 NULL,
 ADD COLUMN updated_by_user_id int4 NULL,
 ADD COLUMN updated_at timestamptz NULL;
+
+COMMENT ON COLUMN feature_drawn_lines.created_at IS 'Timestamp of when the line feature was created';
+COMMENT ON COLUMN feature_drawn_lines.created_by_user_id IS 'User ID of the creator of the line feature';
+COMMENT ON COLUMN feature_drawn_lines.updated_by_user_id IS 'User ID of the last updater of the line feature';
+COMMENT ON COLUMN feature_drawn_lines.updated_at IS 'Timestamp of the last update of the line feature';
+
+COMMENT ON COLUMN feature_drawn_points.created_at IS 'Timestamp of when the point feature was created';
+COMMENT ON COLUMN feature_drawn_points.created_by_user_id IS 'User ID of the creator of the point feature';
+COMMENT ON COLUMN feature_drawn_points.updated_by_user_id IS 'User ID of the last updater of the point feature';
+COMMENT ON COLUMN feature_drawn_points.updated_at IS 'Timestamp of the last update of the point feature';
+
+COMMENT ON COLUMN feature_intersections.created_at IS 'Timestamp of when the intersection feature was created';
+COMMENT ON COLUMN feature_intersections.created_by_user_id IS 'User ID of the creator of the intersection feature';
+COMMENT ON COLUMN feature_intersections.updated_by_user_id IS 'User ID of the last updater of the intersection feature';
+COMMENT ON COLUMN feature_intersections.updated_at IS 'Timestamp of the last update of the intersection feature';
+
+COMMENT ON COLUMN feature_signals.created_at IS 'Timestamp of when the signal feature was created';
+COMMENT ON COLUMN feature_signals.created_by_user_id IS 'User ID of the creator of the signal feature';
+COMMENT ON COLUMN feature_signals.updated_by_user_id IS 'User ID of the last updater of the signal feature';
+COMMENT ON COLUMN feature_signals.updated_at IS 'Timestamp of the last update of the signal feature';
+
+COMMENT ON COLUMN feature_street_segments.created_at IS 'Timestamp of when the street segment feature was created';
+COMMENT ON COLUMN feature_street_segments.created_by_user_id IS 'User ID of the creator of the street segment feature';
+COMMENT ON COLUMN feature_street_segments.updated_by_user_id IS 'User ID of the last updater of the street segment feature';
+COMMENT ON COLUMN feature_street_segments.updated_at IS 'Timestamp of the last update of the street segment feature';
+
+
