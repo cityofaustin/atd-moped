5eda7b31f229bba5890ff69cbcb0d7da66ba4cc0
Mike <mike.dilley@gmail.com>
2023-11-29 12:11:34 -0600

    Fix secrets template key names

--- moped-toolbox/append_srts_desc_to_components_desc/secrets_template.py
+++ moped-toolbox/append_srts_desc_to_components_desc/secrets_template.py
@@ -1,10 +1,10 @@
 HASURA = {
-    "HASURA_GRAPHQL_ENDPOINT": {
+    "HASURA_ENDPOINT": {
         "local": "http://localhost:8080/v1/graphql",
         "staging": "",
         "prod": "",
     },
-    "HASURA_GRAPHQL_ADMIN_SECRET": {
+    "HASURA_ADMIN_SECRET": {
         "local": "hasurapassword",
         "staging": "",
         "prod": "",
