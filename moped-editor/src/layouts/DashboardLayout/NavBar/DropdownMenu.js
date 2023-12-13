3cd3833e30cab2565922faad32e441f30955da3f
John Clary <john.clary@austintexas.gov>
2023-12-13 14:59:50 -0500

    add agol viewer and rework icons

--- moped-editor/src/layouts/DashboardLayout/NavBar/DropdownMenu.js
+++ moped-editor/src/layouts/DashboardLayout/NavBar/DropdownMenu.js
@@ -11,9 +11,13 @@ import {
 import makeStyles from "@mui/styles/makeStyles";
 import { useNavigate } from "react-router-dom";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
-import OpenInNewIcon from "@mui/icons-material/OpenInNew";
-import MenuIcon from "@mui/icons-material/Menu";
+import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 import MenuBookOutlined from "@mui/icons-material/MenuBookOutlined";
+import MenuIcon from "@mui/icons-material/Menu";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import CDNAvatar from "../../../components/CDN/Avatar";
 import { getSessionDatabaseData, useUser } from "../../../auth/user";
 import { getInitials } from "src/utils/userNames";
@@ -30,12 +34,20 @@ export const helpItems = [
   {
     linkType: "external",
     link: "https://atd.knack.com/dts#new-service-request/?view_249_vars=%7B%22field_399%22%3A%22Moped%22%7D",
-    title: "Ask for help",
+    title: "Contact support",
+    Icon: <LocalPhoneIcon fontSize="small" />,
   },
   {
     linkType: "external",
     link: "https://atd-dts.gitbook.io/moped/",
-    title: "Moped user guide ",
+    title: "User guide ",
+    Icon: <HelpOutlineOutlinedIcon fontSize="small" />,
+  },
+  {
+    linkType: "external",
+    link: "https://teams.microsoft.com/l/channel/19%3ab1179ddfc92d44ea9abb23db713eb60c%40thread.tacv2/General?groupId=54a90854-d3fa-4053-9173-5352715bab37&tenantId=5c5e19f6-a6ab-4b45-b1d0-be4608a9a67f",
+    title: "Micrsoft Teams",
+    Icon: <ChatOutlinedIcon fontSize="small" />,
   },
   {
     linkType: "internal",
@@ -120,6 +132,21 @@ const DropdownMenu = ({
             />
           </ListItemIcon>
           Account
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleDropdownClose}>
+          <ListItemIcon>
+            <MapOutlinedIcon fontSize="small" />
+          </ListItemIcon>
+          <Link
+            href="https://austin.maps.arcgis.com/apps/webappviewer/index.html?id=404d31d56b57491abe53ccfd718fcaee"
+            target="_blank"
+            rel="noopener noreferrer"
+            color="inherit"
+            underline="none"
+          >
+            ArcGIS Online Map
+          </Link>
         </MenuItem>
         <Divider />
         <span className={classes.helpHeader}>
