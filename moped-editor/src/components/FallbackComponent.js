4616df25ae2828a060b07b7dcf1fb21c3c98c504
chiaberry <analuciaby@gmail.com>
2023-12-19 17:03:09 -0600

    include email in bug report

--- moped-editor/src/components/FallbackComponent.js
+++ moped-editor/src/components/FallbackComponent.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Box, Container, Typography } from "@mui/material";
 import makeStyles from "@mui/styles/makeStyles";
 import ExternalLink from "./ExternalLink";
+import { useUser } from "src/auth/user";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,11 +28,16 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const createBugReportLink = (error) =>
-  `https://atd.knack.com/dts#new-service-request/?view_249_vars=%7B%22field_398%22%3A%22Bug%20Report%20%E2%80%94%20Something%20is%20not%20working%22%2C%22field_399%22%3A%22Moped%22%2C%22field_400%22%3A%22${error.message}%22%7D`;
+// includes the error.message as the text in field 400 ("Describe the problem")
+// and the email of the user logged in in field 406
+const createBugReportLink = (error, userEmail) => {
+  return `https://atd.knack.com/dts#new-service-request/?view_249_vars=%7B%22field_398%22%3A%22Bug%20Report%20%E2%80%94%20Something%20is%20not%20working%22%2C%22field_399%22%3A%22Moped%22%2C%22field_400%22%3A%22${error.message}%22%2C%22field_406%22%3A%22${userEmail}%22%7D`;
+}
 
 const FallbackComponent = ({ error, resetErrorBoundary }) => {
   const classes = useStyles();
+  const { user } = useUser();
+  const userEmail = user?.idToken?.payload?.email
 
   return (
     <Box
@@ -65,7 +71,7 @@ const FallbackComponent = ({ error, resetErrorBoundary }) => {
           You can try refreshing the page or{" "}
           {
             <ExternalLink
-              url={createBugReportLink(error)}
+              url={createBugReportLink(error, userEmail)}
               text={"click here"}
             />
           }{" "}
