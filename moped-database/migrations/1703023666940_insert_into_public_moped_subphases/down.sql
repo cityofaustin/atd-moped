1f07257c0cb3682ffa11e752ba08d2808f3f1cf9
rose <rose.eichelmann@gmail.com>
2023-12-19 16:19:53 -0600

    add new subphases

---
+++ moped-database/migrations/1703023666940_insert_into_public_moped_subphases/down.sql
@@ -0,0 +1,5 @@
+DELETE FROM "public"."moped_subphases" WHERE "subphase_id" = 28;
+DELETE FROM "public"."moped_subphases" WHERE "subphase_id" = 29;
+DELETE FROM "public"."moped_subphases" WHERE "subphase_id" = 30;
+DELETE FROM "public"."moped_subphases" WHERE "subphase_id" = 31;
+DELETE FROM "public"."moped_subphases" WHERE "subphase_id" = 32;
---
+++ moped-database/migrations/1703023666940_insert_into_public_moped_subphases/up.sql
@@ -0,0 +1,5 @@
+INSERT INTO "public"."moped_subphases"("subphase_id", "subphase_name", "subphase_description", "related_phase_id") VALUES (28, E'Discovery', null, 1);
+INSERT INTO "public"."moped_subphases"("subphase_id", "subphase_name", "subphase_description", "related_phase_id") VALUES (29, E'Vetting', null, 4);
+INSERT INTO "public"."moped_subphases"("subphase_id", "subphase_name", "subphase_description", "related_phase_id") VALUES (30, E'Commissioning', null, 7);
+INSERT INTO "public"."moped_subphases"("subphase_id", "subphase_name", "subphase_description", "related_phase_id") VALUES (31, E'In-Service', null, 9);
+INSERT INTO "public"."moped_subphases"("subphase_id", "subphase_name", "subphase_description", "related_phase_id") VALUES (32, E'Close-Out', null, 11);
