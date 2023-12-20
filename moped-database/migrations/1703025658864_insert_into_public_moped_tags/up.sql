dbc050a6acb4c2a0e8210b5b8a897cefc1a4428f
Tilly Whitson <35410637+tillyw@users.noreply.github.com>
2023-12-19 18:27:04 -0600

    updates moped tags

---
+++ moped-database/migrations/1703025658864_insert_into_public_moped_tags/up.sql
@@ -0,0 +1,3 @@
+UPDATE "public"."moped_tags" SET "name" = 'Arterial Management - Inspection' WHERE "slug" = 'arterial_management';
+INSERT INTO "public"."moped_tags"("is_deleted", "id", "name", "slug", "type") VALUES (false, 62, E'Arterial Management - Request', E'arterial_management_request', E'Other');
+INSERT INTO "public"."moped_tags"("is_deleted", "id", "name", "slug", "type") VALUES (false, 61, E'Street Impact Fee (SIF)', E'street_impact_fee', E'Other');
