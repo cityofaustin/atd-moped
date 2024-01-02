0731090a1655bfe343abc3bef9be14840195a90c
chiaberry <analuciaby@gmail.com>
2024-01-02 14:02:46 -0600

    also add it to the project list view

--- moped-editor/src/views/projects/projectsListView/ProjectsListView.js
+++ moped-editor/src/views/projects/projectsListView/ProjectsListView.js
@@ -1,7 +1,9 @@
 import React from "react";
 import makeStyles from "@mui/styles/makeStyles";
+import { ErrorBoundary } from "react-error-boundary";
 import Page from "src/components/Page";
 import ProjectsListViewTable from "./ProjectsListViewTable";
+import FallbackComponent from "src/components/FallbackComponent";
 
 // Styles
 const useStyles = makeStyles((theme) => ({
@@ -22,7 +24,9 @@ const ProjectsListView = () => {
 
   return (
     <Page className={classes.root} title="Projects">
-      <ProjectsListViewTable title={"Projects"} />
+      <ErrorBoundary FallbackComponent={FallbackComponent}>
+        <ProjectsListViewTable title={"Projects"} />
+      </ErrorBoundary>
     </Page>
   );
 };
