a2a214ef3d747ac42f13ec93ebe7dc5b331e3764
Tilly Whitson <35410637+tillyw@users.noreply.github.com>
2024-01-02 16:07:49 -0600

    update slug and remove manual id insertion

--- moped-database/migrations/1703025658864_insert_into_public_moped_tags/down.sql
+++ moped-database/migrations/1703025658864_insert_into_public_moped_tags/down.sql
@@ -1,3 +1,3 @@
-UPDATE "public"."moped_tags" SET "name" = 'Arterial Management' WHERE "slug" = 'arterial_management';
+UPDATE "public"."moped_tags" SET "name" = 'Arterial Management', "slug" = 'arterial_management' WHERE "slug" = 'arterial_management_inspection';
 DELETE FROM "public"."moped_tags" WHERE "slug" = 'arterial_management_request';
 DELETE FROM "public"."moped_tags" WHERE "slug" = 'street_impact_fee';
--- moped-database/migrations/1703025658864_insert_into_public_moped_tags/up.sql
+++ moped-database/migrations/1703025658864_insert_into_public_moped_tags/up.sql
@@ -1,3 +1,3 @@
-UPDATE "public"."moped_tags" SET "name" = 'Arterial Management - Inspection' WHERE "slug" = 'arterial_management';
-INSERT INTO "public"."moped_tags"("is_deleted", "id", "name", "slug", "type") VALUES (false, 62, E'Arterial Management - Request', E'arterial_management_request', E'Other');
-INSERT INTO "public"."moped_tags"("is_deleted", "id", "name", "slug", "type") VALUES (false, 61, E'Street Impact Fee (SIF)', E'street_impact_fee', E'Other');
+UPDATE "public"."moped_tags" SET "name" = 'Arterial Management - Inspection', "slug" = 'arterial_management_inspection' WHERE "slug" = 'arterial_management';
+INSERT INTO "public"."moped_tags"("is_deleted", "name", "slug", "type") VALUES (false, E'Arterial Management - Request', E'arterial_management_request', E'Other');
+INSERT INTO "public"."moped_tags"("is_deleted", "name", "slug", "type") VALUES (false, E'Street Impact Fee (SIF)', E'street_impact_fee', E'Other');
