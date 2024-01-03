7bef946392fced34d1b28fa8605f62e1ef9799e4
John Clary <john.clary@austintexas.gov>
2024-01-02 21:01:12 -0500

    use subphase_name and related_phase_id in down migra

--- moped-database/migrations/1703023666940_insert_into_public_moped_subphases/down.sql
+++ moped-database/migrations/1703023666940_insert_into_public_moped_subphases/down.sql
@@ -1,5 +1,5 @@
-DELETE FROM "public"."moped_subphases" WHERE "subphase_id" = 28;
-DELETE FROM "public"."moped_subphases" WHERE "subphase_id" = 29;
-DELETE FROM "public"."moped_subphases" WHERE "subphase_id" = 30;
-DELETE FROM "public"."moped_subphases" WHERE "subphase_id" = 31;
-DELETE FROM "public"."moped_subphases" WHERE "subphase_id" = 32;
+DELETE FROM "public"."moped_subphases" WHERE "subphase_name" = 'Discovery' and "related_phase_id" = 1;
+DELETE FROM "public"."moped_subphases" WHERE "subphase_name" = 'Vetting'  and "related_phase_id" = 4;
+DELETE FROM "public"."moped_subphases" WHERE "subphase_name" = 'Commissioning'  and "related_phase_id" = 7;
+DELETE FROM "public"."moped_subphases" WHERE "subphase_name" = 'In-Service'  and "related_phase_id" = 9;
+DELETE FROM "public"."moped_subphases" WHERE "subphase_name" = 'Close-Out'  and "related_phase_id" = 11;
