bbf734928dd5db8043c66c803c2e3f436ba9b2ad
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-10 14:45:14 -0600

    Rename columns and add comments for updated_at and updated_by_user_id in moped_proj_funding table

--- moped-database/migrations/1704919187107_moped_proj_funding_rename_columns/up.sql
+++ moped-database/migrations/1704919187107_moped_proj_funding_rename_columns/up.sql
@@ -3,3 +3,6 @@ ALTER TABLE moped_proj_funding RENAME COLUMN added_by TO created_by_user_id;
 
 ALTER TABLE moped_proj_funding ADD COLUMN updated_at TIMESTAMP WITH TIME ZONE;
 ALTER TABLE moped_proj_funding ADD COLUMN updated_by_user_id INTEGER;
+
+COMMENT ON COLUMN moped_proj_funding.updated_at IS 'Timestamp when the record was last updated';
+COMMENT ON COLUMN moped_proj_funding.updated_by_user_id IS 'ID of the user who last updated the record';
