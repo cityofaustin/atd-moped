2a2f2961002cfa41c8ca80d1764e60574725538d
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-11 10:29:06 -0600

    Clean-up, organize and comments

--- moped-database/migrations/1704919187107_moped_proj_funding_rename_columns/down.sql
+++ moped-database/migrations/1704919187107_moped_proj_funding_rename_columns/down.sql
@@ -1,10 +1,13 @@
--- DROP TRIGGER set_updated_at_before_update ON moped_proj_funding;
-
-ALTER TABLE moped_proj_funding DROP COLUMN updated_at;
-ALTER TABLE moped_proj_funding DROP COLUMN updated_by_user_id;
-
-ALTER TABLE moped_proj_funding RENAME COLUMN created_at TO date_added;
-ALTER TABLE moped_proj_funding RENAME COLUMN created_by_user_id TO added_by;
-
+-- Drop the trigger
 DROP TRIGGER update_self_and_project_audit_fields_audit_fields ON moped_proj_funding;
+
+-- Drop the function
 DROP FUNCTION public.update_self_and_project_updated_audit_fields();
+
+-- Drop the new columns
+ALTER TABLE moped_proj_funding DROP COLUMN updated_by_user_id;
+ALTER TABLE moped_proj_funding DROP COLUMN updated_at;
+
+-- Rename the old columns back to their original names
+ALTER TABLE moped_proj_funding RENAME COLUMN created_by_user_id TO added_by;
+ALTER TABLE moped_proj_funding RENAME COLUMN created_at TO date_added;
