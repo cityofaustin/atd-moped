20fba7816ba75353944f5a98f8b7a42d35d98ed2
John Clary <john.clary@austintexas.gov>
2024-01-12 13:16:02 -0500

    exclude deleted subcomponents and component tags

--- moped-database/views/component_arcgis_online_view.sql
+++ moped-database/views/component_arcgis_online_view.sql
@@ -174,7 +174,8 @@ CREATE OR REPLACE VIEW component_arcgis_online_view AS (
             string_agg(ms.subcomponent_name, ', ') subcomponents 
         FROM 
             moped_proj_components_subcomponents mpcs 
-            LEFT JOIN moped_subcomponents ms ON mpcs.subcomponent_id = ms.subcomponent_id 
+            LEFT JOIN moped_subcomponents ms ON mpcs.subcomponent_id = ms.subcomponent_id
+            WHERE mpcs.is_deleted = FALSE
         GROUP BY 
             project_component_id
         ) subcomponents ON subcomponents.project_component_id = mpc.project_component_id 
@@ -196,7 +197,8 @@ CREATE OR REPLACE VIEW component_arcgis_online_view AS (
             string_agg(mct.type || ' - ' || mct.name, ', ') component_tags 
         FROM 
             moped_proj_component_tags mpct 
-            LEFT JOIN moped_component_tags mct ON mpct.component_tag_id = mct.id 
+            LEFT JOIN moped_component_tags mct ON mpct.component_tag_id = mct.id
+            WHERE mpct.is_deleted = FALSE
         GROUP BY 
             project_component_id
         ) component_tags ON component_tags.project_component_id = mpc.project_component_id 
