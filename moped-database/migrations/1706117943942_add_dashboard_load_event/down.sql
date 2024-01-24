d41b8a01979f04aa29563926b2a0252d47c778b2
John Clary <john.clary@austintexas.gov>
2024-01-24 12:41:07 -0500

    add dashboard_load event logger

---
+++ moped-database/migrations/1706117943942_add_dashboard_load_event/down.sql
@@ -0,0 +1,4 @@
+ALTER TABLE moped_user_events DROP CONSTRAINT moped_user_events_event_name_check;
+
+ALTER TABLE moped_user_events
+    ADD CONSTRAINT moped_user_events_event_name_check CHECK (event_name in('app_load'));
