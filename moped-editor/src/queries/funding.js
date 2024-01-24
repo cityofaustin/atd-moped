4c44b0784e8729d522335633b29b3298d467a25a
rose <rose.eichelmann@gmail.com>
2024-01-24 11:05:41 -0600

    test for netlify

--- moped-editor/src/queries/funding.js
+++ moped-editor/src/queries/funding.js
@@ -45,7 +45,7 @@ export const FUNDING_QUERY = gql`
 export const UPDATE_PROJECT_FUNDING = gql`
   mutation UpdateProjectFunding(
     $proj_funding_id: Int!
-    $funding_amount: Int
+    $funding_amount: Int!
     $funding_description: String
     $funding_program_id: Int
     $funding_source_id: Int
