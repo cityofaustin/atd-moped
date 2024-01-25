7cfe51bda15feb420ce39be8545046825936cfb1
John Clary <john.clary@austintexas.gov>
2024-01-25 14:43:29 -0500

    create phase audit field migrations

---
+++ moped-database/migrations/1706210016477_moped_proj_phases_audit_fields/down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE moped_proj_phases RENAME COLUMN created_at TO date_added;
+ALTER TABLE moped_proj_phases DROP COLUMN created_by_user_id;
+ALTER TABLE moped_proj_phases DROP COLUMN updated_at;
+ALTER TABLE moped_proj_phases DROP COLUMN updated_by_user_id;
+ALTER TABLE moped_proj_phases DROP TRIGGER update_moped_proj_phases_and_project_audit_fields;
