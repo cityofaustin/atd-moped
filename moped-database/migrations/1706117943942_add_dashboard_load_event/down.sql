6ce059a7c6e6e0400aab4eb30cb42c5490634460
John Clary <john.clary@austintexas.gov>
2024-01-26 10:07:06 -0500

    fix down migra

--- moped-database/migrations/1706117943942_add_dashboard_load_event/down.sql
+++ moped-database/migrations/1706117943942_add_dashboard_load_event/down.sql
@@ -1,3 +1,5 @@
+DELETE FROM moped_user_events where event_name = 'dashboard_load';
+
 ALTER TABLE moped_user_events DROP CONSTRAINT moped_user_events_event_name_check;
 
 ALTER TABLE moped_user_events
