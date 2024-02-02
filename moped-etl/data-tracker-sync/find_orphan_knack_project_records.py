c26c36807a927c0d5ecdcb85581af8ff30271b60
Mike <mike.dilley@gmail.com>
2024-02-02 17:59:19 -0600

    Get work order signals connected to projects table records

--- moped-etl/data-tracker-sync/find_orphan_knack_project_records.py
+++ moped-etl/data-tracker-sync/find_orphan_knack_project_records.py
@@ -14,6 +14,9 @@ KNACK_DATA_TRACKER_APP_ID = os.getenv("KNACK_DATA_TRACKER_APP_ID")
 KNACK_DATA_TRACKER_API_KEY = os.getenv("KNACK_DATA_TRACKER_API_KEY")
 
 KNACK_DATA_TRACKER_PROJECT_OBJECT = "object_201"
+KNACK_DATA_TRACKER_WORK_ORDER_SIGNALS_OBJECT = "object_31"
+
+WORK_ORDER_SIGNALS_PROJECT_FIELD = "field_3965"
 
 GET_MOPED_PROJECTS = """
 query GetMopedProjects {
@@ -25,41 +28,13 @@ query GetMopedProjects {
 """
 
 
-# def create_knack_project_from_moped_project(moped_project_record, is_test=False):
-#     """
-#     Create a Knack project record to sync a Moped project to Data Tracker
-
-#     Parameters:
-#         moped_project_record (dict): A Moped project record
-#         is_test (boolean): test flag added to add a complatible Knack signal record id to payload
-
-#     Returns:
-#         String: Knack record ID of created record
-#     """
-#     knack_project_record = build_knack_project_from_moped_project(
-#         moped_project_record=moped_project_record, is_test=is_test
-#     )
-
-#     created = knackpy.api.record(
-#         app_id=KNACK_DATA_TRACKER_APP_ID,
-#         api_key=KNACK_DATA_TRACKER_API_KEY,
-#         method="create",
-#         data=knack_project_record,
-#         obj=KNACK_DATA_TRACKER_PROJECT_OBJECT,
-#     )
-
-#     logger.debug(f"Created Knack record: {created}")
-#     knack_record_id = created["id"]
-#     return knack_record_id
-
-
 def get_synced_moped_project_knack_ids():
     data = make_hasura_request(query=GET_MOPED_PROJECTS)
     projects = data["moped_project"]
     knack_ids = [project["knack_project_id"] for project in projects]
 
-    logger.debug(f"Found Knack IDs: {knack_ids}")
-    logger.info(f"Found {len(knack_ids)} Knack IDs")
+    logger.debug(f"Found Moped Project Knack IDs: {knack_ids}")
+    logger.info(f"Found {len(knack_ids)} Moped Project Knack IDs")
     return knack_ids
 
 
@@ -70,11 +45,29 @@ def get_knack_project_record_ids():
         obj=KNACK_DATA_TRACKER_PROJECT_OBJECT,
     )
     knack_ids = [project["id"] for project in knack_projects]
-    logger.info(knack_projects[0])
 
     logger.debug(f"Found Knack projects: {knack_ids}")
     logger.info(f"Found {len(knack_ids)} Knack projects")
     return knack_ids
+
+
+def check_for_work_order_signals_connection(knack_id):
+    work_orders = knackpy.api.get(
+        app_id=KNACK_DATA_TRACKER_APP_ID,
+        api_key=KNACK_DATA_TRACKER_API_KEY,
+        obj=KNACK_DATA_TRACKER_WORK_ORDER_SIGNALS_OBJECT,
+        filters=[
+            {
+                "field": WORK_ORDER_SIGNALS_PROJECT_FIELD,
+                "operator": "is",
+                "value": knack_id,
+            }
+        ],
+    )
+
+    logger.debug(f"Found Knack work orders: {work_orders}")
+    logger.info(f"Found {len(work_orders)} Knack work orders")
+    return work_orders
 
 
 def main(args):
@@ -84,23 +77,30 @@ def main(args):
     ids_in_both_tables = list(
         set(knack_project_ids_in_knack) & set(knack_project_ids_in_moped)
     )
+    logger.info(f"Found {len(ids_in_both_tables)} records in both tables")
+
     ids_not_in_both_tables = list(
         set(knack_project_ids_in_knack) - set(knack_project_ids_in_moped)
     )
-
-    logger.info(f"Found {len(ids_in_both_tables)} records in both tables")
-    # logger.debug(f"IDs in both tables: {ids_in_both_tables}")
-
     logger.info(
         f"Found {len(ids_not_in_both_tables)} records in Knack but not in Moped"
     )
-    # logger.debug(f"IDs in Knack but not in Moped: {ids_not_in_both_tables}")
 
-    # logger.info(f"Done syncing.")
-    # logger.info(f"Created {len(created_knack_records)} new Knack records")
-    # logger.debug(f"Records created: {created_knack_records}")
-    # logger.info(f"Updated {len(updated_knack_records)} existing Knack records")
-    # logger.debug(f"Updated Knack records: {updated_knack_records}")
+    deletes_to_skip = []
+    for id in ids_not_in_both_tables:
+        project = check_for_work_order_signals_connection(id)
+        logger.debug(
+            f"Found work order signals connected to project ID {id}: {project}"
+        )
+        if len(project) > 0:
+            deletes_to_skip.append(id)
+
+    logger.info(f"Found {len(deletes_to_skip)} records to skip deleting")
+    logger.debug(f"Records to skip deleting: {deletes_to_skip}")
+
+    # TODO: Do the delete part
+
+    logger.info(f"Done.")
 
 
 if __name__ == "__main__":
@@ -110,7 +110,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    log_level = logging.INFO
+    log_level = logging.DEBUG
     logger = get_logger(name="knack-orphan-records", level=log_level)
     logger.info(f"Starting.")
 
