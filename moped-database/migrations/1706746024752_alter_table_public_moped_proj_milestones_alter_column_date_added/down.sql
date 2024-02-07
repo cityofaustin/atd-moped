f6db79a676b349f9288a14a5dcea2dfb9ff8a0ac
Tilly Whitson <35410637+tillyw@users.noreply.github.com>
2024-02-06 18:11:14 -0600

    add new columns

--- moped-database/migrations/1706746024752_alter_table_public_moped_proj_milestones_alter_column_date_added/down.sql
+++ moped-database/migrations/1706746024752_alter_table_public_moped_proj_milestones_alter_column_date_added/down.sql
@@ -1,1 +1,4 @@
 alter table "public"."moped_proj_milestones" rename column "created_at" to "date_added";
+alter table "public"."moped_proj_milestones" drop column "created_by_user_id";
+alter table "public"."moped_proj_milestones" drop column "updated_at";
+alter table "public"."moped_proj_milestones" drop column "updated_by_user_id";
--- moped-database/migrations/1706746024752_alter_table_public_moped_proj_milestones_alter_column_date_added/up.sql
+++ moped-database/migrations/1706746024752_alter_table_public_moped_proj_milestones_alter_column_date_added/up.sql
@@ -1,1 +1,7 @@
 alter table "public"."moped_proj_milestones" rename column "date_added" to "created_at";
+alter table "public"."moped_proj_milestones" add column "created_by_user_id" integer
+ null;
+alter table "public"."moped_proj_milestones" add column "updated_at" timestamptz
+ null;
+alter table "public"."moped_proj_milestones" add column "updated_by_user_id" integer
+ null;
