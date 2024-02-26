36bed5e44a2286539a864b8e8b20fb6023befad5
Frank Hereford <frankhereford@users.noreply.github.com>
2024-02-26 14:14:02 -0600

    modify view

--- moped-database/migrations/1708978121747_demo-view-exporter/up.sql
+++ moped-database/migrations/1708978121747_demo-view-exporter/up.sql
@@ -1,4 +1,4 @@
 CREATE VIEW dad_joke AS
 SELECT
     'Why did the scarecrow win an award?' AS setup,
-    'Because he was outstanding in his field!' AS punchline;
+    'Because he was outstanding in his field! 🥁' AS punchline;
