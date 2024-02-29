e75d1f8bd1dcd56eb0daeabcd8fe1f874caf7b9c
Tilly Whitson <35410637+tillyw@users.noreply.github.com>
2024-02-29 14:03:58 -0600

    remove redundant useSearch function

--- moped-editor/src/utils/gridTableHelpers.js
+++ moped-editor/src/utils/gridTableHelpers.js
@@ -7,12 +7,12 @@
  * @param {*} value - The value in question
  * @returns {*} - The value output
  */
-export const getSearchValue = (query, column, value) => {
+export const getSearchValue = (queryConfig, column, value) => {
   // Retrieve the type of field (string, float, int, etc)
-  const type = query.config.columns[column].type.toLowerCase();
+  const type = queryConfig.columns[column].type.toLowerCase();
   // Get the invalidValueDefault in the search config object
   const invalidValueDefault =
-    query.config.columns[column].search?.invalidValueDefault ?? null;
+    queryConfig.columns[column].search?.invalidValueDefault ?? null;
   // If the type is number of float, attempt to parse as such
   if (["number", "float", "double"].includes(type)) {
     value = Number.parseFloat(value) || invalidValueDefault;
@@ -20,6 +20,10 @@ export const getSearchValue = (query, column, value) => {
   // If integer, attempt to parse as integer
   if (["int", "integer"].includes(type)) {
     value = Number.parseInt(value) || invalidValueDefault;
+  }
+  // If string, remove unrecognized graphQL characters (double-quotes or backslash)
+  if (typeof value === "string") {
+    value = value.replace(/"|\\/g, "");
   }
   // Any other value types are pass-through for now
   return value;
