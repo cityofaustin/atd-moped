1d356d9ce0e53ea5d7d2eef25e365f204f40ed0a
chiaberry <analuciaby@gmail.com>
2024-03-07 17:13:18 -0600

    wrong table name in down migration

--- moped-database/migrations/1709753345334_audit_fields_proj_files/down.sql
+++ moped-database/migrations/1709753345334_audit_fields_proj_files/down.sql
@@ -1,6 +1,6 @@
-ALTER TABLE moped_proj_files RENAME COLUMN created_at TO create_date;
-ALTER TABLE moped_proj_files RENAME COLUMN created_by_user_id TO created_by;
-ALTER TABLE moped_proj_files DROP COLUMN updated_at;
-ALTER TABLE moped_proj_files DROP COLUMN updated_by_user_id;
+ALTER TABLE moped_project_files RENAME COLUMN created_at TO create_date;
+ALTER TABLE moped_project_files RENAME COLUMN created_by_user_id TO created_by;
+ALTER TABLE moped_project_files DROP COLUMN updated_at;
+ALTER TABLE moped_project_files DROP COLUMN updated_by_user_id;
 
-DROP TRIGGER update_moped_proj_files_and_project_audit_fields ON moped_proj_files;
+DROP TRIGGER update_moped_project_files_and_project_audit_fields ON moped_project_files;
