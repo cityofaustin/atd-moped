6357fc29e36a95671409c7cedc08950a9de44ef3
Frank Hereford <frank@frankhereford.com>
2024-03-23 16:24:50 -0500

    rework layout from a fresh start

--- moped-editor/src/views/projects/projectView/ProjectNameComplex.js
+++ moped-editor/src/views/projects/projectView/ProjectNameComplex.js
@@ -4,19 +4,18 @@ import makeStyles from "@mui/styles/makeStyles";
 import {
   Grid,
   Icon,
+  Box,
   Typography,
   TextField,
 } from "@mui/material";
+import ProjectStatusBadge from "./ProjectStatusBadge";
 
 const useStyles = makeStyles((theme) => ({
-  editIconConfirm: {
+  editIcons: {
     cursor: "pointer",
     marginTop: "16px",
     fontSize: "2rem",
   },
-  fieldGridItemButtons: {
-    width: "70px",
-  }
 }));
 
 
@@ -30,70 +29,122 @@ const ProjectNameComplex = (props) => {
   // setIsEditing={setIsEditing}
   // updatedCallback={handleNameUpdate}
 
-  if (props.isEditing) {
+  if (!props.isEditing) {
     return (
-      <Grid container>
-        <Grid item xs={7}>
-          <ProjectNameEditable
-            projectName={props.projectData.project_name}
-            projectId={props.projectId}
-            editable={true}
-            isEditing={props.isEditing}
-            setIsEditing={props.setIsEditing}
-            updatedCallback={props.handleNameUpdate}
-          />
-        </Grid>
-        <Grid item xs="4">
-          <ProjectSecondaryNameEditable
-            projectSecondaryName={props.projectData.project_name_secondary}
-            projectId={props.projectId}
-            editable={true}
-            isEditing={props.isEditing}
-            setIsEditing={props.setIsEditing}
-            updatedCallback={props.handleNameUpdate}
-          />
-        </Grid>
-        <form onSubmit={(e) => props.handleAcceptClick(e)}>
-          <Grid item>
-            <Grid container>
-              <Grid item xs={12} sm={12} className={classes.fieldGridItemButtons}>
-                <Icon
-                  className={classes.editIconConfirm}
-                  onClick={props.handleAcceptClick}
-                >
-                  check
-                </Icon>
-                <Icon
-                  className={classes.editIconConfirm}
-                  onClick={(e) => props.handleCancelClick(e)}
-                >
-                  close
-                </Icon>
-              </Grid>
-            </Grid>
+      <>
+        <Grid container>
+          <Grid item xs={12}>
+            <Box sx={{ display: "inline", cursor: "pointer" }}>
+              <Typography
+                color="textPrimary"
+                variant="h2"
+                sx={{ display: "inline" }}
+                onClick={() => props.setIsEditing(true)}
+              >
+                {props.projectData.project_name}
+              </Typography>
+            </Box>
+            <Box sx={{ display: "inline", paddingLeft: "10px" }}>
+              <Typography
+                color="textSecondary"
+                variant="h2"
+                sx={{ display: "inline" }}
+              >
+                #{props.projectId}
+              </Typography>
+            </Box>
+            <Box sx={{ display: "inline", position: "relative", top: "-5px" }}>
+              <ProjectStatusBadge
+                phaseKey={props.currentPhase?.phase_key}
+                phaseName={props.currentPhase?.phase_name}
+              />
+            </Box>
           </Grid>
-        </form>
-      </Grid>
+          {props.projectData.project_name_secondary && props.projectData.project_name_secondary.length > 0 ? (
+            <Grid item xs={12}>
+              <Box sx={{ display: "inline" }}>
+                <Typography
+                  color="textSecondary"
+                  variant="h2"
+                  sx={{ display: "inline", fontSize: "1.2rem"}}
+                  onClick={() => props.setIsEditing(true)}
+                >
+                  {props.projectData.project_name_secondary}
+                </Typography>
+              </Box>
+            </Grid>
+          ) : null}
+        </Grid>
+      </>
     );
   } else {
     return (
-    <>
-          <ProjectNameEditable
-            projectName={props.projectData.project_name}
-            projectId={props.projectId}
-            editable={true}
-            isEditing={props.isEditing}
-            setIsEditing={props.setIsEditing}
-            updatedCallback={props.handleNameUpdate}
-          />
-          <Typography
-            color="textSecondary"
-            variant="h2"
-            className={classes.titleId}
-          >
-            &nbsp;#{props.projectId}
-          </Typography>
-    </>
+      <>
+        <Grid container>
+          <Grid item xs={12} sm={5} sx={{ paddingRight: "30px" }}>
+            <TextField
+              variant="standard"
+              fullWidth
+              id="project_name"
+              label={"Project Name"}
+              type="text"
+              // defaultValue={projectName ?? props?.projectName}
+              // error={titleError}
+              // placeholder={
+              //   titleError ? "Title cannot be blank" : "Enter project name"
+              // }
+              multiline={false}
+              rows={1}
+              // onChange={(e) => handleProjectNameChange(e)}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={4} sx={{ paddingRight: "30px" }}>
+            <TextField
+              variant="standard"
+              fullWidth
+              id="secondary_name"
+              label={"Secondary Name"}
+              type="text"
+              // defaultValue={projectName ?? props?.projectName}
+              // error={titleError}
+              // placeholder={
+              //   titleError ? "Title cannot be blank" : "Enter project name"
+              // }
+              multiline={false}
+              rows={1}
+              // onChange={(e) => handleProjectNameChange(e)}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={1} sx={{ minWidth: "65px" }}>
+            <Icon
+              className={classes.editIcons}
+              // onClick={handleAcceptClick}
+            >
+              check
+            </Icon>
+            <Icon
+              className={classes.editIcons}
+              // onClick={(e) => handleCancelClick(e)}
+            >
+              close
+            </Icon>
+          </Grid>
+          <Grid item xs={12} md={2}>
+            <Box sx={{ display: "inline", position: "relative", top: "5px" }}>
+              <ProjectStatusBadge
+                phaseKey={props.currentPhase?.phase_key}
+                phaseName={props.currentPhase?.phase_name}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+      </>
     );
   }
 };
