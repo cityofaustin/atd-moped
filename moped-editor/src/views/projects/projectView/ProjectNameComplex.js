106be06cb147ed4babd65af06f3d4403f01dd8ac
Frank Hereford <frank@frankhereford.com>
2024-03-23 16:46:47 -0500

    filling in the rest of the input props

--- moped-editor/src/views/projects/projectView/ProjectNameComplex.js
+++ moped-editor/src/views/projects/projectView/ProjectNameComplex.js
@@ -1,13 +1,6 @@
-import ProjectNameEditable from "./ProjectNameEditable";
-import ProjectSecondaryNameEditable from "./ProjectSecondaryNameEditable";
+import React, { useState } from "react";
 import makeStyles from "@mui/styles/makeStyles";
-import {
-  Grid,
-  Icon,
-  Box,
-  Typography,
-  TextField,
-} from "@mui/material";
+import { Grid, Icon, Box, Typography, TextField } from "@mui/material";
 import ProjectStatusBadge from "./ProjectStatusBadge";
 
 const useStyles = makeStyles((theme) => ({
@@ -16,11 +9,18 @@ const useStyles = makeStyles((theme) => ({
     marginTop: "16px",
     fontSize: "2rem",
   },
+  projectNameEditField: {
+    "font-size": "1.5rem",
+    "font-weight": "bold",
+  },
+  projectSecondaryNameEditField: {
+    "font-size": "1.0rem",
+    "font-weight": "bold",
+    "padding-top": "0.95rem",
+  },
 }));
 
-
 const ProjectNameComplex = (props) => {
-  const classes = useStyles();
   // here are the props, i miss typescript
   // projectData={data.moped_project[0]}
   // projectId={projectId}
@@ -28,6 +28,11 @@ const ProjectNameComplex = (props) => {
   // isEditing={isEditing}
   // setIsEditing={setIsEditing}
   // updatedCallback={handleNameUpdate}
+
+  const [primaryTitleError, setPrimaryTitleError] = useState(false);
+  const [secondaryTitleError, setSecondaryTitleError] = useState(false);
+
+  const classes = useStyles();
 
   if (!props.isEditing) {
     return (
@@ -60,13 +65,14 @@ const ProjectNameComplex = (props) => {
               />
             </Box>
           </Grid>
-          {props.projectData.project_name_secondary && props.projectData.project_name_secondary.length > 0 ? (
+          {props.projectData.project_name_secondary &&
+          props.projectData.project_name_secondary.length > 0 ? (
             <Grid item xs={12}>
               <Box sx={{ display: "inline" }}>
                 <Typography
                   color="textSecondary"
                   variant="h2"
-                  sx={{ display: "inline", fontSize: "1.2rem"}}
+                  sx={{ display: "inline", fontSize: "1.2rem" }}
                   onClick={() => props.setIsEditing(true)}
                 >
                   {props.projectData.project_name_secondary}
@@ -81,43 +87,53 @@ const ProjectNameComplex = (props) => {
     return (
       <>
         <Grid container>
-          <Grid item xs={12} sm={5} sx={{ paddingRight: "30px" }}>
+          <Grid item xs={12} sm={6} sx={{ paddingRight: "30px" }}>
             <TextField
               variant="standard"
               fullWidth
               id="project_name"
               label={"Project Name"}
               type="text"
-              // defaultValue={projectName ?? props?.projectName}
-              // error={titleError}
-              // placeholder={
-              //   titleError ? "Title cannot be blank" : "Enter project name"
-              // }
+              defaultValue={props.projectData.project_name}
+              error={primaryTitleError}
+              placeholder={
+                primaryTitleError ? "Title cannot be blank" : "Enter project name"
+              }
               multiline={false}
               rows={1}
               // onChange={(e) => handleProjectNameChange(e)}
               InputLabelProps={{
                 shrink: true,
               }}
+              InputProps={{
+                classes: {
+                  input: classes.projectNameEditField,
+                },
+              }}
             />
           </Grid>
-          <Grid item xs={12} sm={4} sx={{ paddingRight: "30px" }}>
+          <Grid item xs={12} sm={3} sx={{ paddingRight: "30px" }}>
             <TextField
               variant="standard"
               fullWidth
               id="secondary_name"
               label={"Secondary Name"}
               type="text"
-              // defaultValue={projectName ?? props?.projectName}
-              // error={titleError}
-              // placeholder={
-              //   titleError ? "Title cannot be blank" : "Enter project name"
-              // }
+              defaultValue={props.projectData.project_name_secondary}
+              error={secondaryTitleError}
+              placeholder={
+                secondaryTitleError ? "Secondary Name Error" : "Project Byline"
+              }
               multiline={false}
               rows={1}
               // onChange={(e) => handleProjectNameChange(e)}
               InputLabelProps={{
                 shrink: true,
+              }}
+              InputProps={{
+                classes: {
+                  input: classes.projectSecondaryNameEditField,
+                },
               }}
             />
           </Grid>
