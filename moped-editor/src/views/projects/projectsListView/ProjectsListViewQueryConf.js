c94ee9e4d94ae387c70610a6db8de1f9ac7a8379
Tilly Whitson <35410637+tillyw@users.noreply.github.com>
2024-03-26 13:02:50 -0500

    cleanup

--- moped-editor/src/views/projects/projectsListView/ProjectsListViewQueryConf.js
+++ moped-editor/src/views/projects/projectsListView/ProjectsListViewQueryConf.js
@@ -69,8 +69,8 @@ export const PROJECT_LIST_VIEW_QUERY_CONFIG = {
       searchable: true,
       sortable: true,
       label: "Project description",
-      showInTable: true,
       defaultHidden: true,
+      showInTable: true,
       search: {
         label: "Search by project description",
         operator: "_ilike",
