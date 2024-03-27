850c169a3adda475664dadc7d6e40e6efd8756e7
Frank Hereford <frank@frankhereford.com>
2024-03-27 10:16:47 -0500

    Restore input page to working and refine CSS

--- moped-editor/src/views/projects/newProjectView/DefineProjectForm.js
+++ moped-editor/src/views/projects/newProjectView/DefineProjectForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { TextField, Grid, InputLabel, Switch } from "@mui/material";
+import { TextField, Grid, InputLabel, Switch, Box } from "@mui/material";
 import SignalAutocomplete from "./SignalAutocomplete";
 
 const DefineProjectForm = ({
@@ -24,62 +24,44 @@ const DefineProjectForm = ({
     <form style={{ padding: 25 }}>
       <Grid container spacing={3} style={{ margin: 20 }}>
         <Grid item xs={6}>
-          {!fromSignalAsset && (
-            <>
-              <TextField
-                sx={{ marginBottom: "1.5rem" }}
-                required
-                autoFocus
-                label="Project name"
-                name="project_name"
-                type="text"
-                fullWidth
-                value={projectDetails.project_name}
-                error={nameError}
-                helperText="Required"
-                InputLabelProps={{ required: false }}
-                onChange={(e) =>
-                  handleFieldChange(e.target.value, e.target.name)
-                }
-              />
-              <TextField
-                sx={{ marginBottom: "2.5rem" }}
-                label="Secondary name"
-                name="project_name_secondary"
-                type="text"
-                fullWidth
-                value={projectDetails.project_name_secondary || ""}
-                onChange={(e) =>
-                  handleFieldChange(e.target.value, e.target.name)
-                }
-              />
-              <TextField
-                required
-                label="Description"
-                name="project_description"
-                multiline={true}
-                type="text"
-                fullWidth
-                value={projectDetails.project_description}
-                error={descriptionError}
-                helperText="Required"
-                InputLabelProps={{ required: false }}
-                onChange={(e) =>
-                  handleFieldChange(e.target.value, e.target.name)
-                }
-              />
-            </>
-          )}
-          {fromSignalAsset && (
-            <SignalAutocomplete
-              signal={signal}
-              setSignal={setSignal}
-              projectDetails={projectDetails}
-              setProjectDetails={setProjectDetails}
-              setSignalRecord={setSignalRecord}
-              signalError={signalError}
+          {!fromSignalAsset && ( // if
+            <TextField
+              sx={{ marginBottom: "2rem" }}
+              required
+              autoFocus
+              label="Project name"
+              name="project_name"
+              type="text"
+              fullWidth
+              value={projectDetails.project_name}
+              error={nameError}
+              helperText="Required"
+              InputLabelProps={{ required: false }}
+              onChange={(e) => handleFieldChange(e.target.value, e.target.name)}
             />
           )}
+          {fromSignalAsset && ( // else
+            <Box sx={{ marginBottom: "2.1rem" }}>
+              <SignalAutocomplete
+                signal={signal}
+                setSignal={setSignal}
+                projectDetails={projectDetails}
+                setProjectDetails={setProjectDetails}
+                setSignalRecord={setSignalRecord}
+                signalError={signalError}
+              />
+            </Box>
+          )}
+
+          <TextField
+            sx={{ marginBottom: ".5rem" }}
+            label="Secondary name"
+            name="project_name_secondary"
+            type="text"
+            fullWidth
+            value={projectDetails.project_name_secondary || ""}
+            onChange={(e) => handleFieldChange(e.target.value, e.target.name)}
+          />
         </Grid>
         <Grid item xs={3}>
           <InputLabel>Use signal asset</InputLabel>
@@ -90,6 +72,24 @@ const DefineProjectForm = ({
             name="use_signal_id"
             inputProps={{ "aria-label": "primary checkbox" }}
             onChange={(e) => setFromSignalAsset(e.target.checked)}
+          />
+        </Grid>
+      </Grid>
+      <Grid container spacing={3} style={{ margin: 20 }}>
+        <Grid item xs={6}>
+          <TextField
+            required
+            minRows={5}
+            label="Description"
+            name="project_description"
+            multiline={true}
+            type="text"
+            fullWidth
+            value={projectDetails.project_description}
+            error={descriptionError}
+            helperText="Required"
+            InputLabelProps={{ required: false }}
+            onChange={(e) => handleFieldChange(e.target.value, e.target.name)}
           />
         </Grid>
       </Grid>
