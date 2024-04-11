3d97de7375c7740c36a7fbcfd7afde510d002670
chiaberry <analuciaby@gmail.com>
2024-04-11 15:37:36 -0500

    add ATSD as workgroup to new entities

--- moped-database/migrations/1712796816384_add_new_entities/up.sql
+++ moped-database/migrations/1712796816384_add_new_entities/up.sql
@@ -1,3 +1,3 @@
 INSERT INTO "public"."moped_entity" (entity_name, organization_id, department_id, workgroup_id) VALUES
-('COA ATD Bikeways Program', 1, 1, null), -- check on the workgroup id
-('COA ATD Pedestrian Crossing Program', 1, 1, null); -- also check this one
+('COA ATD Bikeways Program', 1, 1, 1),
+('COA ATD Pedestrian Crossing Program', 1, 1, 1);
