56b738740179cf9004714e777b50bd56069ed325
chiaberry <analuciaby@gmail.com>
2024-04-15 14:00:29 -0500

    add map bikeways tag for 2025

---
+++ moped-database/migrations/1713206648608_add_2025_component_tag/down.sql
@@ -0,0 +1,1 @@
+DELETE FROM public.moped_component_tags WHERE slug = 'map_bikeways_2025'
---
+++ moped-database/migrations/1713206648608_add_2025_component_tag/up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.moped_component_tags ("type", name, slug) VALUES
+('MAP Bikeways', '2025', 'map_bikeways_2025');
