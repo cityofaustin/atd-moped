859efe10d156de29ebd976509276b792267bdb85
Mike <mike.dilley@gmail.com>
2024-06-03 16:01:49 -0500

    Update down migration to remove map load events before updating event_name constraint

--- moped-database/migrations/1717191608945_update_user_events_constraint/down.sql
+++ moped-database/migrations/1717191608945_update_user_events_constraint/down.sql
@@ -1,3 +1,5 @@
+DELETE FROM moped_user_events WHERE event_name = 'projects_map_load';
+
 ALTER TABLE moped_user_events DROP CONSTRAINT moped_user_events_event_name_check;
 
 ALTER TABLE moped_user_events
