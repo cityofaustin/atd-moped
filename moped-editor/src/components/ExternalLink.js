814337d807e83e732a220933975f93333ef72ab9
Mike <mike.dilley@gmail.com>
2024-08-02 16:02:10 -0500

    Reuse and update existing ExternalLink component for summary website

--- moped-editor/src/components/ExternalLink.js
+++ moped-editor/src/components/ExternalLink.js
@@ -1,18 +1,27 @@
 import React from "react";
 import Link from "@mui/material/Link";
-import makeStyles from '@mui/styles/makeStyles';
+import makeStyles from "@mui/styles/makeStyles";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   iconStyle: {
-    fontSize: "14px",
+    fontSize: "1rem",
     marginLeft: "2px",
     position: "relative",
     bottom: "-3px",
   },
 }));
 
-const ExternalLink = ({ url, text, linkColor, underline, iconStyle }) => {
+/**
+ * LinkWithIcon component
+ * @param {string} url - link url
+ * @param {string} text - link text
+ * @param {string} linkColor - color of the link
+ * @param {Object} linkProps - Props supported by MUI Link to override defaults or set other options
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const ExternalLink = ({ url, text, linkColor, iconStyle, linkProps }) => {
   const classes = useStyles();
 
   return (
@@ -23,7 +32,7 @@ const ExternalLink = ({ url, text, linkColor, underline, iconStyle }) => {
           target="_blank"
           rel="noopener noreferrer"
           color={linkColor ?? "primary"}
-          underline={underline ?? "hover"}
+          {...linkProps}
         >
           {text}
           <OpenInNewIcon
