6789809a14b971d7a45786930dba399b0203a814
Mike <mike.dilley@gmail.com>
2024-08-12 16:09:03 -0500

    Add if exists to down migration; update metadata

--- moped-database/migrations/1723496266685_add_fdu_columns/down.sql
+++ moped-database/migrations/1723496266685_add_fdu_columns/down.sql
@@ -1,2 +1,2 @@
 -- Drop generated columns generated columns for fund_dept_unit and fund_name
-ALTER TABLE moped_proj_funding DROP COLUMN fund_dept_unit, DROP COLUMN fund_name;
+ALTER TABLE moped_proj_funding DROP COLUMN IF EXISTS fund_dept_unit, DROP COLUMN IF EXISTS fund_name;
