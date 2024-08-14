7d40b676469cb828bf1b6e48ab1aa4615ab25b4d
Mike <mike.dilley@gmail.com>
2024-08-14 16:42:31 -0500

    Add bike share station migration to pick back up on

---
+++ moped-database/migrations/1723496266686_add_bike_share_station_comp/up.sql
@@ -0,0 +1,6 @@
+INSERT INTO moped_components (
+    component_name, line_representation, feature_layer_id
+) VALUES
+('Bike Share Station', FALSE, 5);
+
+-- TODO: Add work types for bike share station
