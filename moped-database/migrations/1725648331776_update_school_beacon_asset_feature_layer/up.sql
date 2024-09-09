b579686580ec0b2f8112975aa1e578452854cb0a
chiaberry <analuciaby@gmail.com>
2024-09-09 13:30:14 -0500

    the feature layer id is already 5

--- moped-database/migrations/1725648331776_update_school_beacon_asset_feature_layer/up.sql
+++ moped-database/migrations/1725648331776_update_school_beacon_asset_feature_layer/up.sql
@@ -1,7 +1,6 @@
 --  set asset layer for school beacon to signals 
 UPDATE moped_components
 SET
-    feature_layer_id = 5,
     asset_feature_layer_id = 3
 WHERE
     component_subtype = 'School Zone Beacon';
