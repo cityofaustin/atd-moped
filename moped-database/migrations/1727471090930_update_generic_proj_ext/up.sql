06f62accebe7e759569983f4f300c5ca04b921ee
Mike <mike.dilley@gmail.com>
2024-09-27 16:27:17 -0500

    Start on component work types

--- moped-database/migrations/1727471090930_update_generic_proj_ext/up.sql
+++ moped-database/migrations/1727471090930_update_generic_proj_ext/up.sql
@@ -5,3 +5,24 @@ WHERE component_name = 'Project Extent - Generic' AND line_representation = true
 -- Insert new point component for generic project extent
 INSERT INTO moped_components (component_name, line_representation, feature_layer_id)
 VALUES ('Project Extent - Generic', false, 5);
+
+-- Insert same work types for generic project extent point as exist for generic project extent line
+WITH inserts_todo AS (
+    SELECT
+        mcwt.id AS work_type_id,
+        mc.component_id AS component_id
+    FROM
+        moped_component_work_types AS mcwt,
+        moped_components AS mc
+    WHERE
+        mcwt.component_id = 0
+        AND mc.component_name = 'Project Extent - Generic'
+)
+
+INSERT INTO moped_component_work_types (work_type_id, component_id)
+SELECT
+    work_type_id,
+    component_id
+FROM
+    inserts_todo
+ON CONFLICT DO NOTHING;
