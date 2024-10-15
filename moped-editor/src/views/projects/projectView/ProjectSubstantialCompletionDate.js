b2f635eaf35a9bac0cc8970d33b7149445272735
chiaberry <analuciaby@gmail.com>
2024-10-15 12:46:21 -0500

    format date type

--- moped-editor/src/views/projects/projectView/ProjectSubstantialCompletionDate.js
+++ moped-editor/src/views/projects/projectView/ProjectSubstantialCompletionDate.js
@@ -1,31 +1,8 @@
 import { Box, Typography, Tooltip } from "@mui/material";
 import makeStyles from "@mui/styles/makeStyles";
+import { formatDateType } from "src/utils/dateAndTime";
 
 const useStyles = makeStyles((theme) => ({
-  // fieldGridItem: {
-  //     margin: theme.spacing(2),
-  //   },
-  //   linkIcon: {
-  //     fontSize: "1rem",
-  //   },
-  //   editIconFunding: {
-  //     cursor: "pointer",
-  //     margin: "0.5rem",
-  //     fontSize: "1.5rem",
-  //   },
-  //   editIconContainer: {
-  //     minWidth: "8rem",
-  //     marginLeft: "8px",
-  //   },
-  //   editIconButton: {
-  //     margin: "8px 0",
-  //     padding: "8px",
-  //   },
-  //   editIconConfirm: {
-  //     cursor: "pointer",
-  //     margin: ".25rem 0",
-  //     fontSize: "24px",
-  //   },
   fieldLabel: {
     width: "100%",
     color: theme.palette.text.secondary,
@@ -37,7 +14,6 @@ const useStyles = makeStyles((theme) => ({
   },
   fieldLabelText: {
     width: "calc(100% - 2rem)",
-    paddingLeft: theme.spacing(0.5),
   },
 }));
 
@@ -61,7 +37,7 @@ const ProjectSubstantialCompletionDate = ({ completionDate }) => {
             {/* If there is no input, render a "-" */}
             {(!completionDate || completionDate.length === 0) && <span>-</span>}
             {/* i dont think we need the span do we*/}
-            <span>{completionDate}</span>
+            <span>{formatDateType(completionDate)}</span>
           </Typography>
         </Tooltip>
       </Box>
