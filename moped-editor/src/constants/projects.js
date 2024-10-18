fd6f90188bd0a1370110f77d2dd99feb5369b59c
chiaberry <analuciaby@gmail.com>
2024-10-18 15:28:29 -0500

    update tooltip text

--- moped-editor/src/constants/projects.js
+++ moped-editor/src/constants/projects.js
@@ -1,2 +1,2 @@
 export const substantialCompletionDateTooltipText =
-  "The earliest confirmed start date of a project phase that has a simple phase name of Complete (Complete or Post-construction)";
+  "The earliest confirmed date of any Complete or Post-construction phase";
