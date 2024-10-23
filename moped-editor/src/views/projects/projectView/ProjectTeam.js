12ff85a0e065fca51f927c2fb81cd3726445fab2
mateoclarke <mateosalinasclarke@proton.me>
2024-10-23 01:04:51 -0600

    wip build out datagridpro alternative

--- moped-editor/src/views/projects/projectView/ProjectTeam.js
+++ moped-editor/src/views/projects/projectView/ProjectTeam.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ProjectTeamTable from "./ProjectTeamTable";
+import ProjectTeamTableDataGridPro from "./ProjectTeamTableDataGridPro";
 import { useParams } from "react-router-dom";
 
 import { CardContent, Grid } from "@mui/material";
@@ -12,6 +13,7 @@ const ProjectTeam = () => {
       <Grid container spacing={2}>
         <Grid item xs={12}>
           <ProjectTeamTable projectId={projectId} />
+          <ProjectTeamTableDataGridPro projectId={projectId} />
         </Grid>
       </Grid>
     </CardContent>
