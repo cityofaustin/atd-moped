e6612b53d052d59b96e7cbb15bb63d6b8c46ece8
Mike <mike.dilley@gmail.com>
2024-11-06 16:41:42 -0600

    Update insert statement

--- moped-database/migrations/1730931541882_add_active_work_activity_status/up.sql
+++ moped-database/migrations/1730931541882_add_active_work_activity_status/up.sql
@@ -1,2 +1,10 @@
-INSERT INTO moped_proj_work_activity_status ("key", "name") VALUES
-('active', 'Active');
+INSERT INTO moped_proj_work_activity_status (key, name)
+SELECT
+    'active',
+    'Active'
+WHERE NOT EXISTS (
+        SELECT *
+        FROM
+            moped_proj_work_activity_status
+        WHERE (key, name) = ('active', 'Active')
+    );
