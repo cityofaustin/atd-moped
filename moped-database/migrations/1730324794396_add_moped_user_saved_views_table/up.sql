b53c2238fc95355f764f4982ce45bd4499a03e4a
tillyw <tilly.whitson@austintexas.gov>
2024-11-07 13:27:29 -0600

    addressed requested changes

--- moped-database/migrations/1730324794396_add_moped_user_saved_views_table/up.sql
+++ moped-database/migrations/1730324794396_add_moped_user_saved_views_table/up.sql
@@ -1,13 +1,13 @@
 CREATE TABLE public.moped_user_saved_views (
     id serial NOT NULL,
-    description text,
+    description text NOT NULL,
     url text NOT NULL,
     query_filters jsonb,
     created_by_user_id int4 NOT NULL,
     updated_by_user_id int4 NOT NULL,
-    is_deleted boolean NOT NULL,
     created_at timestamptz NOT NULL DEFAULT now(),
-    updated_at timestamptz NOT NULL DEFAULT now()
+    updated_at timestamptz NOT NULL DEFAULT now(),
+    is_deleted boolean NOT NULL DEFAULT false
 );
 
 ALTER TABLE moped_user_saved_views
@@ -15,10 +15,15 @@ ADD CONSTRAINT fk_moped_user_saved_views_created_by FOREIGN KEY (created_by_user
 ADD CONSTRAINT fk_moped_user_saved_views_updated_by FOREIGN KEY (updated_by_user_id) REFERENCES moped_users (user_id);
 
 -- Adding comments for audit fields
-COMMENT ON COLUMN moped_user_saved_views.created_at IS 'Timestamp of when the view was created';
+-- TO DO: add comments to all fields
+COMMENT ON COLUMN moped_user_saved_views.description IS 'Description entered by the creator of the view';
+COMMENT ON COLUMN moped_user_saved_views.url IS 'URL string associated with the view (may break if database fields or operators are changed)';
+COMMENT ON COLUMN moped_user_saved_views.query_filters IS 'JSON blob of filters that make up the query';
 COMMENT ON COLUMN moped_user_saved_views.created_by_user_id IS 'User ID of the creator of the view';
 COMMENT ON COLUMN moped_user_saved_views.updated_by_user_id IS 'User ID of the last updater of the view';
+COMMENT ON COLUMN moped_user_saved_views.created_at IS 'Timestamp of when the view was created';
 COMMENT ON COLUMN moped_user_saved_views.updated_at IS 'Timestamp of the last update of the view';
+COMMENT ON COLUMN moped_user_saved_views.query_filters IS 'Boolean indicating whether the view has been soft deleted and thereby not rendered in the UI';
 
 -- Adding comments for moped_user_saved_views constraints
 COMMENT ON CONSTRAINT fk_moped_user_saved_views_created_by ON moped_user_saved_views IS 'Foreign key constraint linking created_by_user_id to moped_users table.';
