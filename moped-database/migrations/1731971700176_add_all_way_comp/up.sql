b6073667c5afdcc88950f0e5a27257de37f80505
Mike <mike.dilley@gmail.com>
2024-11-18 17:22:08 -0600

    Add migration for Signage - All Way component

---
+++ moped-database/migrations/1731971700176_add_all_way_comp/up.sql
@@ -0,0 +1,37 @@
+-- add bike share station component type (point) to moped_components table if it doesn't already exist
+INSERT INTO moped_components (component_name, component_subtype, line_representation, feature_layer_id)
+SELECT
+    'Signage',
+    'All Way',
+    FALSE,
+    5
+WHERE NOT EXISTS (
+        SELECT *
+        FROM
+            moped_components
+        WHERE (component_name, component_subtype, line_representation, feature_layer_id) = ('Signage', 'All Way', FALSE, 5)
+    );
+
+-- insert new, mod, and replacement work types for bike share station in moped_component_work_types table
+WITH inserts_todo AS (
+    SELECT
+        mwt.id AS work_type_id,
+        mc.component_id AS component_id
+    FROM
+        moped_work_types AS mwt,
+        moped_components AS mc
+    WHERE
+        mwt.name IN (
+            'New'
+        )
+        AND mc.component_name = 'Signage'
+        AND mc.component_subtype = 'All Way'
+)
+
+INSERT INTO moped_component_work_types (work_type_id, component_id)
+SELECT
+    work_type_id,
+    component_id
+FROM
+    inserts_todo
+ON CONFLICT DO NOTHING;
