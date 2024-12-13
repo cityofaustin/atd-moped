a713f0e7cbd5083c18f28231f6f811bd10922a86
Mike <mike.dilley@gmail.com>
2024-12-13 11:56:54 -0600

    Fill out down migration

--- moped-database/migrations/1734112355149_add_ss4a_proj_tags/down.sql
+++ moped-database/migrations/1734112355149_add_ss4a_proj_tags/down.sql
@@ -1,5 +1,1 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- INSERT INTO moped_tags (name, type, slug, is_deleted) VALUES
--- ('SS4A Vision Zero 2022', 'Work Plan', 'ss4a_vision_zero_2022', FALSE),
--- ('SS4A Vision Zero 2024', 'Work Plan', 'ss4a_vision_zero_2024', FALSE);
+DELETE FROM public.moped_tags WHERE slug = 'ss4a_vision_zero_2022' OR slug = 'ss4a_vision_zero_2024';
