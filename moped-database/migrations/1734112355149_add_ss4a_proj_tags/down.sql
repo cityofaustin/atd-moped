c9e0d6c48a26d86e4917314d4f3ee7ba5d3d74cb
Mike <mike.dilley@gmail.com>
2024-12-13 11:53:55 -0600

    Add migrations to add/remove ss4a project tags

---
+++ moped-database/migrations/1734112355149_add_ss4a_proj_tags/down.sql
@@ -0,0 +1,5 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- INSERT INTO moped_tags (name, type, slug, is_deleted) VALUES
+-- ('SS4A Vision Zero 2022', 'Work Plan', 'ss4a_vision_zero_2022', FALSE),
+-- ('SS4A Vision Zero 2024', 'Work Plan', 'ss4a_vision_zero_2024', FALSE);
