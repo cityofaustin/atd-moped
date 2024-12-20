800bcd370e93134428ab483848bc9631a84e849a
Mike <mike.dilley@gmail.com>
2024-12-20 16:27:07 -0600

    Check for array values in DataGridAction validation too

--- moped-editor/src/components/DataGridPro/DataGridActions.js
+++ moped-editor/src/components/DataGridPro/DataGridActions.js
@@ -45,8 +45,17 @@ const DataGridActions = ({
    */
   const hasRequiredFields = useGridSelector(apiRef, () => {
     const editState = apiRef.current.state.editRows;
+
     for (const field of requiredFields) {
-      if (!editState[id]?.[field]?.value) {
+      const value = editState[id]?.[field]?.value;
+
+      const isArray = Array.isArray(value);
+
+      if (isArray && !value.length) {
+        return false;
+      }
+
+      if (!value) {
         return false;
       }
     }
@@ -58,6 +67,7 @@ const DataGridActions = ({
       <GridActionsCellItem
         icon={<CheckIcon sx={defaultEditColumnIconStyle} />}
         label="Save"
+        key="save"
         sx={{
           color: "primary.main",
         }}
@@ -67,6 +77,7 @@ const DataGridActions = ({
       <GridActionsCellItem
         icon={<CloseIcon sx={defaultEditColumnIconStyle} />}
         label="Cancel"
+        key="cancel"
         className="textPrimary"
         onClick={handleCancelClick(id)}
         color="inherit"
@@ -79,6 +90,7 @@ const DataGridActions = ({
       <GridActionsCellItem
         icon={<EditOutlinedIcon sx={defaultEditColumnIconStyle} />}
         label="Edit"
+        key="edit"
         className="textPrimary"
         onClick={handleEditClick(id)}
         color="inherit"
@@ -87,6 +99,7 @@ const DataGridActions = ({
     <GridActionsCellItem
       icon={<DeleteOutlineIcon sx={defaultEditColumnIconStyle} />}
       label="Delete"
+      key="delete"
       onClick={handleDeleteOpen(id)}
       color="inherit"
     />,
