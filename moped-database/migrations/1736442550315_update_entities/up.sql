17e1cbb6bf1ea93b464f26bf2b1fa5f30362b6e7
Mike <mike.dilley@gmail.com>
2025-01-09 12:15:46 -0600

    Insert new SUTD entity

--- moped-database/migrations/1736442550315_update_entities/up.sql
+++ moped-database/migrations/1736442550315_update_entities/up.sql
@@ -9,11 +9,46 @@ ALTER COLUMN is_deleted SET NOT NULL;
 ALTER TABLE moped_entity
 ADD COLUMN is_deleted BOOLEAN DEFAULT FALSE NOT NULL;
 
----- moped_workgroup
-SELECT workgroup_id FROM moped_workgroup WHERE workgroup_name = 'Sidewalks and Urban Trails';
+-- Insert new COA TPW Sidewalks and Urban Trails entity with appropriate department_id, workgroup_id, and organization_id
+WITH
+workgroup_todo AS (
+    SELECT workgroup_id
+    FROM
+        moped_workgroup
+    WHERE
+        workgroup_name = 'Sidewalks and Urban Trails'
+),
 
--- moped_organization
-SELECT organization_id FROM moped_organization WHERE organization_name = 'City of Austin';
+organization_todo AS (
+    SELECT organization_id
+    FROM
+        moped_organization
+    WHERE
+        organization_name = 'City of Austin'
+),
 
--- moped_department
-SELECT department_id FROM moped_department WHERE department_name = 'Austin Transportation and Public Works';
+department_todo AS (
+    SELECT department_id
+    FROM
+        moped_department
+    WHERE
+        department_name = 'Austin Transportation and Public Works'
+)
+
+INSERT INTO
+moped_entity (entity_name, department_id, workgroup_id, organization_id)
+SELECT
+    'COA TPW Sidewalks and Urban Trails' AS entity_name,
+    d.department_id,
+    w.workgroup_id,
+    o.organization_id
+FROM
+    department_todo AS d,
+    workgroup_todo AS w,
+    organization_todo AS o;
+
+
+-- Create new rows:
+-- 2. COA TPW
+--    department_id is id that matches Austin Transportation and Public Works in moped_department table
+--    workgroup_id is NULL
