756f81bff539d8e65883b8ac80a08a67265d91d5
Mike <mike.dilley@gmail.com>
2025-03-06 17:04:49 -0600

    Recycle name and desc inputs

--- moped-editor/src/views/projects/newProjectView/DefineProjectForm.js
+++ moped-editor/src/views/projects/newProjectView/DefineProjectForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { TextField, Grid, InputLabel, Switch, Box } from "@mui/material";
+import { Grid, InputLabel, Switch, Box } from "@mui/material";
 import SignalAutocomplete from "src/views/projects/newProjectView/SignalAutocomplete";
 
 const DefineProjectForm = ({
@@ -14,31 +14,22 @@ const DefineProjectForm = ({
   setSignal,
   signalError,
 }) => {
-  const handleFieldChange = (value, name) => {
-    const updatedProjectDetails = { ...projectDetails, [name]: value };
-
-    setProjectDetails(updatedProjectDetails);
-  };
-
   return (
     <form style={{ padding: 25 }}>
       <Grid container spacing={3} style={{ margin: 20 }}>
         <Grid item xs={6}>
           {!fromSignalAsset && ( // if
-            <TextField
-              sx={{ marginBottom: "2rem" }}
-              variant="standard"
-              required
+            <ControlledTextInput
               autoFocus
-              label="Project name"
-              name="project_name"
-              type="text"
+              variant="standard"
               fullWidth
-              value={projectDetails.project_name}
-              error={nameError}
-              helperText="Required"
-              InputLabelProps={{ required: false }}
-              onChange={(e) => handleFieldChange(e.target.value, e.target.name)}
+              id="project_name"
+              label="Project name"
+              name="projectName"
+              placeholder="Enter project name"
+              control={control}
+              error={!!errors?.projectName}
+              helperText={errors?.projectName?.message}
             />
           )}
           {fromSignalAsset && ( // else
@@ -53,16 +44,17 @@ const DefineProjectForm = ({
               />
             </Box>
           )}
-
-          <TextField
-            sx={{ marginBottom: ".5rem" }}
+          <ControlledTextInput
             variant="standard"
-            label="Secondary name"
-            name="project_name_secondary"
-            type="text"
             fullWidth
-            value={projectDetails.project_name_secondary || ""}
-            onChange={(e) => handleFieldChange(e.target.value, e.target.name)}
+            autoFocus
+            id="secondary_name"
+            name="projectSecondaryName"
+            placeholder="Secondary name"
+            label="Secondary name"
+            control={control}
+            error={!!errors?.projectSecondaryName}
+            helperText={errors?.projectSecondaryName?.message}
           />
         </Grid>
         <Grid item xs={3}>
@@ -80,19 +72,18 @@ const DefineProjectForm = ({
       </Grid>
       <Grid container spacing={3} style={{ margin: 20 }}>
         <Grid item xs={6}>
-          <TextField
+          <ControlledTextInput
             variant="standard"
-            required
-            label="Description"
-            name="project_description"
-            multiline={true}
-            type="text"
             fullWidth
-            value={projectDetails.project_description}
-            error={descriptionError !== null}
-            helperText={descriptionError?.message}
-            InputLabelProps={{ required: false }}
-            onChange={(e) => handleFieldChange(e.target.value, e.target.name)}
+            multiline
+            minRows={4}
+            maxRows={10}
+            id="description"
+            name="description"
+            size="small"
+            control={control}
+            error={!!errors?.description}
+            helperText={errors?.description?.message}
           />
         </Grid>
       </Grid>
