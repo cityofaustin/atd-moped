d3f1e3698936bf0930c0ee12395e05c2c0017b48
tillyw <tilly.whitson@austintexas.gov>
2025-03-07 12:07:42 -0600

    adds mutations

--- moped-editor/src/queries/userSavedViews.js
+++ moped-editor/src/queries/userSavedViews.js
@@ -1,19 +1,44 @@
 import { gql } from "@apollo/client";
 
 export const USER_SAVED_VIEWS_QUERY = gql`
-    query userSavedViews($userId: Int) {
-        moped_user_saved_views(
-            where: { created_by_user_id: { _eq: $userId }, is_deleted: { _eq: false } }
-        ) {
-            id
-            description
-            url
-            query_filters
-            created_by_user_id
-            updated_by_user_id
-            created_at
-            updated_at
-            is_deleted
-        }
+  query userSavedViews($userId: Int) {
+    moped_user_saved_views(
+      where: {
+        created_by_user_id: { _eq: $userId }
+        is_deleted: { _eq: false }
+      }
+    ) {
+      id
+      description
+      url
+      query_filters
+      created_by_user_id
+      updated_by_user_id
+      created_at
+      updated_at
+      is_deleted
     }
+  }
+`;
+
+export const UPDATE_USER_SAVED_VIEW = gql`
+  mutation UpdateUserSavedView($id: Int!, $description: Int!) {
+    update_moped_proj_funding(
+      where: { id: { _eq: $id } }
+      _set: { description: $description }
+    ) {
+      affected_rows
+    }
+  }
+`;
+
+export const DELETE_USER_SAVED_VIEW = gql`
+  mutation DeleteUserSavedView($id: Int!) {
+    update_moped_user_saved_views(
+      _set: { is_deleted: true }
+      where: { id: { _eq: $id } }
+    ) {
+      affected_rows
+    }
+  }
 `;
