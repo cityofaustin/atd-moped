28a77bdc8b48e13f872442b16ff65e62c033f1ab
rose <rose.eichelmann@gmail.com>
2025-03-19 16:41:57 -0500

    update comment

--- moped-database/migrations/1742416187184_update_current_phase_view_drop_distinct_on/up.sql
+++ moped-database/migrations/1742416187184_update_current_phase_view_drop_distinct_on/up.sql
@@ -1,4 +1,4 @@
--- Removes DISTINCT ON handling which was causing query timeouts
+-- Removes DISTINCT ON handling and ORDER BY which was causing query timeouts
 CREATE OR REPLACE VIEW current_phase_view AS SELECT
     mpp.project_id,
     mpp.project_phase_id,
@@ -9,4 +9,3 @@ CREATE OR REPLACE VIEW current_phase_view AS SELECT
 FROM moped_proj_phases mpp
 LEFT JOIN moped_phases mp ON mp.phase_id = mpp.phase_id
 WHERE mpp.is_deleted = false AND mpp.is_current_phase = true;
-
