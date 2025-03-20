ee102db172af8b29dd2431f0468191cc5d8bdbad
tillyw <tilly.whitson@austintexas.gov>
2025-03-20 18:47:03 -0500

    migrates dashboard tables to datagridpro

--- moped-editor/src/views/dashboard/DashboardView.js
+++ moped-editor/src/views/dashboard/DashboardView.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { format } from "date-fns";
 
@@ -14,7 +14,6 @@ import {
   Tabs,
   Typography,
 } from "@mui/material";
-import MaterialTable from "@material-table/core";
 import makeStyles from "@mui/styles/makeStyles";
 
 import Page from "src/components/Page";
@@ -28,6 +27,8 @@ import FeedbackSnackbar, {
   useFeedbackSnackbar,
 } from "src/components/FeedbackSnackbar";
 import UserSavedViewsTable from "./UserSavedViewsTable";
+import { DataGridPro, GridRowModes } from "@mui/x-data-grid-pro";
+import dataGridProStyleOverrides from "src/styles/dataGridProStylesOverrides";
 
 import typography from "../../theme/typography";
 
@@ -125,47 +126,149 @@ const DashboardView = () => {
     console.log(error);
   }
 
-  let selectedData = [];
+  /** Hook that provides memoized column settings */
+const useColumns = ({ rowModesModel }) =>
+  useMemo(() => {
+    return [
+      {
+        headerName: "ID",
+        field: "project.project_id",
+        editable: false,
+        width: "10%",
+      },
+      {
+        headerName: "Full name",
+        field: "project.project_name_full",
+        editable: false,
+        renderCell: (row) => (
+          <renderCellFieldLink
+            projectId={row.project_id}
+            value={row.project_name_full}
+          />
+        ),
+        width: "20%",
+      },
+      {
+        headerName: "Status",
+        field: "phase_name",
+        editable: false,
+        renderCell: (row) => (
+          <DashboardTimelineModal
+            table="phases"
+            projectId={row.project_id}
+            projectName={row.project.project_name_full}
+            dashboardRefetch={refetch}
+            handleSnackbar={handleSnackbar}
+          >
+            <ProjectStatusBadge
+              phaseName={row.phase_name}
+              phaseKey={row.phase_key}
+              condensed
+              clickable
+            />
+          </DashboardTimelineModal>
+        ),
+        width: "20%",
+      },
+      {
+        headerName: "Status update",
+        field: "status_update", // Status update (from Project details page)
+        editable: false,
+        renderCell: (row) => (
+          <DashboardStatusModal
+            projectId={row.project_id}
+            projectName={row.project.project_name_full}
+            currentPhaseId={row.current_phase_id}
+            modalParent="dashboard"
+            statusUpdate={row.status_update}
+            queryRefetch={refetch}
+            handleSnackbar={handleSnackbar}
+            classes={classes}
+          >
+            {parse(String(row.status_update))}
+          </DashboardStatusModal>
+        ),
+        width: "40%",
+      },
+      {
+        headerName: "Milestones",
+        field: "completed_milestones",
+        renderCell: (row) => (
+          <DashboardTimelineModal
+            table="milestones"
+            projectId={row.project_id}
+            projectName={row.project.project_name_full}
+            handleSnackbar={handleSnackbar}
+            dashboardRefetch={refetch}
+          >
+            <MilestoneProgressMeter
+              completedMilestonesPercentage={
+                row.completed_milestones_percentage
+              }
+            />
+          </DashboardTimelineModal>
+        ),
+        width: "10%",
+      },
+    ];
+  }, [rowModesModel]);
 
-  if (TABS[activeTab].label === "Following" && !!data) {
-    selectedData = data.moped_user_followed_projects;
-  } else if (TABS[activeTab].label === "My projects" && !!data) {
-    selectedData = data.moped_proj_personnel;
-  }
+  // rows and rowModesModel used in DataGrid
+  const [rows, setRows] = useState([]);
+  const [rowModesModel, setRowModesModel] = useState({});
 
-  if (selectedData) {
-    /**
-     * Build data needed in Dashboard Material Table
-     */
-    selectedData.forEach((project) => {
-      project["project_name_full"] = project.project.project_name_full;
-      project["project_id"] = project.project.project_id;
-      project["phase_name"] =
-        project.project.moped_proj_phases?.[0]?.moped_phase.phase_name;
-      project["phase_key"] =
-        project.project.moped_proj_phases?.[0]?.moped_phase.phase_key;
-      project["current_phase_id"] =
-        project.project.moped_proj_phases?.[0]?.moped_phase.phase_id;
+  // sets the data grid row data when query data is fetched
+  useEffect(() => {
+    let selectedData = [];
+
+    if (TABS[activeTab].label === "Following" && !!data) {
+      selectedData = data.moped_user_followed_projects;
+    } else if (TABS[activeTab].label === "My projects" && !!data) {
+      selectedData = data.moped_proj_personnel;
+    }
+
+    if (selectedData) {
       /**
-       * Get percentage of milestones completed
+       * Build data needed in Dashboard Material Table
        */
-      const milestonesTotal = project.project.moped_proj_milestones.length;
-      const milestonesCompleted = project.project.moped_proj_milestones.filter(
-        (milestone) => milestone.completed === true
-      ).length;
-      project["completed_milestones_percentage"] = !!milestonesTotal
-        ? (milestonesCompleted / milestonesTotal) * 100
-        : 0;
+      selectedData.forEach((project) => {
+        project["project_name_full"] = project.project.project_name_full;
+        project["project_id"] = project.project.project_id;
+        project["phase_name"] =
+          project.project.moped_proj_phases?.[0]?.moped_phase.phase_name;
+        project["phase_key"] =
+          project.project.moped_proj_phases?.[0]?.moped_phase.phase_key;
+        project["current_phase_id"] =
+          project.project.moped_proj_phases?.[0]?.moped_phase.phase_id;
+        /**
+         * Get percentage of milestones completed
+         */
+        const milestonesTotal = project.project.moped_proj_milestones.length;
+        const milestonesCompleted =
+          project.project.moped_proj_milestones.filter(
+            (milestone) => milestone.completed === true
+          ).length;
+        project["completed_milestones_percentage"] = !!milestonesTotal
+          ? (milestonesCompleted / milestonesTotal) * 100
+          : 0;
 
-      // project status update equivalent to most recent project note
-      // html is parsed before being rendered in the DashboardStatusModal component
-      project["status_update"] = "";
-      if (project?.project?.moped_proj_notes?.length) {
-        const note = project.project.moped_proj_notes[0]["project_note"];
-        project["status_update"] = note ? note : "";
-      }
-    });
-  }
+        project["id"] = project.project.project_id;
+
+        // project status update equivalent to most recent project note
+        // html is parsed before being rendered in the DashboardStatusModal component
+        project["status_update"] = "";
+        if (project?.project?.moped_proj_notes?.length) {
+          const note = project.project.moped_proj_notes[0]["project_note"];
+          project["status_update"] = note ? note : "";
+        }
+      });
+      setRows(selectedData);
+    }
+  }, [data]);
+
+  const handleRowModesModelChange = (newRowModesModel) => {
+    setRowModesModel(newRowModesModel);
+  };
 
   /** Build custom user greeting
    */
@@ -183,94 +286,14 @@ const DashboardView = () => {
     }
   };
 
-  const columns = [
-    {
-      title: "ID",
-      field: "project.project_id",
-      editable: "never",
-      cellStyle: { ...typographyStyle },
-      width: "10%",
-    },
-    {
-      title: "Full name",
-      field: "project.project_name_full",
-      editable: "never",
-      cellStyle: { ...typographyStyle },
-      render: (entry) => (
-        <RenderFieldLink
-          projectId={entry.project_id}
-          value={entry.project_name_full}
-        />
-      ),
-      width: "20%",
-    },
-    {
-      title: "Status",
-      field: "phase_name",
-      editable: "never",
-      render: (entry) => (
-        <DashboardTimelineModal
-          table="phases"
-          projectId={entry.project_id}
-          projectName={entry.project.project_name_full}
-          dashboardRefetch={refetch}
-          handleSnackbar={handleSnackbar}
-        >
-          <ProjectStatusBadge
-            phaseName={entry.phase_name}
-            phaseKey={entry.phase_key}
-            condensed
-            clickable
-          />
-        </DashboardTimelineModal>
-      ),
-      width: "20%",
-    },
-    {
-      title: "Status update",
-      field: "status_update", // Status update (from Project details page)
-      editable: "never",
-      render: (entry) => (
-        <DashboardStatusModal
-          projectId={entry.project_id}
-          projectName={entry.project.project_name_full}
-          currentPhaseId={entry.current_phase_id}
-          modalParent="dashboard"
-          statusUpdate={entry.status_update}
-          queryRefetch={refetch}
-          handleSnackbar={handleSnackbar}
-          classes={classes}
-        >
-          {parse(String(entry.status_update))}
-        </DashboardStatusModal>
-      ),
-      width: "40%",
-    },
-    {
-      title: "Milestones",
-      field: "completed_milestones",
-      render: (entry) => (
-        <DashboardTimelineModal
-          table="milestones"
-          projectId={entry.project_id}
-          projectName={entry.project.project_name_full}
-          handleSnackbar={handleSnackbar}
-          dashboardRefetch={refetch}
-        >
-          <MilestoneProgressMeter
-            completedMilestonesPercentage={
-              entry.completed_milestones_percentage
-            }
-          />
-        </DashboardTimelineModal>
-      ),
-      width: "10%",
-    },
-  ];
-
   const handleChange = (event, newValue) => {
     setActiveTab(newValue);
   };
+
+  const dataGridColumns = useColumns({
+    data,
+    rowModesModel,
+  });
 
   return (
     <ActivityMetrics eventName="dashboard_load">
@@ -315,33 +338,54 @@ const DashboardView = () => {
                   ) : TABS[activeTab].label === "Saved views" ? (
                     <UserSavedViewsTable handleSnackbar={handleSnackbar} />
                   ) : (
-                    <MaterialTable
-                      columns={columns}
-                      data={selectedData}
-                      localization={{
-                        body: {
-                          emptyDataSourceMessage: (
-                            <Typography>
-                              {TABS[activeTab].label === "Following" &&
-                                "No projects to display. You have not followed any current projects."}
-                              {TABS[activeTab].label === "My projects" &&
-                                "No projects to display. You are not listed as a Team Member on any current projects."}
-                            </Typography>
-                          ),
-                        },
+                    <DataGridPro
+                      sx={dataGridProStyleOverrides}
+                      columns={dataGridColumns}
+                      rows={rows}
+                      autoHeight
+                      getRowId={(row) => row.id}
+                      rowModesModel={rowModesModel}
+                      onRowModesModelChange={handleRowModesModelChange}
+                      onProcessRowUpdateError={(error) => console.error}
+                      editMode="row"
+                      // processRowUpdate={processRowUpdate}
+                      hideFooter
+                      disableRowSelectionOnClick
+                      localeText={{
+                        noRowsLabel: "No projects to display",
                       }}
-                      options={{
-                        search: false,
-                        toolbar: false,
-                        tableLayout: "fixed",
-                        ...(selectedData.length < 51 && {
-                          paging: false,
-                        }),
-                        pageSize: 50,
-                        pageSizeOptions: [10, 50, 100],
-                        idSynonym: "project_id",
+                      initialState={{ pinnedColumns: { right: ["edit"] } }}
+                      onRowEditStart={(params, event) => {
+                        event.defaultMuiPrevented = true; // disable editing rows
                       }}
                     />
+                    // <MaterialTable
+                    //   columns={columns}
+                    //   data={selectedData}
+                    //   localization={{
+                    //     body: {
+                    //       emptyDataSourceMessage: (
+                    //         <Typography>
+                    //           {TABS[activeTab].label === "Following" &&
+                    //             "No projects to display. You have not followed any current projects."}
+                    //           {TABS[activeTab].label === "My projects" &&
+                    //             "No projects to display. You are not listed as a Team Member on any current projects."}
+                    //         </Typography>
+                    //       ),
+                    //     },
+                    //   }}
+                    //   options={{
+                    //     search: false,
+                    //     toolbar: false,
+                    //     tableLayout: "fixed",
+                    //     ...(selectedData.length < 51 && {
+                    //       paging: false,
+                    //     }),
+                    //     pageSize: 50,
+                    //     pageSizeOptions: [10, 50, 100],
+                    //     idSynonym: "project_id",
+                    //   }}
+                    // />
                   )}
                 </Grid>
               </Box>
