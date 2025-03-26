0e84d413a0e1bd1129b5fc24597a0d564300ff7e
chiaberry <analuciaby@gmail.com>
2025-03-26 10:57:46 -0500

    add new file types table to database

---
+++ moped-database/migrations/1743001305353_add_project_files_lookup/up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "public"."moped_file_types" ("id" serial NOT NULL, "name" text NOT NULL UNIQUE, "slug" text NOT NULL UNIQUE, PRIMARY KEY ("id"));
+
+COMMENT ON TABLE "public"."moped_file_types" IS 'lookup table of file types';
+INSERT INTO moped_file_types (id, name, slug) VALUES
+(1, 'Funding', 'funding'),
+(2, 'Plans', 'plans'),
+(3, 'Estimates', 'estimates'),
+(4, 'Other', 'other')
