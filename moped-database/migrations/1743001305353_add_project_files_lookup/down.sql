17a127751879164e1d251e5e7be86bac8c75a170
chiaberry <analuciaby@gmail.com>
2025-03-27 12:40:25 -0500

    add fk constraint

--- moped-database/migrations/1743001305353_add_project_files_lookup/down.sql
+++ moped-database/migrations/1743001305353_add_project_files_lookup/down.sql
@@ -1,1 +1,4 @@
+ALTER TABLE "public"."moped_project_files"
+DROP CONSTRAINT moped_project_files_type_fkey;
+
 DROP TABLE "public"."moped_file_types"
--- moped-database/migrations/1743001305353_add_project_files_lookup/up.sql
+++ moped-database/migrations/1743001305353_add_project_files_lookup/up.sql
@@ -5,4 +5,8 @@ INSERT INTO moped_file_types (id, name, slug) VALUES
 (1, 'Funding', 'funding'),
 (2, 'Plans', 'plans'),
 (3, 'Estimates', 'estimates'),
-(4, 'Other', 'other')
+(4, 'Other', 'other');
+
+ALTER TABLE "public"."moped_project_files"
+ADD CONSTRAINT moped_project_files_type_fkey FOREIGN KEY ("file_type")
+REFERENCES moped_file_types ("id");
