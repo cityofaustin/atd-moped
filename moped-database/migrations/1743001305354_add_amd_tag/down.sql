36ee8068a5ed8f55026f1fd23196642369501c1e
Mike <mike.dilley@gmail.com>
2025-04-04 14:13:44 -0500

    Add migrations to add new AMD tag

---
+++ moped-database/migrations/1743001305354_add_amd_tag/down.sql
@@ -0,0 +1,1 @@
+DELETE FROM public.moped_tags WHERE slug = 'arterial_management_small_scale_projects';
---
+++ moped-database/migrations/1743001305354_add_amd_tag/up.sql
@@ -0,0 +1,2 @@
+INSERT INTO moped_tags (name, type, slug, is_deleted) VALUES
+('Arterial Management - Small Scale Projects', 'Other', 'arterial_management_small_scale_projects', FALSE);
