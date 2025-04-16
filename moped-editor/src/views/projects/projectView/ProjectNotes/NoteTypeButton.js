48b8a88fdf9f570fdf474bd6aac1f1b05b52a25c
chiaberry <analuciaby@gmail.com>
2025-04-16 15:14:39 -0500

    add doc strings

--- moped-editor/src/views/projects/projectView/ProjectNotes/NoteTypeButton.js
+++ moped-editor/src/views/projects/projectView/ProjectNotes/NoteTypeButton.js
@@ -3,6 +3,11 @@ import { Button } from "@mui/material";
 /**
  * Defines the NoteTypeButton with a toggle style-change behavior.
  * @param {Object} props
+ * @param {number} noteTypeId - id of note type for button
+ * @param {string} showButtonItemStyle - class name for style
+ * @param {function} setFilterNoteType - function to set the filter note type state
+ * @param {string} filterNoteType - id for toggled note type
+ * @param {string} label - button label
  * @return {JSX.Element}
  * @constructor
  */
