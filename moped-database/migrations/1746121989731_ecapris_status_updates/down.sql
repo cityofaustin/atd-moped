1592be4f119367c9462ebf72fed138c2a98e9064
Mike <mike.dilley@gmail.com>
2025-05-02 18:10:10 -0500

    Track ecapris statuses table in Hasura metadata

--- moped-database/migrations/1746121989731_ecapris_status_updates/down.sql
+++ moped-database/migrations/1746121989731_ecapris_status_updates/down.sql
@@ -5,11 +5,5 @@ DROP COLUMN should_sync_ecapris_statuses;
 -- Drop ecapris_status_updates table
 DROP TABLE IF EXISTS public.ecapris_subproject_statuses;
 
--- Drop trigger for updated_at column
-DROP TRIGGER IF EXISTS set_ecapris_subproject_statuses_updated_at ON public.ecapris_subproject_statuses;
-
 -- Drop function to match user email and set created_by_user_id
 DROP FUNCTION IF EXISTS public.find_user_match_by_email;
-
--- Drop trigger for created_by_user_id column
-DROP TRIGGER IF EXISTS public.find_ecapris_user_match_by_email ON public.ecapris_subproject_statuses;
