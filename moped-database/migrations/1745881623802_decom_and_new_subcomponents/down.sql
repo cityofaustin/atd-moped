781a8a700be4c2ea89424cea288cbfc6b911dc1f
Mike <mike.dilley@gmail.com>
2025-05-07 18:23:29 -0500

    Add unique constraint on moped_components_subcomponents table and comment

--- moped-database/migrations/1745881623802_decom_and_new_subcomponents/down.sql
+++ moped-database/migrations/1745881623802_decom_and_new_subcomponents/down.sql
@@ -1,4 +1,8 @@
--- if we delete the component tag in the future, we will soft delete and leave existing data intact
+-- if we delete the added component tag in the future, we will soft delete and leave existing data intact
+
+-- Remove unique constraint to allow for duplicate component/subcomponent associations
+ALTER TABLE moped_components_subcomponents
+DROP CONSTRAINT unique_component_subcomponents;
 
 -- Restore previous component tag name
 UPDATE moped_component_tags SET name = 'Upgrades Existing', type = 'Bikeways - Performance Measure', slug = 'bikeways_performance_measure_upgrades_existing'
