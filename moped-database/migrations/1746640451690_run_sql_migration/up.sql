ff5a4a0ef23e2e01c0d4aacbeb33b8f6279f5df2
rose <rose.eichelmann@gmail.com>
2025-05-07 15:40:16 -0500

    haha just a typo

--- moped-database/migrations/1746640451690_run_sql_migration/up.sql
+++ moped-database/migrations/1746640451690_run_sql_migration/up.sql
@@ -1,3 +1,4 @@
 -- Ensures that projects can only have one current phase
-REATE UNIQUE INDEX moped_proj_phases_partial_constraint ON moped_proj_phases (project_id)
+CREATE UNIQUE INDEX moped_proj_phases_partial_constraint ON moped_proj_phases (project_id)
     WHERE (is_current_phase = TRUE AND is_deleted = FALSE);
+    
