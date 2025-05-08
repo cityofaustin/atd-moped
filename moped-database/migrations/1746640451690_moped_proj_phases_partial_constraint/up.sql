733b12fcdbe45425a7a5433d3550a4199e37fb38
rose <rose.eichelmann@gmail.com>
2025-05-08 10:04:40 -0500

    fix error

--- moped-database/migrations/1746640451690_moped_proj_phases_partial_constraint/up.sql
+++ moped-database/migrations/1746640451690_moped_proj_phases_partial_constraint/up.sql
@@ -2,4 +2,4 @@
 CREATE UNIQUE INDEX moped_proj_phases_partial_constraint ON moped_proj_phases (project_id)
     WHERE (is_current_phase = TRUE AND is_deleted = FALSE);
 
-comment on index moped_proj_phases_partial_constraint is "Ensures each project can only have one active current phase";
+COMMENT ON INDEX moped_proj_phases_partial_constraint IS 'Ensures each project can only have one active current phase.';
