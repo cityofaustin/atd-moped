1f26877991d34748b1ad751e1afd64a69805776b
rose <rose.eichelmann@gmail.com>
2025-05-08 10:00:10 -0500

    update folder name and add comment to index

--- moped-database/migrations/1746640451690_moped_proj_phases_partial_constraint/up.sql
+++ moped-database/migrations/1746640451690_moped_proj_phases_partial_constraint/up.sql
@@ -1,4 +1,5 @@
 -- Ensures that projects can only have one current phase
 CREATE UNIQUE INDEX moped_proj_phases_partial_constraint ON moped_proj_phases (project_id)
     WHERE (is_current_phase = TRUE AND is_deleted = FALSE);
-    
+
+comment on index moped_proj_phases_partial_constraint is "Ensures each project can only have one active current phase";
