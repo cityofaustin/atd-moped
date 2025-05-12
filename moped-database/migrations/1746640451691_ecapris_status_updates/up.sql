8ca4b1173c649f84e39155ee843bf07e0506766d
Mike <mike.dilley@gmail.com>
2025-05-12 14:27:32 -0500

    Update comments

--- moped-database/migrations/1746640451691_ecapris_status_updates/up.sql
+++ moped-database/migrations/1746640451691_ecapris_status_updates/up.sql
@@ -34,8 +34,8 @@ COMMENT ON COLUMN public.ecapris_subproject_statuses.current_status_fl IS 'Is th
 COMMENT ON COLUMN public.ecapris_subproject_statuses.sub_project_status_desc IS 'Content of the subproject status';
 COMMENT ON COLUMN public.ecapris_subproject_statuses.review_timestamp IS 'Timestamp of the status update - MM/DD/YYYY HH:MM:SS format';
 COMMENT ON COLUMN public.ecapris_subproject_statuses.subproject_status_impacts IS 'Updates on project blockers';
-COMMENT ON COLUMN public.ecapris_subproject_statuses.summary_description IS 'More of a public-ready status could be used down the road';
-COMMENT ON COLUMN public.ecapris_subproject_statuses.reviewed_by_name IS 'First and last name of author';
+COMMENT ON COLUMN public.ecapris_subproject_statuses.summary_description IS 'More of a public-ready status could be used down the road; nullable in eCapris';
+COMMENT ON COLUMN public.ecapris_subproject_statuses.reviewed_by_name IS 'First and last name of author; nullable in eCapris';
 COMMENT ON COLUMN public.ecapris_subproject_statuses.review_by_email IS 'Email of author';
 COMMENT ON COLUMN public.ecapris_subproject_statuses.created_at IS 'Timestamp when the record was created';
 COMMENT ON COLUMN public.ecapris_subproject_statuses.updated_at IS 'Timestamp when the record was last updated';
@@ -45,12 +45,13 @@ COMMENT ON COLUMN public.ecapris_subproject_statuses.updated_by_user_id IS 'ID o
 -- Create trigger to set updated_at audit column before update
 CREATE TRIGGER set_ecapris_subproject_statuses_updated_at BEFORE UPDATE ON public.ecapris_subproject_statuses FOR EACH ROW EXECUTE FUNCTION public.set_updated_at();
 
--- Create function to search for eCapris subproject status author match by email
--- Function to match user email and set created_by_user_id
-CREATE OR REPLACE FUNCTION public.find_user_match_by_email()
+COMMENT ON TRIGGER set_ecapris_subproject_statuses_updated_at ON public.ecapris_subproject_statuses IS 'Trigger to set updated_at on row update';
+
+-- Create function to search for eCapris subproject status author match by email and set created_by_user_id if found
+CREATE OR REPLACE FUNCTION public.find_ecapris_user_match_by_email()
 RETURNS TRIGGER AS $$
 BEGIN
-    -- Try to find matching user by email (case-insensitive)email 
+    -- Try to find matching user by email (case-insensitive) email 
     NEW.created_by_user_id := (
         SELECT user_id 
         FROM moped_users 
@@ -62,6 +63,10 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+COMMENT ON FUNCTION public.find_ecapris_user_match_by_email() IS 'Function to attempt matching eCapris status author email to Moped users by email';
+
 -- Create trigger to call function to try match author (if there is one) before insert
 CREATE TRIGGER find_ecapris_user_match_by_email BEFORE INSERT ON public.ecapris_subproject_statuses
-FOR EACH ROW EXECUTE FUNCTION public.find_user_match_by_email();
+FOR EACH ROW EXECUTE FUNCTION public.find_ecapris_user_match_by_email();
+
+COMMENT ON TRIGGER find_ecapris_user_match_by_email ON public.ecapris_subproject_statuses IS 'Trigger to attempt match of eCapris author to Moped user by email on insert';
