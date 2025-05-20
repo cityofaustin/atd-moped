66772db7a9550c64c3d8676bddff69f46716ca29
chiaberry <analuciaby@gmail.com>
2025-05-20 14:20:25 -0500

    removing the old config

--- moped-editor/src/layouts/DashboardLayout/NavBar/NavigationSearchQueryConf.js
+++ moped-editor/src/layouts/DashboardLayout/NavBar/NavigationSearchQueryConf.js
@@ -1,5 +1,5 @@
 /**
- * The Query configuration (now also including filters)
+ * The Query configuration for the navigation search input
  * @constant
  * @type {object}
  * @augments NavigationSearchFiltersConf
@@ -47,59 +47,4 @@ export const NAVIGATION_SEARCH_QUERY_CONFIG = {
       type: "string",
     },
   },
-};
-
-export const NavigationSearchQueryConf = {
-  options: {
-    useQuery: {
-      fetchPolicy: "cache-first", // Default is "cache-first", or use "no-cache"
-    },
-  },
-  table: "project_list_view",
-  columns: {
-    project_id: {
-      searchable: true,
-      search: {
-        operator: "_eq",
-        quoted: false,
-        envelope: "%{VALUE}%",
-        invalidValueDefault: 0,
-      },
-      type: "Int",
-    },
-    project_name_full: {
-      searchable: true,
-      search: {
-        operator: "_ilike",
-        quoted: true,
-        envelope: "%{VALUE}%",
-      },
-      type: "String",
-    },
-    project_description: {
-      searchable: true,
-      search: {
-        operator: "_ilike",
-        quoted: true,
-        envelope: "%{VALUE}%",
-      },
-      type: "String",
-    },
-    ecapris_subproject_id: {
-      searchable: true,
-      search: {
-        operator: "_ilike",
-        quoted: true,
-        envelope: "%{VALUE}%",
-      },
-      type: "string",
-    },
-  },
-  // This object gets consumed into the GQLAbstract system, and here is the single, un-nested order_by directive. ✅
-  order_by: { updated_at: "desc" },
-  where: null,
-  or: null,
-  and: null,
-  limit: 250,
-  offset: 0,
 };
