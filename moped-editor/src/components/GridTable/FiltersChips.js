0e82362747f2aaaf9ee85fc20980bb90ff84b904
tillyw <tilly.whitson@austintexas.gov>
2025-05-20 16:21:18 -0500

    reenables save view with new search term

--- moped-editor/src/components/GridTable/FiltersChips.js
+++ moped-editor/src/components/GridTable/FiltersChips.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Typography, Chip, Grid, Button, Tooltip } from "@mui/material";
 import makeStyles from "@mui/styles/makeStyles";
 import {
@@ -120,15 +120,17 @@ const FiltersChips = ({
     setIsSaveViewModalOpen(true);
   };
 
+  /**
+   * Reenables the save view button when a new search term is applied
+   */
+  useEffect(() => {
+    setIsViewSaved(false);
+  }, [searchTerm]);
+
   return (
     <Box className={classes.filtersList}>
       <Typography className={classes.filtersText} component="span">
-        <Grid
-          container
-          display="flex"
-          justifyContent="flex-end"
-          spacing={0.5}
-        >
+        <Grid container display="flex" justifyContent="flex-end" spacing={0.5}>
           <Grid item>
             <Tooltip
               placement="bottom-start"
