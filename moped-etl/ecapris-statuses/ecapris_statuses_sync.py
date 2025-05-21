299a90e2481435df8a778b6b41a4d270cb3ea209
Mike <mike.dilley@gmail.com>
2025-05-21 18:18:09 -0500

    Black formatting

--- moped-etl/ecapris-statuses/ecapris_statuses_sync.py
+++ moped-etl/ecapris-statuses/ecapris_statuses_sync.py
@@ -20,6 +20,7 @@ ORACLE_HOST = os.getenv("ORACLE_HOST")
 ORACLE_PORT = os.getenv("ORACLE_PORT")
 ORACLE_SERVICE = os.getenv("ORACLE_SERVICE")
 
+
 def get_conn(host, port, service, user, password):
     # Need to run this once if you want to work locally
     # Change lib_dir to your cx_Oracle library location
@@ -30,21 +31,28 @@ def get_conn(host, port, service, user, password):
     dsn_tns = cx_Oracle.makedsn(host, port, service_name=service)
     return cx_Oracle.connect(user=user, password=password, dsn=dsn_tns)
 
+
 def main():
     # Connect to Moped DB and get distinct eCapris subproject IDs set on any project
     results = make_hasura_request(query=GRAPHQL_QUERIES["subproject_statuses"])
-    
+
     # eCapris subproject IDs are de-duplicated in the GraphQL query
-    distinct_project_ecapris_ids = [project["ecapris_subproject_id"] for project in results["moped_project"]]
-    logger.info(f"Found {len(distinct_project_ecapris_ids)} unique eCapris subproject IDs to sync: {', '.join(distinct_project_ecapris_ids)}")
-    
+    distinct_project_ecapris_ids = [
+        project["ecapris_subproject_id"] for project in results["moped_project"]
+    ]
+    logger.info(
+        f"Found {len(distinct_project_ecapris_ids)} unique eCapris subproject IDs to sync: {', '.join(distinct_project_ecapris_ids)}"
+    )
+
     # Connect to Oracle DB and query status updates matching those subproject IDs
-    conn = get_conn(ORACLE_HOST, ORACLE_PORT, ORACLE_SERVICE, ORACLE_USER, ORACLE_PASSWORD)
+    conn = get_conn(
+        ORACLE_HOST, ORACLE_PORT, ORACLE_SERVICE, ORACLE_USER, ORACLE_PASSWORD
+    )
     cursor = conn.cursor()
     cursor.prepare(ORACLE_QUERIES["subproject_statuses"])
 
     # Loop through the distinct project eCapris IDs, query the status updates, and collect them by id
-    statuses_by_ecapris_id = {} 
+    statuses_by_ecapris_id = {}
     no_result_ids = []
 
     for i, sp_number in enumerate(distinct_project_ecapris_ids):
@@ -58,11 +66,11 @@ def main():
 
         results = cursor.fetchall()
 
-        if(len(results) > 0):
+        if len(results) > 0:
             statuses_by_ecapris_id[sp_number] = results
         else:
             no_result_ids.append(sp_number)
-            
+
     logger.info(f"No results for eCapris IDs: {', '.join(no_result_ids)}")
 
     # Loop through the eCapris IDs and upsert the status updates into the Moped DB
@@ -73,30 +81,42 @@ def main():
 
         for status in statuses:
             review_timestamp = status["STATUS_REVIEW_DATE"]
-            timezone_aware_review_timestamp = convert_to_timezone_aware_timestamp(review_timestamp)
+            timezone_aware_review_timestamp = convert_to_timezone_aware_timestamp(
+                review_timestamp
+            )
 
-            payload.append({
-                "subproject_status_id": status["SUB_PROJECT_STATUS_ID"],
-                "subproject_name": status["SP_NAME"],
-                "ecapris_subproject_id": status["SP_NUMBER"],
-                "current_status_fl": status["CURR_STATUS_FL"],
-                "sub_project_status_desc": status["SUB_PROJECT_STATUS_DESC"],
-                "review_timestamp": timezone_aware_review_timestamp,
-                "subproject_status_impacts": status["SUB_PROJECT_STATUS_IMPACTS"],
-                "summary_description": status["SUMM_DESC"],
-                "reviewed_by_name": status["REVIEWED_BY"],
-                "reviewed_by_email": status["REVIEWED_BY_EMAIL"],
-            })
+            payload.append(
+                {
+                    "subproject_status_id": status["SUB_PROJECT_STATUS_ID"],
+                    "subproject_name": status["SP_NAME"],
+                    "ecapris_subproject_id": status["SP_NUMBER"],
+                    "current_status_fl": status["CURR_STATUS_FL"],
+                    "sub_project_status_desc": status["SUB_PROJECT_STATUS_DESC"],
+                    "review_timestamp": timezone_aware_review_timestamp,
+                    "subproject_status_impacts": status["SUB_PROJECT_STATUS_IMPACTS"],
+                    "summary_description": status["SUMM_DESC"],
+                    "reviewed_by_name": status["REVIEWED_BY"],
+                    "reviewed_by_email": status["REVIEWED_BY_EMAIL"],
+                }
+            )
 
-        results = make_hasura_request(query=GRAPHQL_QUERIES["subproject_statuses_upsert"], variables={"objects": payload})
+        results = make_hasura_request(
+            query=GRAPHQL_QUERIES["subproject_statuses_upsert"],
+            variables={"objects": payload},
+        )
 
         if len(results["insert_ecapris_subproject_statuses"]["returning"]) > 0:
             updated_ecapris_ids.append(ecapris_id)
 
     if len(updated_ecapris_ids) == 0:
-        logger.info("No new eCapris statuses were found for subproject IDs associated with Moped projects.")
+        logger.info(
+            "No new eCapris statuses were found for subproject IDs associated with Moped projects."
+        )
     else:
-        logger.info(f"Upserted eCapris statuses for eCapris IDs: {', '.join(updated_ecapris_ids)}")
+        logger.info(
+            f"Upserted eCapris statuses for eCapris IDs: {', '.join(updated_ecapris_ids)}"
+        )
+
 
 if __name__ == "__main__":
     log_level = logging.DEBUG
--- moped-etl/ecapris-statuses/process/time.py
+++ moped-etl/ecapris-statuses/process/time.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone
 from zoneinfo import ZoneInfo
 
+
 def convert_to_timezone_aware_timestamp(date_str):
     """
     Converts a timestamp string in the format 'YYYY-MM-DD HH:MM:SS' to a timezone-aware
@@ -14,13 +15,13 @@ def convert_to_timezone_aware_timestamp(date_str):
     """
     # Update the date format to match your input without milliseconds
     date_format = "%Y-%m-%d %H:%M:%S"
-    
+
     # Parse the timestamp string
     parsed_date = datetime.strptime(date_str, date_format)
-    
+
     # Convert to timezone-aware by attaching the America/Chicago timezone
     parsed_date_tz = parsed_date.replace(tzinfo=ZoneInfo("America/Chicago"))
-    
+
     # Convert to ISO format
     timestamptz = parsed_date_tz.isoformat()
 
