a4cd402265c78ade04edd672b11070378d48a62a
Mike <mike.dilley@gmail.com>
2025-05-29 11:08:55 -0500

    Add initial view

---
+++ moped-database/migrations/default/1748534889272_create_notes_view/up.sql
@@ -0,0 +1,32 @@
+CREATE OR REPLACE VIEW combined_project_notes AS
+SELECT 
+    project_note_id,
+    project_note,
+    created_at,
+    project_id,
+    created_by_user_id,
+    project_note_type,
+    is_deleted,
+    phase_id,
+    updated_at,
+    updated_by_user_id,
+    true as is_editable,
+    NULL as ecapris_subproject_id
+FROM 
+    moped_proj_notes
+UNION ALL
+SELECT 
+    id AS project_note_id,
+    sub_project_status_desc AS project_note,
+    review_timestamp AS created_at,
+    NULL::integer AS project_id, -- You may need to adjust this mapping
+    created_by_user_id,
+    3 AS project_note_type, -- Setting to 3 as requested
+    false AS is_deleted,
+    NULL::integer AS phase_id,
+    updated_at,
+    updated_by_user_id,
+    false AS is_editable,
+    ecapris_subproject_id
+FROM 
+    ecapris_subproject_statuses;
