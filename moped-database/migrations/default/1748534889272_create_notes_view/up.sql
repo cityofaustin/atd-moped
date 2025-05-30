e993458872eefb3d88b05a50e32b64bbd455f692
Mike <mike.dilley@gmail.com>
2025-05-30 17:34:07 -0500

    Update down migration and update view with table references and user table joins

--- moped-database/migrations/default/1748534889272_create_notes_view/up.sql
+++ moped-database/migrations/default/1748534889272_create_notes_view/up.sql
@@ -1,34 +1,32 @@
 CREATE OR REPLACE VIEW combined_project_notes AS
 SELECT
-    project_note_id,
-    project_note,
-    created_at,
-    project_id,
-    created_by_user_id,
-    project_note_type,
-    is_deleted,
-    phase_id,
-    updated_at,
-    updated_by_user_id,
-    true AS is_editable,
-    null AS ecapris_subproject_id
+    ('M' || moped_proj_notes.project_note_id) AS id,
+    moped_proj_notes.project_note,
+    moped_proj_notes.created_at,
+    moped_proj_notes.project_id,
+    (moped_users.first_name || ' ' || moped_users.last_name) AS author,
+    moped_proj_notes.project_note_type,
+    moped_proj_notes.is_deleted,
+    moped_proj_notes.phase_id,
+    TRUE AS is_editable,
+    NULL AS ecapris_subproject_id
 FROM
     moped_proj_notes
+LEFT JOIN moped_users ON moped_proj_notes.created_by_user_id = moped_users.user_id
 UNION ALL
 SELECT
-    id AS project_note_id,
-    sub_project_status_desc AS project_note,
-    review_timestamp AS created_at,
-    null::integer AS project_id, -- You may need to adjust this mapping
-    created_by_user_id,
-    3 AS project_note_type, -- Setting to 3 as requested
-    false AS is_deleted,
-    null::integer AS phase_id,
-    updated_at,
-    updated_by_user_id,
-    false AS is_editable,
-    ecapris_subproject_id
+    ('E' || ecapris_subproject_statuses.id) AS id,
+    ecapris_subproject_statuses.sub_project_status_desc AS project_note,
+    ecapris_subproject_statuses.review_timestamp AS created_at,
+    NULL::integer AS project_id,
+    COALESCE((moped_users.first_name || ' ' || moped_users.last_name), LOWER(ecapris_subproject_statuses.reviewed_by_email), ecapris_subproject_statuses.reviewed_by_name) AS author,
+    3 AS project_note_type,
+    FALSE AS is_deleted,
+    NULL AS phase_id,
+    FALSE AS is_editable,
+    ecapris_subproject_statuses.ecapris_subproject_id
 FROM
-    ecapris_subproject_statuses;
-
--- TODO Update trigger to match user by email to run on update too?
+    ecapris_subproject_statuses
+LEFT JOIN moped_users ON ecapris_subproject_statuses.created_by_user_id = moped_users.user_id
+ORDER BY
+    created_at DESC;
