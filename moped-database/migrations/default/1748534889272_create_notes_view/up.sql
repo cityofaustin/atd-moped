35af23013dd1472b58fd5be74190ed9c394298e4
Mike <mike.dilley@gmail.com>
2025-05-30 09:54:52 -0500

    Add todo

--- moped-database/migrations/default/1748534889272_create_notes_view/up.sql
+++ moped-database/migrations/default/1748534889272_create_notes_view/up.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW combined_project_notes AS
-SELECT 
+SELECT
     project_note_id,
     project_note,
     created_at,
@@ -10,23 +10,25 @@ SELECT
     phase_id,
     updated_at,
     updated_by_user_id,
-    true as is_editable,
-    NULL as ecapris_subproject_id
-FROM 
+    true AS is_editable,
+    null AS ecapris_subproject_id
+FROM
     moped_proj_notes
 UNION ALL
-SELECT 
+SELECT
     id AS project_note_id,
     sub_project_status_desc AS project_note,
     review_timestamp AS created_at,
-    NULL::integer AS project_id, -- You may need to adjust this mapping
+    null::integer AS project_id, -- You may need to adjust this mapping
     created_by_user_id,
     3 AS project_note_type, -- Setting to 3 as requested
     false AS is_deleted,
-    NULL::integer AS phase_id,
+    null::integer AS phase_id,
     updated_at,
     updated_by_user_id,
     false AS is_editable,
     ecapris_subproject_id
-FROM 
+FROM
     ecapris_subproject_statuses;
+
+-- TODO Update trigger to match user by email to run on update too?
