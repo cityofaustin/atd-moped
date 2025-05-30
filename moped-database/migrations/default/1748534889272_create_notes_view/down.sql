9779ffa670d24e0695b173712565d0fed5db4efd
Mike <mike.dilley@gmail.com>
2025-05-30 17:55:29 -0500

    Add to project note types

--- moped-database/migrations/default/1748534889272_create_notes_view/down.sql
+++ moped-database/migrations/default/1748534889272_create_notes_view/down.sql
@@ -1,1 +1,6 @@
 DROP VIEW IF EXISTS combined_project_notes;
+
+DELETE FROM moped_note_types
+WHERE slug = 'ecapris_status';
+
+ALTER TABLE moped_note_types DROP COLUMN source;
--- moped-database/migrations/default/1748534889272_create_notes_view/up.sql
+++ moped-database/migrations/default/1748534889272_create_notes_view/up.sql
@@ -1,3 +1,12 @@
+-- Update moped_note_types to have source column
+ALTER TABLE moped_note_types
+ADD COLUMN source text DEFAULT 'moped';
+
+-- Insert eCapris status note type that will be used in notes view
+INSERT INTO moped_note_types (name, slug, source)
+VALUES
+('eCapris status', 'ecapris_status', 'ecapris');
+
 CREATE OR REPLACE VIEW combined_project_notes AS
 SELECT
     ('M' || moped_proj_notes.project_note_id) AS id,
@@ -5,7 +14,7 @@ SELECT
     moped_proj_notes.created_at,
     moped_proj_notes.project_id,
     (moped_users.first_name || ' ' || moped_users.last_name) AS author,
-    moped_proj_notes.project_note_type,
+    moped_note_types.name,
     moped_proj_notes.is_deleted,
     moped_proj_notes.phase_id,
     TRUE AS is_editable,
@@ -13,6 +22,7 @@ SELECT
 FROM
     moped_proj_notes
 LEFT JOIN moped_users ON moped_proj_notes.created_by_user_id = moped_users.user_id
+LEFT JOIN moped_note_types ON moped_proj_notes.project_note_type = moped_note_types.id
 UNION ALL
 SELECT
     ('E' || ecapris_subproject_statuses.id) AS id,
@@ -20,7 +30,7 @@ SELECT
     ecapris_subproject_statuses.review_timestamp AS created_at,
     NULL::integer AS project_id,
     COALESCE((moped_users.first_name || ' ' || moped_users.last_name), LOWER(ecapris_subproject_statuses.reviewed_by_email), ecapris_subproject_statuses.reviewed_by_name) AS author,
-    3 AS project_note_type,
+    moped_note_types.name,
     FALSE AS is_deleted,
     NULL AS phase_id,
     FALSE AS is_editable,
@@ -28,5 +38,6 @@ SELECT
 FROM
     ecapris_subproject_statuses
 LEFT JOIN moped_users ON ecapris_subproject_statuses.created_by_user_id = moped_users.user_id
+LEFT JOIN moped_note_types ON moped_note_types.slug = 'ecapris_status'
 ORDER BY
     created_at DESC;
