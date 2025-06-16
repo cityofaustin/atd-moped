14e3d7ac77af70d18f7a73d7f02e20519b686975
rose <rose.eichelmann@gmail.com>
2025-06-16 14:54:01 -0500

    add back in this

--- moped-editor/src/views/projects/projectView/ProjectView.js
+++ moped-editor/src/views/projects/projectView/ProjectView.js
@@ -227,7 +227,7 @@ const ProjectView = () => {
           state: locationState, // Preserve the location state when changing tabs
         }
       );
-      refetch();
+      if (newTab === 0) refetch();
     },
     [refetch, setSearchParams, locationState]
   );
