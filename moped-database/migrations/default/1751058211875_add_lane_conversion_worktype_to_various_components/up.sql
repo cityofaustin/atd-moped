a86661bebee4fe696c230faa8ad5c90e90ed79ad
tillyw <tilly.whitson@austintexas.gov>
2025-07-01 18:27:49 -0500

    update inserts to use component_name_full instead of id

--- moped-database/migrations/default/1751058211875_add_lane_conversion_worktype_to_various_components/up.sql
+++ moped-database/migrations/default/1751058211875_add_lane_conversion_worktype_to_various_components/up.sql
@@ -1,5 +1,7 @@
-INSERT INTO public.moped_component_work_types (component_id, work_type_id) VALUES (0, 4);
-INSERT INTO public.moped_component_work_types (component_id, work_type_id) VALUES (52, 4);
-INSERT INTO public.moped_component_work_types (component_id, work_type_id) VALUES (53, 4);
-INSERT INTO public.moped_component_work_types (component_id, work_type_id) VALUES (54, 4);
-INSERT INTO public.moped_component_work_types (component_id, work_type_id) VALUES (55, 4);
+INSERT INTO public.moped_component_work_types (component_id, work_type_id)
+SELECT
+    moped_components.component_id,
+    moped_work_types.id
+FROM moped_components, moped_work_types
+WHERE moped_components.component_name_full IN ('Project Extent - Generic (linear)', 'Transit - Lane', 'Transit - Managed Lane Access Point', 'Transit - Transit/Bike Lane', 'Transit - Transit Queue Jump')
+    AND moped_work_types.key = 'lane_conversion';
