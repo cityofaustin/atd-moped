233d0a51d23f98f7361e0afbc57779684c6413d8
rose <rose.eichelmann@gmail.com>
2025-07-02 14:48:39 -0500

    disable triggers

--- moped-database/migrations/default/1750186599182_set_should_sync_ecapris_false/up.sql
+++ moped-database/migrations/default/1750186599182_set_should_sync_ecapris_false/up.sql
@@ -1,4 +1,10 @@
+-- use replica mode to disable triggers
+set session_replication_role = replica;
+
 -- should_sync_ecapris_statuses will default to false for projects without an eCAPRIS subproject id
 update moped_project set should_sync_ecapris_statuses = false where ecapris_subproject_id is null;
 
 alter table "public"."moped_project" alter column "should_sync_ecapris_statuses" set default 'false';
+
+-- reset the session replication role
+set session_replication_role = default;
