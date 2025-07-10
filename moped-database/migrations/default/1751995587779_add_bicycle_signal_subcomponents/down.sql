253852306f4526f5d14dba5f368c4a50cb90dc9b
mateoclarke <mateosalinasclarke@proton.me>
2025-07-10 11:24:01 -0600

    implement PR feedback from Tilly

--- moped-database/migrations/default/1751995587779_add_bicycle_signal_subcomponents/down.sql
+++ moped-database/migrations/default/1751995587779_add_bicycle_signal_subcomponents/down.sql
@@ -1,6 +1,6 @@
 -- Remove the associations between Signal - Traffic component and the new subcomponents
 DELETE FROM moped_components_subcomponents 
-WHERE component_id = 18 
+WHERE component_id = (SELECT component_id FROM moped_components WHERE component_name = 'Signal' AND component_subtype = 'Traffic')
 AND subcomponent_id IN (
     SELECT subcomponent_id 
     FROM moped_subcomponents 
@@ -15,4 +15,4 @@ DELETE FROM moped_subcomponents
 WHERE subcomponent_name IN (
     'Bicycle signal (may use pedestrian signal sign)',
     'Bicycle signal (permissive conflicts)'
-); 
+);
--- moped-database/migrations/default/1751995587779_add_bicycle_signal_subcomponents/up.sql
+++ moped-database/migrations/default/1751995587779_add_bicycle_signal_subcomponents/up.sql
@@ -3,13 +3,13 @@ INSERT INTO moped_subcomponents (subcomponent_name) VALUES
     ('Bicycle signal (may use pedestrian signal sign)'),
     ('Bicycle signal (permissive conflicts)');
 
--- Associate the new subcomponents with Signal - Traffic component (component_id = 18)
+-- Associate the new subcomponents with Signal - Traffic component
 INSERT INTO moped_components_subcomponents (component_id, subcomponent_id)
 SELECT 
-    18 as component_id,
+    (SELECT component_id FROM moped_components WHERE component_name = 'Signal' AND component_subtype = 'Traffic') as component_id,
     subcomponent_id
 FROM moped_subcomponents 
 WHERE subcomponent_name IN (
     'Bicycle signal (may use pedestrian signal sign)',
     'Bicycle signal (permissive conflicts)'
-); 
+);
