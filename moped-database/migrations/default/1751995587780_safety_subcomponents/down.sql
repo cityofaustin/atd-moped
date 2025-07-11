5677f2b47afa228448e81c109a4926b4571f056b
Mike <mike.dilley@gmail.com>
2025-07-11 12:07:12 -0500

    Fix formatting and down migration syntax

--- moped-database/migrations/default/1751995587780_safety_subcomponents/down.sql
+++ moped-database/migrations/default/1751995587780_safety_subcomponents/down.sql
@@ -3,14 +3,19 @@ UPDATE moped_subcomponents SET subcomponent_name = 'Bicycle signal face (interim
 WHERE subcomponent_name = 'Bicycle signal face (interim approval / protected only)';
 
 -- Remove Speed Limit Changes subcomponent from Project Extent - Generic (linear) and Roadway components
-DELETE FROM moped_components_subcomponents 
-WHERE component_id = (SELECT component_id FROM moped_components WHERE component_name_full = 'Project Extent - Generic (linear' OR component_name_full = 'Roadway')
-AND subcomponent_id = (
-    SELECT subcomponent_id 
-    FROM moped_subcomponents 
-    WHERE subcomponent_name = 'Speed Limit Changes';
-);
+DELETE FROM moped_components_subcomponents
+WHERE component_id IN (
+        SELECT component_id
+        FROM moped_components
+        WHERE component_name_full = 'Project Extent - Generic (linear)'
+            OR component_name_full = 'Roadway'
+    )
+    AND subcomponent_id = (
+        SELECT subcomponent_id
+        FROM moped_subcomponents
+        WHERE subcomponent_name = 'Speed Limit Changes'
+    );
 
 -- Remove new Speed Limit Changes subcomponent
-DELETE FROM moped_subcomponents 
-WHERE subcomponent_name = 'Speed Limit Changes'
+DELETE FROM moped_subcomponents
+WHERE subcomponent_name = 'Speed Limit Changes';
--- moped-database/migrations/default/1751995587780_safety_subcomponents/up.sql
+++ moped-database/migrations/default/1751995587780_safety_subcomponents/up.sql
@@ -7,10 +7,10 @@ INSERT INTO moped_subcomponents (subcomponent_name) VALUES ('Speed Limit Changes
 
 -- Add Speed Limit Changes subcomponent to Project Extent - Generic (linear) and Roadway
 INSERT INTO moped_components_subcomponents (component_id, subcomponent_id)
-SELECT 
+SELECT
     mc.component_id,
     ms.subcomponent_id
-FROM moped_components mc
-CROSS JOIN moped_subcomponents ms
+FROM moped_components AS mc
+CROSS JOIN moped_subcomponents AS ms
 WHERE mc.component_name_full IN ('Project Extent - Generic (linear)', 'Roadway')
-AND ms.subcomponent_name = 'Speed Limit Changes';
+    AND ms.subcomponent_name = 'Speed Limit Changes';
