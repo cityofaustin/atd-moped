e5e95fe22d4ea97f8ee015b7a7c50b13387e4dfb
Mike <mike.dilley@gmail.com>
2025-07-11 11:49:41 -0500

    Add Speed Limit Updates subcomponent

--- moped-database/migrations/default/1751995587780_safety_subcomponents/down.sql
+++ moped-database/migrations/default/1751995587780_safety_subcomponents/down.sql
@@ -1,14 +1,16 @@
--- Update existing subcomponent
+-- Revert update to existing subcomponent
 UPDATE moped_subcomponents SET subcomponent_name = 'Bicycle signal face (interim approval)'
 WHERE subcomponent_name = 'Bicycle signal face (interim approval / protected only)';
 
--- Remove Speed Limit Changes subcomponent from Project Extent - Generic (linear) and Roadway
--- INSERT INTO moped_components_subcomponents (component_id, subcomponent_id)
--- SELECT 
---     (SELECT component_id FROM moped_components WHERE component_name = 'Signal' AND component_subtype = 'Traffic') as component_id,
---     subcomponent_id
--- FROM moped_subcomponents 
--- WHERE subcomponent_name IN (
---     'Bicycle signal (may use pedestrian signal sign)',
---     'Bicycle signal (permissive conflicts)'
--- );
+-- Remove Speed Limit Changes subcomponent from Project Extent - Generic (linear) and Roadway components
+DELETE FROM moped_components_subcomponents 
+WHERE component_id = (SELECT component_id FROM moped_components WHERE component_name_full = 'Project Extent - Generic (linear' OR component_name_full = 'Roadway')
+AND subcomponent_id = (
+    SELECT subcomponent_id 
+    FROM moped_subcomponents 
+    WHERE subcomponent_name = 'Speed Limit Changes';
+);
+
+-- Remove new Speed Limit Changes subcomponent
+DELETE FROM moped_subcomponents 
+WHERE subcomponent_name = 'Speed Limit Changes'
--- moped-database/migrations/default/1751995587780_safety_subcomponents/up.sql
+++ moped-database/migrations/default/1751995587780_safety_subcomponents/up.sql
@@ -2,15 +2,15 @@
 UPDATE moped_subcomponents SET subcomponent_name = 'Bicycle signal face (interim approval / protected only)'
 WHERE subcomponent_name = 'Bicycle signal face (interim approval)';
 
+-- Add new Speed Limit Changes subcomponent
+INSERT INTO moped_subcomponents (subcomponent_name) VALUES ('Speed Limit Changes');
+
 -- Add Speed Limit Changes subcomponent to Project Extent - Generic (linear) and Roadway
-
-
--- INSERT INTO moped_components_subcomponents (component_id, subcomponent_id)
--- SELECT 
---     (SELECT component_id FROM moped_components WHERE component_name = 'Signal' AND component_subtype = 'Traffic') as component_id,
---     subcomponent_id
--- FROM moped_subcomponents 
--- WHERE subcomponent_name IN (
---     'Bicycle signal (may use pedestrian signal sign)',
---     'Bicycle signal (permissive conflicts)'
--- );
+INSERT INTO moped_components_subcomponents (component_id, subcomponent_id)
+SELECT 
+    mc.component_id,
+    ms.subcomponent_id
+FROM moped_components mc
+CROSS JOIN moped_subcomponents ms
+WHERE mc.component_name_full IN ('Project Extent - Generic (linear)', 'Roadway')
+AND ms.subcomponent_name = 'Speed Limit Changes';
