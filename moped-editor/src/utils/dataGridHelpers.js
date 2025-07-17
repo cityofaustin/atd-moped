a294209951db2e5929d4b5550513acdf6b4c6b86
rose <rose.eichelmann@gmail.com>
2025-07-17 16:01:38 -0500

    handel escape key press

--- moped-editor/src/utils/dataGridHelpers.js
+++ moped-editor/src/utils/dataGridHelpers.js
@@ -2,8 +2,14 @@ import { GridRowEditStopReasons } from "@mui/x-data-grid-pro";
 
 export const defaultEditColumnIconStyle = { fontSize: "24px" };
 
-export const handleRowEditStop = (params, event) => {
+export const handleRowEditStop = (rows, setRows) => (params, event) => {
+  const id = params.row.id;
   if (params.reason === GridRowEditStopReasons.enterKeyDown) {
     event.defaultMuiPrevented = true;
+  }
+  if (params.reason === GridRowEditStopReasons.escapeKeyDown) {
+    if (params.row.isNew) {
+      setRows(rows.filter((row) => row.id !== id));
+    }
   }
 };
