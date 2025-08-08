cb866eb1980b32ea82f9e072ff2e942f5c3c3ad9
mateoclarke <mateosalinasclarke@proton.me>
2025-08-08 11:48:46 -0600

    sx conversion of `ComponentListItem` & `ComponentProperties`

--- moped-editor/src/views/projects/projectView/ProjectComponents/ComponentProperties.js
+++ moped-editor/src/views/projects/projectView/ProjectComponents/ComponentProperties.js
@@ -1,54 +1,45 @@
 import React from "react";
 import { Divider, Typography, Box, Grid } from "@mui/material";
-import makeStyles from "@mui/styles/makeStyles";
-
-const useStyles = makeStyles((theme) => ({
-  dividerSpacing: {
-    marginTop: theme.spacing(2),
-    marginBottom: theme.spacing(2),
-  },
-  propertyItem: {
-    marginBottom: theme.spacing(2),
-    display: "inline-block",
-    marginRight: theme.spacing(4),
-  },
-  propertyLabel: {
-    width: "100%",
-    color: theme.palette.text.secondary,
-    fontSize: ".8rem",
-  },
-  propertyLabelText: {},
-}));
 
 const ComponentProperties = ({ component }) => {
-  const classes = useStyles();
+  // Reusable sx objects for better maintainability
+  const dividerSx = {
+    marginTop: (theme) => theme.spacing(2),
+    marginBottom: (theme) => theme.spacing(2),
+  };
+
+  const propertyItemSx = {
+    marginBottom: (theme) => theme.spacing(2),
+    display: "inline-block",
+    marginRight: (theme) => theme.spacing(4),
+  };
+
+  const propertyLabelSx = {
+    width: "100%",
+    color: (theme) => theme.palette.text.secondary,
+    fontSize: ".8rem",
+  };
 
   return (
     <>
-      <Divider className={classes.dividerSpacing} />
-      <Grid item xs={12} className={classes.propertyItem}>
-        <Typography className={classes.propertyLabel}>Component ID</Typography>
-        <Box className={classes.fieldBox}>
-          <Typography className={classes.propertyLabelText}>
-            {component.projectComponentId}
-          </Typography>
+      <Divider sx={dividerSx} />
+      <Grid item xs={12} sx={propertyItemSx}>
+        <Typography sx={propertyLabelSx}>Component ID</Typography>
+        <Box>
+          <Typography>{component.projectComponentId}</Typography>
         </Box>
       </Grid>
-      <Grid item xs={12} className={classes.propertyItem}>
-        <Typography className={classes.propertyLabel}>
-          Council district(s)
-        </Typography>
-        <Box className={classes.fieldBox}>
-          <Typography className={classes.propertyLabelText}>
-            {component.councilDistrict}
-          </Typography>
+      <Grid item xs={12} sx={propertyItemSx}>
+        <Typography sx={propertyLabelSx}>Council district(s)</Typography>
+        <Box>
+          <Typography>{component.councilDistrict}</Typography>
         </Box>
       </Grid>
       {component.componentLength > 0 && (
-        <Grid item xs={12} className={classes.propertyItem}>
-          <Typography className={classes.propertyLabel}>Length</Typography>
-          <Box className={classes.fieldBox}>
-            <Typography className={classes.propertyLabelText}>
+        <Grid item xs={12} sx={propertyItemSx}>
+          <Typography sx={propertyLabelSx}>Length</Typography>
+          <Box>
+            <Typography>
               {component.componentLength.toLocaleString()}'
             </Typography>
           </Box>
