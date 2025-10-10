e9fae19da4fda9342ac249aae040c744c3856d22
Mike <mike.dilley@gmail.com>
2025-10-10 13:21:11 -0500

    Add comments to the rest of the moped_prod_funding columns

--- moped-database/migrations/default/1759785362249_ecapris_funding/up.sql
+++ moped-database/migrations/default/1759785362249_ecapris_funding/up.sql
@@ -10,7 +10,8 @@ COMMENT ON COLUMN moped_project.should_sync_ecapris_statuses IS 'Indicates if pr
 ALTER TABLE moped_proj_funding
 ADD COLUMN ecapris_funding_id INTEGER,
 ADD COLUMN is_legacy_funding_record BOOLEAN DEFAULT FALSE,
-ADD COLUMN is_editable BOOLEAN GENERATED ALWAYS AS (ecapris_funding_id IS NOT NULL) STORED;
+ADD COLUMN is_editable BOOLEAN GENERATED ALWAYS AS (ecapris_funding_id IS NOT NULL) STORED,
+ADD COLUMN fdu TEXT DEFAULT NULL;
 
 COMMENT ON COLUMN moped_proj_funding.ecapris_funding_id IS 'References the eCAPRIS FDU unique fao_id if applicable';
 COMMENT ON COLUMN moped_proj_funding.is_legacy_funding_record IS 'Indicates if the funding record was created before eCAPRIS sync integration (Nov 2025)';
@@ -18,3 +19,13 @@ COMMENT ON COLUMN moped_proj_funding.is_legacy_funding_record IS 'Indicates if t
 -- Mark all existing funding records as legacy before eCAPRIS sync integration
 UPDATE moped_proj_funding
 SET is_legacy_funding_record = TRUE;
+
+-- Add comments on other existing moped_proj_funding columns
+COMMENT ON COLUMN moped_proj_funding.project_funding_id IS 'Primary key for the project funding record';
+COMMENT ON COLUMN moped_proj_funding.project_id IS 'References the project this funding record is associated with';
+COMMENT ON COLUMN moped_proj_funding.funding_program_id IS 'References the funding program for this funding record';
+COMMENT ON COLUMN moped_proj_funding.funding_amount IS 'The amount of funding allocated from this funding source';
+COMMENT ON COLUMN moped_proj_funding.funding_description IS 'A description of the funding source';
+COMMENT ON COLUMN moped_proj_funding.funding_status_id IS 'References the current status of this funding record';
+COMMENT ON COLUMN moped_proj_funding.fund IS 'Legacy JSONB object containing additional fund details from eCAPRIS (Socrata jega-nqf6)';
+COMMENT ON COLUMN moped_proj_funding.dept_unit IS 'Legacy JSONB object containing additional department/unit details from eCAPRIS (Socrata bgrt-2m2z)';
