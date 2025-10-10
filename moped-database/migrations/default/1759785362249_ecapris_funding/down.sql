9431936336f3caafeea5703b150fdc157dec5bbf
Mike <mike.dilley@gmail.com>
2025-10-10 16:27:50 -0500

    Update down migration; update permissions; add more comments

--- moped-database/migrations/default/1759785362249_ecapris_funding/down.sql
+++ moped-database/migrations/default/1759785362249_ecapris_funding/down.sql
@@ -5,4 +5,6 @@ DROP COLUMN should_sync_ecapris_funding;
 -- Remove added columns from moped_proj_funding table
 ALTER TABLE moped_proj_funding
 DROP COLUMN ecapris_funding_id,
-DROP COLUMN is_legacy_funding_record;
+DROP COLUMN is_legacy_funding_record,
+DROP COLUMN is_editable,
+DROP COLUMN fdu;
--- moped-database/migrations/default/1759785362249_ecapris_funding/up.sql
+++ moped-database/migrations/default/1759785362249_ecapris_funding/up.sql
@@ -10,7 +10,7 @@ COMMENT ON COLUMN moped_project.should_sync_ecapris_statuses IS 'Indicates if pr
 ALTER TABLE moped_proj_funding
 ADD COLUMN ecapris_funding_id INTEGER,
 ADD COLUMN is_legacy_funding_record BOOLEAN DEFAULT FALSE,
-ADD COLUMN is_editable BOOLEAN GENERATED ALWAYS AS (ecapris_funding_id IS NOT NULL) STORED,
+ADD COLUMN is_editable BOOLEAN GENERATED ALWAYS AS (ecapris_funding_id IS NULL) STORED,
 ADD COLUMN fdu TEXT DEFAULT NULL;
 
 COMMENT ON COLUMN moped_proj_funding.ecapris_funding_id IS 'References the eCAPRIS FDU unique fao_id if applicable';
@@ -22,10 +22,15 @@ SET is_legacy_funding_record = TRUE;
 
 -- Add comments on other existing moped_proj_funding columns
 COMMENT ON COLUMN moped_proj_funding.proj_funding_id IS 'Primary key for the project funding record';
+COMMENT ON COLUMN moped_proj_funding.created_by_user_id IS 'ID of the user who last created the record';
+COMMENT ON COLUMN moped_proj_funding.created_at IS 'Timestamp when the record was last created';
 COMMENT ON COLUMN moped_proj_funding.project_id IS 'References the project this funding record is associated with';
+COMMENT ON COLUMN moped_proj_funding.funding_source_id IS 'References the funding source for this funding record';
 COMMENT ON COLUMN moped_proj_funding.funding_program_id IS 'References the funding program for this funding record';
 COMMENT ON COLUMN moped_proj_funding.funding_amount IS 'The amount of funding allocated from this funding source';
 COMMENT ON COLUMN moped_proj_funding.funding_description IS 'A description of the funding source';
 COMMENT ON COLUMN moped_proj_funding.funding_status_id IS 'References the current status of this funding record';
 COMMENT ON COLUMN moped_proj_funding.fund IS 'Legacy JSONB object containing additional fund details from eCAPRIS (Socrata jega-nqf6)';
 COMMENT ON COLUMN moped_proj_funding.dept_unit IS 'Legacy JSONB object containing additional department/unit details from eCAPRIS (Socrata bgrt-2m2z)';
+COMMENT ON COLUMN moped_proj_funding.is_editable IS 'Indicates if the funding record is editable (false if linked to an eCAPRIS funding record)';
+COMMENT ON COLUMN moped_proj_funding.fdu IS 'The FDU (Fund-Dept-Unit) code associated with this funding record';
