a926a48d82475d9d50f69869d62d3b22acdd672d
tillyw <tilly.whitson@austintexas.gov>
2025-10-15 17:23:34 -0500

    adds map bikeways 2026 and 2027 tags

---
+++ moped-database/migrations/default/1760565637429_add_map_bikeways_2026_and_2027_tags/down.sql
@@ -0,0 +1,2 @@
+DELETE FROM public.moped_component_tags WHERE slug IN ('map_bikeways_2026', 'map_bikeways_2027');
+DELETE FROM public.moped_tags WHERE slug = 'map_bikeways_2026';
---
+++ moped-database/migrations/default/1760565637429_add_map_bikeways_2026_and_2027_tags/up.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.moped_component_tags (type, name, slug) VALUES
+('MAP Bikeways', '2026', 'map_bikeways_2026'),
+('MAP Bikeways', '2027', 'map_bikeways_2027');
+
+INSERT INTO moped_tags (name, type, slug, is_deleted) VALUES
+('MAP Bikeways 2026', 'Work Plan', 'map_bikeways_2026', FALSE);
