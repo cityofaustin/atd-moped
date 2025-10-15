a99ca4bbb3c8be2c22f37bee35efb35dba9f96fa
Mike <mike.dilley@gmail.com>
2025-10-15 18:17:08 -0500

    Copy fund_dept_unit and unit_long_name from legacy data; update Power BI funding view

--- moped-database/migrations/default/1759785362249_ecapris_funding/down.sql
+++ moped-database/migrations/default/1759785362249_ecapris_funding/down.sql
@@ -9,3 +9,24 @@ DROP COLUMN is_legacy_funding_record,
 DROP COLUMN is_editable,
 DROP COLUMN fdu,
 DROP COLUMN unit_long_name;
+
+-- Restore previous view to use fund_dept_unit column
+DROP VIEW IF EXISTS project_funding_view;
+
+CREATE OR REPLACE VIEW project_funding_view AS SELECT
+    mp.project_id,
+    mpf.proj_funding_id,
+    mpf.funding_amount,
+    mpf.funding_description,
+    mpf.fund_dept_unit,
+    mpf.created_at,
+    mpf.updated_at,
+    mfs.funding_source_name,
+    mfp.funding_program_name,
+    mfst.funding_status_name
+FROM moped_project AS mp
+LEFT JOIN moped_proj_funding AS mpf ON mp.project_id = mpf.project_id
+LEFT JOIN moped_fund_sources AS mfs ON mpf.funding_source_id = mfs.funding_source_id
+LEFT JOIN moped_fund_programs AS mfp ON mpf.funding_program_id = mfp.funding_program_id
+LEFT JOIN moped_fund_status AS mfst ON mpf.funding_status_id = mfst.funding_status_id
+WHERE true AND mp.is_deleted = false AND mpf.is_deleted = false;
--- moped-database/migrations/default/1759785362249_ecapris_funding/up.sql
+++ moped-database/migrations/default/1759785362249_ecapris_funding/up.sql
@@ -17,7 +17,7 @@ ADD COLUMN unit_long_name TEXT DEFAULT NULL;
 COMMENT ON COLUMN moped_proj_funding.ecapris_funding_id IS 'References the eCAPRIS FDU unique fao_id if applicable';
 COMMENT ON COLUMN moped_proj_funding.is_legacy_funding_record IS 'Indicates if the funding record was created before eCAPRIS sync integration (Nov 2025)';
 
--- Mark all existing funding records as legacy before eCAPRIS sync integration
+-- Mark all existing funding records as legacy before eCAPRIS sync integration launches
 UPDATE moped_proj_funding
 SET is_legacy_funding_record = TRUE;
 
@@ -36,3 +36,33 @@ COMMENT ON COLUMN moped_proj_funding.dept_unit IS 'Legacy JSONB object containin
 COMMENT ON COLUMN moped_proj_funding.is_editable IS 'Indicates if the funding record is editable (false if linked to an eCAPRIS funding record)';
 COMMENT ON COLUMN moped_proj_funding.fdu IS 'The FDU (Fund-Dept-Unit) code associated with this funding record';
 COMMENT ON COLUMN moped_proj_funding.unit_long_name IS 'The long name of the unit associated with this funding record';
+
+-- Populate new fdu column based on existing fund_dept_unit data if available and 
+-- populate unit_long_name from dept_unit JSONB
+-- Note: fund_dept_unit is a generated column that is null if fund or dept_unit is null
+UPDATE moped_proj_funding
+SET
+    fdu = fund_dept_unit,
+    unit_long_name = (dept_unit ->> 'unit_long_name')
+WHERE fund_dept_unit IS NOT NULL;
+
+-- Drop and recreate view to use new fdu column instead of fund_dept_unit
+DROP VIEW IF EXISTS project_funding_view;
+
+CREATE OR REPLACE VIEW project_funding_view AS SELECT
+    mp.project_id,
+    mpf.proj_funding_id,
+    mpf.funding_amount,
+    mpf.funding_description,
+    mpf.fdu AS fund_dept_unit,
+    mpf.created_at,
+    mpf.updated_at,
+    mfs.funding_source_name,
+    mfp.funding_program_name,
+    mfst.funding_status_name
+FROM moped_project AS mp
+LEFT JOIN moped_proj_funding AS mpf ON mp.project_id = mpf.project_id
+LEFT JOIN moped_fund_sources AS mfs ON mpf.funding_source_id = mfs.funding_source_id
+LEFT JOIN moped_fund_programs AS mfp ON mpf.funding_program_id = mfp.funding_program_id
+LEFT JOIN moped_fund_status AS mfst ON mpf.funding_status_id = mfst.funding_status_id
+WHERE TRUE AND mp.is_deleted = FALSE AND mpf.is_deleted = FALSE;
