fbeb01685bd16675b1c2e5bb36a422c2b79c4242
tillyw <tilly.whitson@austintexas.gov>
2025-10-16 12:54:08 -0500

    update comment

--- moped-database/migrations/default/1760565637429_add_map_bikeways_2026_and_2027_tags/down.sql
+++ moped-database/migrations/default/1760565637429_add_map_bikeways_2026_and_2027_tags/down.sql
@@ -1,2 +1,2 @@
--- If we remove the MAP Bikeways 2026 and 2027 tags, we will soft delete
+-- If we remove the MAP Bikeways 2026 and 2027 component tags and MAP Bikeways 2026 project tag, we will soft delete
 SELECT 0;
