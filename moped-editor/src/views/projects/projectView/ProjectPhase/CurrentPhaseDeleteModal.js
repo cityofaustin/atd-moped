17995cf5b23ae81fdf03254fb813b98bf2af335e
chiaberry <analuciaby@gmail.com>
2025-10-28 09:27:20 -0500

    remove children

--- moped-editor/src/views/projects/projectView/ProjectPhase/CurrentPhaseDeleteModal.js
+++ moped-editor/src/views/projects/projectView/ProjectPhase/CurrentPhaseDeleteModal.js
@@ -7,32 +7,29 @@ import {
   DialogContentText,
 } from "@mui/material";
 
-const CurrentPhaseDeleteModal = ({ isOpen, setIsOpen, children }) => {
+const CurrentPhaseDeleteModal = ({ isOpen, setIsOpen }) => {
   const handleClose = () => {
     setIsOpen(false);
   };
 
   return (
-    <span>
-      {children}
-      <Dialog
-        open={isOpen}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Mark another phase as current before removing this phase
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button color="primary" onClick={handleClose} autoFocus>
-            Cancel
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </span>
+    <Dialog
+      open={isOpen}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          Mark another phase as current before removing this phase
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button color="primary" onClick={handleClose} autoFocus>
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
