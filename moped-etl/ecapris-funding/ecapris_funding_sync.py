ebccef7ec1d42777a9aa9f764d63b1e27b00b320
Mike <mike.dilley@gmail.com>
2025-10-31 12:15:41 -0500

    Update service name and remove Oracle lib and connect

--- moped-etl/ecapris-funding/ecapris_funding_sync.py
+++ moped-etl/ecapris-funding/ecapris_funding_sync.py
@@ -1,35 +1,15 @@
 #!/usr/bin/env python3
 """
-Fetch ecapris status records from the FSD Data Warehouse and sync to Moped ecapris_subproject_statuses table
+Fetch eCAPRIS funding records from the ODP and sync to Moped moped_proj_funding table
 """
 import os
 import sys
 import logging
 
-import oracledb as cx_Oracle
-
 from process.request import make_hasura_request
-from process.queries import GRAPHQL_QUERIES, ORACLE_QUERIES
+from process.queries import GRAPHQL_QUERIES
 from process.logging import get_logger
 from process.time import convert_to_timezone_aware_timestamp
-
-# FSD Data Warehouse
-ORACLE_USER = os.getenv("ORACLE_USER")
-ORACLE_PASSWORD = os.getenv("ORACLE_PASSWORD")
-ORACLE_HOST = os.getenv("ORACLE_HOST")
-ORACLE_PORT = os.getenv("ORACLE_PORT")
-ORACLE_SERVICE = os.getenv("ORACLE_SERVICE")
-
-
-def get_conn(host, port, service, user, password):
-    # Need to run this once if you want to work locally
-    # Change lib_dir to your cx_Oracle library location
-    # https://stackoverflow.com/questions/56119490/cx-oracle-error-dpi-1047-cannot-locate-a-64-bit-oracle-client-library
-    # lib_dir = r"/Users/charliehenry/instantclient_23_3"
-    # cx_Oracle.init_oracle_client(lib_dir="/Users/atd/lib/oracle/instantclient_23_3")
-    cx_Oracle.init_oracle_client()
-    dsn_tns = cx_Oracle.makedsn(host, port, service_name=service)
-    return cx_Oracle.connect(user=user, password=password, dsn=dsn_tns)
 
 
 def main():
@@ -43,7 +23,7 @@ def main():
         query=GRAPHQL_QUERIES["subprojects_to_query_for_funding"]
     )
     print(results)
-    # 2. For each project, query Socrata endpoint for FDUs for that subproject ID
+    # 2. For each project, query ODP endpoint for FDUs for that subproject ID
     # 3. For each FDU, check if it already exists on the project
     # 4. If it doesn't exist, insert it as a synced FDU
     # 5. If it does exist, skip it
@@ -53,7 +33,7 @@ if __name__ == "__main__":
     log_level = logging.DEBUG
     logger = get_logger(name="moped-ecapris-funding-sync", level=log_level)
     logger.info(
-        f"Starting sync. Transferring eCapris funding updates from FSD Data Warehouse to Moped DB."
+        f"Starting sync. Transferring eCapris funding records from ODP to Moped DB."
     )
 
     main()
