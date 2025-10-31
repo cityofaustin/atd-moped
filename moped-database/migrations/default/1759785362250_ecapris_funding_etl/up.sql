485eed9f9112ec37fdc33249d475d9f458ab41da
Mike <mike.dilley@gmail.com>
2025-10-31 14:30:34 -0500

    Add sodapy and fetch all records

--- moped-database/migrations/default/1759785362250_ecapris_funding_etl/up.sql
+++ moped-database/migrations/default/1759785362250_ecapris_funding_etl/up.sql
@@ -1,3 +1,12 @@
 -- Switch on sync for projects with ecapris_subproject_id set
 UPDATE moped_project SET should_sync_ecapris_funding = TRUE
 WHERE ecapris_subproject_id IS NOT NULL;
+
+-- Add unique constraint on (fdu, project_id) in moped_ecapris_funding table so we prevent duplicates
+ALTER TABLE moped_proj_funding
+ADD CONSTRAINT unique_fdu_project_id UNIQUE (fdu, project_id);
+
+COMMENT ON CONSTRAINT unique_fdu_project_id ON moped_proj_funding IS 'Ensures that each (fdu, project_id) pair is unique in the moped_proj_funding table.';
+
+ALTER TABLE moped_proj_funding ADD COLUMN is_synced BOOLEAN DEFAULT FALSE NOT NULL;
+COMMENT ON COLUMN moped_proj_funding.is_synced IS 'Indicates whether this funding record was synced from the eCAPRIS funding data source.';
--- moped-etl/ecapris-funding/ecapris_funding_sync.py
+++ moped-etl/ecapris-funding/ecapris_funding_sync.py
@@ -5,10 +5,27 @@ Fetch eCAPRIS funding records from the ODP and sync to Moped moped_proj_funding 
 import os
 import sys
 import logging
+import sodapy
 
 from process.request import make_hasura_request
 from process.queries import GRAPHQL_QUERIES
 from process.logging import get_logger
+
+SO_WEB = os.getenv("SO_WEB")
+SO_TOKEN = os.getenv("SO_TOKEN")
+SO_USER = os.getenv("SO_USER")
+SO_PASS = os.getenv("SO_PASS")
+FUNDING_DATASET_IDENTIFIER = os.getenv("FUNDING_DATASET_IDENTIFIER")
+
+
+def get_socrata_client():
+    return sodapy.Socrata(
+        SO_WEB,
+        SO_TOKEN,
+        username=SO_USER,
+        password=SO_PASS,
+        timeout=60,
+    )
 
 
 def main():
@@ -28,10 +45,36 @@ def main():
     unique_ecapris_ids = set(
         project["ecapris_subproject_id"] for project in results["moped_project"]
     )
+
     logger.info(
         f"Found {len(project_ids_to_sync)} projects to sync funding for with {len(unique_ecapris_ids)} unique eCAPRIS subproject ids."
     )
+
     # 2. For unique eCAPRIS subproject id, query ODP endpoint for associated FDUs
+    socrata_client = get_socrata_client()
+
+    # Build mapping of eCAPRIS subproject ID to Moped project IDs
+    ecapris_id_to_project_ids = {}
+    for project in results["moped_project"]:
+        ecapris_id = project["ecapris_subproject_id"]
+        project_id = project["project_id"]
+
+        if ecapris_id not in ecapris_id_to_project_ids:
+            ecapris_id_to_project_ids[ecapris_id] = []
+
+        ecapris_id_to_project_ids[ecapris_id].append(project_id)
+
+    # Fetch all funding records from ODP
+    logger.info(f"Fetching all eCAPRIS funding records from ODP...")
+    ecapris_funding_records = socrata_client.get(
+        FUNDING_DATASET_IDENTIFIER, limit=100000
+    )
+
+    logger.info(
+        f"Fetched {len(ecapris_funding_records)} total eCAPRIS funding records from ODP."
+    )
+
+    # Use map of eCAPRIS subproject ID to project IDs to sync funding records
 
     # 3. For each FDU, check if it already exists on the project
     # 4. If it doesn't exist, insert it as a synced FDU
