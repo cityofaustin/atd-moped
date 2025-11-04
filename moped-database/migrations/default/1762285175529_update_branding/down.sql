f31593437f5213c509c7440383f7a987d8bfb277
Mike <mike.dilley@gmail.com>
2025-11-04 14:35:13 -0600

    Add mobility hub component, component work types, and component tag

--- moped-database/migrations/default/1762285175529_update_branding/down.sql
+++ moped-database/migrations/default/1762285175529_update_branding/down.sql
@@ -1,4 +1,4 @@
--- We will soft delete added workgroups in the future if needed.
+-- We will soft delete added workgroups and component in the future if needed.
 
 -- Revert all ATPWs to TPWs in entity names
 UPDATE moped_entity
