469fb84d5dbb6fa5dba7a790a323379337778f55
tillyw <tilly.whitson@austintexas.gov>
2025-11-04 16:37:38 -0600

    fix comments and remove extra insert

--- moped-database/migrations/default/1762294199895_add_map_signals_and_bikeshare_tags/up.sql
+++ moped-database/migrations/default/1762294199895_add_map_signals_and_bikeshare_tags/up.sql
@@ -1,10 +1,10 @@
--- Add MAP Bikeways 2026 and 2027 component tags and MAP Bikeways 2026 project tag
+-- Add MAP Signals, Map Bikeshare 2025/2026 and Arterial Management - PIP component tags
 INSERT INTO public.moped_component_tags (type, name, slug) VALUES
 ('MAP Signals', '2026', 'map_signals_2026'),
 ('MAP Bikeshare', '2025', 'map_bikeshare_2025'),
 ('MAP Bikeshare', '2026', 'map_bikeshare_2026'),
 ('Arterial Management', 'PIP', 'arterial_management_pip');
 
+-- Add Arterial Management - PIP project tag
 INSERT INTO moped_tags (name, type, slug, is_deleted) VALUES
-('MAP Bikeways 2026', 'Work Plan', 'map_bikeways_2026', FALSE),
 ('Arterial Management - PIP', 'Other', 'arterial_management_pip', FALSE);
