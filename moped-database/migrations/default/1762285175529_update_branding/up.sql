ab005971e723eda62de9e635a457149ba423b7af
Mike <mike.dilley@gmail.com>
2025-11-06 09:19:42 -0600

    Remove duplicate Shared Mobility insert

--- moped-database/migrations/default/1762285175529_update_branding/up.sql
+++ moped-database/migrations/default/1762285175529_update_branding/up.sql
@@ -4,11 +4,6 @@ public.moped_workgroup (workgroup_name, department_id, workgroup_abbreviation)
 VALUES
 ('Community Services Division', 11, 'CSD');
 
-INSERT INTO
-public.moped_workgroup (workgroup_name, department_id, workgroup_abbreviation)
-VALUES
-('Shared Mobility Services', 11, 'SMS');
-
 -- Update all TPWs to ATPWs in entity names
 UPDATE moped_entity
 SET
