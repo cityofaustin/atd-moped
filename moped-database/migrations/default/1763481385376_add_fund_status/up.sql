58981b367c881b0d34281a407751d8a7bee957c7
Mike <mike.dilley@gmail.com>
2025-11-21 16:02:49 -0600

    Add fdu status to ETL script

--- moped-database/migrations/default/1763481385376_add_fund_status/up.sql
+++ moped-database/migrations/default/1763481385376_add_fund_status/up.sql
@@ -3,8 +3,8 @@ ALTER TABLE public.ecapris_subproject_funding
 ADD COLUMN fdu_status TEXT DEFAULT NULL;
 
 COMMENT ON COLUMN public.ecapris_subproject_funding.fdu_status IS 'The status of the FDU (Fund-Dept-Unit) code (Active or Inactive)';
-COMMENT ON COLUMN public.ecapris_subproject_funding.program IS 'Timestamp when the record was created';
-COMMENT ON COLUMN public.ecapris_subproject_funding.subprogram IS 'Timestamp when the record was created';
+COMMENT ON COLUMN public.ecapris_subproject_funding.program IS 'Program name associated with the funding record by atd-finance-data script';
+COMMENT ON COLUMN public.ecapris_subproject_funding.subprogram IS 'Subprogram name associated with the funding record by atd-finance-data script';
 COMMENT ON COLUMN public.ecapris_subproject_funding.created_at IS 'Timestamp when the record was created';
 COMMENT ON COLUMN public.ecapris_subproject_funding.updated_at IS 'Timestamp when the record was last updated';
 COMMENT ON COLUMN public.ecapris_subproject_funding.created_by_user_id IS 'ID of the user who created the record';
