0877dadb9eac4ce27e05b4587fc8b45e30a202c4
Mike <mike.dilley@gmail.com>
2025-11-21 15:58:33 -0600

    Add fdu status column to ecapris funding table and fix comments

---
+++ moped-database/migrations/default/1763481385376_add_fund_status/up.sql
@@ -0,0 +1,11 @@
+-- Update ecapris_subproject_funding table with status column
+ALTER TABLE public.ecapris_subproject_funding
+ADD COLUMN fdu_status TEXT DEFAULT NULL;
+
+COMMENT ON COLUMN public.ecapris_subproject_funding.fdu_status IS 'The status of the FDU (Fund-Dept-Unit) code (Active or Inactive)';
+COMMENT ON COLUMN public.ecapris_subproject_funding.program IS 'Timestamp when the record was created';
+COMMENT ON COLUMN public.ecapris_subproject_funding.subprogram IS 'Timestamp when the record was created';
+COMMENT ON COLUMN public.ecapris_subproject_funding.created_at IS 'Timestamp when the record was created';
+COMMENT ON COLUMN public.ecapris_subproject_funding.updated_at IS 'Timestamp when the record was last updated';
+COMMENT ON COLUMN public.ecapris_subproject_funding.created_by_user_id IS 'ID of the user who created the record';
+COMMENT ON COLUMN public.ecapris_subproject_funding.updated_by_user_id IS 'ID of the user who updated the record';
