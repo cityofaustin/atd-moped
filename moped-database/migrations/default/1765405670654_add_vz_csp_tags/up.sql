918578a38e77a9ef283023216f2c7161f2e2d410
Mike <mike.dilley@gmail.com>
2025-12-10 16:47:21 -0600

    Add tags migration

---
+++ moped-database/migrations/default/1765405670654_add_vz_csp_tags/up.sql
@@ -0,0 +1,22 @@
+-- Add VZ CSP MC1.2 project and component tags
+INSERT INTO moped_component_tags (type, name, slug)
+SELECT
+    'Vision Zero',
+    'CSP MC1.2',
+    'vision_zero_csv_mc1.2'
+WHERE NOT EXISTS (
+        SELECT 1 FROM moped_component_tags
+        WHERE type = 'Vision Zero'
+            AND name = 'CSP MC1.2'
+    );
+
+INSERT INTO moped_tags (name, type, slug)
+SELECT
+    'Vision Zero - CSP MC1.2',
+    'Other',
+    'vision_zero_csv_mc1.2'
+WHERE NOT EXISTS (
+        SELECT 1 FROM moped_tags
+        WHERE name = 'Vision Zero - CSP MC1.2'
+            AND type = 'Other'
+    );
