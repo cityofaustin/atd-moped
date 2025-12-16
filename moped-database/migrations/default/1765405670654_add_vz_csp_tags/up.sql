2a32b7e7495c61e5f6477367d694090d6747889c
Mike <mike.dilley@gmail.com>
2025-12-16 12:11:02 -0600

    Update slug to be more readable

--- moped-database/migrations/default/1765405670654_add_vz_csp_tags/up.sql
+++ moped-database/migrations/default/1765405670654_add_vz_csp_tags/up.sql
@@ -3,7 +3,7 @@ INSERT INTO moped_component_tags (type, name, slug)
 SELECT
     'Vision Zero',
     'CSP MC1.2',
-    'vision_zero_csv_mc1.2'
+    'vision_zero_citywide_strategic_plan_mc1_2'
 WHERE NOT EXISTS (
         SELECT 1 FROM moped_component_tags
         WHERE type = 'Vision Zero'
@@ -14,7 +14,7 @@ INSERT INTO moped_tags (name, type, slug)
 SELECT
     'Vision Zero - CSP MC1.2',
     'Other',
-    'vision_zero_csv_mc1.2'
+    'vision_zero_citywide_strategic_plan_mc1_2'
 WHERE NOT EXISTS (
         SELECT 1 FROM moped_tags
         WHERE name = 'Vision Zero - CSP MC1.2'
