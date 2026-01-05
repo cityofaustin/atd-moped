9139a58555ed23837b13b354339f6a7aa74e8f6c
Mike <mike.dilley@gmail.com>
2026-01-05 13:54:37 -0600

    Use lodash.get after all

--- moped-editor/src/views/projects/projectView/ProjectComponents/utils/form.js
+++ moped-editor/src/views/projects/projectView/ProjectComponents/utils/form.js
@@ -9,6 +9,7 @@ import {
   RoomOutlined as RoomOutlinedIcon,
   Timeline as TimelineIcon,
 } from "@mui/icons-material";
+import get from "lodash.get";
 
 /**
  * Allows the component work type to be defaulted to `New` -
@@ -400,8 +401,8 @@ export const useResetDependentFieldOnParentFieldChange = ({
     // keep update from firing if the parent value hasn't changed
     if (comparisonVariable) {
       if (
-        parentValue?.[comparisonVariable] ===
-        previousParentFormValue?.[comparisonVariable]
+        get(parentValue, comparisonVariable) ===
+        get(previousParentFormValue, comparisonVariable)
       )
         return;
     } else if (parentValue === previousParentFormValue) {
