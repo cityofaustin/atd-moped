32b675ed788fd587d6b447d7e9662caf652d49e3
chiaberry <analuciaby@gmail.com>
2026-01-14 16:39:51 -0600

    add update mutation

--- moped-editor/src/views/projects/projectView/ProjectFunding/OverrideFundingForm.js
+++ moped-editor/src/views/projects/projectView/ProjectFunding/OverrideFundingForm.js
@@ -9,6 +9,7 @@ import { currencyFormatter } from "src/utils/numberFormatters";
 import {
   ADD_PROJECT_FUNDING,
   ECAPRIS_SUBPROJECT_FUNDING_QUERY,
+  UPDATE_PROJECT_FUNDING,
 } from "src/queries/funding";
 import { transformGridToDatabase } from "src/views/projects/projectView/ProjectFunding/helpers";
 
@@ -30,10 +31,16 @@ const OverrideFundingForm = ({
     },
   });
 
-  const [addProjectFunding] = useMutation(ADD_PROJECT_FUNDING);
+  // if record is synced from ecapris and not yet manual, its first time overriding amount and description
+  const isNewOverride =
+    fundingRecord.is_synced_from_ecapris && !fundingRecord.is_manual;
+
+  const [mutate] = useMutation(
+    isNewOverride ? ADD_PROJECT_FUNDING : UPDATE_PROJECT_FUNDING
+  );
 
   const { data: fduData } = useQuery(ECAPRIS_SUBPROJECT_FUNDING_QUERY, {
-    variables: { fdu: fundingRecord.fdu.fdu },
+    variables: { fdu: fundingRecord?.fdu?.fdu },
   });
 
   const appropriatedFunding = fduData?.ecapris_subproject_funding
@@ -46,23 +53,40 @@ const OverrideFundingForm = ({
     transformedRecord.funding_description = data.description;
     transformedRecord.funding_amount = data.funding_amount;
 
-    addProjectFunding({
-      variables: {
-        objects: {
-          ...transformedRecord,
-          ecapris_subproject_id: fundingRecord.ecapris_subproject_id,
-          project_id: Number(projectId),
+    if (isNewOverride) {
+      mutate({
+        variables: {
+          objects: {
+            ...transformedRecord,
+            ecapris_subproject_id: fundingRecord.ecapris_subproject_id,
+            project_id: Number(projectId),
+          },
         },
-      },
-    })
-      .then(() => {
-        handleSnackbar(true, "Funding source added", "success");
-        onSubmitCallback();
-        onClose();
       })
-      .catch((error) => {
-        handleSnackbar(true, "Error adding funding source", "error", error);
-      });
+        .then(() => {
+          handleSnackbar(true, "Funding source added", "success");
+          onSubmitCallback();
+          onClose();
+        })
+        .catch((error) => {
+          handleSnackbar(true, "Error adding funding source", "error", error);
+        });
+    } else {
+      mutate({
+        variables: {
+          ...transformedRecord,
+          proj_funding_id: fundingRecord.proj_funding_id,
+        },
+      })
+        .then(() => {
+          handleSnackbar(true, "Funding source updated", "success");
+          onSubmitCallback();
+          onClose();
+        })
+        .catch((error) => {
+          handleSnackbar(true, "Error updating funding source", "error", error);
+        });
+    }
   };
 
   return (
