df34d77f1d3a7244a47730a3d8d5037e0d4340c1
Mike <mike.dilley@gmail.com>
2026-01-16 16:05:46 -0600

    Add more sources and add based on replaced programs

--- moped-database/migrations/default/1768253839445_update_programs_sources/up.sql
+++ moped-database/migrations/default/1768253839445_update_programs_sources/up.sql
@@ -36,18 +36,6 @@ SELECT 'Street Programs' WHERE NOT EXISTS (
     );
 
 INSERT INTO moped_fund_programs (funding_program_name)
-SELECT 'Vision Zero - Major Safety' WHERE NOT EXISTS (
-        SELECT 1 FROM moped_fund_programs
-        WHERE funding_program_name = 'Vision Zero - Major Safety'
-    );
-
-INSERT INTO moped_fund_programs (funding_program_name)
-SELECT 'Vision Zero - Pedestrian Safety' WHERE NOT EXISTS (
-        SELECT 1 FROM moped_fund_programs
-        WHERE funding_program_name = 'Vision Zero - Pedestrian Safety'
-    );
-
-INSERT INTO moped_fund_programs (funding_program_name)
 SELECT 'Vision Zero - Signal Safety' WHERE NOT EXISTS (
         SELECT 1 FROM moped_fund_programs
         WHERE funding_program_name = 'Vision Zero - Signal Safety'
@@ -65,13 +53,42 @@ SELECT 'Developer Transportation Improvements Program' WHERE NOT EXISTS (
         WHERE funding_program_name = 'Developer Transportation Improvements Program'
     );
 
--- Add sources
+-- Add new sources
 INSERT INTO moped_fund_sources (funding_source_name)
 SELECT 'Highway Safety Improvements' WHERE NOT EXISTS (
         SELECT 1 FROM moped_fund_sources
         WHERE funding_source_name = 'Highway Safety Improvements'
     );
 
+INSERT INTO moped_fund_sources (funding_source_name)
+SELECT 'ATPW Operating Fund' WHERE NOT EXISTS (
+        SELECT 1 FROM moped_fund_sources
+        WHERE funding_source_name = 'ATPW Operating Fund'
+    );
+
+INSERT INTO moped_fund_sources (funding_source_name)
+SELECT 'Transit ILA 2018' WHERE NOT EXISTS (
+        SELECT 1 FROM moped_fund_sources
+        WHERE funding_source_name = 'Transit ILA 2018'
+    );
+
+INSERT INTO moped_fund_sources (funding_source_name)
+SELECT 'Private Development - Fiscal Surety' WHERE NOT EXISTS (
+        SELECT 1 FROM moped_fund_sources
+        WHERE funding_source_name = 'Private Development - Fiscal Surety'
+    );
+
+INSERT INTO moped_fund_sources (funding_source_name)
+SELECT 'Private Development - Fee In Lieu' WHERE NOT EXISTS (
+        SELECT 1 FROM moped_fund_sources
+        WHERE funding_source_name = 'Private Development - Fee In Lieu'
+    );
+
+INSERT INTO moped_fund_sources (funding_source_name)
+SELECT 'Private Development - Street Impact Fee' WHERE NOT EXISTS (
+        SELECT 1 FROM moped_fund_sources
+        WHERE funding_source_name = 'Private Development - Street Impact Fee'
+    );
 
 -- Soft delete programs no longer needed
 UPDATE moped_fund_programs
@@ -79,7 +96,6 @@ SET is_deleted = TRUE
 WHERE funding_program_name = 'Operating Fund'
     OR funding_program_name = 'Large CIP'
     OR funding_program_name = '2018 Interlocal Agreement'
-    OR funding_program_name = 'Intersection Safety'
     OR funding_program_name = 'Pedestrian Crossing'
     OR funding_program_name = 'Highway Safety Improvements'
     OR funding_program_name = 'IH35'
@@ -124,7 +140,7 @@ UPDATE moped_fund_programs
 SET funding_program_name = 'Vision Zero - Speed Management'
 WHERE funding_program_name = 'Speed Management';
 
--- Update project funding records with existing program value to new program value
+-- Update project funding records with 2018 Interlocal Agreement program to new Local Transit - Local Transit Enhancement program
 WITH
 updated_program AS (
     SELECT funding_program_id
@@ -144,6 +160,7 @@ WHERE
     moped_proj_funding.funding_program_id = moped_fund_programs.funding_program_id
     AND moped_fund_programs.funding_program_name = '2018 Interlocal Agreement';
 
+-- Update project funding records with Mitigation Fee and Traffic Mitigation Fees programs to new Developer Transportation Improvements Program
 WITH
 updated_program AS (
     SELECT funding_program_id
@@ -162,6 +179,46 @@ FROM
 WHERE
     moped_proj_funding.funding_program_id = moped_fund_programs.funding_program_id
     AND (moped_fund_programs.funding_program_name = 'Mitigation Fees' OR moped_fund_programs.funding_program_name = 'Traffic Mitigation Fees');
+
+-- Update project funding records with Operating Fund program to new ATPW Operating Fund source
+WITH
+updated_source AS (
+    SELECT funding_source_id
+    FROM
+        moped_fund_sources
+    WHERE
+        funding_source_name = 'ATPW Operating Fund'
+)
+
+UPDATE moped_proj_funding
+SET
+    funding_source_id = updated_source.funding_source_id
+FROM
+    updated_source,
+    moped_fund_programs
+WHERE
+    moped_proj_funding.funding_program_id = moped_fund_programs.funding_program_id
+    AND (moped_fund_programs.funding_program_name = 'Operating Fund');
+
+-- Update project funding records with 2018 Interlocal Agreement program to new Transit ILA 2018 source
+WITH
+updated_source AS (
+    SELECT funding_source_id
+    FROM
+        moped_fund_sources
+    WHERE
+        funding_source_name = 'Transit ILA 2018'
+)
+
+UPDATE moped_proj_funding
+SET
+    funding_source_id = updated_source.funding_source_id
+FROM
+    updated_source,
+    moped_fund_programs
+WHERE
+    moped_proj_funding.funding_program_id = moped_fund_programs.funding_program_id
+    AND (moped_fund_programs.funding_program_name = '2018 Interlocal Agreement');
 
 -- TODO: Add trigger to attempt assigning program foreign key on insert/update of eCAPRIS funding records
 -- TODO: Add trigger to attemp assigning source foreign key on insert/update of eCAPRIS funding records
