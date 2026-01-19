0d4857f34e86d6c589f496ebc63839365ee68b9f
Mike <mike.dilley@gmail.com>
2026-01-19 14:11:42 -0600

    Update combined view to show program and source of synced records

--- moped-database/migrations/default/1768253839445_update_programs_sources/up.sql
+++ moped-database/migrations/default/1768253839445_update_programs_sources/up.sql
@@ -118,8 +118,8 @@ SET funding_program_name = 'Safe Routes'
 WHERE funding_program_name = 'Safe Routes to Schools';
 
 UPDATE moped_fund_programs
-SET funding_program_name = 'Signals'
-WHERE funding_program_name = 'Traffic Signals';
+SET funding_program_name = 'Traffic Signals'
+WHERE funding_program_name = 'Signals';
 
 UPDATE moped_fund_programs
 SET funding_program_name = 'Vision Zero - Major Safety'
@@ -328,3 +328,61 @@ EXECUTE FUNCTION match_ecapris_funding_to_source_and_programs_foreign_keys();
 
 COMMENT ON FUNCTION match_ecapris_funding_to_source_and_programs_foreign_keys() IS 'Attempts matching eCAPRIS funding data to Moped foreign keys using program/subprogram text and bond year.';
 COMMENT ON TRIGGER match_ecapris_funding_keys_trigger ON ecapris_subproject_funding IS 'Fires before INSERT or UPDATE on ecapris_subproject_funding to try populating funding_program_id and funding_source_id foreign keys.';
+
+
+-- Last, update the combind funding view to include eCAPRIS funding source and program info for synced records
+DROP VIEW IF EXISTS combined_project_funding_view;
+
+CREATE OR REPLACE VIEW combined_project_funding_view AS SELECT
+    'moped_'::TEXT || moped_proj_funding.proj_funding_id AS id,
+    moped_proj_funding.proj_funding_id AS original_id,
+    moped_proj_funding.created_at,
+    moped_proj_funding.updated_at,
+    moped_proj_funding.project_id,
+    moped_proj_funding.fdu,
+    moped_proj_funding.unit_long_name,
+    moped_proj_funding.funding_amount AS amount,
+    moped_proj_funding.funding_description AS description,
+    moped_fund_sources.funding_source_name AS source_name,
+    moped_proj_funding.funding_source_id,
+    moped_fund_status.funding_status_name AS status_name,
+    moped_proj_funding.funding_status_id,
+    moped_fund_programs.funding_program_name AS program_name,
+    moped_proj_funding.funding_program_id,
+    NULL::INTEGER AS fao_id,
+    NULL::TEXT AS ecapris_subproject_id,
+    FALSE AS is_synced_from_ecapris
+FROM moped_proj_funding
+LEFT JOIN moped_fund_status ON moped_proj_funding.funding_status_id = moped_fund_status.funding_status_id
+LEFT JOIN moped_fund_sources ON moped_proj_funding.funding_source_id = moped_fund_sources.funding_source_id
+LEFT JOIN moped_fund_programs ON moped_proj_funding.funding_program_id = moped_fund_programs.funding_program_id
+WHERE moped_proj_funding.is_deleted = FALSE
+UNION ALL
+SELECT
+    (('ecapris_'::TEXT || ecapris_subproject_funding.id) || '_moped_'::TEXT) || moped_project.project_id AS id,
+    ecapris_subproject_funding.id AS original_id,
+    ecapris_subproject_funding.created_at,
+    ecapris_subproject_funding.updated_at,
+    moped_project.project_id,
+    ecapris_subproject_funding.fdu,
+    ecapris_subproject_funding.unit_long_name,
+    ecapris_subproject_funding.app AS amount,
+    NULL::TEXT AS description,
+    moped_fund_sources.funding_source_name AS source_name,
+    ecapris_subproject_funding.funding_source_id,
+    'Set up'::TEXT AS status_name,
+    5 AS funding_status_id,
+    moped_fund_programs.funding_program_name AS program_name,
+    ecapris_subproject_funding.funding_program_id,
+    ecapris_subproject_funding.fao_id,
+    ecapris_subproject_funding.ecapris_subproject_id,
+    TRUE AS is_synced_from_ecapris
+FROM ecapris_subproject_funding
+LEFT JOIN moped_fund_sources ON ecapris_subproject_funding.funding_source_id = moped_fund_sources.funding_source_id
+LEFT JOIN moped_fund_programs ON ecapris_subproject_funding.funding_program_id = moped_fund_programs.funding_program_id
+INNER JOIN moped_project ON ecapris_subproject_funding.ecapris_subproject_id = moped_project.ecapris_subproject_id
+WHERE NOT (EXISTS (
+        SELECT 1
+        FROM moped_proj_funding
+        WHERE moped_proj_funding.fdu = ecapris_subproject_funding.fdu AND moped_proj_funding.project_id = moped_project.project_id AND moped_proj_funding.is_deleted = FALSE
+    ));
