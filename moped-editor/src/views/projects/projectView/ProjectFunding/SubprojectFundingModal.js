2cf972feee4ec58ae497eaa7eedbe2e6cb1ef903
chiaberry <analuciaby@gmail.com>
2026-01-27 13:59:38 -0600

    add funding program id and source id to import from eCAPRIS

--- moped-editor/src/views/projects/projectView/ProjectFunding/SubprojectFundingModal.js
+++ moped-editor/src/views/projects/projectView/ProjectFunding/SubprojectFundingModal.js
@@ -109,6 +109,8 @@ const SubprojectFundingModal = ({
       fduRecord.unit_long_name = fdu.unit_long_name;
       // funding status 5 is "Set Up"
       fduRecord.funding_status_id = 5;
+      fduRecord.funding_program_id = fdu.funding_program_id;
+      fduRecord.funding_source_id = fdu.funding_source_id;
       newFunds.push(fduRecord);
     });
 
