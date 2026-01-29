8d899ff07175ae7917661042b690322eb5542555
Frank Hereford <frank@frankhereford.com>
2026-01-29 09:12:46 -0600

    Make the down migration a non-op

--- moped-database/migrations/default/1769617769838_add-tdm-to-two-tables/down.sql
+++ moped-database/migrations/default/1769617769838_add-tdm-to-two-tables/down.sql
@@ -1,9 +1,1 @@
--- Remove Transportation Demand Management (TDM) entity and workgroup added in the up migration.
-
-DELETE FROM moped_entity
-WHERE entity_name = 'COA ATPW Transportation Demand Management';
-
-DELETE FROM moped_workgroup
-WHERE
-    workgroup_name = 'Transportation Demand Management'
-    AND workgroup_abbreviation = 'TDM';
+SELECT 1;
