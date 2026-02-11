c13a78d8dc7cb26203c49b908136ef52d4c93152
mateoclarke <mateosalinasclarke@proton.me>
2026-02-11 14:44:48 -0700

    update down migration since the change is irreversible

--- moped-database/migrations/default/1770830741911_user_events_drop_name_check_constraint/down.sql
+++ moped-database/migrations/default/1770830741911_user_events_drop_name_check_constraint/down.sql
@@ -1,3 +1,4 @@
--- Restore the check constraint with the event names that existed before this migration
-ALTER TABLE moped_user_events
-ADD CONSTRAINT moped_user_events_event_name_check CHECK (event_name IN ('app_load', 'dashboard_load', 'projects_map_load', 'projects_saved_view'));
+-- Rollback is not possible: once the constraint is dropped, new event names
+-- (e.g. funding_ecapris_*) may be logged. Re-adding the constraint would fail
+-- because existing rows would violate it. This change is irreversible.
+SELECT 0;
