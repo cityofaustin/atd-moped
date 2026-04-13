6f19f6ea32d05e9932e62d31a8f5f7b12649a7a8
Mike <mike.dilley@gmail.com>
2026-04-13 16:30:34 -0500

    ON DELETE RESTRICT like other tables

--- moped-database/migrations/default/1775230309536_ecapris_file_attach/up.sql
+++ moped-database/migrations/default/1775230309536_ecapris_file_attach/up.sql
@@ -1,13 +1,13 @@
 -- Create join table to link Moped projects, eCAPRIS subproject funding rows, and Moped project files
 CREATE TABLE public.files_ecapris_funding (
     id SERIAL PRIMARY KEY,
-    project_id int4 NOT NULL REFERENCES public.moped_project(project_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    entity_id int4 NOT NULL REFERENCES public.ecapris_subproject_funding(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    file_id int4 NOT NULL REFERENCES public.moped_project_files(project_file_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    project_id int4 NOT NULL REFERENCES public.moped_project(project_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    entity_id int4 NOT NULL REFERENCES public.ecapris_subproject_funding(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    file_id int4 NOT NULL REFERENCES public.moped_project_files(project_file_id) ON DELETE RESTRICT ON UPDATE CASCADE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     created_by_user_id int4 NOT NULL REFERENCES moped_users (user_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    updated_by_user_id int4 NOT NULL REFERENCES moped_users (user_id) ON DELETE SET NULL ON UPDATE CASCADE,
+    updated_by_user_id int4 NOT NULL REFERENCES moped_users (user_id) ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT files_ecapris_funding_project_id_entity_id_file_id_key UNIQUE (project_id, entity_id, file_id)
 );
 
