f18208f6c8b0d118e11290a377f434cca3cd41ba
chiaberry <analuciaby@gmail.com>
2026-04-17 12:36:04 -0500

    need entity id

--- moped-database/migrations/default/1776441267959_funding-file-attach/up.sql
+++ moped-database/migrations/default/1776441267959_funding-file-attach/up.sql
@@ -1,7 +1,7 @@
 -- Create join table to link Moped projects, moped project funding rows, and Moped project files
 CREATE TABLE public.files_project_funding (
     id SERIAL PRIMARY KEY,
-    project_id int4 NOT NULL REFERENCES public.moped_project(project_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    entity_id int4 NOT NULL REFERENCES public.moped_proj_funding(proj_funding_id) ON DELETE RESTRICT ON UPDATE CASCADE,
     file_id int4 NOT NULL REFERENCES public.moped_project_files(project_file_id) ON DELETE RESTRICT ON UPDATE CASCADE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
@@ -9,8 +9,8 @@ CREATE TABLE public.files_project_funding (
     updated_by_user_id int4 NOT NULL REFERENCES moped_users (user_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
-COMMENT ON TABLE public.files_project_funding IS 'Join table to link Moped projects, Moped funding rows, and Moped project files to create funding file attachments.';
-COMMENT ON COLUMN public.files_project_funding.project_id IS 'References the Moped project to which the file attachment belongs.';
+COMMENT ON TABLE public.files_project_funding IS 'Join table to link Moped funding rows and Moped project files to create funding file attachments.';
+COMMENT ON COLUMN public.files_project_funding.entity_id IS 'References the Moped project funding record to which the file attachment belongs.';
 COMMENT ON COLUMN public.files_project_funding.file_id IS 'References the file that is attached to the moped funding row.';
 COMMENT ON COLUMN public.files_project_funding.created_at IS 'Timestamp for when the record was created.';
 COMMENT ON COLUMN public.files_project_funding.updated_at IS 'Timestamp for when the record was last updated.';
