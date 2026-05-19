29f181f95f53bb2fef391c0a4557197bc20bac34
Mike <mike.dilley@gmail.com>
2026-05-19 10:44:35 -0500

    Add triggers to update updated_at

--- moped-database/migrations/default/1778885531957_add_soft_delete_attachments/down.sql
+++ moped-database/migrations/default/1778885531957_add_soft_delete_attachments/down.sql
@@ -1,2 +1,5 @@
 ALTER TABLE files_ecapris_funding DROP COLUMN is_deleted;
 ALTER TABLE files_project_funding DROP COLUMN is_deleted;
+
+DROP TRIGGER IF EXISTS set_files_updated_at ON files_ecapris_funding;
+DROP TRIGGER IF EXISTS set_files_updated_at ON files_project_funding;
