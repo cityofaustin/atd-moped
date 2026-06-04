9b14cc0cf505038e70e4061f189cb36ceadc9405
Mike <mike.dilley@gmail.com>
2026-06-04 16:51:00 -0500

    Update vite versions and remove env-cmd; update vite config with v8 settings for tsconfig

--- moped-editor/vite.config.js
+++ moped-editor/vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
-import fs from "node:fs/promises";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
   return {
@@ -15,33 +13,15 @@ export default defineConfig(() => {
     build: {
       outDir: "build/moped",
     },
-    /* Use SSL for Cognito sign-in using callback set up with port 3000 in local development;
-    tsconfigPaths so we respect the import path aliases set in tsconfig.json */
-    plugins: [react(), basicSsl(), tsconfigPaths()],
+    /* Use SSL for Cognito sign-in using callback set up with port 3000 in local development */
+    plugins: [react(), basicSsl()],
+    resolve: {
+      /* Respect the import path aliases set in tsconfig.json */
+      tsconfigPaths: true,
+    },
     server: {
       https: true,
       port: 3000,
-    },
-    // TODO: Remove this and rename all .js files to .jsx
-    esbuild: {
-      loader: "jsx",
-      include: /src\/.*\.jsx?$/,
-      exclude: [],
-    },
-    optimizeDeps: {
-      esbuildOptions: {
-        plugins: [
-          {
-            name: "load-js-files-as-jsx",
-            setup(build) {
-              build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
-                loader: "jsx",
-                contents: await fs.readFile(args.path, "utf8"),
-              }));
-            },
-          },
-        ],
-      },
     },
   };
 });
