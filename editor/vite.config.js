697f061e842105e5a31f5ec4d35e5262f391f62b
Mike <mike.dilley@gmail.com>
2026-06-11 12:46:05 -0500

    Test out checker plugin

--- editor/vite.config.js
+++ editor/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
 export default defineConfig(() => {
   return {
@@ -14,7 +15,23 @@ export default defineConfig(() => {
       outDir: "build/moped",
     },
     /* Use SSL for Cognito sign-in using callback set up with port 3000 in local development */
-    plugins: [react(), basicSsl()],
+    plugins: [
+      react(),
+      basicSsl(),
+      checker({
+        eslint: {
+          // for example, lint .ts and .tsx
+          lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
+          dev: {
+            logLevel: ["error"],
+          },
+          overlay: {
+            initialIsOpen: true,
+            position: "tl", // top-left
+          },
+        },
+      }),
+    ],
     resolve: {
       /* Respect the import path aliases set in tsconfig.json */
       tsconfigPaths: true,
