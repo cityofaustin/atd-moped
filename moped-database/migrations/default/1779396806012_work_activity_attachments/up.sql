373006b4da2d3bc3ecb567e88d5a1405d323df93
Mike <mike.dilley@gmail.com>
2026-06-25 17:10:01 -0500

    Add up and down migration

---
+++ moped-database/migrations/default/1779396806012_work_activity_attachments/up.sql
@@ -0,0 +1,33 @@
+-- Create join table to link Moped projects, moped project funding rows, and Moped project files
+CREATE TABLE public.files_project_work_activities (
+    id SERIAL PRIMARY KEY,
+    entity_id INT4 NOT NULL REFERENCES public.moped_proj_work_activity (
+        id
+    ) ON DELETE RESTRICT ON UPDATE CASCADE,
+    file_id INT4 NOT NULL REFERENCES public.moped_project_files (
+        project_file_id
+    ) ON DELETE RESTRICT ON UPDATE CASCADE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by_user_id INT4 NOT NULL REFERENCES moped_users (
+        user_id
+    ) ON DELETE RESTRICT ON UPDATE CASCADE,
+    updated_by_user_id INT4 NOT NULL REFERENCES moped_users (
+        user_id
+    ) ON DELETE RESTRICT ON UPDATE CASCADE,
+    is_deleted BOOLEAN NOT NULL DEFAULT false
+);
+
+-- Add unique constraint for upserts
+ALTER TABLE public.files_project_work_activities
+ADD CONSTRAINT files_project_work_activities_entity_id_file_id_key
+UNIQUE (entity_id, file_id);
+
+COMMENT ON TABLE public.files_project_work_activities IS 'Join table to link Moped work activity rows and Moped project files to create work activity file attachments.';
+COMMENT ON COLUMN public.files_project_work_activities.entity_id IS 'References the Moped project work activity record to which the file attachment belongs.';
+COMMENT ON COLUMN public.files_project_work_activities.file_id IS 'References the file that is attached to the work activity row.';
+COMMENT ON COLUMN public.files_project_work_activities.created_at IS 'Timestamp for when the record was created.';
+COMMENT ON COLUMN public.files_project_work_activities.updated_at IS 'Timestamp for when the record was last updated.';
+COMMENT ON COLUMN public.files_project_work_activities.created_by_user_id IS 'References the user who created the file attachment record.';
+COMMENT ON COLUMN public.files_project_work_activities.updated_by_user_id IS 'References the user who last updated the file attachment record.';
+COMMENT ON COLUMN public.files_project_work_activities.is_deleted IS 'Indicates soft deletion';
