36b42dbf749d55e77f259a76555481483c94f29c
Mike <mike.dilley@gmail.com>
2026-06-26 12:56:20 -0500

    Handle legacy admin user id and add comments

--- moped-database/migrations/default/1779396806012_work_activity_attachments/down.sql
+++ moped-database/migrations/default/1779396806012_work_activity_attachments/down.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS public.files_project_work_activities;
 
+-- Drop foreign key constraints for created_by_user_id and updated_by_user_id before dropping the tables they reference
+-- Updates from user id 29 to 1 in updated_by_user_id columns in the up migration are one-way and cannot be reversed.
 ALTER TABLE moped_proj_components DROP CONSTRAINT IF EXISTS fk_created_by_user_id;
 ALTER TABLE moped_proj_components DROP CONSTRAINT IF EXISTS fk_updated_by_user_id;
 ALTER TABLE moped_proj_milestones DROP CONSTRAINT IF EXISTS fk_created_by_user_id;
--- moped-database/migrations/default/1779396806012_work_activity_attachments/up.sql
+++ moped-database/migrations/default/1779396806012_work_activity_attachments/up.sql
@@ -1,4 +1,4 @@
--- Create join table to link Moped projects, moped project funding rows, and Moped project files
+-- Create join table to link Moped projects, project work activity rows, Moped project file rows
 CREATE TABLE public.files_project_work_activities (
     id SERIAL PRIMARY KEY,
     entity_id INT4 NOT NULL REFERENCES public.moped_proj_work_activity (
@@ -35,6 +35,26 @@ COMMENT ON COLUMN public.files_project_work_activities.created_by_user_id IS 'Re
 COMMENT ON COLUMN public.files_project_work_activities.updated_by_user_id IS 'References the user who last updated the file attachment record.';
 COMMENT ON COLUMN public.files_project_work_activities.is_deleted IS 'Indicates soft deletion';
 
+-- Update moped_proj_components updated_by_user_id that are 29 (former Data and Tech Admin) to 1 (current Data and Tech Admin).
+-- These came from a previous backfill of Safe Routes to School data on project components.
+-- User 29 no longer exists/violates the desired foreign key constraint for the updated_by_user_id column in moped_proj_components.
+-- See https://github.com/cityofaustin/atd-data-tech/issues/15039, https://github.com/cityofaustin/atd-data-tech/issues/14476
+-- Disable Hasura triggers temporarily to allow direct updates to moped_proj_components without generating activity log entries
+-- or updating audit fields with updates not relevant to users.
+DO $$
+BEGIN
+    PERFORM manage_trigger('update_moped_proj_components_and_project_audit_fields', 'moped_proj_components', FALSE);
+    PERFORM manage_trigger('notify_hasura_activity_log_moped_proj_components_UPDATE', 'moped_proj_components', FALSE);
+END $$;
+
+UPDATE moped_proj_components SET updated_by_user_id = 1 WHERE updated_by_user_id = 29;
+
+DO $$
+BEGIN
+    PERFORM manage_trigger('update_moped_proj_components_and_project_audit_fields', 'moped_proj_components', TRUE);
+    PERFORM manage_trigger('notify_hasura_activity_log_moped_proj_components_UPDATE', 'moped_proj_components', TRUE);
+END $$;
+
 -- Add foreign key constraints for created_by_user_id and updated_by_user_id
 ALTER TABLE moped_proj_components ADD CONSTRAINT fk_created_by_user_id FOREIGN KEY (
     created_by_user_id
@@ -51,3 +71,8 @@ ALTER TABLE moped_proj_milestones ADD CONSTRAINT fk_updated_by_user_id FOREIGN K
 ALTER TABLE moped_proj_notes ADD CONSTRAINT fk_updated_by_user_id FOREIGN KEY (
     updated_by_user_id
 ) REFERENCES moped_users (user_id) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+COMMENT ON COLUMN moped_proj_notes.created_by_user_id IS 'ID of the user who created the record';
+COMMENT ON COLUMN moped_proj_components.created_by_user_id IS 'ID of the user who created the record';
+COMMENT ON COLUMN moped_proj_components.updated_by_user_id IS 'ID of the user who last updated the record';
+COMMENT ON COLUMN moped_proj_milestones.created_by_user_id IS 'ID of the user who created the record';
