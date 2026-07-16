e24742b0d71532bf5ad82215ff78ff99add967b4
chiaberry <analuciaby@gmail.com>
2026-07-16 15:44:55 -0500

    add reminder to comment on column

--- moped-database/migrations/default/1784138295424_add_moped_file_type/up.sql
+++ moped-database/migrations/default/1784138295424_add_moped_file_type/up.sql
@@ -1,2 +1,6 @@
 INSERT INTO moped_file_types (id, name, slug) VALUES
 (5, 'Work order link', 'work_order_link')
+
+
+
+-- Update comment on work_order_url in moped_proj_work_activity to indicate it is deprecated
