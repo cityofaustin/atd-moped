a7c45b0aed98f9b868c2be4543f0072024d07329
chiaberry <analuciaby@gmail.com>
2026-07-20 10:25:36 -0500

    update secrets template with pw

--- toolbox/migrate_work_activity_links/secrets_template.py
+++ toolbox/migrate_work_activity_links/secrets_template.py
@@ -4,7 +4,7 @@ HASURA_AUTH = {
         "prod": "replace with production endpoint"
     },
     "hasura_graphql_admin_secret": {
-        "local": "DuMmyApiKeyHFVOVto19otC1wX6sP2N0VSKrCD70L10B7Sm525ZR6L672i2F79M9!",
+        "local": "hasurapassword",
         "prod": "replace with production secret"
     },
 }
