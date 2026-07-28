c09db2539eaf154ba7ed170d0b110a098cd5fdbd
chiaberry <analuciaby@gmail.com>
2026-07-27 21:12:36 -0500

    update down migration to be a non op

--- moped-database/migrations/default/1784138295424_add_moped_file_type/down.sql
+++ moped-database/migrations/default/1784138295424_add_moped_file_type/down.sql
@@ -1,4 +1,4 @@
--- In order to delete work_order_link type, change all existing work order link types to "other"
-update moped_project_files set file_type = 4 where file_type = 5;
-
-delete from moped_file_types where slug = 'work_order_link';
+-- Rollback (delete from moped_file_types where slug = 'work_order_link')
+-- is not possible since work order links were migrated from the work activity table to
+-- moped project files. Since records with this file type exist, we cannot delete.
+SELECT 0;
