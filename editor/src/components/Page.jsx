afada313e52ef401b7c48dfae41f2b7b0ec35ab2
tillyw <tilly.whitson@austintexas.gov>
2026-08-05 14:19:49 -0500

    update react, to v19

--- editor/src/components/Page.jsx
+++ editor/src/components/Page.jsx
@@ -1,13 +1,11 @@
 import React, { forwardRef } from "react";
-import { Helmet } from "react-helmet";
 import Box from "@mui/material/Box";
+import { useDocumentTitle } from "src/utils/documentTitle";
 
 const Page = forwardRef(({ children, title = "", ...rest }, ref) => {
+  useDocumentTitle(title);
   return (
     <Box className="page" ref={ref} {...rest}>
-      <Helmet>
-        <title>{title}</title>
-      </Helmet>
       {children}
     </Box>
   );
---
+++ editor/src/utils/documentTitle.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+
+/**
+ * Custom hook that sets the document title for the page
+ */
+export const useDocumentTitle = (
+  title,
+) => {
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+};
