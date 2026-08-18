5c32e9a0e94d44235439b86c07cd7b636c5919a6
tillyw <tilly.whitson@austintexas.gov>
2026-08-18 16:49:17 -0500

    remove comment

--- editor/src/views/dev/LookupsView/index.jsx
+++ editor/src/views/dev/LookupsView/index.jsx
@@ -22,7 +22,6 @@ import IconButtonWithTooltip from "src/components/IconButtonWithTooltip";
 import Can from "src/auth/Can";
 import { useFeedbackSnackbar } from "src/components/useFeedbackSnackbar";
 
-// To do: replace manual scrolling functionality with <ScrollRestoration>
 /**
  * Scroll to a page element based on its key
  * @param {string} recordKey - The key representing the record type to scroll to
