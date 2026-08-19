6ad56fa0c70adf71e4a26d1cc205b60a1633bb50
tillyw <tilly.whitson@austintexas.gov>
2026-08-19 16:04:23 -0500

    updates ecapris subproject statuses id type and sequernce to bigint

---
+++ moped-database/migrations/default/1787170415812_alter_table_public_ecapris_subproject_statuses_alter_column_subproject_status_id/down.sql
@@ -0,0 +1,4 @@
+ALTER TABLE public.ecapris_subproject_statuses
+ALTER COLUMN id TYPE integer;
+
+ALTER SEQUENCE public.ecapris_subproject_statuses_id_seq AS integer;
---
+++ moped-database/migrations/default/1787170415812_alter_table_public_ecapris_subproject_statuses_alter_column_subproject_status_id/up.sql
@@ -0,0 +1,4 @@
+ALTER SEQUENCE public.ecapris_subproject_statuses_id_seq AS bigint;
+
+ALTER TABLE public.ecapris_subproject_statuses
+ALTER COLUMN id TYPE bigint;
