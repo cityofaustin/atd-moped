a6f8cf8f5342e0e8772a73a5293ca880a3335004
Mike <mike.dilley@gmail.com>
2026-09-11 16:09:19 -0500

    Update a couple more places and test sql view change

---
+++ database/migrations/default/1788304724361_test_sql_bot/up.sql
@@ -0,0 +1,21 @@
+DROP VIEW IF EXISTS exploded_component_arcgis_online_view;
+
+-- test small change to test sql bot
+CREATE OR REPLACE VIEW exploded_component_arcgis_online_view AS
+SELECT
+    mpc.project_id,
+    mpc.project_component_id,
+    st_geometrytype(dump.geom) AS geometry_type,
+    dump.path[1]               AS point_index,
+    comp_geography.geometry    AS original_geometry,
+    st_asgeojson(dump.geom)    AS exploded_geometry,
+    mp.updated_at              AS project_updated_at,
+    mp.date_added              AS project_created_at
+FROM moped_proj_components mpc
+LEFT JOIN
+    component_geography_view comp_geography
+    ON mpc.project_component_id = comp_geography.project_component_id
+LEFT JOIN moped_project mp ON mpc.project_id = mp.project_id
+,
+LATERAL st_dump (st_geomfromgeojson (comp_geography.geometry)) dump (path, geom)
+WHERE mpc.is_deleted = false AND mp.is_deleted = false AND st_geometrytype (st_geomfromgeojson (comp_geography.geometry)) = 'ST_MultiPoint'::text;
