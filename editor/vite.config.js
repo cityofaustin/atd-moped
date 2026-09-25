9400533f2812f06be87990e09eb7fb0e4629bc87
Mike <mike.dilley@gmail.com>
2026-09-25 12:56:01 -0500

    Remove vite config polyfill for amplify v5

--- editor/vite.config.js
+++ editor/vite.config.js
@@ -7,11 +7,6 @@ import codegen from "vite-plugin-graphql-codegen";
 export default defineConfig(() => {
   return {
     base: "/moped/",
-    /* Define global variables for AWS Amplify v5 */
-    define: {
-      global: "globalThis",
-      process: { env: {} },
-    },
     build: {
       outDir: "build/moped",
     },
